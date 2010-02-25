98929f02f874d3f96f8cedb93beb327d98c09351
Leif Hedstrom <zwoop@apache.org>
2010-02-25 17:46:28 +0000

    TS-200: Undo some license attributions.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/trafficserver/traffic/trunk@916370 13f79535-47bb-0310-9956-ffa450edef68

--- libinktomi++/ink_res_mkquery.cc
+++ libinktomi++/ink_res_mkquery.cc
@@ -69,29 +69,6 @@
  */
 
 
-/*
-  Imported from Bind-9.5.2-P2
-  
-  Changes:
-
-  Licensed to the Apache Software Foundation (ASF) under one
-  or more contributor license agreements.  See the NOTICE file
-  distributed with this work for additional information
-  regarding copyright ownership.  The ASF licenses this file
-  to you under the Apache License, Version 2.0 (the
-  "License"); you may not use this file except in compliance
-  with the License.  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
-
-
 #if defined(LIBC_SCCS) && !defined(lint)
 static const char sccsid[] = "@(#)res_mkquery.c	8.1 (Berkeley) 6/4/93";
 static const char rcsid[] = "$Id: res_mkquery.c,v 1.6.672.1 2008/04/03 02:12:21 marka Exp $";
