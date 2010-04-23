7a59c741eb65b8cb71435430eb32405607f417f0
John Bradley Plevyak <jplevyak@apache.org>
2010-04-23 23:18:29 +0000

    update BSD license as per ftp://ftp.cs.berkeley.edu/pub/4bsd/README.Impt.License.Change
    git-svn-id: https://svn.apache.org/repos/asf/incubator/trafficserver/traffic/trunk@937550 13f79535-47bb-0310-9956-ffa450edef68

--- libinktomi++/ink_res_mkquery.cc
+++ libinktomi++/ink_res_mkquery.cc
@@ -10,11 +10,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- * 	This product includes software developed by the University of
- * 	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  * 
@@ -69,10 +65,6 @@
  */
 
 
-#if defined(LIBC_SCCS) && !defined(lint)
-static const char sccsid[] = "@(#)res_mkquery.c	8.1 (Berkeley) 6/4/93";
-static const char rcsid[] = "$Id: res_mkquery.c,v 1.6.672.1 2008/04/03 02:12:21 marka Exp $";
-#endif /* LIBC_SCCS and not lint */
 
 #include "ink_config.h"
 
