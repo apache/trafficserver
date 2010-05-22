bdf064f2be1ff2e1337112938eabe081dabd330d
Mladen Turk <mturk@apache.org>
2010-05-22 19:10:37 +0000

    TS-366: Fix redundant checks. We cannot have backtrace without execinfo.h
    git-svn-id: https://svn.apache.org/repos/asf/trafficserver/traffic/trunk@947324 13f79535-47bb-0310-9956-ffa450edef68

--- libinktomi++/ink_stack_trace.cc
+++ libinktomi++/ink_stack_trace.cc
@@ -29,7 +29,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#if defined(HAVE_EXECINFO_H) && ATS_HAS_BACKTRACE
+#if ATS_HAS_BACKTRACE
 
 #include <execinfo.h>           /* for backtrace_symbols, etc. */
 #include <signal.h>
@@ -114,7 +114,7 @@ ink_stack_trace_dump(int sighandler_frame)
   backtrace_symbols_fd(stack, btl, 2);
 }
 
-#else  /* !(HAVE_EXECINFO_H && ATS_HAS_BACKTRACE) */
+#else  /* !ATS_HAS_BACKTRACE */
 
 void
 ink_stack_trace_dump(int sighandler_frame)
@@ -124,4 +124,4 @@ ink_stack_trace_dump(int sighandler_frame)
       return;
 }
 
-#endif  /* !(HAVE_EXECINFO_H && AST_HAS_BACKTRACE) */
+#endif  /* ATS_HAS_BACKTRACE */
