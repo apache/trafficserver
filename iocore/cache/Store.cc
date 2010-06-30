fe1a61daf23bb22575fa71db34e353e8dc44b90d
Leif Hedstrom <zwoop@apache.org>
2010-06-30 14:53:41 +0000

    Clean up of some old "cache.config" defaults, which not used.
    git-svn-id: https://svn.apache.org/repos/asf/trafficserver/traffic/trunk@959338 13f79535-47bb-0310-9956-ffa450edef68

--- iocore/cache/Store.cc
+++ iocore/cache/Store.cc
@@ -25,57 +25,6 @@
 #include "P_Cache.h"
 #include "I_Layout.h"
 
-const char *STORAGE_CONFIG_HEADER = "# 									\n\
-# Storage Configuration file                                            \n\
-#                                                                       \n\
-#                                                                       \n\
-# The storage configuration is a list of all the storage to             \n\
-# be used by the cache.                                                 \n\
-#                                                                       \n\
-# It has the format:                                                    \n\
-#									\n\
-#    pathname  [size]							\n\
-#									\n\
-# Where 'pathname' is the name of a raw partition, directory or file	\n\
-# and 'size' is size in bytes. See the configuration information for	\n\
-# your specific O/S below. Size must be specified for directories	\n\
-# or files and is optional for raw partitions;				\n\
-#									\n\
-# Example 1:  64MB in the /big_dir directory				\n\
-#    /big_dir          67108864						\n\
-#									\n\
-# Example 2:  64MB in current directory					\n\
-#    .                 67108864						\n\
-#									\n\
-#############################################################		\n\
-##           Solaris Specific Configuration                ##		\n\
-#############################################################		\n\
-# Example 1:  4GB in 2 2GB raw partitions				\n\
-#									\n\
-#    /devices/sbus@1f,0/SUNW,fas@e,8800000/sd@2,0:a			\n\
-#    /devices/sbus@1f,0/SUNW,fas@e,8800000/sd@2,0:b			\n\
-#									\n\
-# For a raw partition or a file, the useful size is limited to 2 GB.	\n\
-#									\n\
-#############################################################           \n\
-##        Digtial Unix Specific Configuration              ##		\n\
-#############################################################		\n\
-# Example 1:  using a naked disk					\n\
-#									\n\
-#    /dev/rz14c								\n\
-#    									\n\
-# You can use any partition. You should not use raw devices		\n\
-# (e.g., /dev/rrz14c). Partitions can be any size.			\n\
-#									\n\
-# Example 2: using an LSM volume					\n\
-#									\n\
-#	/dev/vol/rootdg/cache0						\n\
-#									\n\
-# You can use any LSM organization, but it is recommended that you	\n\
-# use 'naked' disk volumes for best performance.			\n\
-#									\n\
-";
-
 // Global
 Store theStore;
 
