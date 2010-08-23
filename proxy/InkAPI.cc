96ae853d7d7cde7791264701b0077715023f5150
Leif Hedstrom <zwoop@apache.org>
2010-08-23 02:35:30 +0000

    TS-419: Segmentation fault in INKError when error output is made both in error log and as debug messages
    Author: Yakov Markovitch
    Review and minor changes: Leif
    
    git-svn-id: https://svn.apache.org/repos/asf/trafficserver/traffic/trunk@987979 13f79535-47bb-0310-9956-ffa450edef68

--- proxy/InkAPI.cc
+++ proxy/InkAPI.cc
@@ -393,10 +393,13 @@ void
 INKError(const char *fmt, ...)
 {
   va_list args;
-  va_start(args, fmt);
+
   if (is_action_tag_set("deft") || is_action_tag_set("sdk_vbos_errors")) {
+    va_start(args, fmt);
     diags->print_va(NULL, DL_Error, NULL, NULL, fmt, args);
+    va_end(args);
   }
+  va_start(args, fmt);
   Log::va_error((char *) fmt, args);
   va_end(args);
 }
