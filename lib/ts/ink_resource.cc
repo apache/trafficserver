apache/trafficserver

commit 0947ea0fb5e2e2b873bec0a4e03f2907909e87a7

TS-953: consolidate string copy/concat for lib/ts.
git-svn-id: https://svn.apache.org/repos/asf/trafficserver/traffic/trunk@1171837 13f79535-47bb-0310-9956-ffa450edef68

lib/ts/ink_resource.cc
@@ -44,8 +44,7 @@ _xstrdup(const char *str, int length, const char *path)
       length = strlen(str);
 
     newstr = (char *)ats_malloc(length + 1);
-    strncpy(newstr, str, length);
-    newstr[length] = '\0';
+    ink_strlcpy(newstr, str, length);
     return newstr;
   }
   return NULL;

lib/ts/ink_args.cc
@@ -83,7 +83,7 @@ process_arg(ArgumentDescription * argument_descriptions,
         *(int64_t *) argument_descriptions[i].location = ink_atoi64(arg);
         break;
       case 'S':
-        strncpy((char *) argument_descriptions[i].location, arg, atoi(argument_descriptions[i].type + 1));
+        ink_strlcpy((char *) argument_descriptions[i].location, arg, atoi(argument_descriptions[i].type + 1));
         break;
       default:
         ink_fatal(1, (char *) "bad argument description");
@@ -158,7 +158,7 @@ process_args(ArgumentDescription * argument_descriptions, int n_argument_descrip
         *(int64_t *) argument_descriptions[i].location = atoll(env);
         break;
       case 'S':
-        strncpy((char *) argument_descriptions[i].location, env, atoi(argument_descriptions[i].type + 1));
+        ink_strlcpy((char *) argument_descriptions[i].location, env, atoi(argument_descriptions[i].type + 1));
         break;
       }
     }

lib/ts/ink_file.cc
@@ -308,12 +308,12 @@ ink_filepath_make(char *path, int pathsz, const char *rootpath, const char *addp
 
   if (addpath[0] == '/') {
     // If addpath is rooted, then rootpath is unused.
-    ink_strncpy(path, addpath, pathsz);
+    ink_strlcpy(path, addpath, pathsz);
     return 0;
   }
   if (!rootpath || !*rootpath) {
     // If there's no rootpath return the addpath
-    ink_strncpy(path, addpath, pathsz);
+    ink_strlcpy(path, addpath, pathsz);
     return 0;
   }
   rootlen = strlen(rootpath);
@@ -322,10 +322,13 @@ ink_filepath_make(char *path, int pathsz, const char *rootpath, const char *addp
     *path = '\0';
     return (int)maxlen;
   }
-  strcpy(path, rootpath);
+  ink_strlcpy(path, rootpath, pathsz);
   path += rootlen;
-  if (*(path - 1) != '/')
+  pathsz -= rootlen;
+  if (*(path - 1) != '/') {
     *(path++) = '/';
-  strcpy(path, addpath);
+    --pathsz;
+  }
+  ink_strlcpy(path, addpath, pathsz);
   return 0;
 }

lib/ts/ink_inet.cc
@@ -233,10 +233,9 @@ int ink_inet_pton(char const* text, sockaddr* addr) {
     */
 
     size_t n = strlen(text);
-    copy = static_cast<char*>(alloca(n)); // n-1 would probably be OK.
+    copy = static_cast<char*>(alloca(n-1));
     if (']' == text[n-1]) {
-      strncpy(copy, text+1, n-2);
-      copy[n-2] = 0;
+      ink_strlcpy(copy, text+1, n-1);
       text = copy;
     } else {
       // Bad format, getaddrinfo isn't going to succeed.

lib/ts/ink_lockfile.h
@@ -47,7 +47,7 @@ public:
   // coverity[uninit_member]
   Lockfile(const char *filename):fd(0)
   {
-    ink_strncpy(fname, filename, sizeof(fname));
+    ink_strlcpy(fname, filename, sizeof(fname));
   }
 
   ~Lockfile(void)
@@ -56,7 +56,7 @@ public:
 
   void SetLockfileName(const char *filename)
   {
-    ink_strncpy(fname, filename, sizeof(fname));
+    ink_strlcpy(fname, filename, sizeof(fname));
   }
 
   const char *GetLockfileName(void)

lib/ts/ink_res_init.cc
@@ -328,17 +328,14 @@ ink_res_init(
         buf[0] = '.';
       cp = strchr(buf, '.');
       cp = (cp == NULL) ? buf : (cp + 1);
-      strncpy(statp->defdname, cp,
-              sizeof(statp->defdname) - 1);
-      statp->defdname[sizeof(statp->defdname) - 1] = '\0';
+      ink_strlcpy(statp->defdname, cp, sizeof(statp->defdname));
     }
   }
 #endif	/* SOLARIS2 */
 
 	/* Allow user to override the local domain definition */
   if ((cp = getenv("LOCALDOMAIN")) != NULL) {
-    (void)strncpy(statp->defdname, cp, sizeof(statp->defdname) - 1);
-    statp->defdname[sizeof(statp->defdname) - 1] = '\0';
+    (void)ink_strlcpy(statp->defdname, cp, sizeof(statp->defdname));
     haveenv++;
 
     /*
@@ -380,12 +377,12 @@ ink_res_init(
      ---------------------------------------------- */
 
   if (pDefDomain && '\0' != *pDefDomain && '\n' != *pDefDomain) {
-    strncpy(statp->defdname, pDefDomain, sizeof(statp->defdname) - 1);
+    ink_strlcpy(statp->defdname, pDefDomain, sizeof(statp->defdname));
     if ((cp = strpbrk(statp->defdname, " \t\n")) != NULL)
       *cp = '\0';
   }
   if (pSearchList && '\0' != *pSearchList && '\n' != *pSearchList) {
-    strncpy(statp->defdname, pSearchList, sizeof(statp->defdname) - 1);
+    ink_strlcpy(statp->defdname, pSearchList, sizeof(statp->defdname));
     if ((cp = strchr(statp->defdname, '\n')) != NULL)
       *cp = '\0';
     /*
@@ -446,8 +443,7 @@ ink_res_init(
           cp++;
         if ((*cp == '\0') || (*cp == '\n'))
           continue;
-        strncpy(statp->defdname, cp, sizeof(statp->defdname) - 1);
-        statp->defdname[sizeof(statp->defdname) - 1] = '\0';
+        ink_strlcpy(statp->defdname, cp, sizeof(statp->defdname));
         if ((cp = strpbrk(statp->defdname, " \t\n")) != NULL)
           *cp = '\0';
         havesearch = 0;
@@ -462,8 +458,7 @@ ink_res_init(
           cp++;
         if ((*cp == '\0') || (*cp == '\n'))
           continue;
-        strncpy(statp->defdname, cp, sizeof(statp->defdname) - 1);
-        statp->defdname[sizeof(statp->defdname) - 1] = '\0';
+        ink_strlcpy(statp->defdname, cp, sizeof(statp->defdname));
         if ((cp = strchr(statp->defdname, '\n')) != NULL)
           *cp = '\0';
         /*
@@ -528,7 +523,7 @@ ink_res_init(
     statp->nscount = nserv;
 
   if (statp->defdname[0] == 0 && gethostname(buf, sizeof(statp->defdname) - 1) == 0 && (cp = strchr(buf, '.')) != NULL)
-    strcpy(statp->defdname, cp + 1);
+    ink_strlcpy(statp->defdname, cp + 1, sizeof(statp->defdname));
 
   /* find components of local domain that might be searched */
   if (havesearch == 0) {

lib/ts/ink_stack_trace.cc
@@ -103,7 +103,7 @@ ink_stack_trace_dump(int sighandler_frame)
   // Recopy and re-terminate the app name in case it has been trashed.
   char name[256];
   const char *msg = " - STACK TRACE: \n";
-  ink_strncpy(name, program_name, sizeof(name) - 2);
+  ink_strlcpy(name, program_name, sizeof(name));
   if (write(2, name, strlen(name)) == -1)
     return;
   if (write(2, msg, strlen(msg)) == -1)

lib/ts/ink_time.cc
@@ -602,7 +602,7 @@ ink_time_first_day_of_year(int year)
 void
 ink_time_day_to_string(int day, char *buffer, const size_t bufferSize)
 {
-  ink_strncpy(buffer, _day_names[day], bufferSize);
+  ink_strlcpy(buffer, _day_names[day], bufferSize);
 }                               /* End ink_time_day_to_string */
 
 
@@ -617,7 +617,7 @@ ink_time_day_to_string(int day, char *buffer, const size_t bufferSize)
 void
 ink_time_month_to_string(int month, char *buffer, const size_t bufferSize)
 {
-  ink_strncpy(buffer, _month_names[month - 1], bufferSize);
+  ink_strlcpy(buffer, _month_names[month - 1], bufferSize);
 }                               /* End ink_time_month_to_string */
 
 

commit c5da1a87b3c97c1d912c7cf1282177d91d897a8a

TS-953: fix regression introduced by r1171837
git-svn-id: https://svn.apache.org/repos/asf/trafficserver/traffic/trunk@1171870 13f79535-47bb-0310-9956-ffa450edef68

lib/ts/ink_resource.cc
@@ -44,7 +44,7 @@ _xstrdup(const char *str, int length, const char *path)
       length = strlen(str);
 
     newstr = (char *)ats_malloc(length + 1);
-    ink_strlcpy(newstr, str, length);
+    ink_strlcpy(newstr, str, length + 1);
     return newstr;
   }
   return NULL;

proxy/Main.cc
@@ -415,7 +415,7 @@ initialize_process_manager()
   }
 
   if (access(management_directory, R_OK) == -1) {
-    ink_strncpy(management_directory, Layout::get()->sysconfdir, PATH_NAME_MAX);
+    ink_strlcpy(management_directory, Layout::get()->sysconfdir, sizeof(management_directory));
     if (access(management_directory, R_OK) == -1) {
       fprintf(stderr,"unable to access() management path '%s': %d, %s\n", management_directory, errno, strerror(errno));
       fprintf(stderr,"please set management path via command line '-d <managment directory>'\n");
@@ -1222,15 +1222,15 @@ init_http_aeua_filter(void)
       ++cname;
       --j;
     }
-    ink_strncpy(buf, system_config_directory, sizeof(buf));
+    ink_strlcpy(buf, system_config_directory, sizeof(buf));
     if ((i = strlen(buf)) >= 0) {
       if (!i || (buf[i - 1] != '/' && buf[i - 1] != '\\' && i < (int) sizeof(buf))) {
-        strncat(buf, "/", 1);
+        ink_strlcat(buf, "/", sizeof(buf));
         ++i;
       }
     }
     if ((i + j + 1) < (int) sizeof(buf))
-      strncat(buf, cname, sizeof(_cname) - 1);
+      ink_strlcat(buf, cname, sizeof(buf));
   }
 
   i = HttpConfig::init_aeua_filter(buf[0] ? buf : NULL);
@@ -1504,8 +1504,8 @@ main(int argc, char **argv)
 
   // Before accessing file system initialize Layout engine
   Layout::create();
-  ink_strncpy(system_root_dir, Layout::get()->prefix, PATH_NAME_MAX);
-  ink_strncpy(management_directory, Layout::get()->sysconfdir, PATH_NAME_MAX);
+  ink_strlcpy(system_root_dir, Layout::get()->prefix, sizeof(system_root_dir));
+  ink_strlcpy(management_directory, Layout::get()->sysconfdir, sizeof(management_directory));
   chdir_root(); // change directory to the install root of traffic server.
 
   process_args(argument_descriptions, n_argument_descriptions, argv);