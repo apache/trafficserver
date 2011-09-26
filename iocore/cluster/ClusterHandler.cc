381ea2b25f852399a5b9a126d10a030b8deb235d
Jim Jagielski <jim@apache.org>
2011-09-26 13:00:25 +0000

    remove weird cast
    git-svn-id: https://svn.apache.org/repos/asf/trafficserver/traffic/trunk@1175848 13f79535-47bb-0310-9956-ffa450edef68

--- iocore/cluster/ClusterHandler.cc
+++ iocore/cluster/ClusterHandler.cc
@@ -1861,7 +1861,7 @@ ClusterHandler::add_small_controlmsg_descriptors()
     c->freeall();
     p = (char *) DOUBLE_ALIGN(p);
 #ifdef PURIFY
-    if ((uintptr_t) endp < (uintptr_t) p)
+    if (endp < p)
       memset(endp, 0, (p - endp));
 #endif
   }
