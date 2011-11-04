1abf6e0f84fb346214ed6053211a913645d719cc
Igor Galić <igalic@apache.org>
2011-11-04 10:06:24 +0000

    Fix typos, no code change
    git-svn-id: https://svn.apache.org/repos/asf/trafficserver/traffic/trunk@1197477 13f79535-47bb-0310-9956-ffa450edef68

--- lib/ts/Layout.cc
+++ lib/ts/Layout.cc
@@ -54,7 +54,7 @@ layout_relative(const char *root, const char *file)
     if (err == EACCES) {
       ink_error("Cannot merge path '%s' above the root '%s'\n", file, root);
     } else if (err == E2BIG) {
-      ink_error("Excedding file name length limit of %d characters\n", PATH_NAME_MAX);
+      ink_error("Exceeding file name length limit of %d characters\n", PATH_NAME_MAX);
     }
     else {
       // TODO: Make some pretty errors.
@@ -83,7 +83,7 @@ Layout::relative(char *buf, size_t bufsz, const char *file)
     if (err == EACCES) {
       ink_error("Cannot merge path '%s' above the root '%s'\n", file, prefix);
     } else if (err == E2BIG) {
-      ink_error("Excedding file name length limit of %d characters\n", PATH_NAME_MAX);
+      ink_error("Exceeding file name length limit of %d characters\n", PATH_NAME_MAX);
     }
     else {
       // TODO: Make some pretty errors.
@@ -117,7 +117,7 @@ Layout::relative_to(char *buf, size_t bufsz, const char *dir, const char *file)
     if (err == EACCES) {
       ink_error("Cannot merge path '%s' above the root '%s'\n", file, dir);
     } else if (err == E2BIG) {
-      ink_error("Excedding file name length limit of %d characters\n", PATH_NAME_MAX);
+      ink_error("Exceeding file name length limit of %d characters\n", PATH_NAME_MAX);
     }
     else {
       // TODO: Make some pretty errors.
