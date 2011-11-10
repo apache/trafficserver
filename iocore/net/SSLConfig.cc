apache/trafficserver

commit d85ae503cba5b870b8cfd3cd69a7b730f1789647

TS-1018: Remove OpenSSL acceleration code and configs n.b.: This commit *always* enables non-blocking SSL by the virtue of not making it dependent on an SSL accelerator.
git-svn-id: https://svn.apache.org/repos/asf/trafficserver/traffic/trunk@1199566 13f79535-47bb-0310-9956-ffa450edef68

iocore/net/SSLConfig.cc
@@ -54,14 +54,13 @@ SslConfigParams::SslConfigParams()
     clientCertPath = clientKeyPath =
     clientCACertFilename = clientCACertPath =
     cipherSuite =
-    serverKeyPathOnly = ncipherAccelLibPath = cswiftAccelLibPath = atallaAccelLibPath = broadcomAccelLibPath = NULL;
+    serverKeyPathOnly = NULL;
 
-  clientCertLevel = client_verify_depth = verify_depth = clientVerify = sslAccelerator = 0;
+  clientCertLevel = client_verify_depth = verify_depth = clientVerify = 0;
 
   ssl_accept_port_number = -1;
   termMode = SSL_TERM_MODE_NONE;
   ssl_ctx_options = 0;
-  ssl_accelerator_required = SSL_ACCELERATOR_REQ_NO;
   ssl_session_cache = SSL_SESSION_CACHE_MODE_SERVER;
   ssl_session_cache_size = 1024*20;
 }
@@ -122,28 +121,12 @@ SslConfigParams::cleanup()
     ats_free(serverKeyPathOnly);
     serverKeyPathOnly = NULL;
   }
-  if (ncipherAccelLibPath) {
-    ats_free(ncipherAccelLibPath);
-    ncipherAccelLibPath = NULL;
-  }
-  if (cswiftAccelLibPath) {
-    ats_free(cswiftAccelLibPath);
-    cswiftAccelLibPath = NULL;
-  }
-  if (atallaAccelLibPath) {
-    ats_free(atallaAccelLibPath);
-    atallaAccelLibPath = NULL;
-  }
-  if (broadcomAccelLibPath) {
-    ats_free(broadcomAccelLibPath);
-    broadcomAccelLibPath = NULL;
-  }
   if (cipherSuite) {
     ats_free(cipherSuite);
     cipherSuite = NULL;
   }
 
-  clientCertLevel = client_verify_depth = verify_depth = clientVerify = sslAccelerator = 0;
+  clientCertLevel = client_verify_depth = verify_depth = clientVerify = 0;
   ssl_accept_port_number = -1;
   termMode = SSL_TERM_MODE_NONE;
 }
@@ -164,7 +147,6 @@ SslConfigParams::initialize()
   char *multicert_config_file = NULL;
 
   int ssl_mode = SSL_TERM_MODE_NONE;
-  int ret_val = 0;
 #ifdef _WIN32
   int i;
 #endif
@@ -174,85 +156,12 @@ SslConfigParams::initialize()
   //+++++++++++++++++++++++++ Server part +++++++++++++++++++++++++++++++++
   verify_depth = 7;
 
-  IOCORE_ReadConfigInteger(ssl_accelerator_required, "proxy.config.ssl.accelerator_required");
-  ssl_accelerator_required &= SSL_ACCELERATOR_REQ_BOTH;
-
   IOCORE_ReadConfigInteger(ssl_mode, "proxy.config.ssl.enabled");
   ssl_mode &= SSL_TERM_MODE_BOTH;
   termMode = (SSL_TERMINATION_MODE) ssl_mode;
 
   IOCORE_ReadConfigStringAlloc(cipherSuite, "proxy.config.ssl.server.cipher_suite");
 
-  /* if ssl is enabled and we require an accelerator */
-  /* XXX: This code does not work */
-  if ((termMode & SSL_TERM_MODE_BOTH) && (ssl_accelerator_required & SSL_ACCELERATOR_REQ_BOTH)) {
-    if (system(NULL)) {
-      ret_val = system("bin/openssl_accelerated >/dev/null 2>&1");
-      Debug("ssl_accelerator_required", "bin/openssl_accelerated returned %d|%d|(%d)", ret_val,
-            WIFEXITED(ret_val), WEXITSTATUS(ret_val));
-      if (WEXITSTATUS(ret_val) != 1) {
-        if (ssl_accelerator_required & SSL_ACCELERATOR_REQ_MEAN) {
-          Error
-            ("You asked to have ssl acceleration only if you have an accelerator card present (and wanted to exit if you didn't), but you don't appear to have one");
-          exit(-1);
-        } else {
-          Error
-            ("You asked to have ssl acceleration only if you have an accelerator card present, but you don't appear to have one [what does bin/openssl_accelerated return?]");
-          ssl_mode = 0;
-          termMode = (SSL_TERMINATION_MODE) ssl_mode;
-        }
-      }
-    } else {
-      Error
-        ("You asked to have ssl acceleration only if you have an accelerator card present, but I can't determine either way. Disabling for now");
-      ssl_mode = 0;
-      termMode = (SSL_TERMINATION_MODE) ssl_mode;
-    }
-  }
-
-  IOCORE_ReadConfigInteger(sslAccelerator, "proxy.config.ssl.accelerator.type");
-
-  IOCORE_ReadConfigInt32(ssl_accept_port_number, "proxy.config.ssl.server_port");
-  IOCORE_ReadConfigInt32(clientCertLevel, "proxy.config.ssl.client.certification_level");
-
-  IOCORE_ReadConfigStringAlloc(atallaAccelLibPath, "proxy.config.ssl.atalla.lib.path");
-#ifdef _WIN32
-  i = 0;
-  while (atallaAccelLibPath[i] != 0) {
-    if (atallaAccelLibPath[i] == '/')
-      atallaAccelLibPath[i] = '\\';
-    i++;
-  }
-#endif
-
-  IOCORE_ReadConfigStringAlloc(ncipherAccelLibPath, "proxy.config.ssl.ncipher.lib.path");
-#ifdef _WIN32
-  i = 0;
-  while (ncipherAccelLibPath[i] != 0) {
-    if (ncipherAccelLibPath[i] == '/')
-      ncipherAccelLibPath[i] = '\\';
-    i++;
-  }
-#endif
-
-  IOCORE_ReadConfigStringAlloc(cswiftAccelLibPath, "proxy.config.ssl.cswift.lib.path");
-#ifdef _WIN32
-  i = 0;
-  while (cswiftAccelLibPath[i] != 0) {
-    if (cswiftAccelLibPath[i] == '/')
-      cswiftAccelLibPath[i] = '\\';
-    i++;
-  }
-#endif
-  IOCORE_ReadConfigStringAlloc(broadcomAccelLibPath, "proxy.config.ssl.broadcom.lib.path");
-#ifdef _WIN32
-  i = 0;
-  while (broadcomAccelLibPath[i] != 0) {
-    if (broadcomAccelLibPath[i] == '/')
-      broadcomAccelLibPath[i] = '\\';
-    i++;
-  }
-#endif
   int options;
   IOCORE_ReadConfigInteger(options, "proxy.config.ssl.SSLv2");
   if (!options)

contrib/perl/AdminClient/lib/Apache/TS/AdminClient.pm
@@ -675,10 +675,6 @@ The Apache Traffic Server Administration Manual will explain what these strings 
  proxy.config.socks.socks_timeout
  proxy.config.socks.socks_version
  proxy.config.srv_enabled
- proxy.config.ssl.accelerator_required
- proxy.config.ssl.accelerator.type
- proxy.config.ssl.atalla.lib.path
- proxy.config.ssl.broadcom.lib.path
  proxy.config.ssl.CA.cert.filename
  proxy.config.ssl.CA.cert.path
  proxy.config.ssl.client.CA.cert.filename
@@ -689,9 +685,7 @@ The Apache Traffic Server Administration Manual will explain what these strings 
  proxy.config.ssl.client.private_key.filename
  proxy.config.ssl.client.private_key.path
  proxy.config.ssl.client.verify.server
- proxy.config.ssl.cswift.lib.path
  proxy.config.ssl.enabled
- proxy.config.ssl.ncipher.lib.path
  proxy.config.ssl.number.threads
  proxy.config.ssl.server.cert_chain.filename
  proxy.config.ssl.server.cert.filename

iocore/net/P_SSLConfig.h
@@ -33,13 +33,6 @@
 
 #include "libts.h"
 
-#define	    SSL_NO_ACCEL	    0
-#define     SSL_NCIPHER_ACCEL	    1
-#define     SSL_CSWIFT_ACCEL	    2
-#define     SSL_ATALLA_ACCEL        3
-#define     SSL_BROADCOM_ACCEL      4
-
-
 
 //
 // Dynamic updates of SSL settings are not implemented yet.
@@ -65,14 +58,6 @@ public:
     SSL_TERM_MODE_CLIENT = 1,
     SSL_TERM_MODE_SERVER = 2,
     SSL_TERM_MODE_BOTH = SSL_TERM_MODE_CLIENT | SSL_TERM_MODE_SERVER
-  };
-
-  enum SSL_ACCELERATOR_REQ
-  {
-    SSL_ACCELERATOR_REQ_NO = 0,
-    SSL_ACCELERATOR_REQ_YES = 1,
-    SSL_ACCELERATOR_REQ_MEAN = 2,
-    SSL_ACCELERATOR_REQ_BOTH = 3
   };
 
   enum SSL_SESSION_CACHE_MODE
@@ -102,15 +87,10 @@ private:
   char *CACertFilename;
   char *CACertPath;
   char *configFilePath;
-  char *ncipherAccelLibPath;
-  char *cswiftAccelLibPath;
-  char *atallaAccelLibPath;
-  char *broadcomAccelLibPath;
   char *cipherSuite;
   int clientCertLevel;
   int verify_depth;
   int ssl_accept_port_number;
-  int sslAccelerator;
   int ssl_session_cache;
   int ssl_session_cache_size;
 
@@ -122,7 +102,6 @@ private:
   int client_verify_depth;
 
   SSL_TERMINATION_MODE termMode;
-  int ssl_accelerator_required;
 
   long ssl_ctx_options;
 

iocore/net/SSLNet.cc
@@ -246,61 +246,6 @@ SSLNetProcessor::initSSL(SslConfigParams * param)
                         "please check your Traffic Server configurations", accept_port_number);
     return (1);
   }
-  // these are not operating system specific calls and
-  // this ifdef needs to be removed when the openssl-engine
-  // library is built for NT.
-#if !defined (_IOCORE_WIN32)
-
-#ifdef USE_OPENSSL_ENGINES
-  int engineType;
-  char *accelLibPath;
-
-  ENGINE *accleratorEng;
-
-  ENGINE_load_builtin_engines();
-  engineType = param->sslAccelerator;
-
-  if (engineType == SSL_NCIPHER_ACCEL) {
-    accelLibPath = param->ncipherAccelLibPath;
-    accleratorEng = ENGINE_by_id("chil");
-  } else if (engineType == SSL_CSWIFT_ACCEL) {
-    accelLibPath = param->cswiftAccelLibPath;
-    accleratorEng = ENGINE_by_id("cswift");
-  } else if (engineType == SSL_ATALLA_ACCEL) {
-    accelLibPath = param->cswiftAccelLibPath;
-    accleratorEng = ENGINE_by_id("atalla");
-  } else if (engineType == SSL_BROADCOM_ACCEL) {
-    accelLibPath = param->broadcomAccelLibPath;
-    accleratorEng = ENGINE_by_id("ubsec");
-  } else {
-    accelLibPath = NULL;
-    accleratorEng = ENGINE_by_id("dynamic");
-  }
-
-  if (accleratorEng == NULL) {
-    logSSLError("Cannot find SSL hardware accelerator library, SSL will operate without accelerator.");
-    return (-5);
-  }
-
-  if (accelLibPath != NULL) {
-    // Removed, not sure what it does, or how... it's nowhere to be found in OpenSSL /leif.
-    // DSO_set_dl_path (accelLibPath);
-  }
-
-  if (!ENGINE_set_default(accleratorEng, ENGINE_METHOD_ALL)) {
-    logSSLError("Cannot set SSL hardware accelerator card as default, SSL will operate without accelerator.");
-    logSSLError
-      ("Check records.config variable for SSL accelerator hardware library path in the SSL Termination section. ");
-
-    // set software engine as default
-    ENGINE_set_default(ENGINE_by_id("openssl"), ENGINE_METHOD_ALL);
-  }
-
-  ENGINE_free(accleratorEng);
-
-#endif // ! USE_OPENSSL_ENGINES
-
-#endif // !_IOCORE_WIN32
 
   meth = SSLv23_server_method();
   ctx = SSL_CTX_new(meth);

mgmt/RecordsConfig.cc
@@ -1302,19 +1302,7 @@ RecordElement RecordsConfig[] = {
   ,
   {RECT_CONFIG, "proxy.config.ssl.compression", RECD_INT, "1", RECU_RESTART_TS, RR_NULL, RECC_INT, "[0-1]", RECA_NULL}
   ,
-  {RECT_CONFIG, "proxy.config.ssl.accelerator_required", RECD_INT, "0", RECU_RESTART_TS, RR_NULL, RECC_INT, "[0-1]", RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.ssl.accelerator.type", RECD_INT, "0", RECU_RESTART_TS, RR_NULL, RECC_NULL, NULL, RECA_NULL}
-  ,
   {RECT_CONFIG, "proxy.config.ssl.number.threads", RECD_INT, "0", RECU_RESTART_TS, RR_NULL, RECC_NULL, NULL, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.ssl.atalla.lib.path", RECD_STRING, "/opt/atalla/lib", RECU_RESTART_TS, RR_NULL, RECC_NULL, NULL, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.ssl.ncipher.lib.path", RECD_STRING, "/opt/nfast/toolkits/hwcrhk", RECU_RESTART_TS, RR_NULL, RECC_NULL, NULL, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.ssl.cswift.lib.path", RECD_STRING, "/usr/lib", RECU_RESTART_TS, RR_NULL, RECC_NULL, NULL, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.ssl.broadcom.lib.path", RECD_STRING, "/usr/lib", RECU_RESTART_TS, RR_NULL, RECC_NULL, NULL, RECA_NULL}
   ,
   {RECT_CONFIG, "proxy.config.ssl.server.cipher_suite", RECD_STRING, "RC4-SHA:AES128-SHA:DES-CBC3-SHA:AES256-SHA:ALL:!aNULL:!EXP:!LOW:!MD5:!SSLV2:!NULL", RECU_RESTART_TS, RR_NULL, RECC_NULL, NULL, RECA_NULL}
   ,

proxy/Main.cc
@@ -1326,16 +1326,13 @@ getNumSSLThreads(void)
   TS_ReadConfigInteger(ssl_enabled, "proxy.config.ssl.enabled");
 
   // Set number of ssl threads equal to num of processors if
-  // SSL is enabled so it will scale properly.  If an accelerator card
-  // is present, there will be blocking, so scale threads up. If SSL is not
+  // SSL is enabled so it will scale properly. If SSL is not
   // enabled, leave num of ssl threads one, incase a remap rule
   // requires traffic server to act as an ssl client.
   if (ssl_enabled) {
     int config_num_ssl_threads = 0;
-    int ssl_blocking = 0;
 
     TS_ReadConfigInteger(config_num_ssl_threads, "proxy.config.ssl.number.threads");
-    TS_ReadConfigInteger(ssl_blocking, "proxy.config.ssl.accelerator.type");
 
     if (config_num_ssl_threads != 0) {
       num_of_ssl_threads = config_num_ssl_threads;
@@ -1349,8 +1346,6 @@ getNumSSLThreads(void)
       // Last resort
       if (num_of_ssl_threads <= 0)
         num_of_ssl_threads = config_num_ssl_threads * 2;
-      if (ssl_blocking != 0)
-        num_of_ssl_threads *= 2; // Double when blocking I/O
     }
   }
 

commit 50206ef8cb5292e15cfb860b32d7c1acdc41b7a3

Fix an issue introduced by r1199566, set ssl_accept_port to a sane default.
git-svn-id: https://svn.apache.org/repos/asf/trafficserver/traffic/trunk@1200075 13f79535-47bb-0310-9956-ffa450edef68

iocore/net/SSLConfig.cc
@@ -58,7 +58,7 @@ SslConfigParams::SslConfigParams()
 
   clientCertLevel = client_verify_depth = verify_depth = clientVerify = 0;
 
-  ssl_accept_port_number = -1;
+  ssl_accept_port_number = 443;
   termMode = SSL_TERM_MODE_NONE;
   ssl_ctx_options = 0;
   ssl_session_cache = SSL_SESSION_CACHE_MODE_SERVER;
@@ -159,6 +159,10 @@ SslConfigParams::initialize()
   IOCORE_ReadConfigInteger(ssl_mode, "proxy.config.ssl.enabled");
   ssl_mode &= SSL_TERM_MODE_BOTH;
   termMode = (SSL_TERMINATION_MODE) ssl_mode;
+
+  
+  IOCORE_ReadConfigInt32(ssl_accept_port_number, "proxy.config.ssl.server_port");
+  IOCORE_ReadConfigInt32(clientCertLevel, "proxy.config.ssl.client.certification_level");
 
   IOCORE_ReadConfigStringAlloc(cipherSuite, "proxy.config.ssl.server.cipher_suite");
 