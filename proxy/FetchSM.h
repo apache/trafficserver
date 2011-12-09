7802775bb7ec939d3208c4d47e99d10f430fae0c
Igor Galić <igalic@apache.org>
2011-12-09 11:56:40 +0000

    [TS-1042] correct debug message in FetchSM Author: James Peach Test/Review: igalic
    TIL (again) about printf(3)'s "precision", but more importantly about: "field width"
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/trafficserver/traffic/trunk@1212343 13f79535-47bb-0310-9956-ffa450edef68

--- proxy/FetchSM.h
+++ proxy/FetchSM.h
@@ -43,7 +43,9 @@ public:
   void init(Continuation* cont, TSFetchWakeUpOptions options, TSFetchEvent events, const char* headers, int length, unsigned int ip, int port)
   {
     //_headers.assign(headers);
-    Debug("FetchSM", "[%s] FetchSM initialized for request with headers\n--\n%s\n--", __FUNCTION__, headers);
+    Debug("FetchSM",
+	"[%s] FetchSM initialized for request with headers\n--\n%*.*s\n--",
+	__FUNCTION__, length, length, headers);
     req_finished = 0;
     resp_finished = 0;
     header_done = 0;
