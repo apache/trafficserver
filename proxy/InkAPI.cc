09b02e7a98e41055d39a3867c5f2aaf1cc8bf04a
Leif Hedstrom <zwoop@apache.org>
2011-12-19 04:01:44 +0000

    TS-1055 Wrong implementation of TSHttpSsnArgGet
    Author: Yakov Kopel
    Review: Leif
    
    git-svn-id: https://svn.apache.org/repos/asf/trafficserver/traffic/trunk@1220600 13f79535-47bb-0310-9956-ffa450edef68

--- proxy/InkAPI.cc
+++ proxy/InkAPI.cc
@@ -5500,7 +5500,7 @@ TSHttpSsnArgSet(TSHttpSsn ssnp, int arg_idx, void *arg)
 }
 
 void *
-TSHttpSsnArgGet(TSHttpSsn ssnp, int arg_idx, void **argp)
+TSHttpSsnArgGet(TSHttpSsn ssnp, int arg_idx)
 {
   sdk_assert(sdk_sanity_check_http_ssn(ssnp) == TS_SUCCESS);
   sdk_assert(arg_idx >= 0 && arg_idx < HTTP_SSN_TXN_MAX_USER_ARG);
