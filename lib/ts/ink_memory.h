3ae28350fc3771630a28929a84184f407e7d0e25
Alan M. Carroll <amc@network-geographics.com>
2012-01-30 13:00:41 -0600

    Doxygenated ink_zero

--- lib/ts/ink_memory.h
+++ lib/ts/ink_memory.h
@@ -57,13 +57,42 @@ extern "C"
 
 #ifdef __cplusplus
 }
+#endif
 
-template < typename T >
-T& ink_zero(T& t) {
+#ifdef __cplusplus
+/** Set data to zero.
+
+    Calls @c memset on @a t with a value of zero and a length of @c
+    sizeof(t). This can be used on ordinary and array variables. While
+    this can be used on builtin values it's inefficient.
+
+    @note Because this uses templates it cannot be used on unnamed or
+    locally scoped structures / classes. This is an inherent
+    limitation of templates.
+
+    Examples:
+    @code
+    foo bar; // value.
+    ink_zero(bar); // zero bar.
+
+    foo *bar; // pointer.
+    ink_zero(bar); // WRONG - makes the pointer @a bar zero.
+    ink_zero(*bar); // zero what bar points at.
+
+    foo bar[ZOMG]; // Array of structs.
+    ink_zero(bar); // Zero all structs in array.
+
+    foo *bar[ZOMG]; // array of pointers.
+    ink_zero(bar); // zero all pointers in the array.
+    @endcode
+    
+ */
+template < typename T > inline void
+ink_zero(
+	 T& t ///< Object to zero.
+	 ) {
   memset(&t, 0, sizeof(t));
-  return t;
 }
-
-#endif                          /* __cplusplus */
+#endif  /* __cplusplus */
 
 #endif
