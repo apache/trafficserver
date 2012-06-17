9b5bb0ff64ef6f2ae6db607ee65c60f658e1d669
Leif Hedstrom <zwoop@apache.org>
2012-06-16 18:16:27 -0600

    Minor cleanup of the response header.

--- plugins/stats_over_http/stats_over_http.c
+++ plugins/stats_over_http/stats_over_http.c
@@ -84,12 +84,13 @@ stats_add_data_to_resp_buffer(const char *s, stats_state * my_state)
   return s_len;
 }
 
+static const char RESP_HEADER[] =
+  "HTTP/1.0 200 Ok\r\nContent-Type: text/javascript\r\nCache-Control: no-cache\r\n\r\n";
+
 static int
 stats_add_resp_header(stats_state * my_state)
 {
-  char resp[] = "HTTP/1.0 200 Ok\r\nContent-Type: text/javascript\r\nCache-Control: no-cache\r\n\r\n";
-
-  return stats_add_data_to_resp_buffer(resp, my_state);
+  return stats_add_data_to_resp_buffer(RESP_HEADER, my_state);
 }
 
 static void
