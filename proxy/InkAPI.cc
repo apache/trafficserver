c0c15efdbf18d7ca2c316b8ef7a7816ba57d2eae
Leif Hedstrom <zwoop@apache.org>
2012-07-09 23:55:45 -0600

    [TS-1328] TSMgmtIntCreate and TSMgmtStringCreate validation. Author: Yakov Kopel.

--- proxy/InkAPI.cc
+++ proxy/InkAPI.cc
@@ -8258,6 +8258,8 @@ TSMgmtStringCreate(TSRecordType rec_type, const char *name, const TSMgmtString d
                    TSRecordUpdateType update_type, TSRecordCheckType check_type,
                    const char *check_regex, TSRecordAccessType access_type)
 {
+  if (check_regex == NULL && check_type != TS_RECORDCHECK_NULL)
+      return TS_ERROR;
   if (REC_ERR_OKAY != RecRegisterConfigString((enum RecT)rec_type, name, data_default, (enum RecUpdateT)update_type,
                                               (enum RecCheckT)check_type, check_regex, (enum RecAccessT)access_type))
     return TS_ERROR;
@@ -8270,6 +8272,8 @@ TSMgmtIntCreate(TSRecordType rec_type, const char *name, TSMgmtInt data_default,
                 TSRecordUpdateType update_type, TSRecordCheckType check_type,
                 const char *check_regex, TSRecordAccessType access_type)
 {
+  if (check_regex == NULL && check_type != TS_RECORDCHECK_NULL)
+      return TS_ERROR;
   if (REC_ERR_OKAY != RecRegisterConfigInt((enum RecT)rec_type, name, (RecInt)data_default, (enum RecUpdateT)update_type,
                                            (enum RecCheckT)check_type, check_regex, (enum RecAccessT)access_type))
     return TS_ERROR;
