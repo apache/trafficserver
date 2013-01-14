3aa39e572cda564ed48472da9faf4de51cd3fa22
Igor Galić <i.galic@brainsware.org>
2013-01-14 16:35:58 +0100

    TS-1585: plugins need memory barriers for ARM
    In lulu.h barriers are defined only for x86 and x86_64, but not for
    arm.  Well, thankfully implementing barriers for arm was rather
    easy (at least, the tests pass here).
    
    Fix ARM build failures by adding ARM barrier intrinsics which
    actually do something. This fixes build failues on the ARM port
    after promoting the header filter module to main. The patch
    denoted below originally was contributed by a Debian porter.
    The original issue was discussed in TS-1473.
    A no-op fix was proposed to be backported and committed as
    TS-1498. However, later TS-1560 was committed to trunk as an
    improved version of the same problem.
    
    test/review: igalic, zwoop, arno
    backport: igalic

--- plugins/header_filter/lulu.h
+++ plugins/header_filter/lulu.h
@@ -45,10 +45,10 @@
 #define rmb() __asm__ __volatile__ ( "lfence" : : : "memory")
 #define wmb() __asm__ __volatile__ ( "" : : : "memory")
 #elif defined(__arm__)
-/* ToDo: Anything? */
-#define mb()
-#define rmb()
-#define wmb()
+#define mb()  __asm__ __volatile__ ( "dmb" : : : "memory")
+#define rmb() __asm__ __volatile__ ( "dmb" : : : "memory")
+#define wmb() __asm__ __volatile__ ( "" : : : "memory")
+#elif defined(__arm__)
 #else
 #error "Define barriers"
 #endif
