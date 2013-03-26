ca7a84eebd0711c254959fc723d9de73f10aef68
Brian Geffon <bgeffon@bgeffon-mn1.linkedin.biz>
2013-03-26 11:26:36 -0700

    TS-1742: correct 32bit freelist case

--- lib/ts/ink_queue_utils.cc
+++ lib/ts/ink_queue_utils.cc
@@ -71,7 +71,7 @@ ink_queue_load_64(void *dst, void *src)
 {
 #if (defined(__i386__) || defined(__arm__)) && (SIZEOF_VOIDP == 4)
   volatile int32_t src_version = (*(head_p *) src).s.version;
-  volatile void *src_pointer = (*(head_p *) src).s.pointer;
+  void *src_pointer = (*(head_p *) src).s.pointer;
 
   (*(head_p *) dst).s.version = src_version;
   (*(head_p *) dst).s.pointer = src_pointer;
