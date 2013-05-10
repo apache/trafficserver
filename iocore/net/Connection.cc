03cbafef3ce88c4fe39f866532f212a4da820afa
James Peach <jpeach@apache.org>
2013-05-10 16:26:03 -0700

    TS-1909: remove duplicate listen socket conditioning
    Socket options were applied to the listen sockets in both
    Server::setup_fd_for_listen() and Server::listen(). There's really
    only a need for one of these, so remove all the duplicated code.

--- iocore/net/Connection.cc
+++ iocore/net/Connection.cc
@@ -149,31 +149,35 @@ Server::setup_fd_for_listen(
   bool non_blocking,
   int recv_bufsize,
   int send_bufsize,
-  bool transparent
-) {
-
+  bool transparent)
+{
   int res = 0;
 
-  if (http_accept_filter)
+  ink_assert(fd != NO_FD);
+
+  if (http_accept_filter) {
     add_http_filter(fd);
+  }
 
 #ifdef SEND_BUF_SIZE
   {
     int send_buf_size = SEND_BUF_SIZE;
-    if ((res = safe_setsockopt(fd, SOL_SOCKET, SO_SNDBUF, (char *) &send_buf_size, sizeof(int)) < 0))
+    if ((res = safe_setsockopt(fd, SOL_SOCKET, SO_SNDBUF, (char *) &send_buf_size, sizeof(int)) < 0)) {
       goto Lerror;
+    }
   }
 #endif
+
 #ifdef RECV_BUF_SIZE
   {
     int recv_buf_size = RECV_BUF_SIZE;
-    if ((res = safe_setsockopt(fd, SOL_SOCKET, SO_RCVBUF, (char *) &recv_buf_size, sizeof(int))) < 0)
+    if ((res = safe_setsockopt(fd, SOL_SOCKET, SO_RCVBUF, (char *) &recv_buf_size, sizeof(int))) < 0) {
       goto Lerror;
+    }
   }
 #endif
 
   if (recv_bufsize) {
-    // coverity[negative_sink_in_call]
     if (socketManager.set_rcvbuf_size(fd, recv_bufsize)) {
       // Round down until success
       int rbufsz = ROUNDUP(recv_bufsize, 1024);
@@ -186,6 +190,7 @@ Server::setup_fd_for_listen(
       }
     }
   }
+
   if (send_bufsize) {
     if (socketManager.set_sndbuf_size(fd, send_bufsize)) {
       // Round down until success
@@ -199,64 +204,88 @@ Server::setup_fd_for_listen(
       }
     }
   }
+
+#ifdef SET_CLOSE_ON_EXEC
+  if ((res = safe_fcntl(fd, F_SETFD, 1)) < 0) {
+    goto Lerror;
+  }
+#endif
+
 #ifdef SET_NO_LINGER
   {
     struct linger l;
     l.l_onoff = 0;
     l.l_linger = 0;
-    if ((res = safe_setsockopt(fd, SOL_SOCKET, SO_LINGER, (char *) &l, sizeof(l))) < 0)
+    if ((res = safe_setsockopt(fd, SOL_SOCKET, SO_LINGER, (char *) &l, sizeof(l))) < 0) {
       goto Lerror;
+    }
   }
 #endif
-#ifdef SET_TCP_NO_DELAY
-  if ((res = safe_setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, SOCKOPT_ON, sizeof(int))) < 0)
+
+  if (ats_is_ip6(&addr) && (res = safe_setsockopt(fd, IPPROTO_IPV6, IPV6_V6ONLY, SOCKOPT_ON, sizeof(int))) < 0) {
     goto Lerror;
+  }
+
+  if ((res = safe_setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, SOCKOPT_ON, sizeof(int))) < 0) {
+    goto Lerror;
+  }
+
+  if ((res = socketManager.ink_bind(fd, &addr.sa, ats_ip_size(&addr.sa), IPPROTO_TCP)) < 0) {
+    goto Lerror;
+  }
+
+#ifdef SET_TCP_NO_DELAY
+  if ((res = safe_setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, SOCKOPT_ON, sizeof(int))) < 0) {
+    goto Lerror;
+  }
 #endif
+
 #ifdef SET_SO_KEEPALIVE
   // enables 2 hour inactivity probes, also may fix IRIX FIN_WAIT_2 leak
-  if ((res = safe_setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, SOCKOPT_ON, sizeof(int))) < 0)
+  if ((res = safe_setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, SOCKOPT_ON, sizeof(int))) < 0) {
     goto Lerror;
-#endif
-
-#if defined(linux)
-  if (NetProcessor::accept_mss > 0)
-    if ((res = safe_setsockopt(fd, IPPROTO_TCP, TCP_MAXSEG, (char *) &NetProcessor::accept_mss, sizeof(int)) < 0))
-      goto Lerror;
-#endif
-
-  /*
-   * dg: this has been removed since the ISS patch under solaris seems
-   * to not like the socket being listened on twice. This is first done
-   * in the manager when the socket is created.
-   */
-  if (non_blocking)
-    if ((res = safe_nonblocking(fd)) < 0)
-      goto Lerror;
-  {
-    int namelen = sizeof(addr);
-    if ((res = safe_getsockname(fd, &addr.sa, &namelen)))
-      goto Lerror;
   }
+#endif
 
   if (transparent) {
 #if TS_USE_TPROXY
-    int transparent_value = 1;
     Debug("http_tproxy", "Listen port inbound transparency enabled.\n");
-    if (setsockopt(fd, SOL_IP, TS_IP_TRANSPARENT, &transparent_value, sizeof(transparent_value)) == -1) {
-      Error("[Server::setup_fd_for_listen] Unable to set transparent socket option [%d] %s\n", errno, strerror(errno));
+    if (safe_setsockopt(fd, SOL_IP, TS_IP_TRANSPARENT, SOCKOPT_ON, sizeof(int)) < 0) {
+      Error("[Server::listen] Unable to set transparent socket option [%d] %s\n", errno, strerror(errno));
       _exit(1);
     }
 #else
-    Error("[Server::setup_fd_for_listen] Transparency requested but TPROXY not configured\n");
+    Error("[Server::listen] Transparency requested but TPROXY not configured\n");
 #endif
   }
 
-  return 0;
+#if defined(TCP_MAXSEG)
+  if (NetProcessor::accept_mss > 0) {
+    if ((res = safe_setsockopt(fd, IPPROTO_TCP, TCP_MAXSEG, (char *) &NetProcessor::accept_mss, sizeof(int))) < 0) {
+      goto Lerror;
+    }
+  }
+#endif
+
+  if ((res = safe_listen(fd, get_listen_backlog())) < 0) {
+    goto Lerror;
+  }
+
+  if (non_blocking) {
+    if ((res = safe_nonblocking(fd)) < 0) {
+      goto Lerror;
+    }
+  }
+
 Lerror:
   res = -errno;
+
   // coverity[check_after_sink]
-  if (fd != NO_FD)
+  if (fd != NO_FD) {
     close();
+    fd = NO_FD;
+  }
+
   return res;
 }
 
@@ -269,128 +298,35 @@ Server::listen(bool non_blocking, int recv_bufsize, int send_bufsize, bool trans
   int namelen;
 
   if (!ats_is_ip(&accept_addr)) {
-    ats_ip4_set(&addr, INADDR_ANY,0);
+    ats_ip4_set(&addr, INADDR_ANY, 0);
   } else {
     ats_ip_copy(&addr, &accept_addr);
   }
 
-  res = socketManager.socket(addr.sa.sa_family, SOCK_STREAM, IPPROTO_TCP);
-
-  if (res < 0)
-    return res;
-  fd = res;
-
-  if (http_accept_filter)
-    add_http_filter(fd);
-
-#ifdef SEND_BUF_SIZE
-  {
-    int send_buf_size = SEND_BUF_SIZE;
-    if ((res = safe_setsockopt(fd, SOL_SOCKET, SO_SNDBUF, (char *) &send_buf_size, sizeof(int)) < 0))
-      goto Lerror;
-  }
-#endif
-#ifdef RECV_BUF_SIZE
-  {
-    int recv_buf_size = RECV_BUF_SIZE;
-    if ((res = safe_setsockopt(fd, SOL_SOCKET, SO_RCVBUF, (char *) &recv_buf_size, sizeof(int))) < 0)
-      goto Lerror;
-  }
-#endif
-
-  if (recv_bufsize) {
-    if (socketManager.set_rcvbuf_size(fd, recv_bufsize)) {
-      // Round down until success
-      int rbufsz = ROUNDUP(recv_bufsize, 1024);
-      while (rbufsz) {
-        if (socketManager.set_rcvbuf_size(fd, rbufsz)) {
-          rbufsz -= 1024;
-        } else {
-          break;
-        }
-      }
-    }
-  }
-  if (send_bufsize) {
-    if (socketManager.set_sndbuf_size(fd, send_bufsize)) {
-      // Round down until success
-      int sbufsz = ROUNDUP(send_bufsize, 1024);
-      while (sbufsz) {
-        if (socketManager.set_sndbuf_size(fd, sbufsz)) {
-          sbufsz -= 1024;
-        } else {
-          break;
-        }
-      }
-    }
-  }
-#ifdef SET_CLOSE_ON_EXEC
-  if ((res = safe_fcntl(fd, F_SETFD, 1)) < 0)
-    goto Lerror;
-#endif
-
-#ifdef SET_NO_LINGER
-  {
-    struct linger l;
-    l.l_onoff = 0;
-    l.l_linger = 0;
-    if ((res = safe_setsockopt(fd, SOL_SOCKET, SO_LINGER, (char *) &l, sizeof(l))) < 0)
-      goto Lerror;
-  }
-#endif
-
-  if (ats_is_ip6(&addr) && (res = safe_setsockopt(fd, IPPROTO_IPV6, IPV6_V6ONLY, SOCKOPT_ON, sizeof(int))) < 0)
-    goto Lerror;
-
-  if ((res = safe_setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, SOCKOPT_ON, sizeof(int))) < 0)
-    goto Lerror;
-
-  if ((res = socketManager.ink_bind(fd, &addr.sa, ats_ip_size(&addr.sa), IPPROTO_TCP)) < 0) {
+  fd = res = socketManager.socket(addr.sa.sa_family, SOCK_STREAM, IPPROTO_TCP);
+  if (res < 0) {
     goto Lerror;
   }
-#ifdef SET_TCP_NO_DELAY
-  if ((res = safe_setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, SOCKOPT_ON, sizeof(int))) < 0)
-    goto Lerror;
-#endif
-#ifdef SET_SO_KEEPALIVE
-  // enables 2 hour inactivity probes, also may fix IRIX FIN_WAIT_2 leak
-  if ((res = safe_setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, SOCKOPT_ON, sizeof(int))) < 0)
-    goto Lerror;
-#endif
 
-  if (transparent) {
-#if TS_USE_TPROXY
-    int transparent_value = 1;
-    Debug("http_tproxy", "Listen port inbound transparency enabled.\n");
-    if (setsockopt(fd, SOL_IP, TS_IP_TRANSPARENT, &transparent_value, sizeof(transparent_value)) == -1) {
-      Error("[Server::listen] Unable to set transparent socket option [%d] %s\n", errno, strerror(errno));
-      _exit(1);
-    }
-#else
-    Error("[Server::listen] Transparency requested but TPROXY not configured\n");
-#endif
+  res = setup_fd_for_listen(non_blocking, recv_bufsize, send_bufsize, transparent);
+  if (res < 0) {
+    goto Lerror;
   }
 
-#if defined(linux)
-  if (NetProcessor::accept_mss > 0)
-    if ((res = safe_setsockopt(fd, IPPROTO_TCP, TCP_MAXSEG, (char *) &NetProcessor::accept_mss, sizeof(int))) < 0)
-      goto Lerror;
-#endif
-
-  if ((res = safe_listen(fd, get_listen_backlog())) < 0)
-    goto Lerror;
-  if (non_blocking)
-    if ((res = safe_nonblocking(fd)) < 0)
-      goto Lerror;
   // Original just did this on port == 0.
   namelen = sizeof(addr);
-  if ((res = safe_getsockname(fd, &addr.sa, &namelen)))
+  if ((res = safe_getsockname(fd, &addr.sa, &namelen))) {
       goto Lerror;
+  }
+
   return 0;
 
 Lerror:
-  if (fd != NO_FD)
+  if (fd != NO_FD) {
     close();
+    fd = NO_FD;
+  }
+
   Error("Could not bind or listen to port %d (error: %d)", ats_ip_port_host_order(&addr), res);
   return res;
 }
