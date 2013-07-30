f92d413fe15b094e28dc459a089e94dcbe3095a5
James Peach <jpeach@apache.org>
2013-07-30 13:20:41 -0700

    vagrant: fix remaining packages for centos5

--- contrib/manifests/redhat.pp
+++ contrib/manifests/redhat.pp
@@ -30,11 +30,21 @@ package {'epel-release':
 package {[
     'gcc', 'gcc-c++', 'automake', 'autoconf', 'libtool', 'pkgconfig',
     'openssl-devel', 'tcl-devel', 'expat-devel', 'pcre-devel',
-    'ncurses-devel', 'libcurl-devel', 'libaio-devel',
+    'ncurses-devel', 'libaio-devel',
     'hwloc-devel', 'libcap-devel', 'bison', 'flex', 'make',
-    'perl-ExtUtils-MakeMaker',
   ]:
   ensure => latest
+}
+
+package { $lsbmajdistrelease ? { '5' => 'curl-devel', default => 'libcurl-devel'}:
+  ensure => latest
+}
+
+# RHEL/CentOS 5 doesn't have MakeMaker and I don't know what package to use instead :(
+if $lsbmajdistrelease != '5' {
+  package{'perl-ExtUtils-MakeMaker':
+    ensure => latest
+  }
 }
 
 # development extras.
