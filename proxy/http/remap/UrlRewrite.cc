6cb2477c5a2052f16f281d886e196aceea2f7f3c
James Peach <jpeach@apache.org>
2013-11-01 12:47:43 -0700

    TS-2323: remove unnecessary includes from UrlRewrite

--- proxy/http/remap/UrlRewrite.cc
+++ proxy/http/remap/UrlRewrite.cc
@@ -22,20 +22,11 @@
  */
 
 #include "UrlRewrite.h"
-#include "Main.h"
-#include "P_EventSystem.h"
-#include "StatSystem.h"
-#include "P_Cache.h"
 #include "ProxyConfig.h"
 #include "ReverseProxy.h"
-#include "MatcherUtils.h"
-#include "Tokenizer.h"
-#include "api/ts/remap.h"
 #include "UrlMappingPathIndex.h"
 #include "RemapConfig.h"
 #include "I_Layout.h"
-
-#include "ink_string.h"
 
 #define modulePrefix "[ReverseProxy]"
 
