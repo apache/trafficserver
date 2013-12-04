af63c8ed5576245a1d192b4c13a906e2053ac67a
Kit Chan <kichan@apache.org>
2013-12-03 23:25:13 -0800

    TS-2335: getting ts_lua to compile without warnings

--- plugins/experimental/ts_lua/ts_lua_atomic.h
+++ plugins/experimental/ts_lua/ts_lua_atomic.h
@@ -24,18 +24,6 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-typedef struct
-{
-    volatile int64_t head;
-    const char *name;
-    unsigned int offset;
-} ts_lua_atomiclist;
-
-
-void ts_lua_atomiclist_init(ts_lua_atomiclist * l, const char *name, uint32_t offset_to_next);
-void *ts_lua_atomiclist_push(ts_lua_atomiclist * l, void *item);
-void *ts_lua_atomiclist_popall(ts_lua_atomiclist * l);
-
 static inline int ts_lua_atomic_increment(volatile int32_t *mem, int value) { return __sync_fetch_and_add(mem, value); }
 
 #endif
