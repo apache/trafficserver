24926f532da839c0e2cb265d0fe57b216eafdf31
Brian Geffon <briang@apache.org>
2014-03-26 11:46:30 -0700

    [TS-2665] Clean up ink_stack_trace_dump code
    Conflicts:
    	CHANGES

--- lib/ts/ink_stack_trace.cc
+++ lib/ts/ink_stack_trace.cc
@@ -29,6 +29,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#ifndef STDERR_FILENO
+#define STDERR_FILENO 2
+#endif
+
 #if TS_HAS_BACKTRACE
 
 #include <execinfo.h>           /* for backtrace_symbols, etc. */
@@ -40,19 +44,22 @@ ink_stack_trace_dump()
   int btl;
 
   // Recopy and re-terminate the app name in case it has been trashed.
-  char name[256];
   const char *msg = " - STACK TRACE: \n";
-  ink_strlcpy(name, program_name, sizeof(name));
-  if (write(2, name, strlen(name)) == -1)
+  if (write(STDERR_FILENO, program_name, strlen(program_name)) == -1)
     return;
-  if (write(2, msg, strlen(msg)) == -1)
+  if (write(STDERR_FILENO, msg, strlen(msg)) == -1)
     return;
+
+  // In certain situations you can get stuck in malloc waiting for a lock
+  // that your program held when it segfaulted. We set an alarm so that
+  // if this situation happens it will allow traffic_server to exit.
+  alarm(10);
 
   void *stack[INK_STACK_TRACE_MAX_LEVELS + 1];
   memset(stack, 0, sizeof(stack));
   if ((btl = backtrace(stack, INK_STACK_TRACE_MAX_LEVELS)) > 2) {
     // dump the backtrace to stderr
-    backtrace_symbols_fd(stack + 2, btl - 2, 2);
+    backtrace_symbols_fd(stack + 2, btl - 2, STDERR_FILENO);
   }
 }
 
@@ -62,7 +69,7 @@ void
 ink_stack_trace_dump()
 {
   const char msg[] = "ink_stack_trace_dump not implemented on this operating system\n";
-  if (write(2, msg, sizeof(msg) - 1) == -1)
+  if (write(STDERR_FILENO, msg, sizeof(msg) - 1) == -1)
       return;
 }
 
