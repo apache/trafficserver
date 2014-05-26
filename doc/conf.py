0df9621a86f18aec6f74d37bb98b2aef378a0a29
Igor Galić <i.galic@brainsware.org>
2014-05-26 12:20:02 +0200

    remove unused MathJax extension, sort extensions one per line

--- doc/conf.py
+++ doc/conf.py
@@ -45,7 +45,16 @@ from manpages import man_pages
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.graphviz', 'sphinx.ext.intersphinx', 'sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode', 'traffic-server' ]
+extensions = [
+  'sphinx.ext.graphviz',
+  'sphinx.ext.intersphinx',
+  'sphinx.ext.autodoc',
+  'sphinx.ext.todo',
+  'sphinx.ext.coverage',
+  'sphinx.ext.pngmath',
+  'sphinx.ext.viewcode',
+  'traffic-server',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
