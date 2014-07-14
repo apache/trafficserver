28aac2121022235eecaa38ceb63f6f80ab9d5032
Bryan Call <bcall@apache.org>
2014-07-14 14:55:00 -0600

    TS-1475: doing a memset on initialized sockaddr_in

--- tools/jtest/jtest.cc
+++ tools/jtest/jtest.cc
@@ -2327,6 +2327,7 @@ static int make_client (unsigned int addr, int port) {
 
   /* Give the socket a name. */
   struct sockaddr_in name;
+  memset(&name, 0, sizeof(sockaddr_in));
   name.sin_family = AF_INET;
   name.sin_port = htons(port);
   name.sin_addr.s_addr = addr;
