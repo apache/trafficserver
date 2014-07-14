496615f3b29fe7e89662835f52d3a1c44e1b5962
Phil Sorber <sorber@apache.org>
2014-07-14 16:41:24 -0600

    TS-1475: Fix Coverity # 1216790

--- plugins/experimental/collapsed_connection/collapsed_connection.cc
+++ plugins/experimental/collapsed_connection/collapsed_connection.cc
@@ -943,12 +943,13 @@ collapsedConnectionMainHandler(TSCont /* contp ATS_UNUSED */, TSEvent event, voi
             addMutexRetry(txn_data, event, 0);
           } else {
             freeCcTxnData(txn_data);
+            txn_data = NULL;
           }
         } else {
           txn_data->cc_state = CC_REMOVE;
         }
       }
-      if (CC_INSERT == txn_data->cc_state || CC_REMOVE == txn_data->cc_state) {
+      if (txn_data && (CC_INSERT == txn_data->cc_state || CC_REMOVE == txn_data->cc_state)) {
         txn_data->cc_state = CC_REMOVE;
         if (TS_SUCCESS == updateOrRemoveHashEntry(txn_data)) {
           freeCcTxnData(txn_data);
