84ebfc16d29bca25752e1caef1aec9d311def053
Leif Hedstrom <zwoop@apache.org>
2014-07-14 16:52:43 -0600

    TS-1475 Avoid leaking memory. Coverity: 1200016.

--- plugins/experimental/healthchecks/healthchecks.c
+++ plugins/experimental/healthchecks/healthchecks.c
@@ -319,11 +319,8 @@ parse_configs(const char* fname)
     int state = 0;
     char *ok=NULL, *miss=NULL, *mime=NULL;
 
-    /* Allocate a new config-info, if we don't have one already */
-    if (!finfo) {
-      finfo = TSmalloc(sizeof(HCFileInfo));
-      memset(finfo, 0, sizeof(HCFileInfo));
-    }
+    finfo = TSmalloc(sizeof(HCFileInfo));
+    memset(finfo, 0, sizeof(HCFileInfo));
 
     if (fgets(buf, sizeof(buf) - 1, fd)) {
       str = strtok_r(buf, SEPARATORS, &save);
@@ -374,7 +371,8 @@ parse_configs(const char* fname)
           prev_finfo->_next = finfo;
         }
         prev_finfo = finfo;
-        finfo = NULL; /* We used this one up, get a new one next iteration */
+      } else {
+        TSfree(finfo);
       }
     }
   }
