6c60ab0147a9f12a2b989640846a588f5dbf1197
Jack Bates <jack@nottheoilrig.com>
2014-08-29 12:30:05 -0700

    TS-2832: Add links from an API description to the code for that object

--- doc/conf.py
+++ doc/conf.py
@@ -46,6 +46,7 @@ from manpages import man_pages
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
+  'doxygen',
   'sphinx.ext.graphviz',
   'sphinx.ext.intersphinx',
   'sphinx.ext.autodoc',
