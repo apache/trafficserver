627a7011e7b20f8b5526e2d641df28edf8a4098c
Brian Geffon <briang@apache.org>
2014-09-09 13:18:09 -0700

    Fixing small bug w/ url caching

--- lib/atscppapi/src/utils_internal.cc
+++ lib/atscppapi/src/utils_internal.cc
@@ -53,6 +53,10 @@ int handleTransactionEvents(TSCont cont, TSEvent event, void *edata) {
   switch (event) {
   case TS_EVENT_HTTP_POST_REMAP:
     transaction.getClientRequest().getUrl().reset();
+    // This is here to force a refresh of the cached client request url
+    TSMBuffer hdr_buf;
+    TSMLoc hdr_loc;
+    TSHttpTxnClientReqGet(static_cast<TSHttpTxn>(transaction.getAtsHandle()), &hdr_buf, &hdr_loc);
     break;
   case TS_EVENT_HTTP_SEND_REQUEST_HDR:
     utils::internal::initTransactionServerRequest(transaction);
@@ -105,11 +109,6 @@ void inline invokePluginForEvent(Plugin *plugin, TSHttpTxn ats_txn_handle, TSEve
     break;
   case TS_EVENT_HTTP_POST_REMAP:
     plugin->handleReadRequestHeadersPostRemap(transaction);
-
-    // This is here to force a refresh of the cached client request url
-    TSMBuffer hdr_buf;
-    TSMLoc hdr_loc;
-    TSHttpTxnClientReqGet(static_cast<TSHttpTxn>(transaction.getAtsHandle()), &hdr_buf, &hdr_loc);
     break;
   case TS_EVENT_HTTP_SEND_REQUEST_HDR:
     plugin->handleSendRequestHeaders(transaction);
