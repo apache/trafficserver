5ee27f38dd247a2429572e2902442e1116f4a3b8
Brian Geffon <briang@apache.org>
2014-10-06 22:06:04 -0700

    Apparently we check in generated code now :( I should have caught this, I'll back it out then add this back in properly
    Revert "TS-3114: Refactor IpMap to make the RB tree a shared data structure"
    
    This reverts commit 860b6737e939c5ed0fa0744dfb1d7844b7395473.

--- lib/tsconfig/TsConfigGrammar.h
+++ lib/tsconfig/TsConfigGrammar.h
@@ -1,10 +1,8 @@
+/* A Bison parser, made by GNU Bison 2.7.  */
 
-/* A Bison parser, made by GNU Bison 2.4.1.  */
-
-/* Skeleton interface for Bison's Yacc-like parsers in C
+/* Bison interface for Yacc-like parsers in C
    
-      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
+      Copyright (C) 1984, 1989-1990, 2000-2012 Free Software Foundation, Inc.
    
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -32,9 +30,17 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
+#ifndef YY_TSCONFIG_TSCONFIGGRAMMAR_H_INCLUDED
+# define YY_TSCONFIG_TSCONFIGGRAMMAR_H_INCLUDED
+/* Enabling traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int tsconfigdebug;
+#endif
 /* "%code requires" blocks.  */
-
-/* Line 1676 of yacc.c  */
+/* Line 2058 of yacc.c  */
 #line 1 "TsConfigGrammar.y"
 
 /** @file
@@ -61,9 +67,8 @@
  */
 
 
-
-/* Line 1676 of yacc.c  */
-#line 67 "TsConfigGrammar.h"
+/* Line 2058 of yacc.c  */
+#line 72 "TsConfigGrammar.h"
 
 /* Tokens.  */
 #ifndef YYTOKENTYPE
@@ -101,7 +106,6 @@
 
 
 
-
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef int YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
@@ -110,5 +114,18 @@ typedef int YYSTYPE;
 #endif
 
 
+#ifdef YYPARSE_PARAM
+#if defined __STDC__ || defined __cplusplus
+int tsconfigparse (void *YYPARSE_PARAM);
+#else
+int tsconfigparse ();
+#endif
+#else /* ! YYPARSE_PARAM */
+#if defined __STDC__ || defined __cplusplus
+int tsconfigparse (yyscan_t lexer, struct TsConfigHandlers* handlers);
+#else
+int tsconfigparse ();
+#endif
+#endif /* ! YYPARSE_PARAM */
 
-
+#endif /* !YY_TSCONFIG_TSCONFIGGRAMMAR_H_INCLUDED  */
