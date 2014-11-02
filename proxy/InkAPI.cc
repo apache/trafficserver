8d1d1582da02811f59cc4bad9c40f7c02ceb7ecf
sunwei <sunwei@yahoo-inc.com>
2014-11-02 00:45:04 +0000

    TS-3163 Apply socket configurations to outbound connections created by TSNetConnect

--- proxy/InkAPI.cc
+++ proxy/InkAPI.cc
@@ -6482,10 +6482,20 @@ TSNetConnect(TSCont contp, sockaddr const* addr)
   sdk_assert(sdk_sanity_check_continuation(contp) == TS_SUCCESS);
   sdk_assert(ats_is_ip(addr));
 
+  HttpConfigParams *http_config_param = HttpConfig::acquire();
+  NetVCOptions opt;
+  if (http_config_param) {
+    opt.set_sock_param(http_config_param->oride.sock_recv_buffer_size_out,
+                      http_config_param->oride.sock_send_buffer_size_out,
+                      http_config_param->oride.sock_option_flag_out,
+                      http_config_param->oride.sock_packet_mark_out,
+                      http_config_param->oride.sock_packet_tos_out);
+  }
+
   FORCE_PLUGIN_MUTEX(contp);
 
   return reinterpret_cast<TSAction>(
-    netProcessor.connect_re(reinterpret_cast<INKContInternal*>(contp), addr)
+    netProcessor.connect_re(reinterpret_cast<INKContInternal*>(contp), addr, &opt)
   );
 }
 
