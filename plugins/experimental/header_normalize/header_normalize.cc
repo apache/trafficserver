08f19da9bdefd03ee444e7498f53f98b396d64aa
Sudheer Vinukonda <sudheerv@yahoo-inc.com>
2014-11-17 16:36:44 +0000

    Adding AL2 license blurb for the new file

--- plugins/experimental/header_normalize/header_normalize.cc
+++ plugins/experimental/header_normalize/header_normalize.cc
@@ -1,4 +1,28 @@
-//////////////////////////////////////////////////////////////////////////////////////////////
+/** @file
+
+    Plugin to perform background fetches of certain content that would
+    otherwise not be cached. For example, Range: requests / responses.
+
+    @section license License
+
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+////////////////////////////////////////////////////////////////////////////////
 // header_normalize::
 //
 // ATS plugin to convert headers into camel-case. It may be useful to solve
@@ -7,21 +31,10 @@
 //
 // Note that the plugin currently uses READ_REQUEST_HDR_HOOK to camel-case
 // the headers. As an optimization, it can be changed to SEND_REQUEST_HDR_HOOK
-// so that it only converts, if/when the request is being sent to the origin
+// so that it only converts, if/when the request is being sent to the origin.
 //
-// To use this plugin, configure a remap.config rule like
-//
-//   map http://foo.com http://bar.com @plugin=header_normalize.so
-//
-//
-// The list of of all available parameters is in the README.
-//
-//
-// Note that the path to the plugin itself must be absolute, and by default it is
-//
-//    /home/y/libexec/trafficserver/header_normalize.so
-//
-//
+// This plugin supports both global mode as well as per-remap mode activation
+//////////////////////////////////////////////////////////////////////////////////
 
 #define UNUSED __attribute__ ((unused))
 static char UNUSED rcsId__header_normalize_cc[] = "@(#) $Id: header_normalize.cc 218 2014-11-11 01:29:16Z sudheerv $ built on " __DATE__ " " __TIME__;
