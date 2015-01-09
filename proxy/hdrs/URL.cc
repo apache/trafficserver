49f83a4234feedf79196d9e15286c65c149ad839
Leif Hedstrom <zwoop@apache.org>
2015-01-08 17:04:54 -0700

    Revert "Add a simple wrapper to loop over all tsqa scripts"
    This reverts commit 011e43405cb115a0049c1ea13c031fb8bb8ca614.

--- proxy/hdrs/URL.cc
+++ proxy/hdrs/URL.cc
@@ -97,7 +97,7 @@ int url_hash_method = 0;
 
 /*-------------------------------------------------------------------------
   -------------------------------------------------------------------------*/
-URLHashContext::HashType URLHashContext::Setting = URLHashContext::MD5;
+URLHashContext::HashType URLHashContext::Setting = URLHashContext::MMH;
 
 URLHashContext::URLHashContext() {
   switch (Setting) {
