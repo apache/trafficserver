f7f3055a22f175d9158f8a5ae473482519fcce43
Leif Hedstrom <zwoop@apache.org>
2015-01-23 11:40:26 -0700

    TS-3287 Ignore this coverity error
    Coverity CID #1261575

--- proxy/hdrs/HdrTest.cc
+++ proxy/hdrs/HdrTest.cc
@@ -311,7 +311,7 @@ HdrTest::test_format_date()
 
   // (2) test a few times per day from 1/1/1970 to past 2010
 
-  // coverity[secure_coding]
+  // coverity[dont_call]
   for (t = 0; t < 40 * 366 * (24 * 60 * 60); t += (int) (drand48() * (24 * 60 * 60))) {
     cftime_replacement(buffer, sizeof(buffer), "%a, %d %b %Y %T %Z", &t);
     t2 = mime_parse_date(buffer, buffer + (int) strlen(buffer));
