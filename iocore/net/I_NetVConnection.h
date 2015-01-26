55c20701abd5f80e2094a4d30efb32c008e10bbd
Brian Geffon <briang@apache.org>
2015-01-26 11:16:23 -0800

    Fix subtle bug with strlcpy with non-null terminated strings

--- iocore/net/I_NetVConnection.h
+++ iocore/net/I_NetVConnection.h
@@ -185,7 +185,7 @@ struct NetVCOptions {
     IpEndpoint ip;
 
     // Literal IPv4 and IPv6 addresses are not permitted in "HostName".(rfc6066#section-3)
-    if (ats_ip_pton(ts::ConstBuffer(name, len), &ip) != 0) {
+    if (name && len && ats_ip_pton(ts::ConstBuffer(name, len), &ip) != 0) {
       sni_servername = ats_strndup(name, len);
     } else {
       sni_servername = NULL;
