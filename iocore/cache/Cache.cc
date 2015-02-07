apache/trafficserver

commit 01121ce827e246496f74e0ce4ed8770b67cb524d

TS-3287: Test for equality with zero on fd as well
Coverity CID #1021842

iocore/cache/Cache.cc
@@ -728,7 +728,7 @@ CacheProcessor::start_internal(int flags)
     if (fd < 0 && (opts & O_CREAT))  // Try without O_DIRECT if this is a file on filesystem, e.g. tmpfs.
       fd = open(path, DEFAULT_CACHE_OPTIONS | O_CREAT, 0644);
 
-    if (fd > 0) {
+    if (fd >= 0) {
       if (!sd->file_pathname) {
         if (ftruncate(fd, ((uint64_t) blocks) * STORE_BLOCK_SIZE) < 0) {
           Warning("unable to truncate cache file '%s' to %d blocks", path, blocks);
@@ -773,7 +773,7 @@ CacheProcessor::start_internal(int flags)
       else
         Warning("cache unable to open '%s': %s", path, strerror(errno));
     }
-    if (fd > 0) {
+    if (fd >= 0) {
       close(fd);
     }
   }

commit f2a7e289548bea3b17fb962cfdeced9f30cd10ef

TS-3377: Fix bad file descriptor handling
This was caused by commit 01121ce827e246496f74e0ce4ed8770b67cb524d
that was to fix Coverity CID #1021842 and reverted by
4ed316a0cbd136e5f66205e2d363126ac86f90da. I am re-adding that patch
as well.

iocore/cache/Cache.cc
@@ -728,7 +728,7 @@ CacheProcessor::start_internal(int flags)
     if (fd < 0 && (opts & O_CREAT))  // Try without O_DIRECT if this is a file on filesystem, e.g. tmpfs.
       fd = open(path, DEFAULT_CACHE_OPTIONS | O_CREAT, 0644);
 
-    if (fd > 0) {
+    if (fd >= 0) {
       if (!sd->file_pathname) {
         if (ftruncate(fd, ((uint64_t) blocks) * STORE_BLOCK_SIZE) < 0) {
           Warning("unable to truncate cache file '%s' to %d blocks", path, blocks);
@@ -764,7 +764,7 @@ CacheProcessor::start_internal(int flags)
 #else
         gdisks[gndisks]->open(path, blocks, skip, sector_size, fd, clear);
 #endif
-        fd = 0;
+        fd = -1;
         gndisks++;
       }
     } else {
@@ -773,7 +773,7 @@ CacheProcessor::start_internal(int flags)
       else
         Warning("cache unable to open '%s': %s", path, strerror(errno));
     }
-    if (fd > 0) {
+    if (fd >= 0) {
       close(fd);
     }
   }