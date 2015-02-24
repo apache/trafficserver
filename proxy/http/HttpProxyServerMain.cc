488ba88a037b3030d37637d96cdc8affc886486a
Leif Hedstrom <zwoop@apache.org>
2015-02-24 12:36:44 -0700

    TS-3406 Add endpoints for h2-14 as well as h2, at least until 7.0

--- proxy/http/HttpProxyServerMain.cc
+++ proxy/http/HttpProxyServerMain.cc
@@ -221,7 +221,11 @@ MakeHttpProxyAcceptor(HttpProxyAcceptor& acceptor, HttpProxyPort& port, unsigned
 
     // HTTP2
     if (port.m_session_protocol_preference.contains(TS_NPN_PROTOCOL_INDEX_HTTP_2_0)) {
-      ssl->registerEndpoint(TS_NPN_PROTOCOL_HTTP_2_0, new Http2SessionAccept(accept_opt));
+      Http2SessionAccept *acc = new Http2SessionAccept(accept_opt);
+
+      // TODO: Should be removed when h2-14 is gone and dead, and h2 is widely supported in UAs
+      ssl->registerEndpoint(TS_NPN_PROTOCOL_HTTP_2_0_14, acc);
+      ssl->registerEndpoint(TS_NPN_PROTOCOL_HTTP_2_0, acc);
     }
 
     MUTEX_LOCK(lock, ssl_plugin_mutex, this_ethread());
