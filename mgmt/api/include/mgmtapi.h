4cbbf2c8ff930b45cd18e30d226f9a17a73b1e5b
Leif Hedstrom <zwoop@apache.org>
2015-03-02 11:05:58 -0700

    TS-3420 Simpler and functional fixes, but not clean, for redefining mem debugging symbols

--- mgmt/api/include/mgmtapi.h
+++ mgmt/api/include/mgmtapi.h
@@ -44,10 +44,12 @@ extern "C"
 {
 #endif                          /* __cplusplus */
 
+#ifndef TS_RES_MEM_PATH
 #define __TS_RES_PATH(x)   #x
 #define _TS_RES_PATH(x)    __TS_RES_PATH (x)
 #define TS_RES_PATH(x)     x __FILE__ ":" _TS_RES_PATH (__LINE__)
 #define TS_RES_MEM_PATH    TS_RES_PATH ("memory/")
+#endif
 
 /***************************************************************************
  * Error and Return Values
