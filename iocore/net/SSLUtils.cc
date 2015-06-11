c8c9c4fa0dad38552553b4235929a4f7ea8aa322
shinrich <shinrich@yahoo-inc.com>
2015-06-11 17:40:58 -0500

    TS-3687: ATS Session Cache should remove expired sessions.  This closes #222.

--- iocore/net/SSLUtils.cc
+++ iocore/net/SSLUtils.cc
@@ -185,6 +185,12 @@ SSL_CTX_add_extra_chain_cert_file(SSL_CTX *ctx, const char *chainfile)
   return true;
 }
 
+bool ssl_session_timed_out(SSL_SESSION *session) 
+{
+  return SSL_SESSION_get_timeout(session) < (long)(time(NULL) - SSL_SESSION_get_time(session));
+}
+
+static void ssl_rm_cached_session(SSL_CTX *ctx, SSL_SESSION *sess);
 
 static SSL_SESSION *
 ssl_get_cached_session(SSL *ssl, unsigned char *id, int len, int *copy)
@@ -201,10 +207,15 @@ ssl_get_cached_session(SSL *ssl, unsigned char *id, int len, int *copy)
   SSL_SESSION *session = NULL;
 
   if (session_cache->getSession(sid, &session)) {
-    return session;
+    // Double check the timeout
+    if (session &&  ssl_session_timed_out(session)) {
+      // Due to bug in openssl, the timeout is checked, but only removed
+      // from the openssl built-in hash table.  The external remove cb is not called
+      ssl_rm_cached_session(SSL_get_SSL_CTX(ssl), session);
+      session = NULL;
+    }
   }
-
-  return NULL;
+  return session;
 }
 
 static int
