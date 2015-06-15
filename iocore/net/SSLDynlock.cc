2c9acad7dc3ed82e9c463ffd909b648369d43c36
Bryan Call <bcall@apache.org>
2015-06-15 16:12:08 -0700

    TS-1808: OpenSSL dynlock support formating, license, and include ifdefs

--- iocore/net/SSLDynlock.cc
+++ iocore/net/SSLDynlock.cc
@@ -1,19 +1,36 @@
+/** @file
+
+  @section license License
+
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
 #include "libts.h"
 
-struct CRYPTO_dynlock_value
-{
-  CRYPTO_dynlock_value(const char *f, int l): file(f), line(l) {
-    pthread_mutex_init(&mutex, NULL);
-  }
-  ~CRYPTO_dynlock_value() {
-    pthread_mutex_destroy(&mutex);
-  }
+struct CRYPTO_dynlock_value {
+  CRYPTO_dynlock_value(const char *f, int l) : file(f), line(l) { pthread_mutex_init(&mutex, NULL); }
+  ~CRYPTO_dynlock_value() { pthread_mutex_destroy(&mutex); }
   const char *file;
   int line;
   pthread_mutex_t mutex;
 };
 
-struct CRYPTO_dynlock_value *ssl_dyn_create_callback(const char *file, int line)
+struct CRYPTO_dynlock_value *
+ssl_dyn_create_callback(const char *file, int line)
 {
   Debug("v_ssl_lock", "file: %s line: %d", file, line);
 
@@ -21,7 +38,8 @@ struct CRYPTO_dynlock_value *ssl_dyn_create_callback(const char *file, int line)
   return value;
 }
 
-void ssl_dyn_lock_callback(int mode, struct CRYPTO_dynlock_value *value, const char *file, int line)
+void
+ssl_dyn_lock_callback(int mode, struct CRYPTO_dynlock_value *value, const char *file, int line)
 {
   Debug("v_ssl_lock", "file: %s line: %d", file, line);
 
@@ -35,7 +53,8 @@ void ssl_dyn_lock_callback(int mode, struct CRYPTO_dynlock_value *value, const c
   }
 }
 
-void ssl_dyn_destroy_callback(struct CRYPTO_dynlock_value *value, const char *file, int line)
+void
+ssl_dyn_destroy_callback(struct CRYPTO_dynlock_value *value, const char *file, int line)
 {
   Debug("v_ssl_lock", "file: %s line: %d", file, line);
 
