d50a3cba09dba031d3328dc592318a33c2e166e7
Sudheer Vinukonda <sudheerv@yahoo-inc.com>
2015-06-30 15:34:54 +0000

    [TS-3727]: replace ink_get_hrtime_internal() with Thread::get_hrtime().

--- iocore/net/SSLNetVConnection.cc
+++ iocore/net/SSLNetVConnection.cc
@@ -466,7 +466,7 @@ SSLNetVConnection::net_read_io(NetHandler *nh, EThread *lthread)
       readSignalError(nh, err);
     } else if (ret == SSL_HANDSHAKE_WANT_READ || ret == SSL_HANDSHAKE_WANT_ACCEPT) {
       if (SSLConfigParams::ssl_handshake_timeout_in > 0) {
-        double handshake_time = ((ink_get_hrtime_internal() - sslHandshakeBeginTime)/1000000000);
+        double handshake_time = ((Thread::get_hrtime() - sslHandshakeBeginTime)/1000000000);
         Debug ("ssl", "ssl handshake for vc %p, took %.3f seconds, configured handshake_timer: %d", this, handshake_time, SSLConfigParams::ssl_handshake_timeout_in);
         if (handshake_time > SSLConfigParams::ssl_handshake_timeout_in) {
           Debug ("ssl", "ssl handshake for vc %p, expired, release the connection", this);
@@ -851,7 +851,7 @@ int
 SSLNetVConnection::sslStartHandShake(int event, int &err)
 {
   if (sslHandshakeBeginTime == 0) {
-    sslHandshakeBeginTime = ink_get_hrtime_internal();
+    sslHandshakeBeginTime = Thread::get_hrtime();
     // net_activity will not be triggered until after the handshake
     set_inactivity_timeout(HRTIME_SECONDS(SSLConfigParams::ssl_handshake_timeout_in));
   }
