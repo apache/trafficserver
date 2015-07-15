1d81a2d79a4b21c3d7850e3acde40fd612bc6f38
Alan M. Carroll <solidwallofcode@yahoo-inc.com>
2015-07-15 13:53:42 -0500

    TS-3769: Fix coverity complaint about NULL reference.

--- proxy/http/HttpTransact.cc
+++ proxy/http/HttpTransact.cc
@@ -1592,9 +1592,10 @@ HttpTransact::ReDNSRoundRobin(State *s)
 
     // Our ReDNS of the server succeeded so update the necessary
     //  information and try again. Need to preserve the current port value if possible.
-    in_port_t server_port = s->current.server ? s->current.server->dst_addr.host_order_port() : s->server_info.dst_addr.isValid() ?
-                                                s->server_info.dst_addr.host_order_port() :
-                                                s->hdr_info.client_request.port_get();
+    in_port_t server_port = s->current.server->dst_addr.host_order_port();
+    // Temporary check to make sure the port preservation can be depended upon. That should be the case
+    // because we get here only after trying a connection. Remove for 6.2.
+    ink_assert(s->current.server->dst_addr.isValid() && 0 != server_port);
 
     ats_ip_copy(&s->server_info.dst_addr, s->host_db_info.ip());
     s->server_info.dst_addr.port() = htons(server_port);
