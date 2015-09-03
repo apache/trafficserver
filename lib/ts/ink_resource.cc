208e5385a944d6386a2f66d0998ff3a81a954639
Bryan Call <bcall@apache.org>
2015-09-03 11:18:55 -0700

    TS-3877: Add a tracking ClassAllocator to keep track of where the allocation happened Fixed a build issue

--- lib/ts/ink_resource.cc
+++ lib/ts/ink_resource.cc
@@ -37,7 +37,7 @@ ink_mutex ResourceTracker::resourceLock = PTHREAD_MUTEX_INITIALIZER;
 class Resource
 {
 public:
-  Resource() : _incrementCount(0), _decrementCount(0), _value(0), _symbol(NULL), _name("") {}
+  Resource() : _incrementCount(0), _decrementCount(0), _value(0), _symbol(NULL) { _name[0] = '\0'; }
   void increment(const int64_t size);
   int64_t
   getValue() const
