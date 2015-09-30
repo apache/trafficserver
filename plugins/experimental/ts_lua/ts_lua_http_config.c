4f3db399f885236939bfc3689afd566aaa1a6e05
Kit Chan <kichan@apache.org>
2015-09-30 01:13:46 -0700

    TS-3951: adding a static assert on size of the http config vars array

--- plugins/experimental/ts_lua/ts_lua_http_config.c
+++ plugins/experimental/ts_lua/ts_lua_http_config.c
@@ -197,6 +197,10 @@ ts_lua_var_item ts_lua_http_config_vars[] = {
   TS_LUA_MAKE_VAR_ITEM(TS_LUA_CONFIG_HTTP_CACHE_MAX_OPEN_WRITE_RETRIES), TS_LUA_MAKE_VAR_ITEM(TS_LUA_CONFIG_LAST_ENTRY),
 };
 
+// Needed to make sure we have the latest list of overridable http config vars when compiling
+#define NUM_HTTP_CONFIG_VARS (sizeof(ts_lua_http_config_vars) / sizeof(ts_lua_http_config_vars[0]))
+extern char __ts_lua_http_config_vars_static_assert[NUM_HTTP_CONFIG_VARS == TS_CONFIG_LAST_ENTRY + 1 ? 0 : -1];
+
 ts_lua_var_item ts_lua_http_timeout_vars[] = {
   TS_LUA_MAKE_VAR_ITEM(TS_LUA_TIMEOUT_ACTIVE), TS_LUA_MAKE_VAR_ITEM(TS_LUA_TIMEOUT_CONNECT),
   TS_LUA_MAKE_VAR_ITEM(TS_LUA_TIMEOUT_DNS), TS_LUA_MAKE_VAR_ITEM(TS_LUA_TIMEOUT_NO_ACTIVITY),
