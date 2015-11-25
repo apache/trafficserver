e0e08250c0d13dea2c8dc864e6f335c7a36fa2b2
James Peach <jpeach@apache.org>
2015-11-24 16:19:37 -0800

    TS-4099: add Lua bindings for custom metrics
    Implement custom metrics in terms of Lua bindings. We export a
    minimal API that constructs metrics and implicitly registers an
    evaluation function. We periodically evaluate all the evaluation
    functions and update their corresponding metrics.
    
    Metrics are implicitly registered when they are loaded, so there
    is no need to mention anything in mgmt/RecordsConfig.cc.
    
    The metrics registration and evaluation syntax looks like this:
    
      integer 'proxy.node.http.user_agents_total_documents_served' [[
          proxy.process.http.incoming_requests
      ]]

--- cmd/traffic_manager/AddConfigFilesHere.cc
+++ cmd/traffic_manager/AddConfigFilesHere.cc
@@ -84,5 +84,6 @@ initializeRegistry()
   configFiles->addFile("splitdns.config", false);
   configFiles->addFile("ssl_multicert.config", false);
   configFiles->addFile("stats.config.xml", false);
+  configFiles->addFile("metrics.config", false);
   configFiles->registerCallback(testcall);
 }
--- mgmt/RecordsConfig.cc
+++ mgmt/RecordsConfig.cc
@@ -1120,6 +1120,8 @@ static const RecordElement RecordsConfig[] =
   // Jira TS-21
   {RECT_CONFIG, "proxy.config.stats.snap_file", RECD_STRING, "stats.snap", RECU_RESTART_TS, RR_NULL, RECC_NULL, NULL, RECA_NULL}
   ,
+  {RECT_CONFIG, "proxy.config.stats.enable_lua", RECD_INT, "0", RECU_RESTART_TM, RR_NULL, RECC_INT, "[0-1]", RECA_NULL}
+  ,
 
   //        ###########
   //        # Parsing #
