apache/trafficserver

commit bd304288b55e989ff94819818aae93c48cc0c685

TS-953: consolidate string copy/concat for iocore/cache/.
git-svn-id: https://svn.apache.org/repos/asf/trafficserver/traffic/trunk@1174262 13f79535-47bb-0310-9956-ffa450edef68

iocore/cache/CachePages.cc
@@ -73,10 +73,8 @@ struct ShowCache: public ShowCont {
 
     // process the query string
     if (u->query_get(&query_len)) {
-      strncpy(query, u->query_get(&query_len), query_len);
-      query[query_len] = '\0';
-      strncpy(unescapedQuery, query, query_len);
-      unescapedQuery[query_len] = '\0';
+      ink_strlcpy(query, u->query_get(&query_len), query_len);
+      ink_strlcpy(unescapedQuery, query, query_len);
 
       query_len = unescapifyStr(query);
 
@@ -103,9 +101,7 @@ struct ShowCache: public ShowCont {
       }
       // initialize url array
       show_cache_urlstrs = NEW(new char[nstrings + 1][500]);
-      for (int si = 0; si < nstrings + 1; si++)
-        for (int sj = 0; sj < 500; sj++)
-          show_cache_urlstrs[si][sj] = '\0';    // zeroing out mem
+      memset(show_cache_urlstrs, '\0', (nstrings + 1) * 500 * sizeof (char *));
 
       char *q, *t;
       p = strstr(unescapedQuery, "url=");
@@ -119,8 +115,7 @@ struct ShowCache: public ShowCont {
           q = strstr(p, "%0D%0A");      // we used this in the JS to separate urls
           if (!q)
             q = t;
-          strncpy(show_cache_urlstrs[s], p, q - p);
-          show_cache_urlstrs[s][q - p] = '\0';
+          ink_strlcpy(show_cache_urlstrs[s], p, sizeof(show_cache_urlstrs[s]));
           p = q + 6;            // +6 ==> strlen(%0D%0A)
         }
       }

iocore/cache/Cache.cc
@@ -1038,7 +1038,7 @@ Vol::init(char *s, off_t blocks, off_t dir_skip, bool clear)
   path = ats_strdup(s);
   const size_t hash_id_size = strlen(s) + 32;
   hash_id = (char *)ats_malloc(hash_id_size);
-  ink_strncpy(hash_id, s, hash_id_size);
+  ink_strlcpy(hash_id, s, hash_id_size);
   const size_t s_size = strlen(s);
   snprintf(hash_id + s_size, (hash_id_size - s_size), " %" PRIu64 ":%" PRIu64 "",
            (uint64_t)dir_skip, (uint64_t)blocks);

iocore/cache/Store.cc
@@ -260,7 +260,7 @@ Store::read_config(int fd)
     //      inside ink_cache_init() which is called AFTER
     //      initialize_store().
     //
-    // ink_strncpy(p, cache_system_config_directory, sizeof(p));
+    // ink_strlcpy(p, cache_system_config_directory, sizeof(p));
     IOCORE_ReadConfigString(storage_file, "proxy.config.cache.storage_filename", PATH_NAME_MAX);
     Layout::relative_to(storage_path, PATH_NAME_MAX, Layout::get()->sysconfdir, storage_file);
     Debug("cache_init", "Store::read_config, fd = -1, \"%s\"", storage_path);

commit a70e9473cabba89861965d5ab775dbe4f20c720b

TS-4106 Allow Cache Inspector to split properly on multiline URLs
Leif: This was broken in commit bd304288b55e989, and the fixes seems
to restore original behavior (while retaining the use of strlcpy() ).

This closes #401

iocore/cache/CachePages.cc
@@ -126,7 +126,7 @@ struct ShowCache : public ShowCont {
           q = strstr(p, "%0D%0A" /* \r\n */); // we used this in the JS to separate urls
           if (!q)
             q = t;
-          ink_strlcpy(show_cache_urlstrs[s], p, sizeof(show_cache_urlstrs[s]));
+          ink_strlcpy(show_cache_urlstrs[s], p, q ? q - p + 1: sizeof(show_cache_urlstrs[s]));
           p = q + 6; // +6 ==> strlen(%0D%0A)
         }
       }