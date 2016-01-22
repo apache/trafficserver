c59b94663555210b724fc138ca40ddf73833d5a1
Leif Hedstrom <zwoop@apache.org>
2016-01-22 12:12:39 -0700

    TS-4070 Fixes clang-format
    The fixes on master covered more than just this, so had to do this
    as a separate commit.

--- proxy/http/remap/RemapProcessor.cc
+++ proxy/http/remap/RemapProcessor.cc
@@ -88,8 +88,8 @@ RemapProcessor::setup_for_remap(HttpTransact::State *s)
 
   if (rewrite_table->num_rules_forward_with_recv_port) {
     Debug("url_rewrite", "[lookup] forward mappings with recv port found; Using recv port %d", s->client_info.dst_addr.port());
-    if (rewrite_table->forwardMappingWithRecvPortLookup(request_url, s->client_info.dst_addr.port(), request_host,
-                                                        request_host_len, s->url_map)) {
+    if (rewrite_table->forwardMappingWithRecvPortLookup(request_url, s->client_info.dst_addr.port(), request_host, request_host_len,
+                                                        s->url_map)) {
       Debug("url_rewrite", "Found forward mapping with recv port");
       mapping_found = true;
     } else if (rewrite_table->num_rules_forward == 0) {
