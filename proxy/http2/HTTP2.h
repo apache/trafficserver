2aae7251b793c9db3cb3ddd71f666e872522732f
Masaori Koshiba <masaori@apache.org>
2016-01-29 18:35:55 +0900

    TS-4162: Add proxy.config.http2.active_timeout_in
    Current default value is 0 for compatibility with existing 6.x code.
    This is going to be changed by TS-4167 in 7.0.0.
    
    This closes #444

--- proxy/http2/HTTP2.h
+++ proxy/http2/HTTP2.h
@@ -350,6 +350,7 @@ public:
   static uint32_t max_request_header_size;
   static uint32_t accept_no_activity_timeout;
   static uint32_t no_activity_timeout_in;
+  static uint32_t active_timeout_in;
 
   static void init();
 };
