5ab92ff3bd9784d742c12590b9337afc206aa20f
Daniel Xu <dlxu2@yahoo.com>
2016-02-17 21:18:05 +0000

    TS-4214 HttpSM debug message incorrect
    This closes #483
    
    The debug message didn't make any sense before since it was
    displaying the expected value as actual and vis versa.

--- proxy/http/HttpSM.cc
+++ proxy/http/HttpSM.cc
@@ -2842,7 +2842,7 @@ HttpSM::is_http_server_eos_truncation(HttpTunnelProducer *p)
   int64_t cl = t_state.hdr_info.server_response.get_content_length();
 
   if (cl != UNDEFINED_COUNT && cl > server_response_body_bytes) {
-    DebugSM("http", "[%" PRId64 "] server eos after %" PRId64 ".  Expected %" PRId64, sm_id, cl, server_response_body_bytes);
+    DebugSM("http", "[%" PRId64 "] server eos after %" PRId64 ".  Expected %" PRId64, sm_id, server_response_body_bytes, cl);
     return true;
   } else {
     return false;
