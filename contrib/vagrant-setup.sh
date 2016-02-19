16b134ab9f528bcfbf5350c2f9f813bfd0f0d12b
Christoph Keller <keller.christoph86@gmail.com>
2016-02-20 00:13:51 +0100

    added omnios and forced updates of packagelists

--- contrib/vagrant-setup.sh
+++ contrib/vagrant-setup.sh
@@ -6,10 +6,14 @@ set -o pipefail
 case ${1} in
 trusty*|jessie*)
     sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
+    apt-get update
     apt-get install -y build-essential autoconf libpcre3-dev libssl-dev tcl-dev libxml2-dev
 ;;
 centos*)
     yum install -y autoconf gcc gcc-c++ pcre-devel openssl-devel tcl-devel libxml2-devel
+;;
+omnios*)
+    sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
 ;;
 *)
     echo "no config for ${1}"
