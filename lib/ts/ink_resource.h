35e93fb0a1112e2e9f66f406abd90b6b55fec8c6
Bryan Call <bcall@apache.org>
2016-02-23 15:06:58 -0800

    TS-4212: Add option to track memory allocation with OpenSSL
    This closes #489

--- lib/ts/ink_resource.h
+++ lib/ts/ink_resource.h
@@ -29,6 +29,8 @@
 #include <string>
 
 extern volatile int res_track_memory; /* set this to zero to disable resource tracking */
+extern uint64_t ssl_memory_allocated;
+extern uint64_t ssl_memory_freed;
 
 #define __RES_PATH(x) #x
 #define _RES_PATH(x) __RES_PATH(x)
