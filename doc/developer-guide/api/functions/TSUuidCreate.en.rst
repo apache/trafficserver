9f774fb656b44930375746e63df1b2cdb583bd18
Leif Hedstrom <zwoop@apache.org>
2016-06-10 22:03:35 -0600

    TS-4518 Add the basic APIs and code for UUIDs

---
+++ doc/developer-guide/api/functions/TSUuidCreate.en.rst
@@ -0,0 +1,136 @@
+.. Licensed to the Apache Software Foundation (ASF) under one
+   or more contributor license agreements.  See the NOTICE file
+   distributed with this work for additional information
+   regarding copyright ownership.  The ASF licenses this file
+   to you under the Apache License, Version 2.0 (the
+   "License"); you may not use this file except in compliance
+   with the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+.. include:: ../../../common.defs
+
+.. default-domain:: c
+
+TSUuidCreate
+************
+
+Traffic Server UUID construction APIs.
+
+Synopsis
+========
+
+`#include <ts/ts.h>`
+
+.. function:: TSUuid TSUuidCreate(void);
+.. function:: TSReturnCode TSUuidInitialize(TSUuid uuid, TSUuidVersion v);
+.. function:: void TSUuidDestroy(TSUuid uuid);
+.. function:: TSReturnCode TSUuidCopy(TSUuid dest, const TSUuid src);
+.. function:: const char *TSUuidStringGet(const TSUuid uuid);
+.. function:: TSUuidVersion TSUuidVersionGet(const TSUuid uuid);
+.. function:: TSReturnCode TSUuidStringParse(TSUuid uuid, const char *uuid_str);
+.. function:: const TSUuid TSProcessUuidGet(void);
+
+Description
+===========
+
+These APIs are used to create, manage, and use UUIDs in a plugin, implementing
+part of RFC 4122. Currently, only the V4 variant of the specifications is
+implemented. In addition, an internal, process unique UUID is provided, which
+can be used to uniquely identifying the running Traffic Server process.
+
+:func:`TSUuidCreate` creates a new :type:`TSUuid` object, which is returned
+and can be used by the other APIs. Similarly, a read-only global process UUID
+is returned from the function :func:`TSProcessUuidGet`. You must not attempt
+to modify any data as returned by either of these functions.
+
+:func:`TSUuidInitialize` initializes a :type:`TSUuid` object, using the
+algorithm defined for the specified version. Note that only the V4 variants is
+currently supported. You can call :func:`TSUuidInitialize` repeatedly, which
+each generates a new UUID, but this will overwrite any existing UUID data in
+the object. This also implies that any strings retrieved using
+:func:`TSUuidStringGet` are also modified accordingly.
+
+:func:`TSUuidDestroy` destroys (releases) an :type:`TSUuid` object, and frees
+all memory associated with this object. This includes any strings as returned
+by e.g. :func:`TSUuidStringGet`.
+
+:func:`TSUuidCopy` copies one :type:`TSUuid` to another, making an exact
+duplicate. Note that both the source and the destination UUIDs must be created
+appropriately, and should not have been previously destroyed.
+
+:func:`TSUuidVersionGet` returns the version number for the
+:type:`TSUuid`. This will work properly for any RFC 4122 initialized UUID
+object, e.g. if you parse a string with :func:`TSUuidStringParse` this will
+return the correct variant ID.
+
+:func:`TSUuidStringGet` returns a pointer to the internal string
+representation of the :type:`TSUuid` object. It's important to know that there
+is no transfer of ownership of this string. If you need a copy of it, you are
+responsible of doing so yourself. In particular, using a string as returned by
+:func:`TSUuidStringGet` **after** you have called :func:`TSUuidDestroy` on the
+corresponding :type:`TSUuid` object is a serious error. The UUID object does
+not do any sort of reference counting on the string, and you must absolutely
+not free the memory as returned by this API.
+
+Finally, :func:`TSUuidStringParse` can be used to convert an existing
+:type:`TSUuid` string to a Traffic Server UUID object. This will only succeed
+if the :type:`TSUuid` string is a proper *RFC 4122* UUID. The :type:`TSUuid`
+argument passed to this function must be a properly :func:`TSUuidCreate`
+object, but it does not need to be previously initialized.
+
+Return Values
+=============
+
+The :type:`TSUuid` type is an opaque pointer to an internal representation of
+the UUID object. Several of the functions returns a normal Traffic Server
+return status code, :type:`TSReturnCode`. You should verify the success of
+those APIs, of course.
+
+The :func:`TSUuidStringGet` function will return ``NULL`` if the :type:`TSUuid`
+object is not properly inititialized. Likewise, :func:`TSUuidVersionGet` would
+then return ``TS_UUID_UNDEFINED``.
+
+The :func:`TSUuidDestroy` function can not fail, and does not have a return
+value,  but you are of course responsible for providing a valid :type:`TSUuid`
+object.
+
+Examples
+========
+
+.. code-block:: c
+
+    #include <ts/ts.h>
+
+    TSUuid machine, uuid;
+
+    machine = TSProcessUuidGet();
+    printf("Machine UUID is %s\n", TSUuidStringGet(machine);
+
+    if (uuid = TSUuidCreate()) {
+      if (TS_SUCCESS == TSUuidInitialize(uuid, TS_UUID_V4) {
+        printf("My UUID is %s\n", TSUuidStringGet(uuid));
+      }
+      TSUuidDestroy(uuid);
+    }
+
+    const char* str = "c71e2bab-90dc-4770-9535-c9304c3de38e";
+
+    if (TS_SUCCESS == TSUuidStringParse(uuid, str)) {
+      if (TS_UUID_V4 == TSUuidVersionGet(uuid)) {
+        // Yes!
+      }
+    }
+
+See Also
+========
+
+:manpage:`TSAPI(3ts)`,
+:manpage:`TSUuid(3ts)`,
+:manpage:`TSReturnCode(3ts)`,
--- proxy/InkAPITest.cc
+++ proxy/InkAPITest.cc
@@ -7775,3 +7775,149 @@ REGRESSION_TEST(SDK_API_DEBUG_NAME_LOOKUPS)(RegressionTest *test, int /* atype A
 
   return;
 }
+
+////////////////////////////////////////////////
+// SDK_API_UUID
+//
+// Unit Test for API: TSUuidCreate
+//                    TSUuidDestroy
+//                    TSUuidCopy
+//                    TSUuidInitialize
+//                    TSProcessUuidGet
+//                    TSUuidStringGet
+//                    TSUuidVersionGet
+//                    TSUuidStringParse
+////////////////////////////////////////////////
+#include "ts/ink_uuid.h"
+
+REGRESSION_TEST(SDK_API_UUID)(RegressionTest *test, int /* atype ATS_UNUSED */, int *pstatus)
+{
+  TSUuid machine, uuid;
+  const char *str1;
+  const char *str2;
+  static const char uuid_v1[] = "5de5f9ec-30f4-11e6-a073-002590a33e4e";
+  static const char uuid_v4[] = "0e95fe5f-295a-401d-9ae4-eb32756d73cb";
+
+  *pstatus = REGRESSION_TEST_INPROGRESS;
+
+  // Test TSProcessUuidGet(), should just return a non-NULL pointer now.
+  machine = TSProcessUuidGet();
+  if (!machine) {
+    SDK_RPRINT(test, "TSProcessUuidGet", "TestCase1", TC_FAIL, "Returned a NULL pointer");
+    *pstatus = REGRESSION_TEST_FAILED;
+    return;
+  } else if (!((ATSUuid *)machine)->valid()) {
+    SDK_RPRINT(test, "TSProcessUuidGet", "TestCase2", TC_FAIL, "Returned an invalid UUID object");
+    *pstatus = REGRESSION_TEST_FAILED;
+    return;
+  } else {
+    SDK_RPRINT(test, "TSProcessUuidGet", "TestCase1", TC_PASS, "ok");
+    SDK_RPRINT(test, "TSProcessUuidGet", "TestCase2", TC_PASS, "ok");
+  }
+
+  // Test TSUuidStringGet, should return a random string (so can't check the string value itself)
+  str1 = TSUuidStringGet(machine);
+  if (!str1 || (TS_UUID_STRING_LEN != strlen(str1))) {
+    SDK_RPRINT(test, "TSUuidStringGet", "TestCase1", TC_FAIL, "Did not return a valid UUID string representation");
+    *pstatus = REGRESSION_TEST_FAILED;
+    return;
+  } else {
+    SDK_RPRINT(test, "TSUuidStringGet", "TestCase1", TC_PASS, "ok");
+  }
+
+  // Test TSUuidCreate
+  if (!(uuid = TSUuidCreate())) {
+    SDK_RPRINT(test, "TSUuidCreate", "TestCase1", TC_FAIL, "Failed to crete a UUID object");
+    *pstatus = REGRESSION_TEST_FAILED;
+    return;
+  } else {
+    SDK_RPRINT(test, "TSUuidCreate", "TestCase1", TC_PASS, "ok");
+    if (TS_SUCCESS != TSUuidInitialize(uuid, TS_UUID_V4)) {
+      SDK_RPRINT(test, "TSUuidInitialize", "TestCase1", TC_FAIL, "Failed to Initialize a V4 UUID");
+      *pstatus = REGRESSION_TEST_FAILED;
+      return;
+    } else {
+      SDK_RPRINT(test, "TSUuidInitialize", "TestCase1", TC_PASS, "ok");
+    }
+  }
+
+  // Test TSUuidVersion
+  if (TS_UUID_V4 != TSUuidVersionGet(uuid)) {
+    SDK_RPRINT(test, "TSUuidVersionGet", "TestCase1", TC_FAIL, "Failed to get the UUID version");
+    *pstatus = REGRESSION_TEST_FAILED;
+    return;
+  } else {
+    SDK_RPRINT(test, "TSUuidVersionGet", "TestCase1", TC_PASS, "ok");
+  }
+
+  // Test TSUuidCopy
+  if (TS_SUCCESS != TSUuidCopy(uuid, machine)) {
+    SDK_RPRINT(test, "TSUuidCopy", "TestCase1", TC_FAIL, "Failed to copy the Machine UUID object");
+    *pstatus = REGRESSION_TEST_FAILED;
+    return;
+  } else {
+    SDK_RPRINT(test, "TSUuidCopy", "TestCase1", TC_PASS, "ok");
+    str2 = TSUuidStringGet(uuid);
+    if (!str2 || (TS_UUID_STRING_LEN != strlen(str2)) || strcmp(str1, str2)) {
+      SDK_RPRINT(test, "TSUuidCopy", "TestCase2", TC_FAIL, "The copied UUID strings are not identical");
+      *pstatus = REGRESSION_TEST_FAILED;
+      return;
+    } else {
+      SDK_RPRINT(test, "TSUuidCopy", "TestCase2", TC_PASS, "ok");
+    }
+  }
+
+  // Test TSUuidInitialize again, make sure they take effect when called multiple times
+  if (TS_SUCCESS != TSUuidInitialize(uuid, TS_UUID_V4)) {
+    SDK_RPRINT(test, "TSUuidInitialize", "TestCase2", TC_FAIL, "Failed to re-initialize the UUID object");
+    *pstatus = REGRESSION_TEST_FAILED;
+    return;
+  } else {
+    SDK_RPRINT(test, "TSUuidInitialize", "TestCase2", TC_PASS, "ok");
+    str2 = TSUuidStringGet(uuid);
+    if (!str2 || (TS_UUID_STRING_LEN != strlen(str2)) || !strcmp(str1, str2)) {
+      SDK_RPRINT(test, "TSUuidInitialize", "TestCase3", TC_FAIL, "The re-initialized string is the same as before");
+      *pstatus = REGRESSION_TEST_FAILED;
+      return;
+    } else {
+      SDK_RPRINT(test, "TSUuidInitialize", "TestCase3", TC_PASS, "ok");
+    }
+  }
+
+  // Test TSUuidStringParse
+  if ((TS_SUCCESS != TSUuidStringParse(uuid, uuid_v1)) || (TS_UUID_V1 != TSUuidVersionGet(uuid))) {
+    SDK_RPRINT(test, "TSUuidStringParse", "TestCase1", TC_FAIL, "Failed to parse the UUID v1 string");
+    *pstatus = REGRESSION_TEST_FAILED;
+    return;
+  } else {
+    SDK_RPRINT(test, "TSUuidStringParse", "TestCase1", TC_PASS, "ok");
+    str1 = TSUuidStringGet(uuid);
+    if (!str1 || (TS_UUID_STRING_LEN != strlen(str1)) || strcmp(str1, uuid_v1)) {
+      SDK_RPRINT(test, "TSUuidStringString", "TestCase2", TC_FAIL, "The parse UUID v1 string does not match the original");
+      *pstatus = REGRESSION_TEST_FAILED;
+      return;
+    } else {
+      SDK_RPRINT(test, "TSUuidStringParse", "TestCase2", TC_PASS, "ok");
+    }
+  }
+  if ((TS_SUCCESS != TSUuidStringParse(uuid, uuid_v4)) || (TS_UUID_V4 != TSUuidVersionGet(uuid))) {
+    SDK_RPRINT(test, "TSUuidStringParse", "TestCase3", TC_FAIL, "Failed to parse the UUID v4 string");
+    *pstatus = REGRESSION_TEST_FAILED;
+    return;
+  } else {
+    SDK_RPRINT(test, "TSUuidStringParse", "TestCase3", TC_PASS, "ok");
+    str1 = TSUuidStringGet(uuid);
+    if (!str1 || (TS_UUID_STRING_LEN != strlen(str1)) || strcmp(str1, uuid_v4)) {
+      SDK_RPRINT(test, "TSUuidStringParse", "TestCase4", TC_FAIL, "The parse UUID v4 string does not match the original");
+      *pstatus = REGRESSION_TEST_FAILED;
+      return;
+    } else {
+      SDK_RPRINT(test, "TSUuidStringParse", "TestCase4", TC_PASS, "ok");
+    }
+  }
+
+  TSUuidDestroy(uuid);
+
+  *pstatus = REGRESSION_TEST_PASSED;
+  return;
+}
