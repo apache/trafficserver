dda9df7ca7b886b5fd54d3ff6e9ed4f741948ba3
Leif Hedstrom <zwoop@apache.org>
2016-06-14 11:10:27 -0600

    TS-4519 Adds a new log tag, %<puuid> for the process UUID

--- proxy/logging/Log.cc
+++ proxy/logging/Log.cc
@@ -476,6 +476,11 @@ Log::init_fields()
   global_field_list.add(field, false);
   ink_hash_table_insert(field_symbol_hash, "cluc", field);
 
+  field = new LogField("process_uuid", "puuid", LogField::STRING, &LogAccess::marshal_process_uuid,
+                       (LogField::UnmarshalFunc)&LogAccess::unmarshal_str);
+  global_field_list.add(field, false);
+  ink_hash_table_insert(field_symbol_hash, "puuid", field);
+
   field = new LogField("client_req_body_len", "cqbl", LogField::sINT, &LogAccess::marshal_client_req_body_len,
                        &LogAccess::unmarshal_int_to_str);
   global_field_list.add(field, false);
