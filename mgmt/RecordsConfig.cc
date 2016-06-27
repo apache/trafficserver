986e56bb542e662329d267ff1ee73c28d7ee8510
Syeda Aziz <persia@kickedpredict.corp.ne1.yahoo.com>
2016-06-27 16:40:51 +0000

    TS-3371: Programmatic disabling of SSL ticket use.
    unwanted changes removed

--- mgmt/RecordsConfig.cc
+++ mgmt/RecordsConfig.cc
@@ -1215,6 +1215,8 @@ static const RecordElement RecordsConfig[] =
   //##############################################################################
   {RECT_CONFIG, "proxy.config.ssl.enabled", RECD_INT, "0", RECU_RESTART_TS, RR_NULL, RECC_INT, "[0-1]", RECA_NULL}
   ,
+  {RECT_CONFIG, "proxy.config.ssl.server.session_ticket.enable", RECD_INT, "1", RECU_DYNAMIC, RR_NULL, RECC_INT, "[0-1]", RECA_NULL}
+  ,
   {RECT_CONFIG, "proxy.config.ssl.TLSv1", RECD_INT, "1", RECU_RESTART_TS, RR_NULL, RECC_INT, "[0-1]", RECA_NULL}
   ,
   {RECT_CONFIG, "proxy.config.ssl.TLSv1_1", RECD_INT, "1", RECU_RESTART_TS, RR_NULL, RECC_INT, "[0-1]", RECA_NULL}
