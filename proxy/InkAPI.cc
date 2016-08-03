87b898c146ad4b4408030390e7b82fcaee423792
Masaori Koshiba <masaori@apache.org>
2016-08-03 11:26:31 +0900

    TS-4713: Remove obsolete TSFetchClientProtoStackSet/Get

--- proxy/InkAPI.cc
+++ proxy/InkAPI.cc
@@ -7592,24 +7592,6 @@ TSFetchUserDataSet(TSFetchSM fetch_sm, void *data)
   ((FetchSM *)fetch_sm)->ext_set_user_data(data);
 }
 
-#if 0
-void
-TSFetchClientProtoStackSet(TSFetchSM fetch_sm, TSClientProtoStack proto_stack)
-{
-  sdk_assert(sdk_sanity_check_fetch_sm(fetch_sm) == TS_SUCCESS);
-
-  ((FetchSM*)fetch_sm)->ext_set_proto_stack(proto_stack);
-}
-
-TSClientProtoStack
-TSFetchClientProtoStackGet(TSFetchSM fetch_sm)
-{
-  sdk_assert(sdk_sanity_check_fetch_sm(fetch_sm) == TS_SUCCESS);
-
-  return ((FetchSM*)fetch_sm)->ext_get_proto_stack();
-}
-#endif
-
 void *
 TSFetchUserDataGet(TSFetchSM fetch_sm)
 {
