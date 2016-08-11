4adcfae47a4a575157c8845c8da906ed4bf94d15
Bryon Gloden, CISSP® <cissp@bryongloden.com>
2016-08-11 18:59:36 -0400

    TS-4794: Fix memory leak in test_P_Net.cc
    This closes #855.

--- iocore/net/test_P_Net.cc
+++ iocore/net/test_P_Net.cc
@@ -45,7 +45,7 @@ struct NetTesterSM : public Continuation {
   handle_read(int event, void *data)
   {
     int r;
-    char *str;
+    char *str = NULL;
     switch (event) {
     case VC_EVENT_READ_READY:
       r   = reader->read_avail();
@@ -68,6 +68,7 @@ struct NetTesterSM : public Continuation {
     default:
       ink_release_assert(!"unknown event");
     }
+    delete[] str;
     return EVENT_CONT;
   }
 };
