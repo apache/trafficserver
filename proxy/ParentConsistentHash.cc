473ed61b89c764be909a91c79dc0c84333bc75aa
keith2008 <56985076@qq.com>
2016-08-18 14:46:02 +0800

    Update ParentConsistentHash.cc
    (TS-4744) ParentConsistentHash::selectParent mayselect the unavailable parent

--- proxy/ParentConsistentHash.cc
+++ proxy/ParentConsistentHash.cc
@@ -143,6 +143,8 @@ ParentConsistentHash::selectParent(const ParentSelectionPolicy *policy, bool fir
         if (prtmp) {
           pRec = (parents[last_lookup] + prtmp->idx);
         }
+        else  
+>         pRec = NULL; 
       } while (prtmp && strcmp(prtmp->hostname, result->hostname) == 0);
     }
   }
@@ -185,6 +187,8 @@ ParentConsistentHash::selectParent(const ParentSelectionPolicy *policy, bool fir
           pRec = (parents[last_lookup] + prtmp->idx);
           Debug("parent_select", "Selected a new parent: %s.", pRec->hostname);
         }
+        else
+>         pRec = NULL;
       }
       if (wrap_around[PRIMARY] && chash[SECONDARY] == NULL) {
         Debug("parent_select", "No available parents.");
