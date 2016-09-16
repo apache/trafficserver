08c318a7c08481ac67a18aa8c81bb31e74618d1d
Leif Hedstrom <zwoop@apache.org>
2016-09-16 11:22:05 -0600

    Updated the ATS config generator for docs/ci

--- ci/jenkins/ats_conf.pl
+++ ci/jenkins/ats_conf.pl
@@ -16,37 +16,48 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+use lib '/opt/ats/share/perl5';
 use Apache::TS::Config::Records;
 use File::Copy;
 
-chdir("/usr/local");
+#chdir("/usr/local");
+chdir("/opt/ats");
 
 my $recedit = new Apache::TS::Config::Records(file => "etc/trafficserver/records.config.default");
 
 $recedit->append(line => "");
 $recedit->append(line => "# My local stuff");
+$recedit->append(line => "CONFIG proxy.config.proxy_name STRING ATS");
+$recedit->append(line => "CONFIG proxy.config.proxy_binary_opts STRING -M --disable_freelist");
+#$recedit->append(line => "CONFIG proxy.config.crash_log_helper STRING /home/admin/bin/invoker_wrap.sh");
 
 # Port setup
 $recedit->set(conf => "proxy.config.http.server_ports", val => "80 80:ipv6 443:ssl 443:ipv6:ssl");
+$recedit->set(conf => "proxy.config.admin.autoconf_port", val => "48083");
+$recedit->set(conf => "proxy.config.process_manager.mgmt_port", val => "48084");
 
 # Threads
 $recedit->set(conf => "proxy.config.exec_thread.autoconfig", val => "0");
-$recedit->set(conf => "proxy.config.exec_thread.limit", val => "4");
+$recedit->set(conf => "proxy.config.exec_thread.limit", val => "8");
 $recedit->set(conf => "proxy.config.cache.threads_per_disk", val => "8");
 $recedit->set(conf => "proxy.config.accept_threads", val => "0");
 $recedit->set(conf => "proxy.config.exec_thread.affinity", val => "1");
 
 # TLS
-$recedit->set(conf => "proxy.config.ssl.server.cipher_suite", val => "ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:DHE-DSS-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA256:DHE-RSA-AES128-SHA256:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA:DHE-DSS-AES256-SHA:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA:AES256-GCM-SHA384:AES128-GCM-SHA256:AES256-SHA256:AES128-SHA256:AES256-SHA:AES128-SHA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA");
-$recedit->set(conf => "proxy.config.ssl.hsts_max_age", val => "86400");
-$recedit->set(conf => "proxy.config.ssl.max_record_size", val => "4k");
-$recedit->set(conf => "proxy.config.ssl.session_cache", val => "1"); # TLS default session cache
+#$recedit->set(conf => "proxy.config.ssl.server.cipher_suite", val => "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA384:AES128-GCM-SHA256:AES256-GCM-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES256-SHA:AES128-SHA:AES256-SHA:DES-CBC3-SHA!SRP:!DSS:!PSK:!aNULL:!eNULL:!SSLv2:!RC4");
+$recedit->set(conf => "proxy.config.ssl.hsts_max_age", val => "17280000");
+#$recedit->set(conf => "proxy.config.ssl.max_record_size", val => "-1");
+$recedit->set(conf => "proxy.config.ssl.session_cache", val => "2");
+$recedit->set(conf => "proxy.config.ssl.ocsp.enabled", val => "1");
+$recedit->set(conf => "proxy.config.http2.stream_priority_enabled", val => "1");
 
 # Cache setup
-$recedit->set(conf => "proxy.config.cache.ram_cache.size", val => "1G");
+$recedit->set(conf => "proxy.config.cache.ram_cache.size", val => "1536M");
 $recedit->set(conf => "proxy.config.cache.ram_cache_cutoff", val => "4M");
 $recedit->set(conf => "proxy.config.cache.limits.http.max_alts", val => "4");
 $recedit->set(conf => "proxy.config.cache.dir.sync_frequency", val => "600"); # 10 minutes intervals
+$recedit->set(conf => "proxy.config.http.cache.ignore_client_cc_max_age", val => "1");
+$recedit->set(conf => "proxy.config.allocator.hugepages", val => "1");
 
 # HTTP caching related stuff
 $recedit->set(conf => "proxy.config.http.cache.required_headers", val => "1");
@@ -67,20 +78,17 @@ $recedit->set(conf => "proxy.config.http.transaction_active_timeout_out", val =>
 $recedit->set(conf => "proxy.config.http.accept_no_activity_timeout", val => "30");
 
 # DNS / HostDB
-$recedit->set(conf => "proxy.config.hostdb.size", val => "1000");
-$recedit->set(conf => "proxy.config.hostdb.storage_size", val => "1M");
-$recedit->set(conf => "proxy.config.cache.hostdb.sync_frequency",  val => "1800");
+$recedit->set(conf => "proxy.config.cache.hostdb.sync_frequency",  val => "0");
 
 # Logging
 $recedit->set(conf => "proxy.config.log.logging_enabled", val => "3");
-$recedit->set(conf => "proxy.config.log.squid_log_enabled", val => "0");
 $recedit->set(conf => "proxy.config.log.max_space_mb_for_logs",  val => "4096");
 $recedit->set(conf => "proxy.config.log.max_space_mb_headroom",  val => "64");
 
 # Network
 $recedit->set(conf => "proxy.config.net.connections_throttle", val => "10000");
-$recedit->set(conf => "proxy.config.net.sock_send_buffer_size_in", val => "1024K");
-$recedit->set(conf => "proxy.config.net.sock_recv_buffer_size_out", val => "1024K");
+$recedit->set(conf => "proxy.config.net.sock_send_buffer_size_in", val => "4M");
+$recedit->set(conf => "proxy.config.net.sock_recv_buffer_size_out", val => "4M");
 $recedit->set(conf => "proxy.config.net.poll_timeout",  val => "30");
 
 # Local additions (typically not found in the records.config.default)
