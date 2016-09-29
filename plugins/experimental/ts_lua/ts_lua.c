bd33c4d24ca26399dd674ac4503167fc82c06ef4
rb304g <rb304g@att.com>
2016-09-29 18:42:56 +0000

    TS-4911: ts_lua plugin is modified to clear 'client_response_hdrp' pointer when it enters SEND_RESPONSE_HDR hook.
    (cherry picked from commit 7662c30e48a444d0d8d37b2e2d34f71b21e3bfcd)

--- plugins/experimental/ts_lua/ts_lua.c
+++ plugins/experimental/ts_lua/ts_lua.c
@@ -263,6 +263,13 @@ globalHookHandler(TSCont contp, TSEvent event ATS_UNUSED, void *edata)
     break;
 
   case TS_EVENT_HTTP_SEND_RESPONSE_HDR:
+    // client response can be changed within a transaction
+    // (e.g. due to the follow redirect feature). So, clearing the pointers
+    // to allow API(s) to fetch the pointers again when it re-enters the hook
+    if (http_ctx->client_response_hdrp != NULL) {
+      TSHandleMLocRelease(http_ctx->client_response_bufp, TS_NULL_MLOC, http_ctx->client_response_hdrp);
+      http_ctx->client_response_hdrp = NULL;
+    }
     lua_getglobal(l, TS_LUA_FUNCTION_G_SEND_RESPONSE);
     break;
 
