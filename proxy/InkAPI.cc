239c3104e2ef680bcfa35d997645bdf1485e161a
Phil Sorber <sorber@apache.org>
2016-10-26 12:09:25 -0600

    TS-5006: Fix CID 1306780
    Only release if not NULL.

--- proxy/InkAPI.cc
+++ proxy/InkAPI.cc
@@ -6734,8 +6734,8 @@ TSNetConnect(TSCont contp, sockaddr const *addr)
     opt.set_sock_param(http_config_param->oride.sock_recv_buffer_size_out, http_config_param->oride.sock_send_buffer_size_out,
                        http_config_param->oride.sock_option_flag_out, http_config_param->oride.sock_packet_mark_out,
                        http_config_param->oride.sock_packet_tos_out);
+    HttpConfig::release(http_config_param);
   }
-  HttpConfig::release(http_config_param);
 
   FORCE_PLUGIN_SCOPED_MUTEX(contp);
 
