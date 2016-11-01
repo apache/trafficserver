f1e5c2e889cefe99ebe38ee65d0cb37121ed635e
Leif Hedstrom <zwoop@apache.org>
2016-11-01 08:25:07 -0600

    TS-5024: Limits the gzip to 200 OK responses

--- plugins/gzip/gzip.cc
+++ plugins/gzip/gzip.cc
@@ -480,8 +480,8 @@ gzip_transformable(TSHttpTxn txnp, bool server, HostConfiguration *host_configur
 
   resp_status = TSHttpHdrStatusGet(bufp, hdr_loc);
 
-  // conservatively pick some statusses to compress
-  if (!(resp_status == 200 || resp_status == 404 || resp_status == 500)) {
+  // conservatively pick some statusses to compress. ToDo: Make this configurable?
+  if (resp_status != 200) {
     info("http response status [%d] is not compressible", resp_status);
     TSHandleMLocRelease(bufp, TS_NULL_MLOC, hdr_loc);
     return 0;
