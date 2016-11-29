ddf966b8666e31cd67654c0eae25250bff1d206e
Masa Sekimura <sekimura@gmail.com>
2016-11-28 17:12:44 -0800

    TS-5067 Fix required action for http2.enabled config

--- mgmt/RecordsConfig.cc
+++ mgmt/RecordsConfig.cc
@@ -1980,7 +1980,7 @@ static const RecordElement RecordsConfig[] =
   //# HTTP/2 global configuration.
   //#
   //############
-  {RECT_CONFIG, "proxy.config.http2.enabled", RECD_INT, "0", RECU_DYNAMIC, RR_NULL, RECC_INT, "[0-1]", RECA_NULL}
+  {RECT_CONFIG, "proxy.config.http2.enabled", RECD_INT, "0", RECU_RESTART_TS, RR_NULL, RECC_INT, "[0-1]", RECA_NULL}
   ,
   {RECT_CONFIG, "proxy.config.http2.stream_priority_enabled", RECD_INT, "0", RECU_DYNAMIC, RR_NULL, RECC_INT, "[0-1]", RECA_NULL}
   ,
