24aa4a4bdc997b9df58d46ccca93733e5f31f1c3
Jean Baptiste Favre <debian@jbfavre.org>
2017-01-04 13:38:25 +0100

    Fix documentation build for docutils 0.13.1 (1/2)
    * Remove docutils internal Inliner monkey patching
    * Add new role for :ts:jira: in traffic_server Sphinx extension
    * Add new role for :ts:github: in traffic_server Sphinx extension

--- doc/conf.py
+++ doc/conf.py
@@ -166,44 +166,9 @@ pygments_style = 'sphinx'
 nitpicky=1
 
 # Autolink issue references
-
-from docutils import nodes
-from docutils.parsers.rst import states
-from docutils.utils import unescape
-
-# Customize parser.inliner in the only way that Sphinx supports.
-# docutils.parsers.rst.Parser takes an instance of states.Inliner or a
-# subclass but Sphinx initializes it from
-# SphinxStandaloneReader.set_parser('restructuredtext') which is called
-# from Publisher.set_components() and initializes the parser without
-# arguments.
-
-BaseInliner = states.Inliner
-class Inliner(BaseInliner):
-  def __init__(self):
-    BaseInliner.__init__(self)
-
-    issue_pattern = re.compile(u'''
-      {start_string_prefix}
-      TS-\d+
-      {end_string_suffix}'''.format(
-        start_string_prefix=self.start_string_prefix,
-        end_string_suffix=self.end_string_suffix),
-      re.VERBOSE | re.UNICODE)
-
-    self.implicit_dispatch.append((issue_pattern, self.issue_reference))
-
-  def issue_reference(self, match, lineno):
-    text = match.group(0)
-
-    rawsource = unescape(text, True)
-    text = unescape(text, False)
-
-    refuri = 'https://issues.apache.org/jira/browse/' + text
-
-    return [nodes.reference(rawsource, text, refuri=refuri)]
-
-states.Inliner = Inliner
+# moved into traffic_server Sphinx extension
+trafficserver_jira_url='https://issues.apache.org/jira/browse/'
+trafficserver_github_url='https://github.com/apache/trafficserver/issues/'
 
 # -- Options for HTML output ---------------------------------------------------
 
@@ -352,6 +317,8 @@ latex_documents = [
 # documents and includes the same brief description in both the HTML
 # and manual page outputs.
 
+from docutils import nodes
+from docutils.utils import unescape
 from docutils.transforms import frontmatter
 from sphinx.writers import manpage
 
