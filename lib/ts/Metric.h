d2bf39a990d5ae83a7463937d8d05788cfef10ae
Alan M. Carroll <amc@apache.org>
2017-01-18 07:13:28 -0600

    clang-format.

--- lib/ts/Metric.h
+++ lib/ts/Metric.h
@@ -33,260 +33,297 @@
 
 namespace ApacheTrafficServer
 {
-  /** A class to hold scaled values.
+/** A class to hold scaled values.
 
-      Instances of this class have a @a count and a @a scale. The "value" of the instance is @a
-      count * @a scale.  The scale is stored in the compiler in the class symbol table and so only
-      the count is a run time value. An instance with a large scale can be assign to an instance
-      with a smaller scale and the conversion is done automatically. Conversions from a smaller to
-      larger scale must be explicit using @c metric_round_up and @c metric_round_down. This prevents
-      inadvertent changes in value. Because the scales are not the same these conversions can be
-      lossy and the two conversions determine whether, in such a case, the result should be rounded
-      up or down to the nearest scale value.
+    Instances of this class have a @a count and a @a scale. The "value" of the instance is @a
+    count * @a scale.  The scale is stored in the compiler in the class symbol table and so only
+    the count is a run time value. An instance with a large scale can be assign to an instance
+    with a smaller scale and the conversion is done automatically. Conversions from a smaller to
+    larger scale must be explicit using @c metric_round_up and @c metric_round_down. This prevents
+    inadvertent changes in value. Because the scales are not the same these conversions can be
+    lossy and the two conversions determine whether, in such a case, the result should be rounded
+    up or down to the nearest scale value.
 
-      @a N sets the scale. @a T is the type used to hold the count, which is in units of @a N.
+    @a N sets the scale. @a T is the type used to hold the count, which is in units of @a N.
 
-      @note This is modeled somewhat on @c std::chrono and serves a similar function for different
-      and simpler cases (where the ratio is always an integer, never a fraction).
+    @note This is modeled somewhat on @c std::chrono and serves a similar function for different
+    and simpler cases (where the ratio is always an integer, never a fraction).
 
-      @see metric_round_up
-      @see metric_round_down
-   */
-  template < intmax_t N, typename T = int >
-  class Metric
-  {
-    typedef Metric self; ///< Self reference type.
+    @see metric_round_up
+    @see metric_round_down
+ */
+template <intmax_t N, typename T = int> class Metric
+{
+  typedef Metric self; ///< Self reference type.
 
-  public:
-    /// Scaling factor for instances.
-    /// Make it externally accessible.
-    constexpr static intmax_t SCALE = N;
-    typedef T Count; ///< Type used to hold the count.
+public:
+  /// Scaling factor for instances.
+  /// Make it externally accessible.
+  constexpr static intmax_t SCALE = N;
+  typedef T Count; ///< Type used to hold the count.
 
-    constexpr Metric(); ///< Default contructor.
-    ///< Construct to have @a n scaled units.
-    constexpr Metric(Count n);
+  constexpr Metric(); ///< Default contructor.
+  ///< Construct to have @a n scaled units.
+  constexpr Metric(Count n);
 
-    /// Copy constructor for same scale.
-    template < typename C >
-    Metric(Metric<N, C> const& that);
+  /// Copy constructor for same scale.
+  template <typename C> Metric(Metric<N, C> const &that);
 
-    /// Copy / conversion constructor.
-    /// @note Requires that @c S be an integer multiple of @c SCALE.
-    template < intmax_t S, typename I >
-    Metric(Metric<S,I> const& that);
+  /// Copy / conversion constructor.
+  /// @note Requires that @c S be an integer multiple of @c SCALE.
+  template <intmax_t S, typename I> Metric(Metric<S, I> const &that);
 
-    /// Direct assignment.
-    /// The count is set to @a n.
-    self& operator = (Count n);
+  /// Direct assignment.
+  /// The count is set to @a n.
+  self &operator=(Count n);
 
-    /// The number of scale units.
-    constexpr Count count() const;
-    /// The absolute value, scaled up.
-    constexpr Count units() const;
+  /// The number of scale units.
+  constexpr Count count() const;
+  /// The absolute value, scaled up.
+  constexpr Count units() const;
 
-    /// Assignment operator.
-    /// @note Requires the scale of @c S be an integer multiple of the scale of this.
-    template < intmax_t S, typename I >
-    self& operator = (Metric<S,I> const& that);
-    /// Assignment from same scale.
-    self& operator = (self const& that);
+  /// Assignment operator.
+  /// @note Requires the scale of @c S be an integer multiple of the scale of this.
+  template <intmax_t S, typename I> self &operator=(Metric<S, I> const &that);
+  /// Assignment from same scale.
+  self &operator=(self const &that);
 
-    /// Run time access to the scale of this metric (template arg @a N).
-    static constexpr intmax_t scale();
+  /// Run time access to the scale of this metric (template arg @a N).
+  static constexpr intmax_t scale();
 
-  protected:
-    Count _n; ///< Number of scale units.
-  };
+protected:
+  Count _n; ///< Number of scale units.
+};
 
-  template < intmax_t N, typename C >
-  constexpr Metric<N,C>::Metric() : _n() {}
-  template < intmax_t N, typename C >
-  constexpr Metric<N,C>::Metric(Count n) : _n(n) {}
-  template < intmax_t N, typename C >
-  constexpr auto Metric<N,C>::count() const -> Count { return _n; }
-  template < intmax_t N, typename C >
-  constexpr auto Metric<N,C>::units() const -> Count { return _n * SCALE; }
-  template < intmax_t N, typename C >
-  inline auto Metric<N,C>::operator = (Count n) -> self& { _n = n; return *this; }
-  template < intmax_t N, typename C >
-  inline auto Metric<N,C>::operator = (self const& that) -> self& { _n = that._n; return *this; }
-  template < intmax_t N, typename C >
-  constexpr inline intmax_t Metric<N,C>::scale() { return SCALE; }
+template <intmax_t N, typename C> constexpr Metric<N, C>::Metric() : _n()
+{
+}
+template <intmax_t N, typename C> constexpr Metric<N, C>::Metric(Count n) : _n(n)
+{
+}
+template <intmax_t N, typename C>
+constexpr auto
+Metric<N, C>::count() const -> Count
+{
+  return _n;
+}
+template <intmax_t N, typename C>
+constexpr auto
+Metric<N, C>::units() const -> Count
+{
+  return _n * SCALE;
+}
+template <intmax_t N, typename C>
+inline auto
+Metric<N, C>::operator=(Count n) -> self &
+{
+  _n = n;
+  return *this;
+}
+template <intmax_t N, typename C>
+inline auto
+Metric<N, C>::operator=(self const &that) -> self &
+{
+  _n = that._n;
+  return *this;
+}
+template <intmax_t N, typename C>
+constexpr inline intmax_t
+Metric<N, C>::scale()
+{
+  return SCALE;
+}
 
-  template <intmax_t N, typename C>
-    template <typename I>
-    Metric<N,C>::Metric(Metric<N,I> const& that) : _n(static_cast<C>(that._n))
-    {
-    }
+template <intmax_t N, typename C> template <typename I> Metric<N, C>::Metric(Metric<N, I> const &that) : _n(static_cast<C>(that._n))
+{
+}
 
-  template <intmax_t N, typename C>
-    template <intmax_t S, typename I>
-    Metric<N,C>::Metric(Metric<S,I> const& that)
-    {
-      typedef std::ratio<S,N> R;
-      static_assert(R::den == 1, "Construction not permitted - target scale is not an integral multiple of source scale.");
-      _n = that.count() * R::num;
-    }
+template <intmax_t N, typename C> template <intmax_t S, typename I> Metric<N, C>::Metric(Metric<S, I> const &that)
+{
+  typedef std::ratio<S, N> R;
+  static_assert(R::den == 1, "Construction not permitted - target scale is not an integral multiple of source scale.");
+  _n = that.count() * R::num;
+}
 
-  template <intmax_t N, typename C>
-    template <intmax_t S, typename I>
-    auto Metric<N,C>::operator = (Metric<S,I> const& that) -> self&
-    {
-      typedef std::ratio<S,N> R;
-      static_assert(R::den == 1, "Assignment not permitted - target scale is not an integral multiple of source scale.");
-      _n = that.count() * R::num;
-      return *this;
-    }
+template <intmax_t N, typename C>
+template <intmax_t S, typename I>
+auto
+Metric<N, C>::operator=(Metric<S, I> const &that) -> self &
+{
+  typedef std::ratio<S, N> R;
+  static_assert(R::den == 1, "Assignment not permitted - target scale is not an integral multiple of source scale.");
+  _n = that.count() * R::num;
+  return *this;
+}
 
-  // -- Free Functions --
+// -- Free Functions --
 
-  /** Convert a metric @a src to a different scale, keeping the unit value as close as possible, rounding up.
-      The resulting count in the return value will be the smallest count that is not smaller than the unit
-      value of @a src.
+/** Convert a metric @a src to a different scale, keeping the unit value as close as possible, rounding up.
+    The resulting count in the return value will be the smallest count that is not smaller than the unit
+    value of @a src.
 
-      @code
-      typedef Metric<16> Paragraphs;
-      typedef Metric<1024> KiloBytes;
+    @code
+    typedef Metric<16> Paragraphs;
+    typedef Metric<1024> KiloBytes;
 
-      Paragraphs src(37459);
-      auto size = metric_round_up<KiloBytes>(src); // size.count() == 586
-      @endcode
-   */
-  template < typename M, intmax_t S, typename I >
-  M metric_round_up(Metric<S,I> const& src)
-  {
-    typedef std::ratio<M::SCALE, S> R;
-    auto c = src.count();
+    Paragraphs src(37459);
+    auto size = metric_round_up<KiloBytes>(src); // size.count() == 586
+    @endcode
+ */
+template <typename M, intmax_t S, typename I>
+M
+metric_round_up(Metric<S, I> const &src)
+{
+  typedef std::ratio<M::SCALE, S> R;
+  auto c = src.count();
 
-    if (M::SCALE == S) {
-      return c;
-    } else if (R::den == 1) {
-      return c / R::num + (0 != c % R::num); // N is a multiple of S.
-    } else if (R::num == 1) {
-      return c * R::den; // S is a multiple of N.
-    } else {
-      return (c / R::num) * R::den + (( c % R::num ) * R::den) / R::num + (0 != (c % R::num));
-    }
+  if (M::SCALE == S) {
+    return c;
+  } else if (R::den == 1) {
+    return c / R::num + (0 != c % R::num); // N is a multiple of S.
+  } else if (R::num == 1) {
+    return c * R::den; // S is a multiple of N.
+  } else {
+    return (c / R::num) * R::den + ((c % R::num) * R::den) / R::num + (0 != (c % R::num));
   }
+}
 
-  /** Convert a metric @a src to a different scale, keeping the unit value as close as possible, rounding down.
-      The resulting count in the return value will be the largest count that is not larger than the unit
-      value of @a src.
+/** Convert a metric @a src to a different scale, keeping the unit value as close as possible, rounding down.
+    The resulting count in the return value will be the largest count that is not larger than the unit
+    value of @a src.
 
-      @code
-      typedef Metric<16> Paragraphs;
-      typedef Metric<1024> KiloBytes;
+    @code
+    typedef Metric<16> Paragraphs;
+    typedef Metric<1024> KiloBytes;
 
-      Paragraphs src(37459);
-      auto size = metric_round_up<KiloBytes>(src); // size.count() == 585
-      @endcode
-   */
-  template < typename M, intmax_t S, typename I >
-    M metric_round_down(Metric<S,I> const& src)
-  {
-    typedef std::ratio<M::SCALE,S> R;
-    auto c = src.count();
+    Paragraphs src(37459);
+    auto size = metric_round_up<KiloBytes>(src); // size.count() == 585
+    @endcode
+ */
+template <typename M, intmax_t S, typename I>
+M
+metric_round_down(Metric<S, I> const &src)
+{
+  typedef std::ratio<M::SCALE, S> R;
+  auto c = src.count();
 
-    if (R::den == 1) {
-      return c / R::num; // S is a multiple of N.
-    } else if (R::num ==1) {
-      return c * R::den; // N is a multiple of S.
-    } else {
-      // General case where neither N nor S are a multiple of the other.
-      // Yes, a bit odd, but this minimizes the risk of integer overflow.
-      // I need to validate that under -O2 the compiler will only do 1 division to get
-      // both the quotient and remainder for (n/N) and (n%N). In cases where N,S are
-      // powers of 2 I have verified recent GNU compilers will optimize to bit operations.
-      return (c / R::num) * R::den + (( c % R::num ) * R::den) / R::num;
-    }
+  if (R::den == 1) {
+    return c / R::num; // S is a multiple of N.
+  } else if (R::num == 1) {
+    return c * R::den; // N is a multiple of S.
+  } else {
+    // General case where neither N nor S are a multiple of the other.
+    // Yes, a bit odd, but this minimizes the risk of integer overflow.
+    // I need to validate that under -O2 the compiler will only do 1 division to get
+    // both the quotient and remainder for (n/N) and (n%N). In cases where N,S are
+    // powers of 2 I have verified recent GNU compilers will optimize to bit operations.
+    return (c / R::num) * R::den + ((c % R::num) * R::den) / R::num;
   }
+}
 
-  /// Convert a unit value @a n to a Metric, rounding down.
-  template < typename M >
-  M metric_round_down(intmax_t n)
-  {
-    return n/M::SCALE; // assuming compiler will optimize out dividing by 1 if needed.
-  }
+/// Convert a unit value @a n to a Metric, rounding down.
+template <typename M>
+M
+metric_round_down(intmax_t n)
+{
+  return n / M::SCALE; // assuming compiler will optimize out dividing by 1 if needed.
+}
 
-  /// Convert a unit value @a n to a Metric, rounding up.
-  template < typename M >
-  M metric_round_up(intmax_t n)
-  {
-    return M::SCALE == 1 ? n : (n/M::SCALE + (0 != (n % M::SCALE)));
-  }
+/// Convert a unit value @a n to a Metric, rounding up.
+template <typename M>
+M
+metric_round_up(intmax_t n)
+{
+  return M::SCALE == 1 ? n : (n / M::SCALE + (0 != (n % M::SCALE)));
+}
 
+// --- Compare operators
 
-  // --- Compare operators
+// Try for a bit of performance boost - if the metrics have the same scale
+// just comparing the counts is sufficient and scaling conversion is avoided.
+template <intmax_t N, typename C1, typename C2>
+bool
+operator<(Metric<N, C1> const &lhs, Metric<N, C2> const &rhs)
+{
+  return lhs.count() < rhs.count();
+}
 
-  // Try for a bit of performance boost - if the metrics have the same scale
-  // just comparing the counts is sufficient and scaling conversion is avoided.
-  template < intmax_t N, typename C1, typename C2 >
-    bool operator < (Metric<N, C1> const& lhs, Metric<N, C2> const& rhs)
-  {
-    return lhs.count() < rhs.count();
-  }
+template <intmax_t N, typename C1, typename C2>
+bool
+operator==(Metric<N, C1> const &lhs, Metric<N, C2> const &rhs)
+{
+  return lhs.count() == rhs.count();
+}
 
-  template < intmax_t N, typename C1, typename C2 >
-    bool operator == (Metric<N, C1> const& lhs, Metric<N, C2> const& rhs)
-  {
-    return lhs.count() == rhs.count();
-  }
+// Could be derived but if we're optimizing let's avoid the extra negation.
+// Or we could check if the compiler can optimize that out anyway.
+template <intmax_t N, typename C1, typename C2>
+bool
+operator<=(Metric<N, C1> const &lhs, Metric<N, C2> const &rhs)
+{
+  return lhs.count() <= rhs.count();
+}
 
-  // Could be derived but if we're optimizing let's avoid the extra negation.
-  // Or we could check if the compiler can optimize that out anyway.
-  template < intmax_t N, typename C1, typename C2 >
-    bool operator <= (Metric<N, C1> const& lhs, Metric<N, C2> const& rhs)
-  {
-    return lhs.count() <= rhs.count();
-  }
+// General base cases.
 
-  // General base cases.
+template <intmax_t N1, typename C1, intmax_t N2, typename C2>
+bool
+operator<(Metric<N1, C1> const &lhs, Metric<N2, C2> const &rhs)
+{
+  typedef std::ratio<N1, N2> R;
+  // Based on tests with the GNU compiler, the fact that the conditionals are compile time
+  // constant causes the never taken paths to be dropped so there are no runtime conditional
+  // checks, even with no optimization at all.
+  if (R::den == 1) {
+    return lhs.count() < rhs.count() * R::num;
+  } else if (R::num == 1) {
+    return lhs.count() * R::den < rhs.count();
+  } else
+    return lhs.units() < rhs.units();
+}
 
-  template < intmax_t N1, typename C1, intmax_t N2, typename C2 >
-    bool operator < (Metric<N1,C1> const& lhs, Metric<N2,C2> const& rhs)
-  {
-    typedef std::ratio<N1, N2> R;
-    // Based on tests with the GNU compiler, the fact that the conditionals are compile time
-    // constant causes the never taken paths to be dropped so there are no runtime conditional
-    // checks, even with no optimization at all.
-    if (R::den == 1) { return lhs.count() < rhs.count() * R::num; }
-    else if (R::num == 1) { return lhs.count() * R::den < rhs.count(); }
-    else return lhs.units() < rhs.units();
-  }
+template <intmax_t N1, typename C1, intmax_t N2, typename C2>
+bool
+operator==(Metric<N1, C1> const &lhs, Metric<N2, C2> const &rhs)
+{
+  typedef std::ratio<N1, N2> R;
+  if (R::den == 1) {
+    return lhs.count() == rhs.count() * R::num;
+  } else if (R::num == 1) {
+    return lhs.count() * R::den == rhs.count();
+  } else
+    return lhs.units() == rhs.units();
+}
 
-  template < intmax_t N1, typename C1, intmax_t N2, typename C2 >
-    bool operator == (Metric<N1,C1> const& lhs, Metric<N2,C2> const& rhs)
-  {
-    typedef std::ratio<N1, N2> R;
-    if (R::den == 1) { return lhs.count() == rhs.count() * R::num; }
-    else if (R::num == 1) { return lhs.count() * R::den == rhs.count(); }
-    else return lhs.units() == rhs.units();
-  }
+template <intmax_t N1, typename C1, intmax_t N2, typename C2>
+bool
+operator<=(Metric<N1, C1> const &lhs, Metric<N2, C2> const &rhs)
+{
+  typedef std::ratio<N1, N2> R;
+  if (R::den == 1) {
+    return lhs.count() <= rhs.count() * R::num;
+  } else if (R::num == 1) {
+    return lhs.count() * R::den <= rhs.count();
+  } else
+    return lhs.units() <= rhs.units();
+}
 
-  template < intmax_t N1, typename C1, intmax_t N2, typename C2 >
-    bool operator <= (Metric<N1,C1> const& lhs, Metric<N2,C2> const& rhs)
-  {
-    typedef std::ratio<N1, N2> R;
-    if (R::den == 1) { return lhs.count() <= rhs.count() * R::num; }
-    else if (R::num == 1) { return lhs.count() * R::den <= rhs.count(); }
-    else return lhs.units() <= rhs.units();
-  }
+// Derived compares. No narrowing optimization needed because if the scales
+// are the same the nested call with be optimized.
 
-  // Derived compares. No narrowing optimization needed because if the scales
-  // are the same the nested call with be optimized.
+template <intmax_t N1, typename C1, intmax_t N2, typename C2>
+bool
+operator>(Metric<N1, C1> const &lhs, Metric<N2, C2> const &rhs)
+{
+  return rhs < lhs;
+}
 
-  template < intmax_t N1, typename C1, intmax_t N2, typename C2 >
-    bool operator > (Metric<N1,C1> const& lhs, Metric<N2,C2> const& rhs)
-  {
-    return rhs < lhs;
-  }
-
-  template < intmax_t N1, typename C1, intmax_t N2, typename C2 >
-    bool operator >= (Metric<N1,C1> const& lhs, Metric<N2,C2> const& rhs)
-  {
-    return rhs <= lhs;
-  }
+template <intmax_t N1, typename C1, intmax_t N2, typename C2>
+bool
+operator>=(Metric<N1, C1> const &lhs, Metric<N2, C2> const &rhs)
+{
+  return rhs <= lhs;
+}
 }
 
 #endif // TS_METRIC_H
--- lib/ts/test_Metric.cc
+++ lib/ts/test_Metric.cc
@@ -25,30 +25,32 @@
 #include <string>
 #include <stdarg.h>
 
-namespace ts {
-  using namespace ApacheTrafficServer;
+namespace ts
+{
+using namespace ApacheTrafficServer;
 }
 
 struct TestBox {
-  typedef TestBox self;  ///< Self reference type.
+  typedef TestBox self; ///< Self reference type.
 
   std::string _name;
 
   static int _count;
   static int _fail;
 
-  TestBox(char const* name) : _name(name) {}
-  TestBox(std::string const& name) : _name(name) {}
+  TestBox(char const *name) : _name(name) {}
+  TestBox(std::string const &name) : _name(name) {}
   bool check(bool result, char const *fmt, ...) __attribute__((format(printf, 3, 4)));
 
-  static void print_summary()
+  static void
+  print_summary()
   {
     printf("Tests: %d of %d passed - %s\n", (_count - _fail), _count, _fail ? "FAIL" : "SUCCESS");
   }
 };
 
 int TestBox::_count = 0;
-int TestBox::_fail = 0;
+int TestBox::_fail  = 0;
 
 bool
 TestBox::check(bool result, char const *fmt, ...)
@@ -57,9 +59,9 @@ TestBox::check(bool result, char const *fmt, ...)
 
   if (!result) {
     static constexpr size_t N = 1 << 16;
-    size_t n = N;
+    size_t n                  = N;
     size_t x;
-    char* s;
+    char *s;
     char buffer[N]; // just stack, go big.
 
     s = buffer;
@@ -108,29 +110,30 @@ Test_2()
   Size_2 sz_d(29 * SCALE_1 / SCALE_2);
 
   auto sz = ts::metric_round_up<Size_1>(sz_a);
-  test.check(sz.count() == 1 , "Rounding up, got %d expected %d", sz.count(), 1);
+  test.check(sz.count() == 1, "Rounding up, got %d expected %d", sz.count(), 1);
   sz = ts::metric_round_down<Size_1>(sz_a);
-  test.check(sz.count() == 0 , "Rounding down: got %d expected %d", sz.count(), 0);
+  test.check(sz.count() == 0, "Rounding down: got %d expected %d", sz.count(), 0);
 
   sz = ts::metric_round_up<Size_1>(sz_b);
-  test.check(sz.count() == 4 , "Rounding up, got %d expected %d", sz.count(), 4);
+  test.check(sz.count() == 4, "Rounding up, got %d expected %d", sz.count(), 4);
   sz = ts::metric_round_down<Size_1>(sz_b);
-  test.check(sz.count() == 3 , "Rounding down, got %d expected %d", sz.count(), 3);
+  test.check(sz.count() == 3, "Rounding down, got %d expected %d", sz.count(), 3);
 
   sz = ts::metric_round_up<Size_1>(sz_c);
-  test.check(sz.count() == 1 , "Rounding up, got %d expected %d", sz.count(), 1);
+  test.check(sz.count() == 1, "Rounding up, got %d expected %d", sz.count(), 1);
   sz = ts::metric_round_down<Size_1>(sz_c);
-  test.check(sz.count() == 1 , "Rounding down, got %d expected %d", sz.count(), 1);
+  test.check(sz.count() == 1, "Rounding down, got %d expected %d", sz.count(), 1);
 
   sz = ts::metric_round_up<Size_1>(sz_d);
-  test.check(sz.count() == 29 , "Rounding up, got %d expected %d", sz.count(), 29);
+  test.check(sz.count() == 29, "Rounding up, got %d expected %d", sz.count(), 29);
   sz = ts::metric_round_down<Size_1>(sz_d);
-  test.check(sz.count() == 29 , "Rounding down, got %d expected %d", sz.count(), 29);
+  test.check(sz.count() == 29, "Rounding down, got %d expected %d", sz.count(), 29);
 
-  sz = 119;
+  sz   = 119;
   sz_b = sz; // Should be OK because SCALE_1 is an integer multiple of SCALE_2
   //  sz = sz_b; // Should not compile.
-  test.check(sz_b.count() == 119 * (SCALE_1/SCALE_2) , "Integral conversion, got %d expected %d", sz_b.count(), 119 * (SCALE_1/SCALE_2));
+  test.check(sz_b.count() == 119 * (SCALE_1 / SCALE_2), "Integral conversion, got %d expected %d", sz_b.count(),
+             119 * (SCALE_1 / SCALE_2));
 }
 
 // Test common factor.
@@ -148,14 +151,14 @@ Test_3()
   Size_2 sz_b(97);
 
   auto sz = ts::metric_round_up<Size_1>(sz_a);
-  test.check(sz.count() ==2 , "Rounding up, got %d expected %d", sz.count(), 2);
+  test.check(sz.count() == 2, "Rounding up, got %d expected %d", sz.count(), 2);
   sz = ts::metric_round_down<Size_1>(sz_a);
-  test.check(sz.count() == 1 , "Rounding down: got %d expected %d", sz.count(), 0);
+  test.check(sz.count() == 1, "Rounding down: got %d expected %d", sz.count(), 0);
 
   sz = ts::metric_round_up<Size_1>(sz_b);
-  test.check(sz.count() == 65 , "Rounding up, got %d expected %d", sz.count(), 65);
+  test.check(sz.count() == 65, "Rounding up, got %d expected %d", sz.count(), 65);
   sz = ts::metric_round_down<Size_1>(sz_b);
-  test.check(sz.count() == 64 , "Rounding down, got %d expected %d", sz.count(), 64);
+  test.check(sz.count() == 64, "Rounding down, got %d expected %d", sz.count(), 64);
 }
 
 void
@@ -171,18 +174,18 @@ Test_4()
   //  m_9 = m_4; // Should fail to compile with static assert.
 
   m_4 = ts::metric_round_up<decltype(m_4)>(m_9);
-  test.check(m_4.count() == 214 , "Rounding down, got %d expected %d", m_4.count(), 214);
+  test.check(m_4.count() == 214, "Rounding down, got %d expected %d", m_4.count(), 214);
   m_4 = ts::metric_round_down<decltype(m_4)>(m_9);
-  test.check(m_4.count() == 213 , "Rounding down, got %d expected %d", m_4.count(), 213);
+  test.check(m_4.count() == 213, "Rounding down, got %d expected %d", m_4.count(), 213);
 
   m_4 = 213;
   m_9 = ts::metric_round_up<decltype(m_9)>(m_4);
-  test.check(m_9.count() == 95 , "Rounding down, got %d expected %d", m_9.count(), 95);
+  test.check(m_9.count() == 95, "Rounding down, got %d expected %d", m_9.count(), 95);
   m_9 = ts::metric_round_down<decltype(m_9)>(m_4);
   test.check(m_9.count() == 94, "Rounding down, got %d expected %d", m_9.count(), 94);
 
   m_test = m_4; // Verify assignment of identical scale values compiles.
-  test.check(m_test.count() == 213 , "Assignment got %d expected %d", m_4.count(), 213);
+  test.check(m_test.count() == 213, "Assignment got %d expected %d", m_4.count(), 213);
 }
 
 int
