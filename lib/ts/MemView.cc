0494d883b6965cccae0beadad6f4ebe9d347f69f
Leif Hedstrom <zwoop@apache.org>
2017-02-15 16:58:10 -0700

    Add missing AL2 license to MemView.cc

--- lib/ts/MemView.cc
+++ lib/ts/MemView.cc
@@ -1,3 +1,28 @@
+/** @file
+
+    Class for handling "views" of a buffer. Views presume the memory for the buffer is managed
+    elsewhere and allow efficient access to segments of the buffer without copies. Views are read
+    only as the view doesn't own the memory. Along with generic buffer methods are specialized
+    methods to support better string parsing, particularly token based parsing.
+
+    @section license License
+
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
 #include <ts/MemView.h>
 #include <sstream>
 #include <ctype.h>
