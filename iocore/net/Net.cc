4a8bd3f1d8b32a49421d99ee9ea9fae97d236646
Alan M. Carroll <solidwallofcode@yahoo-inc.com>
2017-02-16 15:43:59 +0000

    PR-1465: Add TCP accept metric which tracks the total number of TCP connections accepted.

--- iocore/net/Net.cc
+++ iocore/net/Net.cc
@@ -108,6 +108,10 @@ register_net_stats()
   NET_CLEAR_DYN_STAT(keep_alive_queue_timeout_total_stat);
   NET_CLEAR_DYN_STAT(keep_alive_queue_timeout_count_stat);
   NET_CLEAR_DYN_STAT(default_inactivity_timeout_stat);
+
+  RecRegisterRawStat(net_rsb, RECT_PROCESS, "proxy.process.tcp.total_accepts", RECD_INT, RECP_NON_PERSISTENT,
+                     static_cast<int>(net_tcp_accept_stat), RecRawStatSyncSum);
+  NET_CLEAR_DYN_STAT(net_tcp_accept_stat);
 }
 
 void
