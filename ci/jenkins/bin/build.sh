b1c763c12493d866694c747fcd59c7b6f5f3daae
Leif Hedstrom <zwoop@apache.org>
2017-03-07 13:48:25 -0800

    Argh, only turn ccache on for clang on FreeBSD for now

--- ci/jenkins/bin/build.sh
+++ ci/jenkins/bin/build.sh
@@ -20,9 +20,10 @@
 enable_debug=""
 test "${JOB_NAME#*type=debug}" != "${JOB_NAME}" && enable_debug="--enable-debug"
 
-# When to turn on ccache for everything (even clang now)
+# When to turn on ccache, disabled for all clang / llvm builds
 enable_ccache="--enable-ccache"
-#test "${JOB_NAME#*compiler=clang}" != "${JOB_NAME}" && enable_ccache=""
+test "${JOB_NAME#*compiler=clang}" != "${JOB_NAME}" && enable_ccache=""
+test "${JOB_NAME#freebsd_}" != "${JOB_NAME}" && enable_ccache="--enable-ccache"
 
 # When to enable -Werror, turned off for RHEL5 node (due to LuaJIT / gcc issues on RHEL5)
 enable_werror="--enable-werror"
