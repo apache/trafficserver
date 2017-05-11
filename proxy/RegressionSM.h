377f4134e69dd6e5a139f7ccefe4db88a737f0be
Bryan Call <bcall@apache.org>
2017-05-11 17:39:50 -0400

    coverity 1021707: Uninitialized scalar field

--- proxy/RegressionSM.h
+++ proxy/RegressionSM.h
@@ -35,7 +35,7 @@
 */
 
 struct RegressionSM : public Continuation {
-  RegressionTest *t; // for use with rprint
+  RegressionTest *t = nullptr; // for use with rprint
 
   // methods to override
   virtual void run(); // replace with leaf regression
@@ -51,16 +51,17 @@ struct RegressionSM : public Continuation {
   void run_in(int *pstatus, ink_hrtime t);
 
   // internal
-  int status;
-  int *pstatus;
-  RegressionSM *parent;
-  int nwaiting;
-  int nchildren;
-  DynArray<RegressionSM *> children;
-  intptr_t n, ichild;
-  bool parallel;
-  bool repeat;
-  Action *pending_action;
+  int status                        = REGRESSION_TEST_INPROGRESS;
+  int *pstatus                      = nullptr;
+  RegressionSM *parent              = nullptr;
+  int nwaiting                      = 0;
+  int nchildren                     = 0;
+  DynArray<RegressionSM *> children = nullptr;
+  intptr_t n                        = 0;
+  intptr_t ichild                   = 0;
+  bool parallel                     = false;
+  bool repeat                       = false;
+  Action *pending_action            = nullptr;
 
   int regression_sm_start(int event, void *data);
   int regression_sm_waiting(int event, void *data);
@@ -68,21 +69,7 @@ struct RegressionSM : public Continuation {
   void child_done(int status);
   void xrun(RegressionSM *parent);
 
-  RegressionSM(RegressionTest *at = NULL)
-    : t(at),
-      status(REGRESSION_TEST_INPROGRESS),
-      pstatus(0),
-      parent(0),
-      nwaiting(0),
-      nchildren(0),
-      children(0),
-      ichild(0),
-      parallel(false),
-      repeat(false),
-      pending_action(0)
-  {
-    mutex = new_ProxyMutex();
-  }
+  RegressionSM(RegressionTest *at = NULL) : t(at) { mutex = new_ProxyMutex(); }
 
   RegressionSM(const RegressionSM &);
 };
