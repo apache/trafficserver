2fc651d8f60a17296d913330c57ad4b8adb28012
tao_627 <tao_627@aliyun.com>
2017-05-16 18:14:30 +0800

    Output the responding url when "Warning: Document truncated" occurs for a valid request.

--- iocore/cache/CacheRead.cc
+++ iocore/cache/CacheRead.cc
@@ -585,10 +585,17 @@ CacheVC::openReadReadDone(int event, Event *e)
     }
     // fall through for truncated documents
   }
-Lerror:
+Lerror : {
   char tmpstring[100];
-  Warning("Document %s truncated", earliest_key.toHexStr(tmpstring));
+  if (request.valid()) {
+    int url_length;
+    const char *url_text = request.url_get()->string_get_ref(&url_length);
+    Warning("Document %s truncated, url[%.*s]", earliest_key.toHexStr(tmpstring), url_length, url_text);
+  } else {
+    Warning("Document %s truncated", earliest_key.toHexStr(tmpstring));
+  }
   return calluser(VC_EVENT_ERROR);
+}
 Ldone:
   return calluser(VC_EVENT_EOS);
 Lcallreturn:
