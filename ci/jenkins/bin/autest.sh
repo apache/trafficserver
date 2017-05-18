ade891f4fb8ded51bd7890b72f6521595cd28ec3
Leif Hedstrom <zwoop@apache.org>
2017-05-18 15:13:27 -0600

    Add a couple of CI build scripts, to source from Jenkins

---
+++ ci/jenkins/bin/autest.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+cd "${WORKSPACE}/src"
+[ -d tests ] || exit 0
+
+autoreconf -if
+
+INSTALL="${WORKSPACE}/${BUILD_NUMBER}/install"
+SANDBOX="/tmp/ausb-$$"
+
+mkdir -p $INSTALL
+
+./configure --prefix="$INSTALL" \
+            --with-user=jenkins \
+            --enable-experimental-plugins \
+            --enable-ccache \
+            --enable-debug \
+            --enable-werror
+
+# Build and run regressions
+make -j4 && make install
+/usr/bin/autest -D ./tests/gold_tests --sandbox "$SANDBOX" --ats-bin "${INSTALL}/bin"
+status="$?"
+[ -d "$SANDBOX" ] && rm -rf "$SANDBOX"
+
+[ "0" != "$status" ] && exit -1
+exit 0
