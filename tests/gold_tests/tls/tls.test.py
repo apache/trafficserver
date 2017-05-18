c20eed8ef9cb285b7b31c185a4208d9dfcdd3481
Susan Hinrichs <shinrich@spellhotel.corp.ne1.yahoo.com>
2017-05-18 15:08:21 +0000

    Fix SSL handshake problems.

---
+++ tests/gold_tests/tls/tls.test.py
@@ -0,0 +1,101 @@
+'''
+'''
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import os
+Test.Summary = '''
+Test tls
+'''
+
+def Build(Test, filename, host):
+	tr = Test.AddTestRun("Build", "Build test file: {0}".format(filename))
+	tr.Command = 'gcc -o ssl-post -O2 -g {0} -lssl -lpthread -lcrypto'.format(filename)
+	tr.ReturnCode = 0
+	tr = Test.addTestRun("Run-Test")
+	tr.Command = './ssl-post {0} 40 378'.format(host)
+
+# ExtendTest(Build)
+
+
+# need Curl
+Test.SkipUnless(
+    Condition.HasProgram("curl","Curl need to be installed on system for this test to work")
+    )
+Test.ContinueOnFail=True
+# Define default ATS
+ts=Test.MakeATSProcess("ts",select_ports=False)
+server=Test.MakeOriginServer("server")
+
+
+tr=Test.AddTestRun("Build-Test","build test file: ssl-post.c")
+tr.Command = 'gcc -o ssl-post -O2 -g {0}/ssl-post.c -lssl -lpthread -lcrypto'.format(Test.RunDirectory)
+tr.ReturnCode=0
+tr.Setup.CopyAs('ssl-post.c',Test.RunDirectory)
+
+requestLocation = "test2"
+reHost = "www.example.com"
+
+testName = ""
+
+header_count = 378
+
+header_string = "POST /post HTTP/1.1\r\nHost: www.example.com\r\nContent-Length:1000\r\n"
+
+for i in range (0, 378):
+	header_string = "{1}header{0}:{0}\r\n".format(i,header_string)
+header_string = "{0}\r\n".format(header_string)
+
+post_body = ""
+for i in range (0, 1000):
+	post_body = "{0}0".format(post_body)
+
+# Add info the origin server responses
+server.addResponse("sessionlog.json",
+    {"headers": header_string, "timestamp": "1469733493.993", "body": post_body},
+    {"headers": "HTTP/1.1 200 OK\r\nServer: microserver\r\nConnection: close\r\nCache-Control: max-age=3600\r\nContent-Length: 2\r\n\r\n", "timestamp": "1469733493.993", "body": "ok" })
+
+#add ssl materials like key, certificates for the server
+ts.addSSLfile("ssl/server.pem")
+ts.addSSLfile("ssl/server.key")
+
+ts.Variables.ssl_port = 4443
+ts.Disk.remap_config.AddLine(
+    'map / http://127.0.0.1:{0}'.format(server.Variables.Port)
+)
+
+ts.Disk.ssl_multicert_config.AddLine(
+    'dest_ip=* ssl_cert_name=server.pem ssl_key_name=server.key'
+)
+ts.Disk.records_config.update({
+        'proxy.config.ssl.server.cert.path': '{0}'.format(ts.Variables.SSLDir), 
+        'proxy.config.ssl.server.private_key.path': '{0}'.format(ts.Variables.SSLDir),
+        'proxy.config.http.server_ports': '{0} {1}:proto=http2;http:ssl'.format(ts.Variables.port,ts.Variables.ssl_port),  # enable ssl port
+        'proxy.config.ssl.client.verify.server':  0,
+        'proxy.config.ssl.server.cipher_suite' : 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA384:AES128-GCM-SHA256:AES256-GCM-SHA384:ECDHE-RSA-RC4-SHA:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES256-SHA:RC4-SHA:RC4-MD5:AES128-SHA:AES256-SHA:DES-CBC3-SHA!SRP:!DSS:!PSK:!aNULL:!eNULL:!SSLv2',
+    })
+
+tr=Test.AddTestRun("Run-Test")
+tr.Command = './ssl-post 127.0.0.1 40 378 4443'
+tr.ReturnCode=0
+# time delay as proxy.config.http.wait_for_cache could be broken
+tr.Processes.Default.StartBefore(server)
+tr.Processes.Default.StartBefore(Test.Processes.ts, ready=When.PortOpen(ts.Variables.ssl_port))
+tr.Processes.Default.Streams.stdout="gold/ssl-post.gold"
+tr.StillRunningAfter=server
+tr.Processes.Default.TimeOut = 5
+tr.TimeOut = 5
+
