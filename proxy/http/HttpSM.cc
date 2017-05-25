a62437a092bb4de6b94f504fad28d9c562774f30
Persia Aziz <persia@yahoo-inc.com>
2017-05-25 14:17:11 -0500

    Permissive mode for server verification

--- proxy/http/HttpSM.cc
+++ proxy/http/HttpSM.cc
@@ -5279,11 +5279,6 @@ HttpSM::handle_http_server_open()
   //          server session's first transaction.
   if (nullptr != server_session) {
     NetVConnection *vc = server_session->get_netvc();
-
-    //    SSLNetVConnection *ssl_vc = dynamic_cast<SSLNetVConnection *>(vc);
-    //    if (ssl_vc) {
-    //      ssl_vc->setClientVerifyEnable(t_state.txn_conf->ssl_client_verify_server);
-    //    }
     if (vc != NULL && (vc->options.sockopt_flags != t_state.txn_conf->sock_option_flag_out ||
                        vc->options.packet_mark != t_state.txn_conf->sock_packet_mark_out ||
                        vc->options.packet_tos != t_state.txn_conf->sock_packet_tos_out ||
