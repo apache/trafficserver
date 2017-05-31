3a8a58c467e3b1993a64165e6fd4312b797be47d
Alan M. Carroll <solidwallofcode@yahoo-inc.com>
2017-05-31 10:16:46 -0500

    Clang-Tidy cleanup: Bad brace placement in ifdef'd code.

--- lib/ts/Regex.cc
+++ lib/ts/Regex.cc
@@ -127,10 +127,10 @@ Regex::~Regex()
   if (regex_extra) {
 #ifdef PCRE_CONFIG_JIT
     pcre_free_study(regex_extra);
-  }
 #else
     pcre_free(regex_extra);
 #endif
+  }
   if (regex) {
     pcre_free(regex);
   }
