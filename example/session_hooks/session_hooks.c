90d8eeeef22ceaa3e319aba3a85741451a751ea4
Alan M. Carroll <amc@apache.org>
2017-08-05 13:47:08 -0500

    TS-4976: Regularize plugins - session_hooks (nee session-1).

--- example/session_hooks/session_hooks.c
+++ example/session_hooks/session_hooks.c
@@ -1,6 +1,6 @@
 /** @file
 
-  A brief file description
+  An example plugin that demonstrates session hook usage.
 
   @section license License
 
@@ -21,40 +21,33 @@
   limitations under the License.
  */
 
-/* session-1.c: a plugin that illustrates how to use
- *                session hooks
- *
- *
- *  Usage: session-1.so
- *
- */
-
 #include <stdio.h>
 #include "ts/ts.h"
 #include "ts/ink_defs.h"
 
-static INKStat transaction_count;
-static INKStat session_count;
-static INKStat av_transaction;
+#define PLUGIN_NAME "session_hooks"
+
+static int transaction_count_stat;
+static int session_count_stat;
 
 static void
 txn_handler(TSHttpTxn txnp, TSCont contp)
 {
-  int64_t num_txns = 0;
+  TSMgmtInt num_txns = 0;
 
-  INKStatIncrement(transaction_count);
-  num_txns = INKStatIntGet(transaction_count);
-  TSDebug("tag_session", "The number of transactions is %" PRId64, num_txns);
+  TSStatIntIncrement(transaction_count_stat, 1);
+  num_txns = TSStatIntGet(transaction_count_stat);
+  TSDebug(PLUGIN_NAME, "The number of transactions is %" PRId64, num_txns);
 }
 
 static void
 handle_session(TSHttpSsn ssnp, TSCont contp)
 {
-  int64_t num_ssn = 0;
+  TSMgmtInt num_ssn = 0;
 
-  INKStatIncrement(session_count);
-  num_ssn = INKStatIntGet(session_count);
-  TSDebug("tag_session", "The number of sessions is %" PRId64, num_ssn);
+  TSStatIntIncrement(session_count_stat, 1);
+  num_ssn = TSStatIntGet(session_count_stat);
+  TSDebug(PLUGIN_NAME, "The number of sessions is %" PRId64, num_ssn);
   TSHttpSsnHookAdd(ssnp, TS_HTTP_TXN_START_HOOK, contp);
 }
 
@@ -79,7 +72,7 @@ ssn_handler(TSCont contp, TSEvent event, void *edata)
     return 0;
 
   default:
-    TSDebug("tag_session", "In the default case: event = %d", event);
+    TSDebug(PLUGIN_NAME, "In the default case: event = %d", event);
     break;
   }
   return 0;
@@ -91,23 +84,22 @@ TSPluginInit(int argc, const char *argv[])
   TSCont contp;
   TSPluginRegistrationInfo info;
 
-  info.plugin_name   = "session-1";
-  info.vendor_name   = "MyCompany";
-  info.support_email = "ts-api-support@MyCompany.com";
+  info.plugin_name   = PLUGIN_NAME;
+  info.vendor_name   = "Apache Software Foundation";
+  info.support_email = "dev@trafficserver.apache.org";
 
   if (TSPluginRegister(&info) != TS_SUCCESS) {
-    TSError("[session-1] Plugin registration failed.\n");
+    TSError("[%s] Plugin registration failed.\n", PLUGIN_NAME);
 
     goto error;
   }
 
-  transaction_count = INKStatCreate("transaction.count", INKSTAT_TYPE_INT64);
-  session_count     = INKStatCreate("session.count", INKSTAT_TYPE_INT64);
-  av_transaction    = INKStatCreate("avg.transactions", INKSTAT_TYPE_FLOAT);
+  transaction_count_stat = TSStatCreate("transaction.count", TS_RECORDDATATYPE_INT, TS_STAT_NON_PERSISTENT, TS_STAT_SYNC_SUM);
+  session_count_stat     = TSStatCreate("session.count", TS_RECORDDATATYPE_INT, TS_STAT_NON_PERSISTENT, TS_STAT_SYNC_SUM);
 
   contp = TSContCreate(ssn_handler, NULL);
   TSHttpHookAdd(TS_HTTP_SSN_START_HOOK, contp);
 
 error:
-  TSError("[session-1] Plugin not initialized");
+  TSError("[%s] Plugin not initialized", PLUGIN_NAME);
 }
