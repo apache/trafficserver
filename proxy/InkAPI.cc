fb7dcf77ade8c55bd7cc8742bf38e002ab5d18d9
David Calavera <david.calavera@gmail.com>
2017-08-18 17:38:15 -0700

    Preserve Accept-Encoding header for H2 Server Push promise.
    This change maintains API compatibility.
    
    Preserve the Accept-Encoding header from the original client request to
    send H2 Server Push promises. With this change, responses initiated by a
    Server Push promise can be compressed based on the accepted encoding.
    
    Otherwise, the response body can never be compressed.
    
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- proxy/InkAPI.cc
+++ proxy/InkAPI.cc
@@ -7644,12 +7644,19 @@ TSHttpTxnServerPush(TSHttpTxn txnp, const char *url, int url_len)
     url_obj.destroy();
     return;
   }
+
   HttpSM *sm          = reinterpret_cast<HttpSM *>(txnp);
   Http2Stream *stream = dynamic_cast<Http2Stream *>(sm->ua_session);
   if (stream) {
     Http2ClientSession *parent = static_cast<Http2ClientSession *>(stream->get_parent());
     if (!parent->is_url_pushed(url, url_len)) {
-      stream->push_promise(url_obj);
+      HTTPHdr *hptr = &(sm->t_state.hdr_info.client_request);
+      TSMLoc obj    = reinterpret_cast<TSMLoc>(hptr->m_http);
+
+      MIMEHdrImpl *mh = _hdr_mloc_to_mime_hdr_impl(obj);
+      MIMEField *f    = mime_hdr_field_find(mh, MIME_FIELD_ACCEPT_ENCODING, MIME_LEN_ACCEPT_ENCODING);
+      stream->push_promise(url_obj, f);
+
       parent->add_url_to_pushed_table(url, url_len);
     }
   }
