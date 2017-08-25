2ab58998f14337644c6bcf8cec54fec6af986604
Alan M. Carroll <amc@apache.org>
2017-08-25 07:14:22 -0500

    TS-4976: Regularize plugins - ssl_sni

--- example/ssl_sni/ssl_sni.cc
+++ example/ssl_sni/ssl_sni.cc
@@ -1,6 +1,7 @@
-/**
-  @file
-  SSL Preaccept test plugin
+/** @file
+
+  SSL Preaccept test plugin.
+
   Implements blind tunneling based on the client IP address
   The client ip addresses are specified in the plugin's
   config file as an array of IP addresses or IP address ranges under the
@@ -29,7 +30,6 @@
 #include <memory.h>
 #include <cinttypes>
 #include <ts/ts.h>
-#include "ts/ink_config.h"
 #include <tsconfig/TsValue.h>
 #include <openssl/ssl.h>
 #include <getopt.h>
@@ -37,10 +37,8 @@
 using ts::config::Configuration;
 using ts::config::Value;
 
-#define PN "ssl-sni-test"
-#define PCP "[" PN " Plugin] "
-
-#if TS_USE_TLS_SNI
+#define PLUGIN_NAME "ssl_sni"
+#define PCP "[" PLUGIN_NAME "] "
 
 namespace
 {
@@ -93,7 +91,7 @@ CB_servername(TSCont /* contp */, TSEvent /* event */, void *edata)
     if (servername_len >= facebook_name_len) {
       const char *server_ptr = servername + (servername_len - facebook_name_len);
       if (strcmp(server_ptr, "facebook.com") == 0) {
-        TSDebug("skh", "Blind tunnel from SNI callback");
+        TSDebug(PLUGIN_NAME, "Blind tunnel from SNI callback");
         TSVConnTunnel(ssl_vc);
         // Don't reenable to ensure that we break out of the
         // SSL handshake processing
@@ -102,14 +100,14 @@ CB_servername(TSCont /* contp */, TSEvent /* event */, void *edata)
     }
     // If the name is yahoo, look for a context for safelyfiled and use that here
     if (strcmp("www.yahoo.com", servername) == 0) {
-      TSDebug("skh", "SNI name is yahoo ssl obj is %p", sslobj);
+      TSDebug(PLUGIN_NAME, "SNI name is yahoo ssl obj is %p", sslobj);
       if (sslobj) {
         TSSslContext ctxobj = TSSslContextFindByName("safelyfiled.com");
         if (ctxobj != nullptr) {
-          TSDebug("skh", "Found cert for safelyfiled");
+          TSDebug(PLUGIN_NAME, "Found cert for safelyfiled");
           SSL_CTX *ctx = reinterpret_cast<SSL_CTX *>(ctxobj);
           SSL_set_SSL_CTX(ssl, ctx);
-          TSDebug("skh", "SNI plugin cb: replace SSL CTX");
+          TSDebug(PLUGIN_NAME, "SNI plugin cb: replace SSL CTX");
         }
       }
     }
@@ -133,9 +131,9 @@ TSPluginInit(int argc, const char *argv[])
     {const_cast<char *>("config"), required_argument, nullptr, 'c'}, {nullptr, no_argument, nullptr, '\0'},
   };
 
-  info.plugin_name   = const_cast<char *>("SSL SNI callback test");
-  info.vendor_name   = const_cast<char *>("Network Geographics");
-  info.support_email = const_cast<char *>("shinrich@network-geographics.com");
+  info.plugin_name   = PLUGIN_NAME;
+  info.vendor_name   = "Apache Software Foundation";
+  info.support_email = "dev@trafficserver.apache.org";
 
   int opt = 0;
   while (opt >= 0) {
@@ -150,7 +148,7 @@ TSPluginInit(int argc, const char *argv[])
   if (ConfigPath.length() == 0) {
     static const char *const DEFAULT_CONFIG_PATH = "ssl_sni.config";
     ConfigPath                                   = std::string(TSConfigDirGet()) + '/' + std::string(DEFAULT_CONFIG_PATH);
-    TSDebug(PN, "No config path set in arguments, using default: %s", DEFAULT_CONFIG_PATH);
+    TSDebug(PLUGIN_NAME, "No config path set in arguments, using default: %s", DEFAULT_CONFIG_PATH);
   }
 
   if (TS_SUCCESS != TSPluginRegister(&info)) {
@@ -169,17 +167,7 @@ TSPluginInit(int argc, const char *argv[])
   if (!success) {
     TSError(PCP "not initialized");
   }
-  TSDebug(PN, "Plugin %s", success ? "online" : "offline");
+  TSDebug(PLUGIN_NAME, "Plugin %s", success ? "online" : "offline");
 
   return;
 }
-
-#else // ! TS_USE_TLS_SNI
-
-void
-TSPluginInit(int, const char *[])
-{
-  TSError(PCP "requires TLS SNI which is not available");
-}
-
-#endif // TS_USE_TLS_SNI
