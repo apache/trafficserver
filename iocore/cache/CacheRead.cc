6dfd927887e2db9c8862e55455c573f73190637a
Fei Deng <feid@oath.com>
2017-09-05 16:08:57 -0500

    remove unnecessary assertion for "alternate"
    (cherry picked from commit a5fd988170fdacb7df3fad1b2d7cd908a98c28dd)
    
     Conflicts:
    	iocore/cache/CacheRead.cc
    
    Trivially resolved, because of clang-tidy being run on master.

--- iocore/cache/CacheRead.cc
+++ iocore/cache/CacheRead.cc
@@ -245,7 +245,7 @@ CacheVC::openReadChooseWriter(int /* event ATS_UNUSED */, Event * /* e ATS_UNUSE
           continue;
         }
       }
-      ink_assert(w->alternate.valid());
+
       if (w->alternate.valid())
         vector.insert(&w->alternate, alt_ndx);
     }
