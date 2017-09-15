f907ceff6b72358788133bee04b3ffb2473158a3
Derek Dagit <derekd@oath.com>
2017-09-15 18:06:09 +0000

    consolidates functionality into one tcp_client

--- tests/tools/tcp_client.py
+++ tests/tools/tcp_client.py
@@ -20,13 +20,14 @@ A simple command line interface to send/receive bytes over TCP.
 import argparse
 import socket
 import sys
+import time
 
-
-def tcp_client(host, port, data):
-    pass
+def tcp_client(host, port, data, closeDelaySeconds=0):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
     s.sendall(data.encode())
+    if closeDelaySeconds > 0:
+        time.sleep(closeDelaySeconds)
     s.shutdown(socket.SHUT_WR)
     while True:
         output = s.recv(4096)  # suggested bufsize from docs.python.org
@@ -38,25 +39,31 @@ def tcp_client(host, port, data):
 
 
 DESCRIPTION =\
-    """A simple command line interface to send/receive bytes over TCP.
+    """
+A simple command line interface to send/receive bytes over TCP.
 
 The full contents of the given file are sent via a TCP connection to the given
 host and port. Then data is read from the connection and printed to standard
-output. Streaming is not supported."""
+output. Streaming is not supported.
+
+If a delay is specified, the client will pause before closing the connections
+to further writes. This is useful to simulate the sending of partial data.
+"""
 
 
 def main(argv):
-    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser = argparse.ArgumentParser(description=DESCRIPTION,\
+            formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('host', help='the target host')
     parser.add_argument('port', type=int, help='the target port')
     parser.add_argument('file', help='the file with content to be sent')
+    parser.add_argument('--delay-after-send', metavar='SECONDS', type=int, help='after send, delay in seconds before half-close', default=0 )
     args = parser.parse_args()
 
     data = ''
     with open(args.file, 'r') as f:
         data = f.read()
-
-    tcp_client(args.host, args.port, data)
+    tcp_client(args.host, args.port, data, args.delay_after_send)
 
 
 if __name__ == "__main__":
