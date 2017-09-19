8d299d46f8967d2a107bf00ce9515be5ba6cfa1a
Derek Dagit <derekd@oath.com>
2017-09-19 13:10:31 +0000

    working json test

--- tests/gold_tests/headers/http-expect-100.test.py
+++ tests/gold_tests/headers/http-expect-100.test.py
@@ -16,7 +16,9 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import json
 import os
+import re
 import subprocess
 
 Test.Summary = '''
@@ -29,15 +31,23 @@ Test.ContinueOnFail = True
 ts = Test.MakeATSProcess("ts", select_ports=False)
 server = Test.MakeOriginServer("server")
 
-arbitrary_content = 'c4e1a38a-b79a-45d1-a0c6-1fe8b1b05380'
-# We only need one transaction as only the VIA header will be checked.
-request_header = {"headers": "POST / HTTP/1.1\r\nHost: www.example.com\r\n\r\n", "timestamp": "1469733493.993", "body": arbitrary_content}
-response_header = {"headers": "HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n", "timestamp": "1469733493.993", "body": ""}
+test_json_name = Test.Name + '.test.json'
+test_json_desc = 'Test that 100 expected headers are replied to'
+test_json = json.load(open(os.path.join(Test.TestDirectory, test_json_name), 'r'))
+headers = test_json[test_json_desc]\
+        ['transactions'][0]['steps'][-3]['TrafficServer -> Origin']
+match = re.match('(.*?)(\r\n\r\n)(.*)', headers, re.DOTALL)
+request_header = {"headers": match.group(1)+match.group(2), "body": match.group(3), "timestamp": "1469733493.993"}
+headers = test_json[test_json_desc]\
+        ['transactions'][0]['steps'][-2]['TrafficServer <- Origin']
+response_header = {"headers": headers, "timestamp": "1469733493.993"}
 server.addResponse("sessionlog.json", request_header, response_header)
 
 ts.Disk.records_config.update({
     'proxy.config.http.server_ports': 'ipv4:{0}'.format(ts.Variables.port),
-    'proxy.config.http.send_100_continue_response': '1',
+    'proxy.config.http.send_100_continue_response': 1,
+    'proxy.config.diags.debug.enabled': 1,
+    'proxy.config.diags.debug.tags': 'http*',
 })
 
 ts.Disk.remap_config.AddLine(
@@ -54,23 +64,17 @@ tr.Processes.Default.StartBefore(server, ready=CheckPort(server.Variables.Port))
 # Delay on readiness of our ssl ports
 tr.Processes.Default.StartBefore(Test.Processes.ts)
 
-Test.Setup.Copy('json_runner.py')
-Test.Setup.Copy(os.path.join(os.pardir, os.pardir, 'tools', 'tcp_client.py'))
-test_json_name = Test.Name + '.test.json'
+Test.Setup.Copy(os.path.join(os.pardir, os.pardir, 'tools', 'json_runner.py'))
 Test.Setup.Copy(test_json_name)
-import json
 tr.Processes.Default.Command = "python json_runner.py '{}'".format(json.dumps(ts.Variables))
-
-#tr.Processes.Default.Command = 'curl --verbose --ipv4 --http1.1 --proxy localhost:{} http://www.example.com'.format(
-#    ts.Variables.port)
 tr.Processes.Default.ReturnCode = 0
+tr.Processes.Default.TimeOut = 5  # seconds
 
 tr.StillRunningAfter = server
 tr.StillRunningAfter = ts
-test_json = json.load(open(os.path.join(Test.TestDirectory, test_json_name), 'r'))
-expected_output = test_json['Test that 100 expected headers are replied to']\
+expected_output = test_json[test_json_desc]\
         ['transactions'][0]['steps'][-1]['Client <- TrafficServer']
 gold_file_path = os.path.join(Test.TestDirectory, Test.Name + '.gold')
 with open(gold_file_path, 'w') as f:
-    f.write(expected_output)
+    print('{}: PASS'.format(test_json_desc), file=f)
 tr.Processes.Default.Streams.stdout = gold_file_path
---
+++ tests/tools/json_runner.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import asyncio
+import glob
+import json
+import re
+import sys
+
+variables = json.loads(sys.argv[1])
+host = '127.0.0.1'
+port = variables['port']
+
+CLIENT_TO_SERVER_KEY = 'Client -> TrafficServer'
+SERVER_TO_CLIENT_KEY = 'Client <- TrafficServer'
+
+buf = b''
+# This is a sketch of a method that has been added to asyncio in Python 3.5.2.
+# https://github.com/python/asyncio/pull/297/files
+async def readuntil(reader, delim):
+    delim_len = len(delim)
+    if delim_len == 0:
+        raise ValueError('Cannot search for a 0-length delimiter!!')
+    buf = b''
+    while True:
+        data = await reader.readexactly(1)
+        buf += data
+        if len(buf) < delim_len:
+            continue
+        idx = buf.find(delim)
+        if idx != -1:
+            # We found the delimiter.
+            ret = buf[0:idx+delim_len]
+            buf = buf[idx+delim_len:]
+            return ret
+
+async def send_data(writer, data, halfClose=False):
+    writer.write(data.encode())
+    await writer.drain()
+    if halfClose:
+        writer.close()
+
+async def receive_data(reader):
+    if reader.at_eof():
+        reader.close()
+        return
+    return await readuntil(reader, b'\r\n\r\n')
+
+async def main():
+    conn_future = asyncio.open_connection(host, port)
+    reader, writer = await asyncio.wait_for(conn_future, timeout=2)
+    for filename in glob.iglob('**/*.test.json', recursive=True):
+        testObj = json.load(open(filename, 'r'))
+        for testKey in testObj.keys():
+            sys.stdout.write('{}: '.format(testKey))
+            for txn in testObj[testKey]['transactions']:
+                for idx, step in enumerate(txn['steps']):
+                    if CLIENT_TO_SERVER_KEY in step:
+                        data = step[CLIENT_TO_SERVER_KEY]
+                        isLastStep = (idx + 1) == len(txn['steps'])
+                        await send_data(writer, data, halfClose=isLastStep)
+                    elif SERVER_TO_CLIENT_KEY in step:
+                        data = (await receive_data(reader)).decode()
+                        if not re.compile(step[SERVER_TO_CLIENT_KEY]).match(data):
+                            data_hex = ":".join("{:02x}".format(ord(c)) for c in data)
+                            raise Exception("received string:\n\n{}\n'{}'\n\n"+\
+                                    "did not match expected pattern:\n\n'{}'"\
+                                    .format(data, data_hex , repr(step[SERVER_TO_CLIENT_KEY])))
+    print('PASS')
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
