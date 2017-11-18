205b024a9a0c0843d321691a88f2b952ae1e234e
Bryan Call <bcall@apache.org>
2017-11-18 09:31:32 +0800

    Fixed broken build on OSX where st_mtim is not defined

--- iocore/net/SSLConfig.cc
+++ iocore/net/SSLConfig.cc
@@ -555,7 +555,7 @@ SSLTicketParams::LoadTicket()
     // See if the file changed since we last loaded
     struct stat sdata;
     if (stat(ticket_key_filename, &sdata) >= 0) {
-      if (sdata.st_mtim.tv_sec <= last_load_time) {
+      if (sdata.st_mtime <= last_load_time) {
         // No updates since last load
         return false;
       }
