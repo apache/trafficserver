a917f0717b90f8713272a229161d022b89e15adc
Susan Hinrichs <shinrich@apache.org>
2017-11-27 16:46:46 -0600

    Fix crash from HttpSM::setup_100_continue_transfer

--- proxy/http/HttpSM.cc
+++ proxy/http/HttpSM.cc
@@ -6149,6 +6149,10 @@ HttpSM::setup_100_continue_transfer()
 
   HTTP_SM_SET_DEFAULT_HANDLER(&HttpSM::tunnel_handler_100_continue);
 
+  // Clear the decks before we set up new producers.  As things stand, we cannot have two static operators
+  // at once
+  tunnel.reset();
+
   // Setup the tunnel to the client
   HttpTunnelProducer *p = tunnel.add_producer(HTTP_TUNNEL_STATIC_PRODUCER, client_response_hdr_bytes, buf_start,
                                               (HttpProducerHandler) nullptr, HT_STATIC, "internal msg - 100 continue");
