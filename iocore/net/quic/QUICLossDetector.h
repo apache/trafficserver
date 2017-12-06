apache/trafficserver

commit a300d3a828916ce1ec8b6bf5ee818ce4a327bc28

Use ink_hrtime internally for RTT calculation

iocore/net/quic/QUICLossDetector.h
@@ -86,7 +86,7 @@ private:
   uint32_t _time_of_last_sent_packet = 0;
   ink_hrtime _latest_rtt             = 0;
   ink_hrtime _smoothed_rtt           = 0;
-  uint32_t _rttvar                   = 0;
+  ink_hrtime _rttvar                 = 0;
   uint32_t _reordering_threshold;
   double _time_reordering_fraction;
   ink_hrtime _loss_time = 0;
@@ -106,7 +106,7 @@ private:
                        QUICPacketUPtr packet);
   void _on_ack_received(const std::shared_ptr<const QUICAckFrame> &ack_frame);
   void _on_packet_acked(QUICPacketNumber acked_packet_number);
-  void _update_rtt(uint32_t latest_rtt);
+  void _update_rtt(ink_hrtime latest_rtt);
   void _detect_lost_packets(QUICPacketNumber largest_acked);
   void _set_loss_detection_alarm();
   void _on_loss_detection_alarm();

iocore/net/quic/QUICLossDetector.cc
@@ -189,8 +189,9 @@ QUICLossDetector::_on_ack_received(const std::shared_ptr<const QUICAckFrame> &ac
   auto pi = this->_sent_packets.find(ack_frame->largest_acknowledged());
   if (pi != this->_sent_packets.end()) {
     this->_latest_rtt = Thread::get_hrtime() - pi->second->time;
-    if (this->_latest_rtt > ack_frame->ack_delay()) {
-      this->_latest_rtt -= ack_frame->ack_delay();
+    // _latest_rtt is nanosecond but ack_frame->ack_delay is millisecond
+    if (this->_latest_rtt > HRTIME_MSECONDS(ack_frame->ack_delay())) {
+      this->_latest_rtt -= HRTIME_MSECONDS(ack_frame->ack_delay());
     }
     this->_update_rtt(this->_latest_rtt);
   }
@@ -248,22 +249,23 @@ QUICLossDetector::_on_loss_detection_alarm()
     this->_detect_lost_packets(this->_largest_acked_packet);
   } else if (this->_tlp_count < this->_MAX_TLPS) {
     // Tail Loss Probe.
-    // eventProcessor.schedule_imm(this->_handler, ET_CALL, QUIC_EVENT_RETRANSMIT_ONE_PACKET);
+    this->_send_one_packet();
     this->_tlp_count++;
   } else {
     // RTO.
     if (this->_rto_count == 0) {
       this->_largest_sent_before_rto = this->_largest_sent_packet;
     }
-    // eventProcessor.schedule_imm(this->_handler, ET_CALL, QUIC_EVENT_RETRANSMIT_TWO_PACKET);
+    this->_send_two_packets();
     this->_rto_count++;
   }
-  QUICLDDebug("Unacked handshake pkt %u, retransmittable pkt %u", this->_handshake_outstanding, this->_retransmittable_outstanding);
+  QUICLDDebug("Unacked packets %lu (handshake pkt %u, retransmittable pkt %u)", this->_sent_packets.size(),
+              this->_handshake_outstanding.load(), this->_retransmittable_outstanding.load());
   this->_set_loss_detection_alarm();
 }
 
 void
-QUICLossDetector::_update_rtt(uint32_t latest_rtt)
+QUICLossDetector::_update_rtt(ink_hrtime latest_rtt)
 {
   // Based on {{RFC6298}}.
   if (this->_smoothed_rtt == 0) {

commit f0c333cec0aefb1421cfa5e24ba8e1a4f8a703eb

Fix a bug introduced with a300d3a828916ce1ec8b6bf5ee818ce4a327bc28

iocore/net/quic/QUICLossDetector.h
@@ -76,17 +76,17 @@ private:
   ink_hrtime _DEFAULT_INITIAL_RTT  = HRTIME_MSECONDS(100);
 
   // 3.2.2.  Variables of interest
-  Action *_loss_detection_alarm      = nullptr;
-  uint32_t _handshake_count          = 0;
-  uint32_t _tlp_count                = 0;
-  uint32_t _rto_count                = 0;
-  uint32_t _largest_sent_before_rto  = 0;
-  uint32_t _largest_sent_packet      = 0;
-  uint32_t _largest_acked_packet     = 0;
-  uint32_t _time_of_last_sent_packet = 0;
-  ink_hrtime _latest_rtt             = 0;
-  ink_hrtime _smoothed_rtt           = 0;
-  ink_hrtime _rttvar                 = 0;
+  Action *_loss_detection_alarm        = nullptr;
+  uint32_t _handshake_count            = 0;
+  uint32_t _tlp_count                  = 0;
+  uint32_t _rto_count                  = 0;
+  uint32_t _largest_sent_before_rto    = 0;
+  uint32_t _largest_sent_packet        = 0;
+  uint32_t _largest_acked_packet       = 0;
+  ink_hrtime _time_of_last_sent_packet = 0;
+  ink_hrtime _latest_rtt               = 0;
+  ink_hrtime _smoothed_rtt             = 0;
+  ink_hrtime _rttvar                   = 0;
   uint32_t _reordering_threshold;
   double _time_reordering_fraction;
   ink_hrtime _loss_time = 0;