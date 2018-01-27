8be2071db8fec2d9e2f03ce7828e0e99afd14dd0
scw00 <scw00@apache.org>
2018-01-27 12:14:57 +0800

    Replace INK_MD5 since c93790a merged

--- iocore/net/quic/QUICTypes.cc
+++ iocore/net/quic/QUICTypes.cc
@@ -203,16 +203,16 @@ QUICTypeUtil::write_uint_as_nbytes(uint64_t value, uint8_t n, uint8_t *buf, size
 void
 QUICStatelessResetToken::_gen_token(uint64_t data)
 {
-  INK_MD5 _md5;
+  CryptoHash _hash;
   static constexpr char STATELESS_RESET_TOKEN_KEY[] = "stateless_token_reset_key";
-  MD5Context ctx;
+  CryptoContext ctx;
   ctx.update(STATELESS_RESET_TOKEN_KEY, strlen(STATELESS_RESET_TOKEN_KEY));
   ctx.update(reinterpret_cast<void *>(&data), 8);
-  ctx.finalize(_md5);
+  ctx.finalize(_hash);
 
   size_t dummy;
-  QUICTypeUtil::write_uint_as_nbytes(_md5.u64[0], 8, _token, &dummy);
-  QUICTypeUtil::write_uint_as_nbytes(_md5.u64[1], 8, _token + 8, &dummy);
+  QUICTypeUtil::write_uint_as_nbytes(_hash.u64[0], 8, _token, &dummy);
+  QUICTypeUtil::write_uint_as_nbytes(_hash.u64[1], 8, _token + 8, &dummy);
 }
 
 uint16_t
