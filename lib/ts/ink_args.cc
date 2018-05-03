3001e31efce46bf23b69243f9def8d533f0ef2b5
Xavier Chi <chitianhaoxp@gmail.com>
2018-05-03 10:18:49 -0500

    7.1.x runroot parsing fix

--- lib/ts/ink_args.cc
+++ lib/ts/ink_args.cc
@@ -219,9 +219,12 @@ process_args_ex(const AppVersionInfo *appinfo, const ArgumentDescription *argume
     if ((*argv)[1] == '-') {
       // Deal with long options ...
       for (i = 0; i < n_argument_descriptions; i++) {
-        if (!strcmp(argument_descriptions[i].name, "run-root")) {
+        // handle the runroot arg
+        std::string cur_argv = *argv + 2;
+        if (cur_argv.size() >= 8 && cur_argv.substr(0, 8) == "run-root") {
           break;
         }
+        // handle the args
         if (!strcmp(argument_descriptions[i].name, (*argv) + 2)) {
           *argv += strlen(*argv) - 1;
           if (!process_arg(appinfo, argument_descriptions, n_argument_descriptions, i, &argv)) {
