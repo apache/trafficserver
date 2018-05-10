b2d1a0794afabe7f77e9577c656f74d676362896
Susan Hinrichs <shinrich@oath.com>
2018-05-10 13:00:22 +0000

    Remove TSHttpSsnConnectionGet which seems redundant.

--- proxy/InkAPI.cc
+++ proxy/InkAPI.cc
@@ -5446,24 +5446,6 @@ TSHttpTxnTransformRespGet(TSHttpTxn txnp, TSMBuffer *bufp, TSMLoc *obj)
   return TS_ERROR;
 }
 
-void *
-TSHttpSsnSSLConnectionGet(TSHttpSsn ssnp)
-{
-  sdk_assert(sdk_sanity_check_null_ptr((void *)ssnp) == TS_SUCCESS);
-
-  ProxyClientSession *cs = reinterpret_cast<ProxyClientSession *>(ssnp);
-  if (cs == nullptr) {
-    return nullptr;
-  }
-
-  SSLNetVConnection *ssl_vc = dynamic_cast<SSLNetVConnection *>(cs->get_netvc());
-  if (ssl_vc == nullptr) {
-    return nullptr;
-  }
-
-  return (void *)ssl_vc->ssl;
-}
-
 sockaddr const *
 TSHttpSsnClientAddrGet(TSHttpSsn ssnp)
 {
