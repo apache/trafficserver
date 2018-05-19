3faff8091d10ea91da44f5ba9e612e617f050a8e
Leif Hedstrom <zwoop@apache.org>
2018-05-18 19:41:15 -0600

    Replaces the old metrics.config with a simple C++ class
    This retains the ~10 metrics that were calculated before in Lua (and XML
    before that), which are actually useful. I've not examined what other
    derivative metrics be useful to add, but this is a good start. Adding new
    metrics to this "system" is easy, but for now it only supports sum() of
    process metrics into a new derivative metric.
    
    Eventually I imagine this will be part of traffic_server itself, which is
    why I put this in mgmt/ rather than in cmd/traffic_manager, where the old
    Lua code resided.

--- cmd/traffic_manager/traffic_manager.cc
+++ cmd/traffic_manager/traffic_manager.cc
@@ -49,6 +49,7 @@
 #include "RecordsConfig.h"
 
 #include "P_RecLocal.h"
+#include "DerivativeMetrics.h"
 
 #if TS_USE_POSIX_CAP
 #include <sys/capability.h>
@@ -89,8 +90,6 @@ static const char *mgmt_path = nullptr;
 static const char *recs_conf = "records.config";
 
 static int fds_limit;
-
-// ToDo: Any globals for calculated metrics
 
 // TODO: Use positive instead negative selection
 //       This should just be #if defined(solaris)
@@ -714,7 +713,7 @@ main(int argc, const char **argv)
   int sleep_time             = 0;  // sleep_time given in sec
   uint64_t last_start_epoc_s = 0;  // latest start attempt in seconds since epoc
 
-  // ToDo: Initialize whatever is needed for calculated metrics
+  DerivativeMetrics derived; // This is simple class to calculate some useful derived metrics
 
   for (;;) {
     lmgmt->processEventQueue();
@@ -731,7 +730,9 @@ main(int argc, const char **argv)
       mgmt_log("[main] Reading Configuration Files Reread\n");
     }
 
-    // ToDo: Here we should update the calculated metrics
+    // Update the derived metrics. ToDo: this runs once a second, that might be excessive, maybe it should be
+    // done more like every config_update_interval_ms (proxy.config.config_update_interval_ms) ?
+    derived.Update();
 
     if (lmgmt->mgmt_shutdown_outstanding != MGMT_PENDING_NONE) {
       Debug("lm", "pending shutdown %d", lmgmt->mgmt_shutdown_outstanding);
