72471ed52406752217576618b21a5901d336c9cf
Evan Zelkowitz <19699200+ezelkow1@users.noreply.github.com>
2018-05-24 14:33:13 -0600

    Collapsed forwarding support in ATS core
    This adds a new write fail action, CACHE_WL_FAIL_ACTION_COLLAPSED_FORWARDING. If it fails to obtain a write lock then it will loop for open_write_retries*open_read_retry_time, each time doing a DecideCacheLookup.  In the end if it fails to ever either obtain a
    write lock or find the object to read then it does the default write fail action of going to the origin.
    
    In testing using duplicated requests I can see multiple simultaneous requests end up in this read loop and eventually get a hit in cache while the original request gets written.  Occaisonally there is still an extra hit or two to the origin which I believe is
    state machine dependant, so sort of unavoidable, sometimes you can have the original request finish the write while a duplicated request ends up obtaining the write lock directly afterwards and so it hits the origin as well. This may be unavoidable unless cache
    lookups are done everywhere before a write.
    
    On an origin fail it will do the default of sending back an ERR_CONNECT_FAIL

--- proxy/http/HttpTransact.cc
+++ proxy/http/HttpTransact.cc
@@ -2918,6 +2918,22 @@ HttpTransact::handle_cache_write_lock(State *s)
 
       TRANSACT_RETURN(SM_ACTION_SEND_ERROR_CACHE_NOOP, nullptr);
       return;
+    case CACHE_WL_FAIL_ACTION_COLLAPSED_FORWARDING:
+      if (s->state_machine->get_cache_sm().get_open_write_tries() < s->txn_conf->max_cache_open_write_retries) {
+        TxnDebug("http_error", "cache_open_write_fail_action %d, cache miss, retry read with collapsed forwarding try #%d",
+                 s->cache_open_write_fail_action, s->state_machine->get_cache_sm().get_open_write_tries());
+        s->request_sent_time       = UNDEFINED_TIME;
+        s->response_received_time  = UNDEFINED_TIME;
+        s->cache_info.action       = CACHE_DO_LOOKUP;
+        s->cache_info.write_status = CACHE_WRITE_LOCK_MISS;
+        break;
+      } else {
+        TxnDebug("http_error", "cache_open_write_fail_action %d, miss, ran out of write retries, doing default",
+                 s->cache_open_write_fail_action);
+        s->cache_info.write_status = CACHE_WRITE_LOCK_MISS;
+        remove_ims                 = true;
+        break;
+      }
     default:
       s->cache_info.write_status = CACHE_WRITE_LOCK_MISS;
       remove_ims                 = true;
@@ -2967,6 +2983,14 @@ HttpTransact::handle_cache_write_lock(State *s)
     TxnDebug("http_error", "calling hdr_info.server_request.destroy");
     s->hdr_info.server_request.destroy();
     HandleCacheOpenReadHitFreshness(s);
+  } else if ((s->cache_open_write_fail_action == CACHE_WL_FAIL_ACTION_COLLAPSED_FORWARDING) &&
+             (s->state_machine->get_cache_sm().get_open_write_tries() < s->txn_conf->max_cache_open_write_retries)) {
+    TxnDebug("http_error", "setting SM_ACTION_CACHE_LOOKUP for collapsed forwarding");
+    StateMachineAction_t next;
+    next           = SM_ACTION_CACHE_LOOKUP;
+    s->next_action = next;
+    s->hdr_info.server_request.destroy();
+    TRANSACT_RETURN(next, nullptr);
   } else {
     StateMachineAction_t next;
     next = how_to_open_connection(s);
