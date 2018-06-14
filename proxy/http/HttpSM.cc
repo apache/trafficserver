apache/trafficserver

commit 4e62375623b3307b073d9b8375e19171d3af4e54

Remove Congestion Control Feature
This feature has been gathering dust.  I don't beleive anyone is using the
feature, and the feedback I received is that it doesn't really work.

proxy/http/HttpSM.cc
@@ -46,7 +46,6 @@
 #include "IPAllow.h"
 //#include "I_Auth.h"
 //#include "HttpAuthParams.h"
-#include "congest/Congestion.h"
 #include "ts/I_Layout.h"
 
 #define DEFAULT_RESPONSE_BUFFER_SIZE_INDEX 6 // 8K
@@ -1090,10 +1089,6 @@ HttpSM::state_raw_http_server_open(int event, void *data)
 
   case NET_EVENT_OPEN:
 
-    if (t_state.pCongestionEntry != nullptr) {
-      t_state.pCongestionEntry->connection_opened();
-      t_state.congestion_connection_opened = 1;
-    }
     // Record the VC in our table
     server_entry     = vc_table.new_entry();
     server_entry->vc = netvc = (NetVConnection *)data;
@@ -1106,21 +1101,9 @@ HttpSM::state_raw_http_server_open(int event, void *data)
 
   case VC_EVENT_ERROR:
   case NET_EVENT_OPEN_FAILED:
-    if (t_state.pCongestionEntry != nullptr) {
-      t_state.current.state = HttpTransact::CONNECTION_ERROR;
-      call_transact_and_set_next_state(HttpTransact::HandleResponse);
-      return 0;
-    } else {
-      t_state.current.state = HttpTransact::OPEN_RAW_ERROR;
-      // use this value just to get around other values
-      t_state.hdr_info.response_error = HttpTransact::STATUS_CODE_SERVER_ERROR;
-    }
-    break;
-  case CONGESTION_EVENT_CONGESTED_ON_F:
-    t_state.current.state = HttpTransact::CONGEST_CONTROL_CONGESTED_ON_F;
-    break;
-  case CONGESTION_EVENT_CONGESTED_ON_M:
-    t_state.current.state = HttpTransact::CONGEST_CONTROL_CONGESTED_ON_M;
+    t_state.current.state = HttpTransact::OPEN_RAW_ERROR;
+    // use this value just to get around other values
+    t_state.hdr_info.response_error = HttpTransact::STATUS_CODE_SERVER_ERROR;
     break;
 
   default:
@@ -1805,14 +1788,6 @@ HttpSM::state_http_server_open(int event, void *data)
     } else {
       call_transact_and_set_next_state(HttpTransact::HandleResponse);
     }
-    return 0;
-  case CONGESTION_EVENT_CONGESTED_ON_F:
-    t_state.current.state = HttpTransact::CONGEST_CONTROL_CONGESTED_ON_F;
-    call_transact_and_set_next_state(HttpTransact::HandleResponse);
-    return 0;
-  case CONGESTION_EVENT_CONGESTED_ON_M:
-    t_state.current.state = HttpTransact::CONGEST_CONTROL_CONGESTED_ON_M;
-    call_transact_and_set_next_state(HttpTransact::HandleResponse);
     return 0;
 
   default:
@@ -4735,23 +4710,6 @@ HttpSM::do_http_server_open(bool raw)
     }
   }
 
-  // Congestion Check
-  if (t_state.pCongestionEntry != nullptr) {
-    if (t_state.pCongestionEntry->F_congested() &&
-        (!t_state.pCongestionEntry->proxy_retry(milestones[TS_MILESTONE_SERVER_CONNECT]))) {
-      t_state.congestion_congested_or_failed = 1;
-      t_state.pCongestionEntry->stat_inc_F();
-      CONGEST_INCREMENT_DYN_STAT(congested_on_F_stat);
-      handleEvent(CONGESTION_EVENT_CONGESTED_ON_F, nullptr);
-      return;
-    } else if (t_state.pCongestionEntry->M_congested(ink_hrtime_to_sec(milestones[TS_MILESTONE_SERVER_CONNECT]))) {
-      t_state.pCongestionEntry->stat_inc_M();
-      t_state.congestion_congested_or_failed = 1;
-      CONGEST_INCREMENT_DYN_STAT(congested_on_M_stat);
-      handleEvent(CONGESTION_EVENT_CONGESTED_ON_M, nullptr);
-      return;
-    }
-  }
   // If this is not a raw connection, we try to get a session from the
   //  shared session pool.  Raw connections are for SSLs tunnel and
   //  require a new connection
@@ -5028,8 +4986,6 @@ HttpSM::do_http_server_open(bool raw)
       connect_timeout = t_state.txn_conf->post_connect_attempts_timeout;
     } else if (t_state.current.server == &t_state.parent_info) {
       connect_timeout = t_state.txn_conf->parent_connect_timeout;
-    } else if (t_state.pCongestionEntry != nullptr) {
-      connect_timeout = t_state.pCongestionEntry->connect_timeout();
     } else {
       connect_timeout = t_state.txn_conf->connect_attempts_timeout;
     }
@@ -5370,13 +5326,6 @@ HttpSM::handle_http_server_open()
       vc->options.packet_tos             = t_state.txn_conf->sock_packet_tos_out;
       vc->options.clientVerificationFlag = t_state.txn_conf->ssl_client_verify_server;
       vc->apply_options();
-    }
-  }
-
-  if (t_state.pCongestionEntry != nullptr) {
-    if (t_state.congestion_connection_opened == 0) {
-      t_state.congestion_connection_opened = 1;
-      t_state.pCongestionEntry->connection_opened();
     }
   }
 
@@ -5923,9 +5872,6 @@ HttpSM::attach_server_session(HttpServerSession *s)
     connect_timeout = t_state.txn_conf->parent_connect_timeout;
   } else {
     connect_timeout = t_state.txn_conf->connect_attempts_timeout;
-  }
-  if (t_state.pCongestionEntry != nullptr) {
-    connect_timeout = t_state.pCongestionEntry->connect_timeout();
   }
 
   if (t_state.api_txn_connect_timeout_value != -1) {
@@ -6850,12 +6796,6 @@ HttpSM::kill_this()
       plugin_tunnel = nullptr;
     }
 
-    if (t_state.pCongestionEntry != nullptr) {
-      if (t_state.congestion_congested_or_failed != 1) {
-        t_state.pCongestionEntry->go_alive();
-      }
-    }
-
     ink_assert(pending_action == nullptr);
     ink_release_assert(vc_table.is_table_clear() == true);
     ink_release_assert(tunnel.is_tunnel_active() == false);
@@ -7334,13 +7274,6 @@ HttpSM::set_next_state()
   }
 
   case HttpTransact::SM_ACTION_ORIGIN_SERVER_OPEN: {
-    if (congestionControlEnabled && (t_state.congest_saved_next_action == HttpTransact::SM_ACTION_UNDEFINED)) {
-      t_state.congest_saved_next_action = HttpTransact::SM_ACTION_ORIGIN_SERVER_OPEN;
-      HTTP_SM_SET_DEFAULT_HANDLER(&HttpSM::state_congestion_control_lookup);
-      if (!do_congestion_control_lookup()) {
-        break;
-      }
-    }
     HTTP_SM_SET_DEFAULT_HANDLER(&HttpSM::state_http_server_open);
 
     // We need to close the previous attempt
@@ -7530,14 +7463,6 @@ HttpSM::set_next_state()
   }
 
   case HttpTransact::SM_ACTION_ORIGIN_SERVER_RAW_OPEN: {
-    if (congestionControlEnabled && (t_state.congest_saved_next_action == HttpTransact::SM_ACTION_UNDEFINED)) {
-      t_state.congest_saved_next_action = HttpTransact::SM_ACTION_ORIGIN_SERVER_RAW_OPEN;
-      HTTP_SM_SET_DEFAULT_HANDLER(&HttpSM::state_congestion_control_lookup);
-      if (!do_congestion_control_lookup()) {
-        break;
-      }
-    }
-
     HTTP_SM_SET_DEFAULT_HANDLER(&HttpSM::state_raw_http_server_open);
 
     ink_assert(server_entry == nullptr);
@@ -7618,43 +7543,6 @@ HttpSM::set_next_state()
 void
 clear_http_handler_times()
 {
-}
-
-bool
-HttpSM::do_congestion_control_lookup()
-{
-  ink_assert(pending_action == nullptr);
-
-  Action *congestion_control_action_handle = get_congest_entry(this, &t_state.request_data, &t_state.pCongestionEntry);
-  if (congestion_control_action_handle != ACTION_RESULT_DONE) {
-    pending_action = congestion_control_action_handle;
-    return false;
-  }
-
-  return true;
-}
-
-int
-HttpSM::state_congestion_control_lookup(int event, void *data)
-{
-  STATE_ENTER(&HttpSM::state_congestion_control_lookup, event);
-  if (event == CONGESTION_EVENT_CONTROL_LOOKUP_DONE) {
-    pending_action                = nullptr;
-    t_state.next_action           = t_state.congest_saved_next_action;
-    t_state.transact_return_point = nullptr;
-    set_next_state();
-  } else {
-    if (pending_action != nullptr) {
-      pending_action->cancel();
-      pending_action = nullptr;
-    }
-    if (t_state.congest_saved_next_action == HttpTransact::SM_ACTION_ORIGIN_SERVER_OPEN) {
-      return state_http_server_open(event, data);
-    } else if (t_state.congest_saved_next_action == HttpTransact::SM_ACTION_ORIGIN_SERVER_RAW_OPEN) {
-      return state_raw_http_server_open(event, data);
-    }
-  }
-  return 0;
 }
 
 // YTS Team, yamsat Plugin

mgmt/RecordsConfig.cc
@@ -300,40 +300,6 @@ static const RecordElement RecordsConfig[] =
   {RECT_CONFIG, "proxy.config.alarm.script_runtime", RECD_INT, "5", RECU_DYNAMIC, RR_NULL, RECC_INT, "[0-300]", RECA_NULL}
   ,
 
-  //####################################################################
-  //# congestion control
-  //####################################################################
-  {RECT_CONFIG, "proxy.config.http.congestion_control.enabled", RECD_INT, "0", RECU_NULL, RR_NULL, RECC_NULL, nullptr, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.http.congestion_control.localtime", RECD_INT, "0", RECU_NULL, RR_NULL, RECC_NULL, nullptr, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.http.congestion_control.filename", RECD_STRING, "congestion.config", RECU_DYNAMIC, RR_NULL, RECC_NULL, nullptr, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.http.congestion_control.default.max_connection_failures", RECD_INT, "5", RECU_NULL, RR_NULL, RECC_NULL, nullptr, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.http.congestion_control.default.fail_window", RECD_INT, "120", RECU_NULL, RR_NULL, RECC_NULL, nullptr, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.http.congestion_control.default.proxy_retry_interval", RECD_INT, "10", RECU_NULL, RR_NULL, RECC_NULL, nullptr, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.http.congestion_control.default.client_wait_interval", RECD_INT, "300", RECU_NULL, RR_NULL, RECC_NULL, nullptr, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.http.congestion_control.default.wait_interval_alpha", RECD_INT, "30", RECU_NULL, RR_NULL, RECC_NULL, nullptr, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.http.congestion_control.default.live_os_conn_timeout", RECD_INT, "60", RECU_NULL, RR_NULL, RECC_NULL, nullptr, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.http.congestion_control.default.live_os_conn_retries", RECD_INT, "2", RECU_NULL, RR_NULL, RECC_NULL, nullptr, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.http.congestion_control.default.dead_os_conn_timeout", RECD_INT, "15", RECU_NULL, RR_NULL, RECC_NULL, nullptr, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.http.congestion_control.default.dead_os_conn_retries", RECD_INT, "1", RECU_NULL, RR_NULL, RECC_NULL, nullptr, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.http.congestion_control.default.max_connection", RECD_INT, "-1", RECU_NULL, RR_NULL, RECC_NULL, nullptr, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.http.congestion_control.default.error_page", RECD_STRING, "congestion#retryAfter", RECU_NULL, RR_NULL, RECC_NULL, nullptr, RECA_NULL}
-  ,
-  {RECT_CONFIG, "proxy.config.http.congestion_control.default.congestion_scheme", RECD_STRING, "per_ip", RECU_NULL, RR_NULL, RECC_NULL, nullptr, RECA_NULL}
-  ,
-
   //        ###########
   //        # Parsing #
   //        ###########

proxy/ControlMatcher.cc
@@ -45,7 +45,6 @@
 #include "P_Net.h"
 #include "P_Cache.h"
 #include "P_SplitDNS.h"
-#include "congest/Congestion.h"
 
 /****************************************************************
  *   Place all template instantiations at the bottom of the file
@@ -1004,10 +1003,3 @@ template class HostMatcher<CacheControlRecord, CacheControlResult>;
 template class RegexMatcher<CacheControlRecord, CacheControlResult>;
 template class UrlMatcher<CacheControlRecord, CacheControlResult>;
 template class IpMatcher<CacheControlRecord, CacheControlResult>;
-
-template class ControlMatcher<CongestionControlRecord, CongestionControlRule>;
-template class HostMatcher<CongestionControlRecord, CongestionControlRule>;
-template class HostRegexMatcher<CongestionControlRecord, CongestionControlRule>;
-template class RegexMatcher<CongestionControlRecord, CongestionControlRule>;
-template class UrlMatcher<CongestionControlRecord, CongestionControlRule>;
-template class IpMatcher<CongestionControlRecord, CongestionControlRule>;

proxy/Main.cc
@@ -88,7 +88,6 @@ extern "C" int plock(int);
 #include "Plugin.h"
 #include "DiagsConfig.h"
 #include "CoreUtils.h"
-#include "congest/Congestion.h"
 #include "RemapProcessor.h"
 #include "I_Tasks.h"
 #include "InkAPIInternal.h"
@@ -1840,7 +1839,6 @@ main(int /* argc ATS_UNUSED */, const char **argv)
     remapProcessor.start(num_remap_threads, stacksize);
     RecProcessStart();
     initCacheControl();
-    initCongestionControl();
     IpAllow::startup();
     ParentConfig::startup();
 #ifdef SPLIT_DNS

proxy/Makefile.am
@@ -19,7 +19,7 @@
 include $(top_srcdir)/build/tidy.mk
 
 # Note that hdrs is targeted from ../Makefile.am
-SUBDIRS = congest http http2 logging config
+SUBDIRS = http http2 logging config
 noinst_LIBRARIES =
 bin_PROGRAMS = \
   traffic_server \
@@ -181,7 +181,6 @@ traffic_server_LDADD = \
   http/libhttp.a \
   http2/libhttp2.a \
   http/remap/libhttp_remap.a \
-  congest/libCongestionControl.a \
   logging/liblogging.a \
   logging/liblogcollation.a \
   hdrs/libhdrs.a  \

proxy/congest/Congestion.h
@@ -1,470 +1,0 @@
-/** @file
-
-  A brief file description
-
-  @section license License
-
-  Licensed to the Apache Software Foundation (ASF) under one
-  or more contributor license agreements.  See the NOTICE file
-  distributed with this work for additional information
-  regarding copyright ownership.  The ASF licenses this file
-  to you under the Apache License, Version 2.0 (the
-  "License"); you may not use this file except in compliance
-  with the License.  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
- */
-
-/*****************************************************************************
- *
- *  Congestion.h - Implementation of Congestion Control
- *
- *
- ****************************************************************************/
-
-#ifndef CONGESTION_H_
-#define CONGESTION_H_
-
-#include "ts/ink_platform.h"
-#include "ts/Result.h"
-#include "P_EventSystem.h"
-#include "ControlBase.h"
-#include "ControlMatcher.h"
-#include "CongestionStats.h"
-
-#define CONGESTION_EVENT_CONGESTED_ON_M (CONGESTION_EVENT_EVENTS_START + 1)
-#define CONGESTION_EVENT_CONGESTED_ON_F (CONGESTION_EVENT_EVENTS_START + 2)
-#define CONGESTION_EVENT_CONGESTED_LIST_DONE (CONGESTION_EVENT_EVENTS_START + 3)
-#define CONGESTION_EVENT_CONTROL_LOOKUP_DONE (CONGESTION_EVENT_EVENTS_START + 4)
-
-struct RequestData;
-
-extern InkRand CongestionRand;
-
-enum {
-  PER_IP,
-  PER_HOST,
-};
-
-class CongestionControlRecord;
-
-struct CongestionControlRule {
-  CongestionControlRule();
-  ~CongestionControlRule();
-  CongestionControlRecord *record;
-};
-
-class CongestionControlRecord : public ControlBase
-{
-public:
-  CongestionControlRecord();
-  CongestionControlRecord(const CongestionControlRecord &rec);
-  ~CongestionControlRecord();
-  Result Init(matcher_line *line_info);
-  void UpdateMatch(CongestionControlRule *pRule, RequestData *rdata);
-  void Print();
-
-  void cleanup();
-  void setdefault();
-  Result validate();
-
-  int rank; // matching preference
-            /*
-             * Select the first matching rule specified in congestion.config
-             * rank     Matches
-             *   3       dest && prefix && port
-             *   2       dest && port
-             *   1       dest && prefix
-             *   0       dest
-             */
-
-  char *prefix;
-  int prefix_len;
-  unsigned short port;
-  int congestion_scheme;
-  char *error_page;
-
-  int max_connection_failures;
-  int fail_window;
-  int proxy_retry_interval;
-  int client_wait_interval;
-  int wait_interval_alpha;
-  int live_os_conn_timeout;
-  int live_os_conn_retries;
-  int dead_os_conn_timeout;
-  int dead_os_conn_retries;
-  int max_connection;
-
-  CongestionControlRecord *pRecord;
-  int32_t ref_count;
-
-  void
-  get()
-  {
-    ink_atomic_increment(&ref_count, 1);
-  }
-  void
-  put()
-  {
-    if (ink_atomic_increment(&ref_count, -1) == 1)
-      delete this;
-  }
-};
-
-inline CongestionControlRule::CongestionControlRule() : record(NULL)
-{
-}
-
-inline CongestionControlRule::~CongestionControlRule()
-{
-  record = NULL;
-}
-
-inline CongestionControlRecord::CongestionControlRecord()
-  : rank(0),
-    prefix(NULL),
-    prefix_len(0),
-    port(0),
-    congestion_scheme(PER_IP),
-    error_page(NULL),
-    max_connection_failures(5),
-    fail_window(120),
-    proxy_retry_interval(10),
-    client_wait_interval(300),
-    wait_interval_alpha(30),
-    live_os_conn_timeout(60),
-    live_os_conn_retries(2),
-    dead_os_conn_timeout(15),
-    dead_os_conn_retries(1),
-    max_connection(-1),
-    pRecord(NULL),
-    ref_count(0)
-{
-}
-
-inline CongestionControlRecord::~CongestionControlRecord()
-{
-  cleanup();
-}
-inline void
-CongestionControlRecord::cleanup()
-{
-  if (pRecord) {
-    pRecord->put();
-    pRecord = NULL;
-  }
-  ats_free(prefix), prefix         = NULL;
-  ats_free(error_page), error_page = NULL;
-}
-
-typedef unsigned short cong_hist_t;
-#define CONG_HIST_ENTRIES 17
-
-// CongestionEntry
-struct FailHistory {
-  long start;
-  int bin_len;
-  int length;
-  cong_hist_t bins[CONG_HIST_ENTRIES];
-  int cur_index;
-  long last_event;
-  int events;
-
-  FailHistory() : start(0), bin_len(0), length(0), cur_index(0), last_event(0), events(0) { bzero((void *)&bins, sizeof(bins)); }
-  void init(int window);
-  void init_event(long t, int n = 1);
-  int regist_event(long t, int n = 1);
-  int
-  get_bin_events(int index)
-  {
-    return bins[(index + 1 + cur_index) % CONG_HIST_ENTRIES];
-  }
-};
-
-struct CongestionEntry : public RequestData {
-  // key in the hash table;
-  uint64_t m_key;
-  // host info
-  IpEndpoint m_ip;
-  char *m_hostname;
-
-  // Pointer to the congestion.config entry
-  // Remember to update the refcount of pRecord
-  CongestionControlRecord *pRecord;
-
-  // State -- connection failures
-  FailHistory m_history;
-  Ptr<ProxyMutex> m_hist_lock;
-  ink_hrtime m_last_congested;
-  int m_congested; // 0 | 1
-  int m_stat_congested_conn_failures;
-
-  int m_M_congested;
-  ink_hrtime m_last_M_congested;
-
-  // State -- concorrent connections
-  int m_num_connections;
-  int m_stat_congested_max_conn;
-
-  // Reference count
-  int m_ref_count;
-
-  CongestionEntry(const char *hostname, sockaddr const *ip, CongestionControlRecord *rule, uint64_t key);
-  CongestionEntry();
-  virtual ~CongestionEntry();
-
-  /* RequestData virtural functions */
-  virtual char *
-  get_string()
-  {
-    return pRecord->prefix;
-  }
-  virtual const char *
-  get_host()
-  {
-    return m_hostname;
-  }
-  virtual sockaddr const *
-  get_ip()
-  {
-    return &m_ip.sa;
-  }
-  virtual const sockaddr *
-  get_client_ip()
-  {
-    return NULL;
-  }
-
-  /* print the entry into the congested list output buffer */
-  int sprint(char *buf, int buflen, int format = 0);
-
-  /* reference counter manipulation */
-  void get();
-  void put();
-
-  /* congestion control functions */
-  // Is the server congested?
-  bool F_congested();
-  bool M_congested(ink_hrtime t);
-  bool congested();
-
-  // Update state info
-  void go_alive();
-  void failed_at(ink_hrtime t);
-  void connection_opened();
-  void connection_closed();
-
-  // Connection controls
-  bool proxy_retry(ink_hrtime t);
-  int client_retry_after();
-  int connect_retries();
-  int connect_timeout();
-  char *
-  getErrorPage()
-  {
-    return pRecord->error_page;
-  }
-
-  // stats
-  void stat_inc_F();
-  void stat_inc_M();
-
-  // fail history operations
-  void clearFailHistory();
-  bool compCongested();
-
-  // CongestionEntry and CongestionControl rules interaction helper functions
-  bool usefulInfo(ink_hrtime t);
-  bool validate();
-  void applyNewRule(CongestionControlRecord *rule);
-  void init(CongestionControlRecord *rule);
-};
-
-inline bool
-CongestionEntry::usefulInfo(ink_hrtime t)
-{
-  return (m_ref_count > 1 || m_congested != 0 || m_num_connections > 0 ||
-          (m_history.last_event + pRecord->fail_window > t && m_history.events > 0));
-}
-
-inline int
-CongestionEntry::client_retry_after()
-{
-  int prat = 0;
-  if (F_congested()) {
-    prat = pRecord->proxy_retry_interval + m_history.last_event - ink_hrtime_to_sec(Thread::get_hrtime());
-    if (prat < 0)
-      prat = 0;
-  }
-  return (prat + pRecord->client_wait_interval + CongestionRand.random() % pRecord->wait_interval_alpha);
-}
-
-inline bool
-CongestionEntry::proxy_retry(ink_hrtime t)
-{
-  return ((ink_hrtime_to_sec(t) - m_history.last_event) >= pRecord->proxy_retry_interval);
-}
-
-inline bool
-CongestionEntry::F_congested()
-{
-  return m_congested == 1;
-}
-
-inline bool
-CongestionEntry::M_congested(ink_hrtime t)
-{
-  if (pRecord->max_connection >= 0 && m_num_connections >= pRecord->max_connection) {
-    if (ink_atomic_swap(&m_M_congested, 1) == 0) {
-      m_last_M_congested = t;
-      // TODO: Used to signal congestions
-    }
-    return true;
-  }
-  return false;
-}
-
-inline bool
-CongestionEntry::congested()
-{
-  return (F_congested() || m_M_congested == 1);
-}
-
-inline int
-CongestionEntry::connect_retries()
-{
-  if (F_congested()) {
-    return pRecord->dead_os_conn_retries;
-  } else {
-    return pRecord->live_os_conn_retries;
-  }
-}
-
-inline int
-CongestionEntry::connect_timeout()
-{
-  if (F_congested()) {
-    return pRecord->dead_os_conn_timeout;
-  } else {
-    return pRecord->live_os_conn_timeout;
-  }
-}
-
-inline void
-CongestionEntry::stat_inc_F()
-{
-  ink_atomic_increment(&m_stat_congested_conn_failures, 1);
-}
-
-inline void
-CongestionEntry::stat_inc_M()
-{
-  ink_atomic_increment(&m_stat_congested_max_conn, 1);
-}
-
-inline bool
-CongestionEntry::compCongested()
-{
-  if (m_congested)
-    return true;
-  if (pRecord->max_connection_failures == -1)
-    return false;
-  return pRecord->max_connection_failures <= m_history.events;
-}
-
-// return true when max_conn state changed
-inline void
-CongestionEntry::connection_opened()
-{
-  ink_atomic_increment(&m_num_connections, 1);
-}
-
-// return true when max_conn state changed
-inline void
-CongestionEntry::connection_closed()
-{
-  ink_atomic_increment(&m_num_connections, -1);
-  if (ink_atomic_swap(&m_M_congested, 0) == 1) {
-    // TODO: Used to signal not congested
-  }
-}
-
-inline void
-CongestionEntry::clearFailHistory()
-{
-  m_history.init(pRecord->fail_window);
-  m_congested = 0;
-}
-
-inline CongestionEntry::CongestionEntry()
-  : m_key(0),
-    m_hostname(NULL),
-    pRecord(NULL),
-    m_last_congested(0),
-    m_congested(0),
-    m_stat_congested_conn_failures(0),
-    m_M_congested(0),
-    m_last_M_congested(0),
-    m_num_connections(0),
-    m_stat_congested_max_conn(0),
-    m_ref_count(1)
-{
-  memset(&m_ip, 0, sizeof(m_ip));
-  m_hist_lock = new_ProxyMutex();
-}
-
-inline CongestionEntry::~CongestionEntry()
-{
-  if (m_hostname)
-    ats_free(m_hostname), m_hostname = NULL;
-  m_hist_lock = NULL;
-  if (pRecord)
-    pRecord->put(), pRecord = NULL;
-}
-
-inline void
-CongestionEntry::get()
-{
-  ink_atomic_increment(&m_ref_count, 1);
-}
-
-inline void
-CongestionEntry::put()
-{
-  if (ink_atomic_increment(&m_ref_count, -1) == 1) {
-    delete this;
-  }
-}
-
-// API to outside world
-
-extern int congestionControlEnabled;
-extern int congestionControlLocalTime;
-
-void initCongestionControl();
-CongestionControlRecord *CongestionControlled(RequestData *rdata);
-
-uint64_t make_key(char *hostname, int len, sockaddr const *ip, CongestionControlRecord *record);
-uint64_t make_key(char *hostname, sockaddr const *ip, CongestionControlRecord *record);
-uint64_t make_key(char *hostname, int len, sockaddr const *ip, char *prefix, int prelen, short port = 0);
-
-//----------------------------------------------------
-// the following functions are actually declared in
-// CongestionDB.h and defined in CongestionDB.cc
-// They are included here only to make the
-// editing & compiling process faster
-//----------------------------------------------------
-extern Action *get_congest_entry(Continuation *cont, HttpRequestData *data, CongestionEntry **ppEntry);
-extern Action *get_congest_list(Continuation *cont, MIOBuffer *buffer, int format);
-
-extern void remove_congested_entry(uint64_t key);
-extern void remove_all_congested_entry(void);
-extern void remove_congested_entry(char *buf, MIOBuffer *out_buffer);
-
-#endif /* CONGESTTION_H_ */

commit f958c06c382d15a23761f8f64e03c477a0f9379c

Fixed coredump with origin throttling
This was broke by: 4e6237562 Remove Congestion Control Feature

proxy/http/HttpSM.cc
@@ -4637,7 +4637,7 @@ void
 HttpSM::send_origin_throttled_response()
 {
   t_state.current.attempts = t_state.txn_conf->connect_attempts_max_retries;
-  // t_state.current.state = HttpTransact::CONNECTION_ERROR;
+  t_state.current.state    = HttpTransact::CONNECTION_ERROR;
   call_transact_and_set_next_state(HttpTransact::HandleResponse);
 }
 