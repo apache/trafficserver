f0995d3d0a7996b7c72aaf40210f8072018c7c09
Syeda Persia Aziz <persia.aziz@yahoo.com>
2018-06-14 13:39:26 -0500

    set verify locations in the initializer function only
    (cherry picked from commit 7fb12c035957bd0c58bc30a3f99a50f67ac55a9d)
    
     Conflicts:
    	iocore/net/SSLNetVConnection.cc

--- iocore/net/SSLNetVConnection.cc
+++ iocore/net/SSLNetVConnection.cc
@@ -1017,13 +1017,11 @@ SSLNetVConnection::sslStartHandShake(int event, int &err)
         clientCTX = params->client_ctx;
       }
 
-      if (this->options.clientVerificationFlag && params->clientCACertFilename != nullptr && params->clientCACertPath != nullptr) {
-        if (!SSL_CTX_load_verify_locations(clientCTX, params->clientCACertFilename, params->clientCACertPath)) {
-          SSLError("invalid client CA Certificate file (%s) or CA Certificate path (%s)", params->clientCACertFilename,
-                   params->clientCACertPath);
-          return EVENT_ERROR;
-        }
+      if (!clientCTX) {
+        SSLErrorVC(this, "failed to create SSL client session");
+        return EVENT_ERROR;
       }
+
       this->ssl = make_ssl_connection(clientCTX, this);
       if (this->ssl != nullptr) {
         uint8_t clientVerify = this->options.clientVerificationFlag;
