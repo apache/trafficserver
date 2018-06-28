a921baaff32954f53b6c37eaa98718f05a09f153
Derek Dagit <derekd@oath.com>
2018-06-28 14:13:32 -0500

    Revert "coverity: bulkfile ignored sign"
    This reverts commit e6a60c218ac4615a4788e6fcb6c6325bd30d0192.

--- cmd/traffic_cache_tool/File.cc
+++ cmd/traffic_cache_tool/File.cc
@@ -108,10 +108,8 @@ BulkFile::load()
   if (0 == fstat(fd, &info)) {
     size_t n = info.st_size;
     _content.resize(n + 2);
-    auto data     = const_cast<char *>(_content.data());
-    auto read_len = read(fd, data, n);
-    if (0 < read_len) {
-      _len = read_len;
+    auto data = const_cast<char *>(_content.data());
+    if (0 < (_len = read(fd, data, n))) {
       // Force a trailing linefeed and nul.
       memset(data + _len, 0, 2);
       if (data[n - 1] != '\n') {
