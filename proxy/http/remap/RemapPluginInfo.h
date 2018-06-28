f451ec28e311a2672b2898c7f6380465a30b8e01
Derek Dagit <derekd@oath.com>
2018-06-28 14:13:04 -0500

    Revert "This adds a new callback to remap APIs: TSRemapConfigReload"
    This reverts commit 93d42ba8552fa01879a9468a6a216fca7bfd1d07.

--- proxy/http/remap/RemapPluginInfo.h
+++ proxy/http/remap/RemapPluginInfo.h
@@ -28,7 +28,6 @@
 #include "api/ts/remap.h"
 
 #define TSREMAP_FUNCNAME_INIT "TSRemapInit"
-#define TSREMAP_FUNCNAME_CONFIG_RELOAD "TSRemapConfigReload"
 #define TSREMAP_FUNCNAME_DONE "TSRemapDone"
 #define TSREMAP_FUNCNAME_NEW_INSTANCE "TSRemapNewInstance"
 #define TSREMAP_FUNCNAME_DELETE_INSTANCE "TSRemapDeleteInstance"
@@ -44,7 +43,6 @@ class remap_plugin_info
 {
 public:
   typedef TSReturnCode _tsremap_init(TSRemapInterface *api_info, char *errbuf, int errbuf_size);
-  typedef void _tsremap_config_reload();
   typedef void _tsremap_done(void);
   typedef TSReturnCode _tsremap_new_instance(int argc, char *argv[], void **ih, char *errbuf, int errbuf_size);
   typedef void _tsremap_delete_instance(void *);
@@ -56,7 +54,6 @@ public:
   int path_size;
   void *dlh; /* "handle" for the dynamic library */
   _tsremap_init *fp_tsremap_init;
-  _tsremap_config_reload *fp_tsremap_config_reload;
   _tsremap_done *fp_tsremap_done;
   _tsremap_new_instance *fp_tsremap_new_instance;
   _tsremap_delete_instance *fp_tsremap_delete_instance;
@@ -69,7 +66,6 @@ public:
   remap_plugin_info *find_by_path(char *_path);
   void add_to_list(remap_plugin_info *pi);
   void delete_my_list();
-  void indicate_reload();
 };
 
 /**
