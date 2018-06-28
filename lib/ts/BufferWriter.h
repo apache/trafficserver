18898c23e77397f5e3ec6e138dd7290556171fcd
Derek Dagit <derekd@oath.com>
2018-06-28 14:12:50 -0500

    Revert "Fixed clang-analyzer issue with null pointer in BufferWriter"
    This reverts commit bfe594bd6241e043a9f4ce5663b07b4e96f427a4.

--- lib/ts/BufferWriter.h
+++ lib/ts/BufferWriter.h
@@ -256,13 +256,15 @@ public:
   FixedBufferWriter &
   write(const void *data, size_t length) override
   {
-    const size_t newSize = _attempted + length;
+    size_t newSize = _attempted + length;
+#if defined(__clang_analyzer__)
+    ink_assert(_capacity == 0 || _buf != nullptr); // make clang-analyzer happy.
+#endif
 
     if (newSize <= _capacity) {
-      ink_assert(_buf != nullptr); // make clang-analyzer happy.
       std::memcpy(_buf + _attempted, data, length);
+
     } else if (_attempted < _capacity) {
-      ink_assert(_buf != nullptr); // make clang-analyzer happy.
       std::memcpy(_buf + _attempted, data, _capacity - _attempted);
     }
     _attempted = newSize;
