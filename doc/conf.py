d6099b3661b60fe002221d561d3e5f7f3575cf6f
Alan M. Carroll <amc@apache.org>
2018-07-01 19:46:00 -0500

    IntrusiveHashMap: Refresh TSHashTable for C++ eleventy.

--- doc/conf.py
+++ doc/conf.py
@@ -167,8 +167,10 @@ pygments_style = 'sphinx'
 #modindex_common_prefix = []
 
 nitpicky = True
-nitpick_ignore = [ ('cpp:typeOrConcept', 'T')
-                 , ('cpp:typeOrConcept', 'Args')
+nitpick_ignore = [ ('cpp:typeOrConcept', 'T') # template arg
+                 , ('cpp:typeOrConcept', 'F') # template arg
+                 , ('cpp:typeOrConcept', 'Args') # variadic template arg
+                 , ('cpp:typeOrConcept', 'Rest') # variadic template arg
                  ]
 
 # Autolink issue references.
