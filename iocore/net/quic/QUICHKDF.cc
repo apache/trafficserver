5cb3191e4fb30e32c5b4445e63b4f97a032ed252
Masaori Koshiba <masaori@apache.org>
2018-07-20 11:17:49 +0900

    [draft-13] adjust QUICHKDF::expand to use HkdfLabel structure of TLS 1.3

--- iocore/net/quic/QUICHKDF.cc
+++ iocore/net/quic/QUICHKDF.cc
@@ -27,25 +27,34 @@
 
 #include <string_view>
 
-using namespace std::literals;
-
-constexpr static std::string_view QUIC_HKDF_EXPAND_LABEL_PREFIX("quic "sv);
-
+/**
+ * HKDF-Expand-Label function of QUIC
+ * The HKDF-Expand-Label function and HkdfLabel structure is defined in TLS 1.3 Section 7.1.
+ */
 int
 QUICHKDF::expand(uint8_t *dst, size_t *dst_len, const uint8_t *secret, size_t secret_len, const char *label, size_t label_len,
                  uint16_t length)
 {
-  // Create HKDF label
+  // Create HkdfLabel
   uint8_t hkdf_label[512]; // 2 + 255 + 255
   int hkdf_label_len = 0;
-  // Length
+
+  // length field
   hkdf_label[0] = (length >> 8) & 0xFF;
   hkdf_label[1] = length & 0xFF;
   hkdf_label_len += 2;
-  // prefix + Label
-  hkdf_label_len += sprintf(reinterpret_cast<char *>(hkdf_label + hkdf_label_len), "%c%s%.*s", static_cast<int>(5 + label_len),
-                            QUIC_HKDF_EXPAND_LABEL_PREFIX.data(), static_cast<int>(label_len), label);
+
+  // label (prefix + Label) field
+  hkdf_label_len += sprintf(reinterpret_cast<char *>(hkdf_label + hkdf_label_len), "%cquic %.*s", static_cast<int>(5 + label_len),
+                            static_cast<int>(label_len), label);
+
+  // context field
+  // XXX: Assuming Context is zero-length character (indicated by "")
+  // HkdfLabel requires "0" (length) in context field, because `context<0..255>` is valiable-integer.
+  hkdf_label[hkdf_label_len] = 0;
+  ++hkdf_label_len;
 
   HKDF::expand(dst, dst_len, secret, secret_len, hkdf_label, hkdf_label_len, length);
+
   return 1;
 }
