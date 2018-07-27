beb56189c216fa5510f8004de687b19a465765ab
Susan Hinrichs <shinrich@oath.com>
2018-07-27 15:53:31 +0000

    Fix logic to support ASYNC_pause_job

--- iocore/net/P_SSLNetVConnection.h
+++ iocore/net/P_SSLNetVConnection.h
@@ -352,9 +352,6 @@ private:
   bool sslTrace                    = false;
   bool SNIMapping                  = false;
   int64_t redoWriteSize            = 0;
-#ifdef SSL_MODE_ASYNC
-  EventIO signalep;
-#endif
 };
 
 typedef int (SSLNetVConnection::*SSLNetVConnHandler)(int, void *);
