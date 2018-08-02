39b3273a703b15e2d619519f7952e37504738956
Xavier Chi <chitianhaoxp@gmail.com>
2018-08-02 14:16:54 -0500

    Runroot: pass runroot down from manager and error message update

--- tests/gold_tests/runroot/runroot_init.test.py
+++ tests/gold_tests/runroot/runroot_init.test.py
@@ -25,11 +25,8 @@ Test for init of runroot from traffic_layout.
 '''
 Test.ContinueOnFail = True
 
-p = Test.MakeATSProcess("ts")
-ts_root = p.Env['TS_ROOT']
-
 # init from pass in path
-path1 = os.path.join(ts_root, "runroot1")
+path1 = os.path.join(Test.RunDirectory, "runroot1")
 tr = Test.AddTestRun("Test traffic_layout init #1")
 tr.Processes.Default.Command = "$ATS_BIN/traffic_layout init --path " + path1
 tr.Processes.Default.ReturnCode = 0
@@ -37,15 +34,15 @@ f = tr.Disk.File(os.path.join(path1, "runroot_path.yml"))
 f.Exists = True
 
 # init to relative directory
-path2 = os.path.join(ts_root, "runroot2")
+path2 = os.path.join(Test.RunDirectory, "runroot2")
 tr = Test.AddTestRun("Test traffic_layout init #2")
-tr.Processes.Default.Command = "cd " + ts_root + ";$ATS_BIN/traffic_layout init --path runroot2"
+tr.Processes.Default.Command = "cd " + Test.RunDirectory + ";$ATS_BIN/traffic_layout init --path runroot2"
 tr.Processes.Default.ReturnCode = 0
 f = tr.Disk.File(os.path.join(path2, "runroot_path.yml"))
 f.Exists = True
 
 # init to cwd
-path3 = os.path.join(ts_root, "runroot3")
+path3 = os.path.join(Test.RunDirectory, "runroot3")
 tr = Test.AddTestRun("Test traffic_layout init #3")
 tr.Processes.Default.Command = "mkdir " + path3 + ";cd " + path3 + ";$ATS_BIN/traffic_layout init"
 tr.Processes.Default.ReturnCode = 0
@@ -53,7 +50,7 @@ f = tr.Disk.File(os.path.join(path3, "runroot_path.yml"))
 f.Exists = True
 
 # --force init to an non-empty directory
-path4 = os.path.join(ts_root, "runroot4")
+path4 = os.path.join(Test.RunDirectory, "runroot4")
 tr = Test.AddTestRun("Test traffic_layout init #4")
 randomfile = os.path.join(path4, "foo")
 tr.Processes.Default.Command = "mkdir " + path4 + ";touch " + randomfile + ";$ATS_BIN/traffic_layout init --force --path " + path4
@@ -63,7 +60,7 @@ f.Exists = True
 tr.Processes.Default.Streams.All = Testers.ContainsExpression("Forcing creating runroot", "force message")
 
 # create runroot with junk to guarantee only traffic server related files are copied
-path5 = os.path.join(ts_root, "runroot5")
+path5 = os.path.join(Test.RunDirectory, "runroot5")
 junk1 = os.path.join(path1, "bin/junk1")
 junk2 = os.path.join(path1, "lib/junk2")
 junk3 = os.path.join(path1, "var/junk3")
