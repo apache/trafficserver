f6769cb3560dbd3c08c318519760a5ee92f5ea14
Susan Hinrichs <shinrich@oath.com>
2018-08-02 13:44:54 +0000

    Remove the ignore_keep_alive method entirely
    (cherry picked from commit da9edd5feba06da6ebbe47eaf9696c37c5483fa1)
    
    Conflicts:
    	proxy/http/Http1ClientTransaction.h
    	proxy/http/HttpTransact.cc
    	proxy/http2/Http2Stream.h

--- proxy/http2/Http2Stream.h
+++ proxy/http2/Http2Stream.h
@@ -185,7 +185,7 @@ public:
   void update_write_request(IOBufferReader *buf_reader, int64_t write_len, bool send_update);
   void signal_write_event(bool call_update);
   void reenable(VIO *vio) override;
-  virtual void transaction_done() override;
+  void transaction_done() override;
 
   void restart_sending();
   void push_promise(URL &url, const MIMEField *accept_encoding);
