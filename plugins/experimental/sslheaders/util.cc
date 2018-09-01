ca936573f8a84e86ef6e13831ba54480135a770c
Alan M. Carroll <amc@apache.org>
2018-09-01 07:48:59 -0500

    Plugins: Cleanup up dependencies on core headers - sslheaders

--- plugins/experimental/sslheaders/util.cc
+++ plugins/experimental/sslheaders/util.cc
@@ -16,25 +16,29 @@
  * limitations under the License.
  */
 
+#include <array>
 #include "sslheaders.h"
 #include <memory>
-#include "ts/ink_defs.h"
 
 // Count of fields (not including SSL_HEADERS_FIELD_NONE).
 #define NUMFIELDS (SSL_HEADERS_FIELD_MAX - 1)
 
-static const struct _f {
+namespace
+{
+struct _f {
   const char *name;
   ExpansionField field;
-} fields[] = {
-  {"certificate", SSL_HEADERS_FIELD_CERTIFICATE}, {"subject", SSL_HEADERS_FIELD_SUBJECT},
-  {"issuer", SSL_HEADERS_FIELD_ISSUER},           {"serial", SSL_HEADERS_FIELD_SERIAL},
-  {"signature", SSL_HEADERS_FIELD_SIGNATURE},     {"notbefore", SSL_HEADERS_FIELD_NOTBEFORE},
-  {"notafter", SSL_HEADERS_FIELD_NOTAFTER},
 };
-
-// Static assert to guarantee the fields table is current.
-extern char assert_fields_are_populated[((sizeof(fields) / sizeof(fields[0])) - NUMFIELDS) == 0 ? 0 : -1];
+const std::array<_f, SSL_HEADERS_FIELD_MAX - 1> fields = {{
+  {"certificate", SSL_HEADERS_FIELD_CERTIFICATE},
+  {"subject", SSL_HEADERS_FIELD_SUBJECT},
+  {"issuer", SSL_HEADERS_FIELD_ISSUER},
+  {"serial", SSL_HEADERS_FIELD_SERIAL},
+  {"signature", SSL_HEADERS_FIELD_SIGNATURE},
+  {"notbefore", SSL_HEADERS_FIELD_NOTBEFORE},
+  {"notafter", SSL_HEADERS_FIELD_NOTAFTER},
+}};
+} // namespace
 
 bool
 SslHdrParseExpansion(const char *spec, SslHdrExpansion &exp)
@@ -74,7 +78,7 @@ SslHdrParseExpansion(const char *spec, SslHdrExpansion &exp)
 
   // Push sep to point to the field selector.
   selector = sep + 1;
-  for (unsigned i = 0; i < countof(fields); ++i) {
+  for (unsigned i = 0; i < fields.size(); ++i) {
     if (strcmp(selector, fields[i].name) == 0) {
       exp.field = fields[i].field;
       return true;
