c929ed6fdee34a67529b55f9fe1c47d449ee854f
Oknet <xuchao@gmail.com>
2018-10-10 13:50:10 +0800

    Assert if operations on keep_alive_queue and active_queue are not thread-safe
    The `vc->add_to_keep_alive_queue()` is called by:
      - Http1ClientSession::release()
      - Http2ClientSession::cleanup_streams()
      - Http2ClientSession::release_stream()
    
    And the `NetHandler::remove_from_active_queue()` is called by
    `vc->add_to_keep_alive_queue()`.
    
    The NetHandler::keep_alive_queue and NetHandler::active_queue are
    internal queue of NetHandler. We must have acquired the NetHandler's
    lock before doing anything with them.
    
    When reenable a HttpSM from plugin, the HttpSM would be run into ethread
    that different from client_vc lives because TSHttpSMCallback is
    scheduled by `eventProcessor.schedule_imm()`.

--- iocore/net/UnixNetVConnection.cc
+++ iocore/net/UnixNetVConnection.cc
@@ -1451,25 +1451,48 @@ UnixNetVConnection::migrateToCurrentThread(Continuation *cont, EThread *t)
 void
 UnixNetVConnection::add_to_keep_alive_queue()
 {
-  nh->add_to_keep_alive_queue(this);
+  MUTEX_TRY_LOCK(lock, nh->mutex, this_ethread());
+  if (lock.is_locked()) {
+    nh->add_to_keep_alive_queue(this);
+  } else {
+    ink_release_assert(!"BUG: It must have acquired the NetHandler's lock before doing anything on keep_alive_queue.");
+  }
 }
 
 void
 UnixNetVConnection::remove_from_keep_alive_queue()
 {
-  nh->remove_from_keep_alive_queue(this);
+  MUTEX_TRY_LOCK(lock, nh->mutex, this_ethread());
+  if (lock.is_locked()) {
+    nh->remove_from_keep_alive_queue(this);
+  } else {
+    ink_release_assert(!"BUG: It must have acquired the NetHandler's lock before doing anything on keep_alive_queue.");
+  }
 }
 
 bool
 UnixNetVConnection::add_to_active_queue()
 {
-  return nh->add_to_active_queue(this);
+  bool result = false;
+
+  MUTEX_TRY_LOCK(lock, nh->mutex, this_ethread());
+  if (lock.is_locked()) {
+    result = nh->add_to_active_queue(this);
+  } else {
+    ink_release_assert(!"BUG: It must have acquired the NetHandler's lock before doing anything on active_queue.");
+  }
+  return result;
 }
 
 void
 UnixNetVConnection::remove_from_active_queue()
 {
-  nh->remove_from_active_queue(this);
+  MUTEX_TRY_LOCK(lock, nh->mutex, this_ethread());
+  if (lock.is_locked()) {
+    nh->remove_from_active_queue(this);
+  } else {
+    ink_release_assert(!"BUG: It must have acquired the NetHandler's lock before doing anything on active_queue.");
+  }
 }
 
 int
