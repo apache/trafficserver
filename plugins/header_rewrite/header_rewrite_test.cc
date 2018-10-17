apache/trafficserver

commit ed1ded2623674bf6957a0faeb68a8ca10cb0725f

header_rewrite: Adds basic string concatenation
eg:

add-header X-Party "let's party like it's " + %{NOW:YEAR} + "!"

plugins/header_rewrite/parser.cc
@@ -30,7 +30,7 @@
 
 enum ParserState { PARSER_DEFAULT, PARSER_IN_QUOTE, PARSER_IN_REGEX };
 
-Parser::Parser(const std::string &original_line) : _cond(false), _empty(false)
+Parser::Parser(const std::string &original_line, bool preserve_quotes) : _cond(false), _empty(false)
 {
   std::string line        = original_line;
   ParserState state       = PARSER_DEFAULT;
@@ -39,7 +39,8 @@ Parser::Parser(const std::string &original_line) : _cond(false), _empty(false)
   size_t cur_token_length = 0;
 
   for (size_t i = 0; i < line.size(); ++i) {
-    if ((state == PARSER_DEFAULT) && (std::isspace(line[i]) || ((line[i] == '=') || (line[i] == '>') || (line[i] == '<')))) {
+    if ((state == PARSER_DEFAULT) &&
+        (std::isspace(line[i]) || ((line[i] == '=') || (line[i] == '>') || (line[i] == '<') || (line[i] == '+')))) {
       if (extracting_token) {
         cur_token_length = i - cur_token_start;
         if (cur_token_length > 0) {
@@ -77,7 +78,11 @@ Parser::Parser(const std::string &original_line) : _cond(false), _empty(false)
         cur_token_start  = i + 1; // Eat the leading quote
       } else if ((state == PARSER_IN_QUOTE) && extracting_token) {
         cur_token_length = i - cur_token_start;
-        _tokens.push_back(line.substr(cur_token_start, cur_token_length));
+        if (preserve_quotes) {
+          _tokens.push_back(line.substr(cur_token_start - 1, i - cur_token_start + 2));
+        } else {
+          _tokens.push_back(line.substr(cur_token_start, cur_token_length));
+        }
         state            = PARSER_DEFAULT;
         extracting_token = false;
       } else {
@@ -94,7 +99,7 @@ Parser::Parser(const std::string &original_line) : _cond(false), _empty(false)
         break;
       }
 
-      if ((line[i] == '=') || (line[i] == '>') || (line[i] == '<')) {
+      if ((line[i] == '=') || (line[i] == '>') || (line[i] == '<') || (line[i] == '+')) {
         // These are always a seperate token
         _tokens.push_back(std::string(1, line[i]));
         continue;
@@ -158,12 +163,18 @@ Parser::preprocess(std::vector<std::string> tokens)
       return;
     }
   } else {
-    // Operator has no qualifiers, but could take an optional second argumetn
+    // Operator has no qualifiers, but could take an optional second argument
     _op = tokens[0];
     if (tokens.size() > 1) {
       _arg = tokens[1];
+
       if (tokens.size() > 2) {
-        _val = tokens[2];
+        for (auto it = tokens.begin() + 2; it != tokens.end(); it++) {
+          _val = _val + *it;
+          if (std::next(it) != tokens.end()) {
+            _val = _val + " ";
+          }
+        }
       } else {
         _val = "";
       }
@@ -233,4 +244,10 @@ Parser::cond_is_hook(TSHttpHookID &hook) const
   }
 
   return false;
+}
+
+const std::vector<std::string> &
+Parser::get_tokens() const
+{
+  return _tokens;
 }

plugins/header_rewrite/header_rewrite_test.cc
@@ -424,6 +424,7 @@ test_processing()
 
   return errors;
 }
+
 int
 main()
 {

plugins/header_rewrite/Examples/Geo
@@ -16,7 +16,7 @@
 # limitations under the License.
 
 cond %{SEND_RESPONSE_HDR_HOOK} [AND]
-cond %${GEO:COUNTRY} =US
+cond %{GEO:COUNTRY} =US
     set-header ATS-Geo-Country %{GEO:COUNTRY}
     set-header ATS-Geo-Country-ISO %{GEO:COUNTRY-ISO}
     set-header ATS-Geo-ASN %{GEO:ASN}

plugins/header_rewrite/Makefile.inc
@@ -43,6 +43,7 @@ header_rewrite_header_rewrite_la_SOURCES = \
 	header_rewrite/ruleset.h \
 	header_rewrite/statement.cc \
 	header_rewrite/statement.h \
+	header_rewrite/value.cc \
 	header_rewrite/value.h
 
 header_rewrite_parser_la_SOURCES = \

plugins/header_rewrite/condition.h
@@ -112,7 +112,7 @@ public:
     return _qualifier;
   }
 
-  // Virtual methods, has to be implemented by each conditional;
+  // Virtual methods, has to be implemented by each conditional
   void initialize(Parser &p) override;
   virtual void append_value(std::string &s, const Resources &res) = 0;
 

plugins/header_rewrite/conditions.cc
@@ -30,6 +30,7 @@
 #include "ts/ts.h"
 
 #include "conditions.h"
+#include "expander.h"
 #include "lulu.h"
 
 // ConditionStatus
@@ -1372,4 +1373,49 @@ ConditionInbound::append_value(std::string &s, const Resources &res, NetworkSess
   if (zret) {
     s += zret;
   }
+}
+
+ConditionStringLiteral::ConditionStringLiteral(const std::string &v)
+{
+  TSDebug(PLUGIN_NAME_DBG, "Calling CTOR for ConditionStringLiteral");
+  _literal = v;
+}
+
+void
+ConditionStringLiteral::append_value(std::string &s, const Resources &res)
+{
+  s += _literal;
+  TSDebug(PLUGIN_NAME, "Appending '%s' to evaluation value", _literal.c_str());
+}
+
+bool
+ConditionStringLiteral::eval(const Resources &res)
+{
+  TSDebug(PLUGIN_NAME, "Evaluating StringLiteral");
+
+  return static_cast<const MatcherType *>(_matcher)->test(_literal);
+}
+
+ConditionExpandableString::ConditionExpandableString(const std::string &v)
+{
+  TSDebug(PLUGIN_NAME_DBG, "Calling CTOR for ConditionExpandableString");
+  _value = v;
+}
+
+bool
+ConditionExpandableString::eval(const Resources &res)
+{
+  std::string s;
+
+  append_value(s, res);
+
+  return static_cast<const MatcherType *>(_matcher)->test(s);
+}
+
+void
+ConditionExpandableString::append_value(std::string &s, const Resources &res)
+{
+  VariableExpander ve(_value);
+  s += ve.expand(res);
+  TSDebug(PLUGIN_NAME, "Appending to evaluation value -> %s", s.c_str());
 }

plugins/header_rewrite/conditions.h
@@ -541,3 +541,37 @@ protected:
 private:
   NetworkSessionQualifiers _net_qual = NET_QUAL_STACK;
 };
+
+class ConditionStringLiteral : public Condition
+{
+  typedef Matchers<std::string> MatcherType;
+
+public:
+  ConditionStringLiteral(const std::string &v);
+
+  void append_value(std::string &s, const Resources & /* res ATS_UNUSED */) override;
+
+protected:
+  bool eval(const Resources & /* res ATS_UNUSED */) override;
+
+private:
+  std::string _literal;
+  DISALLOW_COPY_AND_ASSIGN(ConditionStringLiteral);
+};
+
+class ConditionExpandableString : public Condition
+{
+  typedef Matchers<std::string> MatcherType;
+
+public:
+  ConditionExpandableString(const std::string &v);
+
+  void append_value(std::string &s, const Resources &res) override;
+
+protected:
+  bool eval(const Resources &res) override;
+
+private:
+  std::string _value;
+  DISALLOW_COPY_AND_ASSIGN(ConditionExpandableString);
+};

plugins/header_rewrite/factory.cc
@@ -144,7 +144,7 @@ condition_factory(const std::string &cond)
   } else if (c_name == "INBOUND") {
     c = new ConditionInbound();
   } else {
-    TSError("[%s] Unknown condition: %s", PLUGIN_NAME, c_name.c_str());
+    TSDebug(PLUGIN_NAME, "Unknown condition: %s", c_name.c_str());
     return nullptr;
   }
 

commit 08d070b15e6fd6a3df97555b0c323236d3f94c8d

Fixes empty string comparison in header_rewrite
This broke with ed1ded2623674bf6957a0faeb68a8ca10cb0725f

plugins/header_rewrite/parser.cc
@@ -77,12 +77,14 @@ Parser::Parser(const std::string &original_line, bool preserve_quotes) : _cond(f
         extracting_token = true;
         cur_token_start  = i + 1; // Eat the leading quote
       } else if ((state == PARSER_IN_QUOTE) && extracting_token) {
-        cur_token_length = i - cur_token_start;
-        if (preserve_quotes) {
+        cur_token_length  = i - cur_token_start;
+        std::string token = line.substr(cur_token_start, cur_token_length);
+        if (preserve_quotes && token != "") {
           _tokens.push_back(line.substr(cur_token_start - 1, i - cur_token_start + 2));
         } else {
-          _tokens.push_back(line.substr(cur_token_start, cur_token_length));
+          _tokens.push_back(token);
         }
+
         state            = PARSER_DEFAULT;
         extracting_token = false;
       } else {

plugins/header_rewrite/header_rewrite_test.cc
@@ -38,7 +38,10 @@ TSError(const char *fmt, ...)
 class ParserTest : public Parser
 {
 public:
-  ParserTest(const std::string &line) : Parser(line), res(true) { std::cout << "Finished parser test: " << line << std::endl; }
+  ParserTest(const std::string &line, bool preserve_quotes = false) : Parser(line, preserve_quotes), res(true)
+  {
+    std::cout << "Finished parser test: " << line << std::endl;
+  }
   std::vector<std::string>
   getTokens()
   {
@@ -191,6 +194,20 @@ test_parsing()
     END_TEST();
   }
 
+  // Same test as above but preserving quotes
+  {
+    ParserTest p(R"(cond %{CLIENT-HEADER:non_existent_header} =""          [AND])", true);
+
+    CHECK_EQ(p.getTokens().size(), 5UL);
+    CHECK_EQ(p.getTokens()[0], "cond");
+    CHECK_EQ(p.getTokens()[1], "%{CLIENT-HEADER:non_existent_header}");
+    CHECK_EQ(p.getTokens()[2], "=");
+    CHECK_EQ(p.getTokens()[3], "");
+    CHECK_EQ(p.getTokens()[4], "[AND]");
+
+    END_TEST();
+  }
+
   {
     ParserTest p(R"(cond %{PATH} /\/foo\/bar/ [OR])");
 
@@ -332,6 +349,22 @@ test_parsing()
     CHECK_EQ(p.getTokens()[0], "add-header");
     CHECK_EQ(p.getTokens()[1], "X-Url");
     CHECK_EQ(p.getTokens()[2], "http://trafficserver.apache.org/");
+
+    END_TEST();
+  }
+
+  // test quote preservation
+  {
+    ParserTest p(R"(add-header X-Party "let's party like it's " + %{NOW:YEAR} + "!")", true);
+
+    CHECK_EQ(p.getTokens().size(), 7UL);
+    CHECK_EQ(p.getTokens()[0], "add-header");
+    CHECK_EQ(p.getTokens()[1], "X-Party");
+    CHECK_EQ(p.getTokens()[2], R"("let's party like it's ")");
+    CHECK_EQ(p.getTokens()[3], "+");
+    CHECK_EQ(p.getTokens()[4], "%{NOW:YEAR}");
+    CHECK_EQ(p.getTokens()[5], "+");
+    CHECK_EQ(p.getTokens()[6], R"("!")");
 
     END_TEST();
   }