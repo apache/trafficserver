bc9d5eef59f23757c711be96a6f861d4bcce89f4
Leif Hedstrom <zwoop@apache.org>
2018-10-18 17:33:06 -0600

    Adds the log tag cqint for internal requests

--- proxy/logging/Log.cc
+++ proxy/logging/Log.cc
@@ -497,6 +497,11 @@ Log::init_fields()
   global_field_list.add(field, false);
   field_symbol_hash.emplace("cqssr", field);
 
+  field = new LogField("client_req_is_internal", "cqint", LogField::sINT, &LogAccess::marshal_client_req_is_internal,
+                       &LogAccess::unmarshal_int_to_str);
+  global_field_list.add(field, false);
+  field_symbol_hash.emplace("cqint", field);
+
   field = new LogField("client_sec_protocol", "cqssv", LogField::STRING, &LogAccess::marshal_client_security_protocol,
                        (LogField::UnmarshalFunc)&LogAccess::unmarshal_str);
   global_field_list.add(field, false);
