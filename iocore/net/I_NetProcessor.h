eb3d15a174404300aec2185cbedfbec28f312753
ezelko260 <evan_zelkowitz@comcast.com>
2018-10-26 21:34:50 +0000

    Reverting fadddccd53c4e8f41553fef6ca0630f0124ed4a0 and fixing up a change from b6f4227fe3f4b9ce344f159ac505258228fa8aeb that does not revert cleanly

--- iocore/net/I_NetProcessor.h
+++ iocore/net/I_NetProcessor.h
@@ -165,6 +165,8 @@ public:
       call back with success. If this behaviour is desired use
       synchronous connect connet_s method.
 
+    @see connect_s()
+
     @param cont Continuation to be called back with events.
     @param addr target address and port to connect to.
     @param options @see NetVCOptions.
@@ -174,11 +176,33 @@ public:
   inkcoreapi Action *connect_re(Continuation *cont, sockaddr const *addr, NetVCOptions *options = nullptr);
 
   /**
+    Open a NetVConnection for connection oriented I/O. This call
+    is simliar to connect method except that the cont is called
+    back only after the connections has been established. In the
+    case of connect the cont could be called back with NET_EVENT_OPEN
+    event and OS could still be in the process of establishing the
+    connection. Re-entrant Callbacks: same as connect. If unix
+    asynchronous type connect is desired use connect_re().
+
+    @param cont Continuation to be called back with events.
+    @param addr Address to which to connect (includes port).
+    @param timeout for connect, the cont will get NET_EVENT_OPEN_FAILED
+      if connection could not be established for timeout msecs. The
+      default is 30 secs.
+    @param options @see NetVCOptions.
+
+    @see connect_re()
+
+  */
+  Action *connect_s(Continuation *cont, sockaddr const *addr, int timeout = NET_CONNECT_TIMEOUT, NetVCOptions *opts = nullptr);
+
+  /**
     Starts the Netprocessor. This has to be called before doing any
     other net call.
 
     @param number_of_net_threads is not used. The net processor
       uses the Event Processor threads for its activity.
+
   */
   virtual int start(int number_of_net_threads, size_t stacksize) = 0;
 
