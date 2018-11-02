42af537ca3e44a2bb3fc39d9f0b807e97f86ddcc
Randall Meyer <randallmeyer@yahoo.com>
2018-11-02 12:38:11 -0700

    Fixes memory leaks in header_rewrite
    Correct botched backport
    
    (cherry picked from commit 8f820c067f92590a8fece61cce98b92f24adb99b)
    
    Conflicts:
            plugins/header_rewrite/condition.h
            plugins/header_rewrite/header_rewrite.cc
            plugins/header_rewrite/ruleset.cc
            plugins/header_rewrite/value.cc
            plugins/header_rewrite/value.h

--- plugins/header_rewrite/value.h
+++ plugins/header_rewrite/value.h
@@ -45,6 +45,12 @@ public:
     TSDebug(PLUGIN_NAME_DBG, "Calling CTOR for Value");
   }
 
+  virtual ~Value()
+  {
+    TSDebug(PLUGIN_NAME_DBG, "Calling DTOR for Value");
+    delete _cond_val;
+  }
+
   void
   set_value(const std::string &val)
   {
