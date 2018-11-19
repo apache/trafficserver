11f076014833cf6ed0b45d8a34d37d56927060e1
Emanuele Rocca <ema@wikimedia.org>
2018-11-19 10:39:11 +0100

    Do not reload remap.config when calling verify_config
    Calling `verify_config` should not swap the currently loaded
    configuration, but simply check whether the configuration files are
    valid.
    
    This fixes issue #4466
    
    (cherry picked from commit 98be1f45a9206c8f388a01cd325d1594437caedd)

--- proxy/ReverseProxy.cc
+++ proxy/ReverseProxy.cc
@@ -118,6 +118,12 @@ struct UR_UpdateContinuation : public Continuation {
   }
 };
 
+bool
+urlRewriteVerify()
+{
+  return UrlRewrite().is_valid();
+}
+
 /**
   Called when the remap.config file changes. Since it called infrequently,
   we do the load of new file as blocking I/O and lock aquire is also
@@ -127,7 +133,7 @@ struct UR_UpdateContinuation : public Continuation {
 bool
 reloadUrlRewrite()
 {
-  UrlRewrite *newTable;
+  UrlRewrite *newTable, *oldTable;
 
   Debug("url_rewrite", "remap.config updated, reloading...");
   newTable = new UrlRewrite();
@@ -137,7 +143,14 @@ reloadUrlRewrite()
     // Hold at least one lease, until we reload the configuration
     newTable->acquire();
 
-    ink_atomic_swap(&rewrite_table, newTable)->release(); // Swap configurations, and release the old one
+    // Swap configurations
+    oldTable = ink_atomic_swap(&rewrite_table, newTable);
+
+    ink_assert(oldTable != nullptr);
+
+    // Release the old one
+    oldTable->release();
+
     Debug("url_rewrite", "%s", msg);
     Note("%s", msg);
     return true;
