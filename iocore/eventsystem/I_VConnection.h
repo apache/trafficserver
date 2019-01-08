e8ee82656d8c7eec0af85e158f7b18bf7e6c5ddd
Bryan Call <bcall@apache.org>
2019-01-08 14:13:18 -0800

    Fixed clang 5.0.0 issue with brace initialization
    (cherry picked from commit 6e84a42f6870a100bc9ea71e968ecb0c611a0375)

--- iocore/eventsystem/I_VConnection.h
+++ iocore/eventsystem/I_VConnection.h
@@ -406,7 +406,7 @@ public:
   };
 
 protected:
-  std::array<void *, TS_VCONN_MAX_USER_ARG> user_args{nullptr};
+  std::array<void *, TS_VCONN_MAX_USER_ARG> user_args{{nullptr}};
 };
 
 struct DummyVConnection : public AnnotatedVConnection {
