3bd51f16ed893687eec4827f8f5e3757f4b8de5b
Masaori Koshiba <masaori@apache.org>
2019-01-22 09:18:01 +0900

    HTTP/3: Add error codes

--- proxy/http3/Http3Types.h
+++ proxy/http3/Http3Types.h
@@ -71,7 +71,34 @@ enum class Http3ErrorClass {
   APPLICATION,
 };
 
-using Http3AppErrorCode = uint16_t;
+// Actual error code of QPACK is not decided yet on qpack-05. It will be changed.
+enum class Http3ErrorCode : uint16_t {
+  NO_ERROR                = 0x00,
+  WRONG_SETTING_DIRECTION = 0x01,
+  PUSH_REFUSED            = 0x02,
+  INTERNAL_ERROR          = 0x03,
+  PUSH_ALREADY_IN_CACHE   = 0x04,
+  REQUEST_CANCELLED       = 0x05,
+  INCOMPLETE_REQUEST      = 0x06,
+  CONNECT_ERROR           = 0x07,
+  EXCESSIVE_LOAD          = 0x08,
+  VERSION_FALLBACK        = 0x09,
+  WRONG_STREAM            = 0x0A,
+  PUSH_LIMIT_EXCEEDED     = 0x0B,
+  DUPLICATE_PUSH          = 0x0C,
+  UNKNOWN_STREAM_TYPE     = 0x0D,
+  WRONG_STREAM_COUNT      = 0x0E,
+  CLOSED_CRITICAL_STREAM  = 0x0F,
+  WRONG_STREAM_DIRECTION  = 0x10,
+  EARLY_RESPONSE          = 0x11,
+  MISSING_SETTINGS        = 0x12,
+  UNEXPECTED_FRAME        = 0x13,
+  QPACK_DECOMPRESSION_FAILED, ///< QPACK Error Code
+  QPACK_ENCODER_STREAM_ERROR, ///< QPACK Error Code
+  QPACK_DECODER_STREAM_ERROR, ///< QPACK Error Code
+  GENERAL_PROTOCOL_ERROR = 0xFF,
+  MALFORMED_FRAME        = 0x0100,
+};
 
 class Http3Error
 {
@@ -81,13 +108,13 @@ public:
 
   Http3ErrorClass cls = Http3ErrorClass::NONE;
   union {
-    Http3AppErrorCode app_error_code;
+    Http3ErrorCode app_error_code;
   };
   const char *msg = nullptr;
 
 protected:
   Http3Error(){};
-  Http3Error(const Http3AppErrorCode error_code, const char *error_msg = nullptr)
+  Http3Error(const Http3ErrorCode error_code, const char *error_msg = nullptr)
     : cls(Http3ErrorClass::APPLICATION), app_error_code(error_code), msg(error_msg){};
 };
 
@@ -101,7 +128,7 @@ class Http3ConnectionError : public Http3Error
 {
 public:
   Http3ConnectionError() : Http3Error() {}
-  Http3ConnectionError(const Http3AppErrorCode error_code, const char *error_msg = nullptr) : Http3Error(error_code, error_msg){};
+  Http3ConnectionError(const Http3ErrorCode error_code, const char *error_msg = nullptr) : Http3Error(error_code, error_msg){};
 };
 
 class Http3Stream;
@@ -110,7 +137,7 @@ class Http3StreamError : public Http3Error
 {
 public:
   Http3StreamError() : Http3Error() {}
-  Http3StreamError(Http3Stream *s, const Http3AppErrorCode error_code, const char *error_msg = nullptr)
+  Http3StreamError(Http3Stream *s, const Http3ErrorCode error_code, const char *error_msg = nullptr)
     : Http3Error(error_code, error_msg), stream(s){};
 
   Http3Stream *stream;
