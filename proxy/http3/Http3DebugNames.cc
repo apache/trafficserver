a6ee9e258d157af8266cc83ea89a2408ec6715f4
Masaori Koshiba <masaori@apache.org>
2019-01-22 09:23:30 +0900

    HTTP/3: Add debug names functions for stream type, settings id, and error code

--- proxy/http3/Http3DebugNames.cc
+++ proxy/http3/Http3DebugNames.cc
@@ -49,3 +49,98 @@ Http3DebugNames::frame_type(Http3FrameType type)
     return "UNKNOWN";
   }
 }
+
+const char *
+Http3DebugNames::stream_type(uint8_t type)
+{
+  switch (type) {
+  case static_cast<uint8_t>(Http3StreamType::CONTROL):
+    return "CONTROL";
+  case static_cast<uint8_t>(Http3StreamType::QPACK_ENCODER):
+    return "QPACK_ENCODER";
+  case static_cast<uint8_t>(Http3StreamType::PUSH):
+    return "PUSH";
+  case static_cast<uint8_t>(Http3StreamType::QPACK_DECODER):
+    return "QPACK_DECODER";
+  default:
+    return "UNKNOWN";
+  }
+}
+
+const char *
+Http3DebugNames::settings_id(uint16_t id)
+{
+  switch (id) {
+  case static_cast<uint16_t>(Http3SettingsId::HEADER_TABLE_SIZE):
+    return "HEADER_TABLE_SIZE";
+  case static_cast<uint16_t>(Http3SettingsId::MAX_HEADER_LIST_SIZE):
+    return "MAX_HEADER_LIST_SIZE";
+  case static_cast<uint16_t>(Http3SettingsId::QPACK_BLOCKED_STREAMS):
+    return "QPACK_BLOCKED_STREAMS";
+  case static_cast<uint16_t>(Http3SettingsId::NUM_PLACEHOLDERS):
+    return "NUM_PLACEHOLDERS";
+  default:
+    return "UNKNOWN";
+  }
+}
+
+const char *
+Http3DebugNames::error_code(uint16_t code)
+{
+  switch (code) {
+  case static_cast<uint16_t>(Http3ErrorCode::NO_ERROR):
+    return "NO_ERROR";
+  case static_cast<uint16_t>(Http3ErrorCode::WRONG_SETTING_DIRECTION):
+    return "WRONG_SETTING_DIRECTION";
+  case static_cast<uint16_t>(Http3ErrorCode::PUSH_REFUSED):
+    return "PUSH_REFUSED";
+  case static_cast<uint16_t>(Http3ErrorCode::INTERNAL_ERROR):
+    return "INTERNAL_ERROR";
+  case static_cast<uint16_t>(Http3ErrorCode::PUSH_ALREADY_IN_CACHE):
+    return "PUSH_ALREADY_IN_CACHE";
+  case static_cast<uint16_t>(Http3ErrorCode::REQUEST_CANCELLED):
+    return "REQUEST_CANCELLED";
+  case static_cast<uint16_t>(Http3ErrorCode::INCOMPLETE_REQUEST):
+    return "INCOMPLETE_REQUEST";
+  case static_cast<uint16_t>(Http3ErrorCode::CONNECT_ERROR):
+    return "CONNECT_ERROR";
+  case static_cast<uint16_t>(Http3ErrorCode::EXCESSIVE_LOAD):
+    return "EXCESSIVE_LOAD";
+  case static_cast<uint16_t>(Http3ErrorCode::VERSION_FALLBACK):
+    return "VERSION_FALLBACK";
+  case static_cast<uint16_t>(Http3ErrorCode::WRONG_STREAM):
+    return "WRONG_STREAM";
+  case static_cast<uint16_t>(Http3ErrorCode::PUSH_LIMIT_EXCEEDED):
+    return "PUSH_LIMIT_EXCEEDED";
+  case static_cast<uint16_t>(Http3ErrorCode::DUPLICATE_PUSH):
+    return "DUPLICATE_PUSH";
+  case static_cast<uint16_t>(Http3ErrorCode::UNKNOWN_STREAM_TYPE):
+    return "UNKNOWN_STREAM_TYPE";
+  case static_cast<uint16_t>(Http3ErrorCode::WRONG_STREAM_COUNT):
+    return "WRONG_STREAM_COUNT";
+  case static_cast<uint16_t>(Http3ErrorCode::CLOSED_CRITICAL_STREAM):
+    return "CLOSED_CRITICAL_STREAM";
+  case static_cast<uint16_t>(Http3ErrorCode::WRONG_STREAM_DIRECTION):
+    return "WRONG_STREAM_DIRECTION";
+  case static_cast<uint16_t>(Http3ErrorCode::EARLY_RESPONSE):
+    return "EARLY_RESPONSE";
+  case static_cast<uint16_t>(Http3ErrorCode::MISSING_SETTINGS):
+    return "MISSING_SETTINGS";
+  case static_cast<uint16_t>(Http3ErrorCode::UNEXPECTED_FRAME):
+    return "UNEXPECTED_FRAME";
+  case static_cast<uint16_t>(Http3ErrorCode::QPACK_DECOMPRESSION_FAILED):
+    return "QPACK_DECOMPRESSION_FAILED";
+  case static_cast<uint16_t>(Http3ErrorCode::QPACK_ENCODER_STREAM_ERROR):
+    return "QPACK_ENCODER_STREAM_ERROR";
+  case static_cast<uint16_t>(Http3ErrorCode::QPACK_DECODER_STREAM_ERROR):
+    return "QPACK_DECODER_STREAM_ERROR";
+  case static_cast<uint16_t>(Http3ErrorCode::GENERAL_PROTOCOL_ERROR):
+    return "GENERAL_PROTOCOL_ERROR";
+  default:
+    if (0x0100 <= code && code <= 0x01FF) {
+      return "MALFORMED_FRAME";
+    }
+
+    return "UNKNOWN";
+  }
+}
