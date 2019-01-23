33f3f5a70cd44a128221f831dec31e1f0d0196f4
Randall Meyer <randallmeyer@yahoo.com>
2019-01-23 12:34:37 -0800

    Cleans up memcached_remap plugin README and code
    Removes unfinished code and support for very old versions of ATS

--- plugins/experimental/memcached_remap/sample.py
+++ plugins/experimental/memcached_remap/sample.py
@@ -25,8 +25,8 @@ import memcache
 mc = memcache.Client(['127.0.0.1:11211'], debug=0)
 
 # Add couple of keys
-mc.set("http://127.0.0.1:80/", "http://127.0.0.1:8080");
-mc.set("http://localhost:80/", "http://localhost:8080");
+mc.set("http://127.0.0.1:80/", "http://127.0.0.1:8080")
+mc.set("http://localhost:80/", "http://localhost:8080")
 
 # Print the keys that are saved
 print "response-1 is '%s'" % (mc.get("http://127.0.0.1:80/"))
