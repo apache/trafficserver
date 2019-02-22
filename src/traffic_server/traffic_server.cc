6a17df2eb4895ac76ff234356b547f849190056d
Bryan Call <bcall@apache.org>
2019-02-22 15:25:49 -0800

    Fixed use after free when running regression under ASAN

--- src/traffic_server/traffic_server.cc
+++ src/traffic_server/traffic_server.cc
@@ -1376,6 +1376,7 @@ struct RegressionCont : public Continuation {
       return EVENT_CONT;
     }
 
+    shutdown_event_system = true;
     fprintf(stderr, "REGRESSION_TEST DONE: %s\n", regression_status_string(res));
     ::exit(res == REGRESSION_TEST_PASSED ? 0 : 1);
     return EVENT_CONT;
