10ebad1bab212ed4ecff77074c45030ad0d6e43d
Masakazu Kitajo <maskit@apache.org>
2019-02-27 10:44:58 +0900

    Small cleanups

--- iocore/net/quic/test/test_QUICPacketFactory.cc
+++ iocore/net/quic/test/test_QUICPacketFactory.cc
@@ -30,7 +30,6 @@ TEST_CASE("QUICPacketFactory_Create_VersionNegotiationPacket", "[quic]")
 {
   MockQUICPacketProtectionKeyInfo pp_key_info;
   QUICPacketFactory factory(pp_key_info);
-  ;
 
   const uint8_t raw_dcid[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
   const uint8_t raw_scid[] = {0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18};
