b8cd666b7b43fa5069228a40e682af01eb7ee7e9
Alan M. Carroll <amc@apache.org>
2019-03-13 18:44:59 -0500

    YAML: Replace ip_allow.config with ip_allow.yaml.

--- proxy/IPAllow.cc
+++ proxy/IPAllow.cc
@@ -27,10 +27,13 @@
 #include <sstream>
 #include "IPAllow.h"
 #include "tscore/BufferWriter.h"
+#include "tscore/ts_file.h"
+#include "tscore/ink_memory.h"
 
-extern char *readIntoBuffer(const char *file_path, const char *module_name, int *read_size_ptr);
+#include "yaml-cpp/yaml.h"
 
 using ts::TextView;
+
 namespace
 {
 void
@@ -42,7 +45,63 @@ SignalError(ts::BufferWriter &w, bool &flag)
   }
   Error("%s", w.data());
 }
+
+template <typename... Args>
+void
+ParseError(ts::TextView fmt, Args &&... args)
+{
+  ts::LocalBufferWriter<1024> w;
+  w.printv(fmt, std::forward_as_tuple(args...));
+  w.write('\0');
+  Warning("%s", w.data());
+}
+
 } // namespace
+
+namespace ts
+{
+BufferWriter &
+bwformat(BufferWriter &w, BWFSpec const &spec, IpAllow const *obj)
+{
+  return w.print("{}[{}]", obj->MODULE_NAME, obj->get_config_file().c_str());
+}
+
+BufferWriter &
+bwformat(BufferWriter &w, BWFSpec const &spec, YAML::Mark const &mark)
+{
+  return w.print("Line {}", mark.line);
+}
+
+BufferWriter &
+bwformat(BufferWriter &w, BWFSpec const &spec, std::error_code const &ec)
+{
+  return w.print("[{}:{}]", ec.value(), ec.message());
+}
+
+} // namespace ts
+
+namespace YAML
+{
+template <> struct convert<ts::TextView> {
+  static Node
+  encode(ts::TextView const &tv)
+  {
+    Node zret;
+    zret = std::string(tv.data(), tv.size());
+    return zret;
+  }
+  static bool
+  decode(const Node &node, ts::TextView &tv)
+  {
+    if (!node.IsScalar()) {
+      return false;
+    }
+    tv.assign(node.Scalar());
+    return true;
+  }
+};
+
+} // namespace YAML
 
 enum AclOp {
   ACL_OP_ALLOW, ///< Allow access.
@@ -77,14 +136,14 @@ IpAllow::reconfigure()
 {
   self_type *new_table;
 
-  Note("ip_allow.config loading ...");
+  Note("ip_allow.yaml loading ...");
 
   new_table = new self_type("proxy.config.cache.ip_allow.filename");
   new_table->BuildTable();
 
   configid = configProcessor.set(configid, new_table);
 
-  Note("ip_allow.config finished loading");
+  Note("ip_allow.yaml finished loading");
 }
 
 IpAllow *
@@ -133,7 +192,7 @@ IpAllow::match(sockaddr const *ip, match_key_t key)
 //   End API functions
 //
 
-IpAllow::IpAllow(const char *config_var) : config_file_path(RecConfigReadConfigPath(config_var)) {}
+IpAllow::IpAllow(const char *config_var) : config_file(ats_scoped_str(RecConfigReadConfigPath(config_var)).get()) {}
 
 void
 IpAllow::PrintMap(IpMap *map)
@@ -194,27 +253,222 @@ IpAllow::Print()
 int
 IpAllow::BuildTable()
 {
-  int file_size = 0;
-  int line_num  = 0;
+  // Table should be empty
+  ink_assert(_src_map.count() == 0 && _dst_map.count() == 0);
+
+  std::error_code ec;
+  std::string content{ts::file::load(config_file, ec)};
+  if (ec.value() == 0) {
+    // If it's a .yaml or the root tag is present, treat as YAML.
+    if (TextView{config_file.view()}.take_suffix_at('.') == "yaml" || std::string::npos != content.find(YAML_TAG_ROOT)) {
+      this->YAMLBuildTable(content);
+    } else {
+      this->ATSBuildTable(content);
+    }
+
+    if (_src_map.count() == 0 && _dst_map.count() == 0) {
+      ParseError("{} - No entries found. All IP Addresses will be blocked", this);
+      return 1;
+    }
+    // convert the coloring from indices to pointers.
+    for (auto &item : _src_map) {
+      item.setData(&_src_acls[reinterpret_cast<size_t>(item.data())]);
+    }
+    for (auto &item : _dst_map) {
+      item.setData(&_dst_acls[reinterpret_cast<size_t>(item.data())]);
+    }
+    if (is_debug_tag_set("ip-allow")) {
+      Print();
+    }
+  } else {
+    ParseError("{} Failed to load {}. All IP Addresses will be blocked", this, ec);
+    return 1;
+  }
+  return 0;
+}
+
+bool
+IpAllow::YAMLLoadMethod(const YAML::Node &node, Record &rec)
+{
+  const std::string &value{node.Scalar()};
+
+  if (0 == strcasecmp(value, YAML_VALUE_METHODS_ALL)) {
+    rec._method_mask = ALL_METHOD_MASK;
+  } else {
+    int method_idx = hdrtoken_tokenize(value.data(), value.size());
+    if (method_idx < HTTP_WKSIDX_CONNECT || method_idx >= HTTP_WKSIDX_CONNECT + HTTP_WKSIDX_METHODS_CNT) {
+      rec._nonstandard_methods.push_back(value);
+      Debug("ip-allow", "Found nonstandard method '%s' at line %d", value.c_str(), node.Mark().line);
+    } else { // valid method.
+      rec._method_mask |= ACL::MethodIdxToMask(method_idx);
+    }
+  }
+  return true;
+}
+
+bool
+IpAllow::YAMLLoadIPAddrRange(const YAML::Node &node, IpMap *map, void *mark)
+{
+  if (node.IsScalar()) {
+    IpAddr min, max;
+    if (0 == ats_ip_range_parse(node.Scalar(), min, max)) {
+      map->fill(min, max, mark);
+      return true;
+    } else {
+      ParseError("{} {} - '{}' is not a valid range.", this, node.Mark(), node.Scalar());
+    }
+  }
+  return false;
+}
+
+bool
+IpAllow::YAMLLoadEntry(const YAML::Node &entry)
+{
+  AclOp op = ACL_OP_DENY; // "shut up", I explained to the compiler.
+  YAML::Node node;
+  IpAddr min, max;
+  std::string value;
+  Record rec;
+  std::vector<Record> *acls{nullptr};
+  IpMap *map = nullptr;
+
+  if (!entry.IsMap()) {
+    ParseError("{} {} - ACL items must be maps.", this, entry.Mark());
+    return false;
+  }
+
+  if (entry[YAML_TAG_APPLY]) {
+    auto apply_node{entry[YAML_TAG_APPLY]};
+    if (apply_node.IsScalar()) {
+      ts::TextView value{apply_node.Scalar()};
+      if (0 == strcasecmp(value, YAML_VALUE_APPLY_IN)) {
+        acls = &_src_acls;
+        map  = &_src_map;
+      } else if (0 == strcasecmp(value, YAML_VALUE_APPLY_OUT)) {
+        acls = &_dst_acls;
+        map  = &_dst_map;
+      } else {
+        ParseError(R"("{}" value at {} must be "{}" or "{}")", YAML_TAG_APPLY, entry.Mark(), YAML_VALUE_APPLY_IN,
+                   YAML_VALUE_APPLY_OUT);
+        return false;
+      }
+    } else {
+      ParseError(R"("{}" value at {} must be a scalar, "{}" or "{}")", YAML_TAG_APPLY, entry.Mark(), YAML_VALUE_APPLY_IN,
+                 YAML_VALUE_APPLY_OUT);
+      return false;
+    }
+  } else {
+    ParseError(R"("Object at {} must have a "{}" key.)", entry.Mark(), YAML_TAG_APPLY);
+    return false;
+  }
+
+  void *ipmap_mark = reinterpret_cast<void *>(acls->size());
+  if (entry[YAML_TAG_IP_ADDRS]) {
+    auto addr_node{entry[YAML_TAG_IP_ADDRS]};
+    if (addr_node.IsSequence()) {
+      for (auto const &n : addr_node) {
+        if (!this->YAMLLoadIPAddrRange(n, map, ipmap_mark)) {
+          return false;
+        }
+      }
+    } else if (!this->YAMLLoadIPAddrRange(addr_node, map, ipmap_mark)) {
+      return false;
+    }
+  }
+
+  if (!entry[YAML_TAG_ACTION]) {
+    ParseError("{} {} - item ignored, required '{}' key not found.", this, entry.Mark(), YAML_TAG_ACTION);
+    return false;
+  }
+
+  node = entry[YAML_TAG_ACTION];
+  if (!node.IsScalar()) {
+    ParseError("{} {} - item ignored, value for tag '{}' must be a string", this, node.Mark(), YAML_TAG_ACTION);
+    return false;
+  }
+  value = node.as<std::string>();
+  if (value == YAML_VALUE_ACTION_ALLOW) {
+    op = ACL_OP_ALLOW;
+  } else if (value == YAML_VALUE_ACTION_DENY) {
+    op = ACL_OP_DENY;
+  } else {
+    ParseError("{} {} - item ignored, value for tag '{}' must be '{}' or '{}'", this, node.Mark(), YAML_TAG_ACTION,
+               YAML_VALUE_ACTION_ALLOW, YAML_VALUE_ACTION_DENY);
+    return false;
+  }
+  if (!entry[YAML_TAG_METHODS]) {
+    rec._method_mask = ALL_METHOD_MASK;
+  } else {
+    node = entry[YAML_TAG_METHODS];
+    if (node.IsScalar()) {
+      this->YAMLLoadMethod(node, rec);
+    } else if (node.IsSequence()) {
+      for (auto const &elt : node) {
+        if (elt.IsScalar()) {
+          this->YAMLLoadMethod(elt, rec);
+          if (rec._method_mask == ALL_METHOD_MASK) {
+            break; // we're done here, nothing else matters.
+          }
+        } else {
+          ParseError("{} {} - item ignored, all values for '{}' must be strings.", this, elt.Mark(), YAML_TAG_METHODS);
+          return false;
+        }
+      }
+    } else {
+      ParseError("{} {} - item ignored, value for '{}' must be a single string or a list of strings.", this, node.Mark(),
+                 YAML_TAG_METHODS);
+    }
+  }
+  if (op == ACL_OP_DENY) {
+    rec._method_mask              = ALL_METHOD_MASK & ~rec._method_mask;
+    rec._deny_nonstandard_methods = true;
+  }
+  rec._src_line = entry.Mark().line;
+  // If we get here, everything parsed OK, add the record.
+  acls->emplace_back(std::move(rec));
+  return true;
+}
+
+int
+IpAllow::YAMLBuildTable(std::string const &content)
+{
+  YAML::Node root{YAML::Load(content)};
+  if (!root.IsMap()) {
+    ParseError("{} - top level object was not a map. All IP Addresses will be blocked", this);
+    return 1;
+  }
+
+  YAML::Node data{root[YAML_TAG_ROOT]};
+  if (!data) {
+    ParseError("{} - root tag '{}' not found. All IP Addresses will be blocked", this, YAML_TAG_ROOT);
+  } else if (data.IsSequence()) {
+    for (auto const &entry : data) {
+      if (!this->YAMLLoadEntry(entry)) {
+        return 1;
+      }
+    }
+  } else if (data.IsMap()) {
+    this->YAMLLoadEntry(data); // singleton, just load it.
+  } else {
+    ParseError("{} - root tag '{}' is not an map or sequence. All IP Addresses will be blocked", this, YAML_TAG_ROOT);
+    return 1;
+  }
+  return 0;
+}
+
+int
+IpAllow::ATSBuildTable(std::string const &content)
+{
+  int line_num = 0;
   IpAddr addr1;
   IpAddr addr2;
   bool alarmAlready = false;
   ts::LocalBufferWriter<1024> bw_err;
 
-  // Table should be empty
-  ink_assert(_src_map.count() == 0 && _dst_map.count() == 0);
-
-  file_buff = readIntoBuffer(config_file_path, "ip-allow", &file_size);
-
-  if (file_buff == nullptr) {
-    Warning("%s Failed to read %s. All IP Addresses will be blocked", MODULE_NAME, config_file_path.get());
-    return 1;
-  }
-
-  TextView src(file_buff, file_size);
+  TextView src(content);
   TextView line;
   auto err_prefix = [&]() -> ts::BufferWriter & {
-    return bw_err.reset().print("{} discarding '{}' entry at line {} : ", MODULE_NAME, config_file_path, line_num);
+    return bw_err.reset().print("{} discarding '{}' entry at line {} : ", MODULE_NAME, config_file.c_str(), line_num);
   };
 
   while (!(line = src.take_prefix_at('\n')).empty()) {
@@ -277,7 +531,7 @@ IpAllow::BuildTable()
               } else {
                 int method_idx = hdrtoken_tokenize(method_name.data(), method_name.size());
                 if (method_idx < HTTP_WKSIDX_CONNECT || method_idx >= HTTP_WKSIDX_CONNECT + HTTP_WKSIDX_METHODS_CNT) {
-                  nonstandard_methods.push_back(method_name);
+                  nonstandard_methods.emplace_back(std::string(method_name.data(), method_name.size()));
                   Debug("ip-allow", "%s",
                         bw_err.reset().print("Found nonstandard method '{}' on line {}\0", method_name, line_num).data());
                 } else { // valid method.
@@ -328,22 +582,6 @@ IpAllow::BuildTable()
         SignalError(bw_err, alarmAlready);
       }
     }
-  }
-
-  if (_src_map.count() == 0 && _dst_map.count() == 0) {
-    Warning("%s No entries in %s. All IP Addresses will be blocked", MODULE_NAME, config_file_path.get());
-  } else {
-    // convert the coloring from indices to pointers.
-    for (auto &item : _src_map) {
-      item.setData(&_src_acls[reinterpret_cast<size_t>(item.data())]);
-    }
-    for (auto &item : _dst_map) {
-      item.setData(&_dst_acls[reinterpret_cast<size_t>(item.data())]);
-    }
-  }
-
-  if (is_debug_tag_set("ip-allow")) {
-    Print();
   }
   return 0;
 }
