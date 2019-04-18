6f291c115002693ebe267ac058adfb863c4085b6
Masakazu Kitajo <maskit@apache.org>
2019-04-18 15:27:19 +0900

    Allow number of settings per H2 session to be configurable
    (cherry picked from commit d91a8ca17bb17b08ce7f040b868e3c286a7bb0a6)
    
     Conflicts:
    	doc/admin-guide/files/records.config.en.rst
    	mgmt/RecordsConfig.cc
    	proxy/http2/HTTP2.cc
    	proxy/http2/HTTP2.h
    	proxy/http2/Http2ConnectionState.cc
    	proxy/http2/Http2ConnectionState.h

--- proxy/http2/Http2ConnectionState.h
+++ proxy/http2/Http2ConnectionState.h
@@ -269,6 +269,9 @@ public:
     }
   }
 
+  void increment_received_settings_count(uint32_t count);
+  uint32_t get_received_settings_count();
+
 private:
   Http2ConnectionState(const Http2ConnectionState &);            // noncopyable
   Http2ConnectionState &operator=(const Http2ConnectionState &); // noncopyable
@@ -294,6 +297,11 @@ private:
 
   // Counter for current active streams and streams in the process of shutting down
   uint32_t total_client_streams_count;
+
+  // Counter for settings received within last 60 seconds
+  // Each item holds a count for 30 seconds.
+  uint16_t settings_count[2]            = {0};
+  ink_hrtime settings_count_last_update = 0;
 
   // NOTE: Id of stream which MUST receive CONTINUATION frame.
   //   - [RFC 7540] 6.2 HEADERS
