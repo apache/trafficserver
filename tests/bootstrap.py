a882d782f4e069659be115870309a1443d613072
Leif Hedstrom <zwoop@apache.org>
2019-05-16 09:24:46 -0600

    Adds a missing dependency, bump autest version
    (cherry picked from commit e2669fb2a32c1da212a753b9afeb7d058e1c5bd3)
    
     Conflicts:
    	tests/bootstrap.py

--- tests/bootstrap.py
+++ tests/bootstrap.py
@@ -26,12 +26,13 @@ import platform
 import sys
 
 pip_packages = [
-    "autest==1.7.0",
+    "autest==1.7.3",
     "hyper",
     "requests",
     "dnslib",
     "httpbin",
-    "traffic-replay" # this should install TRLib, MicroServer, MicroDNS, Traffic-Replay
+    "gunicorn",
+    "traffic-replay"  # this should install TRLib, MicroServer, MicroDNS, Traffic-Replay
 ]
 
 
