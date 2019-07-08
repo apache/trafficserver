6abb4c4fd4964a9743da6f2dfe4d5c1a3c5714ff
scw00 <scw00@apache.org>
2019-07-08 10:08:29 +0800

    QUIC: remove useless params in padder

--- iocore/net/quic/QUICPadder.h
+++ iocore/net/quic/QUICPadder.h
@@ -50,8 +50,7 @@ private:
   Ptr<ProxyMutex> _mutex;
   std::random_device _rnd;
 
-  uint32_t _av_token_len   = 0;
-  uint32_t _latest_seq_num = UINT32_MAX;
+  uint32_t _av_token_len = 0;
   // Initial, 0/1-RTT, and Handshake
   uint64_t _need_to_fire[4]        = {0};
   NetVConnectionContext_t _context = NET_VCONNECTION_OUT;
