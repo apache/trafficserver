6c0eb634b5082532b52272a8d3d7e7905f74d00c
Masakazu Kitajo <maskit@apache.org>
2019-09-10 16:49:01 +0900

    Make code buildable with BoringSSL
    Checked with the latest code f7b830d8df9f5578c748aa0283d44c59ea7eeb25

--- example/plugins/c-api/client_context_dump/client_context_dump.cc
+++ example/plugins/c-api/client_context_dump/client_context_dump.cc
@@ -116,7 +116,7 @@ dump_context(const char *ca_path, const char *ck_path)
 
         // Serial number
         int64_t sn = 0;
-#if OPENSSL_VERSION_NUMBER >= 0x010100000
+#if !defined(OPENSSL_IS_BORINGSSL) && (OPENSSL_VERSION_NUMBER >= 0x010100000)
         ASN1_INTEGER_get_int64(&sn, serial);
 #else
         sn = ASN1_INTEGER_get(serial);
