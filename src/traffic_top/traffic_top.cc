2f6751ff7e8a60364038a1552555f5cb0f98215c
Randall Meyer <randallmeyer@yahoo.com>
2019-10-24 10:30:42 -0500

    Fixes help message for traffic_top's sleep switch
    Closes issue #6051
    
    (cherry picked from commit 0a455638854e3f771c2e92521bf8f17aa38fc836)

--- src/traffic_top/traffic_top.cc
+++ src/traffic_top/traffic_top.cc
@@ -400,7 +400,7 @@ main(int argc, const char **argv)
   version.setup(PACKAGE_NAME, "traffic_top", PACKAGE_VERSION, __DATE__, __TIME__, BUILD_MACHINE, BUILD_PERSON, "");
 
   const ArgumentDescription argument_descriptions[] = {
-    {"sleep", 's', "Enable debugging output", "I", &sleep_time, nullptr, nullptr},
+    {"sleep", 's', "Sets the delay between updates (in seconds)", "I", &sleep_time, nullptr, nullptr},
     HELP_ARGUMENT_DESCRIPTION(),
     VERSION_ARGUMENT_DESCRIPTION(),
     RUNROOT_ARGUMENT_DESCRIPTION(),
