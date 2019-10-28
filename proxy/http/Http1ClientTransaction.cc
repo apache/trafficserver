f770e2895cf5f19351c468313eff44a2207cdcdd
Leif Hedstrom <leif@ogre.com>
2019-10-28 14:43:07 -0600

    Revert "Disable the HttpSM half open logic if the underlying transport is TLS"
    This reverts commit 422e81057070d405a1aa24575d36383e8a7c8d2e.

--- proxy/http/Http1ClientTransaction.cc
+++ proxy/http/Http1ClientTransaction.cc
@@ -71,10 +71,5 @@ Http1ClientTransaction::transaction_done()
 bool
 Http1ClientTransaction::allow_half_open() const
 {
-  bool config_allows_it = (current_reader) ? current_reader->t_state.txn_conf->allow_half_open > 0 : true;
-  if (config_allows_it) {
-    // Check with the session to make sure the underlying transport allows the half open scenario
-    return static_cast<Http1ClientSession *>(parent)->allow_half_open();
-  }
-  return false;
+  return current_reader ? current_reader->t_state.txn_conf->allow_half_open > 0 : true;
 }
