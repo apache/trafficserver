ab5f56c08ebcf5e17525ceb15611320cda77db61
Randall Meyer <rrm@apache.org>
2019-11-01 16:46:03 -0700

    Normalize loopback usage in session_match autest
    (cherry picked from commit 07e6e2d4243ef218410b6dff22f85cfb314492cd)

--- tests/gold_tests/session_sharing/session_match.test.py
+++ tests/gold_tests/session_sharing/session_match.test.py
@@ -70,9 +70,9 @@ class SessionMatchTest:
 
     def _runTraffic(self):
         self._tr.Processes.Default.Command = (
-                'curl -v -H\'Host: www.example.com\' -H\'Connection: close\' http://127.0.1:{port}/one &&'
-                'curl -v -H\'Host: www.example.com\' -H\'Connection: close\' http://127.0.1:{port}/two &&'
-                'curl -v -H\'Host: www.example.com\' -H\'Connection: close\' http://127.0.1:{port}/three'.format(
+                'curl -v -H\'Host: www.example.com\' -H\'Connection: close\' http://127.0.0.1:{port}/one &&'
+                'curl -v -H\'Host: www.example.com\' -H\'Connection: close\' http://127.0.0.1:{port}/two &&'
+                'curl -v -H\'Host: www.example.com\' -H\'Connection: close\' http://127.0.0.1:{port}/three'.format(
                     port = self._ts.Variables.port))
         self._tr.Processes.Default.ReturnCode = 0
         self._tr.Processes.Default.StartBefore(self._server)
