26910cf2284dcbf5a8731296881053b505ae6971
Masakazu Kitajo <maskit@apache.org>
2019-11-15 10:34:32 +0900

    Fix tests

--- iocore/net/quic/test/test_QUICPinger.cc
+++ iocore/net/quic/test/test_QUICPinger.cc
@@ -33,69 +33,69 @@ TEST_CASE("QUICPinger", "[quic]")
   SECTION("request and cancel")
   {
     QUICPinger pinger;
-    pinger.request();
-    REQUIRE(pinger.count() == 1);
-    pinger.request();
-    REQUIRE(pinger.count() == 2);
-    pinger.cancel();
-    REQUIRE(pinger.count() == 1);
+    pinger.request(level);
+    REQUIRE(pinger.count(level) == 1);
+    pinger.request(level);
+    REQUIRE(pinger.count(level) == 2);
+    pinger.cancel(level);
+    REQUIRE(pinger.count(level) == 1);
     REQUIRE(pinger.generate_frame(frame, level, UINT64_MAX, UINT16_MAX, 0, 0) != nullptr);
-    REQUIRE(pinger.count() == 0);
+    REQUIRE(pinger.count(level) == 0);
   }
 
   SECTION("generate PING Frame twice")
   {
     QUICPinger pinger;
-    pinger.request();
-    REQUIRE(pinger.count() == 1);
-    pinger.request();
-    REQUIRE(pinger.count() == 2);
+    pinger.request(level);
+    REQUIRE(pinger.count(level) == 1);
+    pinger.request(level);
+    REQUIRE(pinger.count(level) == 2);
     REQUIRE(pinger.will_generate_frame(level, UINT64_MAX, false, 0) == true);
-    REQUIRE(pinger.count() == 2);
+    REQUIRE(pinger.count(level) == 2);
     REQUIRE(pinger.will_generate_frame(level, UINT64_MAX, false, 0) == false);
-    REQUIRE(pinger.count() == 2);
+    REQUIRE(pinger.count(level) == 2);
   }
 
   SECTION("don't generate frame when packet is ack_elicting")
   {
     QUICPinger pinger;
-    pinger.request();
-    REQUIRE(pinger.count() == 1);
-    pinger.request();
-    REQUIRE(pinger.count() == 2);
+    pinger.request(level);
+    REQUIRE(pinger.count(level) == 1);
+    pinger.request(level);
+    REQUIRE(pinger.count(level) == 2);
     REQUIRE(pinger.will_generate_frame(level, UINT64_MAX, true, 0) == false);
-    REQUIRE(pinger.count() == 1);
+    REQUIRE(pinger.count(level) == 1);
     REQUIRE(pinger.will_generate_frame(level, UINT64_MAX, true, 1) == false);
-    REQUIRE(pinger.count() == 0);
+    REQUIRE(pinger.count(level) == 0);
   }
 
   SECTION("generating PING Frame for next continuos un-ack-eliciting packets")
   {
     QUICPinger pinger;
     REQUIRE(pinger.will_generate_frame(level, UINT64_MAX, false, 0) == true);
-    REQUIRE(pinger.count() == 1);
+    REQUIRE(pinger.count(level) == 1);
     REQUIRE(pinger.will_generate_frame(level, UINT64_MAX, true, 1) == false);
-    REQUIRE(pinger.count() == 0);
+    REQUIRE(pinger.count(level) == 0);
     REQUIRE(pinger.will_generate_frame(level, UINT64_MAX, false, 2) == false);
-    REQUIRE(pinger.count() == 0);
+    REQUIRE(pinger.count(level) == 0);
     REQUIRE(pinger.will_generate_frame(level, UINT64_MAX, false, 3) == true);
-    REQUIRE(pinger.count() == 1);
+    REQUIRE(pinger.count(level) == 1);
   }
 
   SECTION("don't send PING Frame for empty packet")
   {
     QUICPinger pinger;
     REQUIRE(pinger.will_generate_frame(level, 0, false, 0) == false);
-    REQUIRE(pinger.count() == 0);
+    REQUIRE(pinger.count(level) == 0);
     REQUIRE(pinger.will_generate_frame(level, UINT64_MAX, false, 1) == true);
-    REQUIRE(pinger.count() == 1);
+    REQUIRE(pinger.count(level) == 1);
     REQUIRE(pinger.will_generate_frame(level, UINT64_MAX, true, 2) == false);
-    REQUIRE(pinger.count() == 0);
+    REQUIRE(pinger.count(level) == 0);
     REQUIRE(pinger.will_generate_frame(level, UINT64_MAX, false, 3) == false);
-    REQUIRE(pinger.count() == 0);
+    REQUIRE(pinger.count(level) == 0);
     REQUIRE(pinger.will_generate_frame(level, 0, false, 4) == false);
-    REQUIRE(pinger.count() == 0);
+    REQUIRE(pinger.count(level) == 0);
     REQUIRE(pinger.will_generate_frame(level, 1, false, 5) == true);
-    REQUIRE(pinger.count() == 1);
+    REQUIRE(pinger.count(level) == 1);
   }
 }
