fa6e5731762ed41c6f31aa89b18579362302d326
bneradt <bneradt@verizonmedia.com>
2019-11-25 23:09:14 +0000

    auto delete rolled log file fixes
    This fixes auto delete registration for:
    
    - core log files that were not getting deleted (such as error.log and
      manager.log) as well as...
    - plugin log files.
    
    To make log deletion registration more automatic, I placed it in LogObject
    initialization so that it doesn't have to happen in as many disparate places.
    By doing this, plugin log auto delete comes for free.  This is why nothing in
    the core plugin implementation needed to be changed to register their log
    files, for instance. Not all log objects are managed by LogObject, however, so
    we still have some manual calls to registration in LogConfig for core log files
    (such as traffic.out and diags.log, for example).
    
    Before this change, LogConfig manipulated deleting_info to create and maintain
    candidates. To encapsulate this logic, I created RolledLogDeleter and changed
    LogConfig to interact with an instance of that. This simplified the LogConfig
    logic while also enabling unit testability of the main log deletion feature.
    
    This also:
    
    - Adds a rolling_max_count autest.
    - Fixes a shutdown leak concerning LogDeletingInfo.
    - Fixes a paramater mismatch between TextLogObject and LogObject.

---
+++ tests/tools/plugins/test_log_interface.cc
@@ -0,0 +1,97 @@
+/** @file
+
+  Test a plugin's interaction with the logging interface.
+
+  @section license License
+
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+#include <ts/ts.h>
+#include <string>
+#include <string.h>
+
+constexpr auto plugin_name = "test_log_interface";
+
+static TSTextLogObject pluginlog;
+
+bool
+is_get_request(TSHttpTxn transaction)
+{
+  TSMLoc req_loc;
+  TSMBuffer req_bufp;
+  if (TSHttpTxnClientReqGet(transaction, &req_bufp, &req_loc) == TS_ERROR) {
+    TSError("Error while retrieving client request header\n");
+    return false;
+  }
+  int method_len     = 0;
+  const char *method = TSHttpHdrMethodGet(req_bufp, req_loc, &method_len);
+  if (method_len != (int)strlen(TS_HTTP_METHOD_GET) || strncasecmp(method, TS_HTTP_METHOD_GET, method_len) != 0) {
+    TSHandleMLocRelease(req_bufp, TS_NULL_MLOC, req_loc);
+    return false;
+  }
+  TSHandleMLocRelease(req_bufp, TS_NULL_MLOC, req_loc);
+  return true;
+}
+
+int
+global_handler(TSCont continuation, TSEvent event, void *data)
+{
+  TSHttpSsn session     = static_cast<TSHttpSsn>(data);
+  TSHttpTxn transaction = (TSHttpTxn)data;
+
+  switch (event) {
+  case TS_EVENT_HTTP_READ_REQUEST_HDR:
+    if (is_get_request(transaction)) {
+      const std::string long_line(5000, 's');
+      TSTextLogObjectWrite(pluginlog, "Got a GET request. Writing a long line: %s", long_line.c_str());
+    }
+    TSHttpTxnReenable(transaction, TS_EVENT_HTTP_CONTINUE);
+    return 0;
+
+  default:
+    return 0;
+  }
+
+  TSHttpSsnReenable(session, TS_EVENT_HTTP_CONTINUE);
+
+  return 0;
+}
+
+void
+TSPluginInit(int argc, const char **argv)
+{
+  TSPluginRegistrationInfo info;
+
+  info.plugin_name   = const_cast<char *>(plugin_name);
+  info.support_email = const_cast<char *>("dev@trafficserver.apache.org");
+  info.vendor_name   = const_cast<char *>("Verizon Media");
+
+  TSReturnCode ret;
+#if (TS_VERSION_MAJOR >= 7)
+  ret = TSPluginRegister(&info);
+#else
+  ret = TSPluginRegister(TS_SDK_VERSION_3_0, &info);
+#endif
+
+  if (TS_ERROR == ret) {
+    TSError("[%s] plugin registration failed\n", plugin_name);
+    return;
+  }
+
+  TSAssert(TS_SUCCESS == TSTextLogObjectCreate(plugin_name, TS_LOG_MODE_ADD_TIMESTAMP, &pluginlog));
+  TSHttpHookAdd(TS_HTTP_READ_REQUEST_HDR_HOOK, TSContCreate(global_handler, nullptr));
+}
