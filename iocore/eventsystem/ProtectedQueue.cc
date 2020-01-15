20eb4c78baf23fdc8bc7b9cf23b9228224d67d26
Fei Deng <duke8253@gmail.com>
2020-01-15 16:14:14 -0600

    remove dequeue_timed since it is not being used anymore

--- iocore/eventsystem/ProtectedQueue.cc
+++ iocore/eventsystem/ProtectedQueue.cc
@@ -62,16 +62,6 @@ ProtectedQueue::enqueue(Event *e)
 }
 
 void
-ProtectedQueue::dequeue_timed(ink_hrtime cur_time, ink_hrtime timeout, bool sleep)
-{
-  (void)cur_time;
-  if (sleep) {
-    this->wait(timeout);
-  }
-  this->dequeue_external();
-}
-
-void
 ProtectedQueue::dequeue_external()
 {
   Event *e = static_cast<Event *>(ink_atomiclist_popall(&al));
