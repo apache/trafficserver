ee4494d037493595c67c0348489f08461507bc91
Evan Zelkowitz <evan.zelkowitz@gmail.com>
2020-01-29 17:44:27 -0800

    Fix clang-format issue, apparently clang-format does not format all the test dirs as well

--- tests/gold_tests/pluginTest/header_rewrite/header_rewrite_l_value.test.py
+++ tests/gold_tests/pluginTest/header_rewrite/header_rewrite_l_value.test.py
@@ -17,8 +17,8 @@
 #  limitations under the License.
 
 Test.Summary = '''
-Test for a regression of the issue fixed in 
-https://github.com/apache/trafficserver/pull/5423 
+Test for a regression of the issue fixed in
+https://github.com/apache/trafficserver/pull/5423
 Insertion of header rewrite directives in to the output
 '''
 
