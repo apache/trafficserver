9335c0d9e9768447cae0bfaf1d2a22578398914d
David Calavera <david.calavera@gmail.com>
2020-02-04 13:23:50 -0800

    Free TSMgmtString after using it.
    Fixes memory leak.
    
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- plugins/compress/misc.cc
+++ plugins/compress/misc.cc
@@ -172,6 +172,7 @@ init_hidden_header_name()
     hidden_header_name                         = static_cast<char *>(TSmalloc(hidden_header_name_len + 1));
     hidden_header_name[hidden_header_name_len] = 0;
     sprintf(hidden_header_name, "x-accept-encoding-%s", result);
+    TSfree(result);
   }
   return hidden_header_name;
 }
