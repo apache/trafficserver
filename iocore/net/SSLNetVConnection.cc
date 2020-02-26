7318ae5ad145a117b19dcfedf0c0cf28e68b33d7
Susan Hinrichs <shinrich@verizonmedia.com>
2020-02-26 17:04:01 +0000

    Move TestClientAction to SNIConfig class
    (cherry picked from commit 64e444029a8b7b9e45011b7abaec6adc67bc7b9f)

--- iocore/net/SSLNetVConnection.cc
+++ iocore/net/SSLNetVConnection.cc
@@ -1934,22 +1934,3 @@ SSLNetVConnection::set_server_name(std::string_view name)
     _serverName.reset(n);
   }
 }
-
-// See if any of the client-side actions would trigger for this combination of servername and
-// client IP
-// host_sni_policy is an in/out paramter.  It starts with the global policy from the records.config
-// setting proxy.config.http.host_sni_policy and is possibly overridden if the sni policy
-// contains a host_sni_policy entry
-bool
-SSLNetVConnection::TestClientAction(const char *servername, const IpEndpoint &ep, int &host_sni_policy)
-{
-  bool retval = false;
-  SNIConfig::scoped_config params;
-  const actionVector *actionvec = params->get(servername);
-  if (actionvec) {
-    for (auto &&item : *actionvec) {
-      retval |= item->TestClientSNIAction(servername, ep, host_sni_policy);
-    }
-  }
-  return retval;
-}
