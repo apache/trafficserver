8da08d1937379c5aafa1d9ddc8880ff18fe8fdec
Randall Meyer <rrm@apple.com>
2020-04-08 19:05:27 -0700

    Remove enable_tls from other tests
    Follow up to 7bd54d9e16f9c155e118af3b99f01ca4ad8a801e:wq

--- tests/gold_tests/timeout/active_timeout.test.py
+++ tests/gold_tests/timeout/active_timeout.test.py
@@ -23,7 +23,7 @@ Test.SkipUnless(
     Condition.HasCurlFeature('http2')
 )
 
-ts = Test.MakeATSProcess("ts", select_ports=True, enable_tls=True)
+ts = Test.MakeATSProcess("ts", select_ports=True)
 server = Test.MakeOriginServer("server", delay=8)
 
 request_header = {"headers": "GET /file HTTP/1.1\r\nHost: *\r\n\r\n", "timestamp": "5678", "body": ""}
