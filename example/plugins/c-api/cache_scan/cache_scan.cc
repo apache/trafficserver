e9f166a278f721f4cdeb4508bd401680ff8ffdfa
Randall Meyer <rrm@apple.com>
2020-04-17 12:56:00 -0700

    Removes commented out code from example

--- example/plugins/c-api/cache_scan/cache_scan.cc
+++ example/plugins/c-api/cache_scan/cache_scan.cc
@@ -287,7 +287,6 @@ handle_io(TSCont contp, TSEvent event, void * /* edata ATS_UNUSED */)
     cstate->write_pending = false;
     // the cache scan handler should call vio reenable when there is
     // available data
-    // TSVIOReenable(cstate->write_vio);
     return 0;
   } break;
   case TS_EVENT_VCONN_WRITE_COMPLETE: {
