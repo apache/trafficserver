2c6322b06c773ced294184df10393f9d4e2a623d
Bryan Call <bcall@apache.org>
2020-04-22 13:35:00 -0700

    Fixed bug in the calculation of the header block fragment length
    Co-authored-by: Masaori Koshiba <masaori@apache.org>

--- proxy/http2/Http2Stream.h
+++ proxy/http2/Http2Stream.h
@@ -161,10 +161,8 @@ public:
   int64_t read_vio_read_avail();
 
   uint8_t *header_blocks        = nullptr;
-  uint32_t header_blocks_length = 0;  // total length of header blocks (not include
-                                      // Padding or other fields)
-  uint32_t request_header_length = 0; // total length of payload (include Padding
-                                      // and other fields)
+  uint32_t header_blocks_length = 0; // total length of header blocks (not include Padding or other fields)
+
   bool recv_end_stream = false;
   bool send_end_stream = false;
 
