9b626d44c9d54b72e9207b7f4532e8c066335b3f
Susan Hinrichs <shinrich@apache.org>
2020-06-16 15:27:53 +0000

    Disable max_connections_active_in default now that featur works

--- proxy/http2/Http2ClientSession.cc
+++ proxy/http2/Http2ClientSession.cc
@@ -346,6 +346,7 @@ Http2ClientSession::main_event_handler(int event, void *edata)
   case VC_EVENT_INACTIVITY_TIMEOUT:
   case VC_EVENT_ERROR:
   case VC_EVENT_EOS:
+    Http2SsnDebug("Closing event %d", event);
     this->set_dying_event(event);
     this->do_io_close();
     if (_vc != nullptr) {
