4db11b6af1fbd033bb0d01dca5d2fab01f279845
Tomato <245304590@qq.com>
2020-07-17 02:49:23 +0800

    slice: fix throttle not work (#7008)
    (cherry picked from commit 6bdf1beedba76b7598e5bd1942521ed266993eb8)

--- plugins/experimental/slice/client.cc
+++ plugins/experimental/slice/client.cc
@@ -136,9 +136,9 @@ handle_client_resp(TSCont contp, TSEvent event, Data *const data)
         int64_t const output_sent = data->m_bytessent;
         int64_t const threshout   = data->m_config->m_blockbytes;
 
-        if (threshout < (output_done - output_sent)) {
+        if (threshout < (output_sent - output_done)) {
           start_next_block = false;
-          DEBUG_LOG("%p handle_client_resp: throttling %" PRId64, data, (output_done - output_sent));
+          DEBUG_LOG("%p handle_client_resp: throttling %" PRId64, data, (output_sent - output_done));
         }
       }
 
