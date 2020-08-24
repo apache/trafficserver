c454e36f1673c0c69e2801c52ca1fc89a62fb79d
Susan Hinrichs <shinrich@verizonmedia.com>
2020-08-24 16:41:43 +0000

    Do not lose original inactivity timeout on disable

--- iocore/net/P_UnixNet.h
+++ iocore/net/P_UnixNet.h
@@ -548,7 +548,8 @@ static inline void
 read_disable(NetHandler *nh, NetEvent *ne)
 {
   if (!ne->write.enabled) {
-    ne->set_inactivity_timeout(0);
+    // Don't reset the inactivity_timeout_in, so it we be there next time we reset
+    ne->next_inactivity_timeout_at = 0;
     Debug("socket", "read_disable updating inactivity_at %" PRId64 ", NetEvent=%p", ne->next_inactivity_timeout_at, ne);
   }
   ne->read.enabled = 0;
@@ -569,7 +570,8 @@ static inline void
 write_disable(NetHandler *nh, NetEvent *ne)
 {
   if (!ne->read.enabled) {
-    ne->set_inactivity_timeout(0);
+    // Don't reset the inactivity_timeout_in, so it we be there next time we reset
+    ne->next_inactivity_timeout_at = 0;
     Debug("socket", "write_disable updating inactivity_at %" PRId64 ", NetEvent=%p", ne->next_inactivity_timeout_at, ne);
   }
   ne->write.enabled = 0;
