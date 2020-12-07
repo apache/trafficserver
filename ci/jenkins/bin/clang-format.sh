019973a654ae66a6f994dc325eabd48122739695
Brian Neradt <brian.neradt@gmail.com>
2020-12-07 13:13:13 -0600

    In CI, only run autopep8 on branches that enforce autopep8 (#7270)

--- ci/jenkins/bin/clang-format.sh
+++ ci/jenkins/bin/clang-format.sh
@@ -44,8 +44,12 @@ autoreconf -if && ./configure
 ${ATS_MAKE} clang-format
 [ "0" != "$?" ] && exit 1
 
-${ATS_MAKE} autopep8
-[ "0" != "$?" ] && exit 1
+# Only enforce autopep8 on branches where the pre-commit hook was updated to
+# check it. Otherwise, none of the PRs for older branches will pass this check.
+if grep -q autopep8 tools/git/pre-commit; then
+    ${ATS_MAKE} autopep8
+    [ "0" != "$?" ] && exit 1
+fi
 
 git diff --exit-code
 [ "0" != "$?" ] && exit 1
