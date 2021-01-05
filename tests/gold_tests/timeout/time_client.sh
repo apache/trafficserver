5bc0901a07399a7e2d89d01bed8603b63a0f364a
Susan Hinrichs <shinrich@verizonmedia.com>
2021-01-05 00:50:11 +0000

    Fix rat and clang-format errors

--- tests/gold_tests/timeout/time_client.sh
+++ tests/gold_tests/timeout/time_client.sh
@@ -1,12 +1,28 @@
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+nc -l -p $1 -c 'echo -e "This is a reply"' -o test.out &
+echo "This is a test" | openssl s_client -servername bar.com -connect localhost:$2 -ign_eof
 start=`date +%s`
 $1
-echo $?
-#openssl s_client -ign_eof -connect 127.0.0.1:4443  > out 2>&1 
 end=`date +%s`
 
 runtime=$((end-start))
 echo $runtime
-if [ $runtime -lt 6 ] 
+if [ $runtime -lt 6 ]
 then
   echo "Accept timeout $runtime"
   exit 0
@@ -14,7 +30,7 @@ elif [ $runtime -lt 10 ]
 then
   echo "Transaction inactivity timeout $runtime"
   exit 0
-else 
+else
   echo "Default inactivity timeout $runtime"
   exit 1
 fi
