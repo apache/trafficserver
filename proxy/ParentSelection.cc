5d111bea2ce2ff866135eff2e8b6fdf911ad96d3
Evan Zelkowitz <eze@apache.org>
2021-01-11 10:23:10 -0700

    Fix issue with unavailable server retry codes (#7410)
    Currently when populating the list it checks for >500, this should be 499 to not exclude `500`
    
    Also adding warnings during token parsing to notify if a user has added an erroneous err code to the unavailable or simple codes list
    
    (cherry picked from commit 98ec28e64cb93cb40e6cd845da3022bdd68d6148)

--- proxy/ParentSelection.cc
+++ proxy/ParentSelection.cc
@@ -336,15 +336,17 @@ UnavailableServerResponseCodes::UnavailableServerResponseCodes(char *val)
   numTok = pTok.Initialize(val, SHARE_TOKS);
   if (numTok == 0) {
     c = atoi(val);
-    if (c > 500 && c < 600) {
+    if (c > 499 && c < 600) {
       codes.push_back(HTTP_STATUS_SERVICE_UNAVAILABLE);
     }
   }
   for (int i = 0; i < numTok; i++) {
     c = atoi(pTok[i]);
-    if (c > 500 && c < 600) {
+    if (c > 499 && c < 600) {
       Debug("parent_select", "loading response code: %d", c);
       codes.push_back(c);
+    } else {
+      Warning("UnavailableServerResponseCodes received non-5xx code '%s', ignoring!", pTok[i]);
     }
   }
   std::sort(codes.begin(), codes.end());
@@ -372,6 +374,8 @@ SimpleRetryResponseCodes::SimpleRetryResponseCodes(char *val)
     if (c > 399 && c < 500) {
       Debug("parent_select", "loading simple response code: %d", c);
       codes.push_back(c);
+    } else {
+      Warning("SimpleRetryResponseCodes received non-4xx code '%s', ignoring!", pTok[i]);
     }
   }
   std::sort(codes.begin(), codes.end());
