f46ce85cfa86db9ea361659139f621b28dcd54c0
Masaori Koshiba <masaori@apache.org>
2021-02-05 08:19:34 +0900

    Add PROXY Protocol Builder (#7445)
    (cherry picked from commit 1baffec5f2d5ce5272c9af5d46b2b54f4b9bbe6a)

--- iocore/net/ProxyProtocol.h
+++ iocore/net/ProxyProtocol.h
@@ -51,3 +51,6 @@ const size_t PPv1_CONNECTION_HEADER_LEN_MAX = 108;
 const size_t PPv2_CONNECTION_HEADER_LEN     = 16;
 
 extern size_t proxy_protocol_parse(ProxyProtocol *pp_info, ts::TextView tv);
+extern size_t proxy_protocol_build(uint8_t *buf, size_t max_buf_len, const ProxyProtocol &pp_info,
+                                   ProxyProtocolVersion force_version = ProxyProtocolVersion::UNDEFINED);
+extern ProxyProtocolVersion proxy_protocol_version_cast(int i);
