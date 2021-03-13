apache/trafficserver

commit d9807ddb610bc95194125113801c5efe8cbde47e

Call constructors and destructors for H1/2 Session/Transaction via ClassAllocator (#7584)

proxy/http2/Http2Stream.cc
@@ -37,7 +37,7 @@
 #define Http2StreamDebug(fmt, ...) \
   SsnDebug(_proxy_ssn, "http2_stream", "[%" PRId64 "] [%u] " fmt, _proxy_ssn->connection_id(), this->get_id(), ##__VA_ARGS__);
 
-ClassAllocator<Http2Stream> http2StreamAllocator("http2StreamAllocator");
+ClassAllocator<Http2Stream, true> http2StreamAllocator("http2StreamAllocator");
 
 Http2Stream::Http2Stream(ProxySession *session, Http2StreamId sid, ssize_t initial_rwnd)
   : super(session), _id(sid), _client_rwnd(initial_rwnd)

proxy/http/Http1ClientSession.cc
@@ -57,7 +57,7 @@ ink_mutex debug_cs_list_mutex;
 
 #endif /* USE_HTTP_DEBUG_LISTS */
 
-ClassAllocator<Http1ClientSession> http1ClientSessionAllocator("http1ClientSessionAllocator");
+ClassAllocator<Http1ClientSession, true> http1ClientSessionAllocator("http1ClientSessionAllocator");
 
 Http1ClientSession::Http1ClientSession() : super(), trans(this) {}
 
@@ -120,7 +120,6 @@ Http1ClientSession::free()
     _vc = nullptr;
   }
 
-  this->~Http1ClientSession();
   THREAD_FREE(this, http1ClientSessionAllocator, this_thread());
 }
 

proxy/http/Http1ClientSession.h
@@ -129,4 +129,4 @@ public:
   Http1Transaction trans;
 };
 
-extern ClassAllocator<Http1ClientSession> http1ClientSessionAllocator;
+extern ClassAllocator<Http1ClientSession, true> http1ClientSessionAllocator;

proxy/http/Http1ServerSession.cc
@@ -36,7 +36,7 @@
 #include "HttpSessionManager.h"
 #include "HttpSM.h"
 
-ClassAllocator<Http1ServerSession> httpServerSessionAllocator("httpServerSessionAllocator");
+ClassAllocator<Http1ServerSession, true> httpServerSessionAllocator("httpServerSessionAllocator");
 
 void
 Http1ServerSession::destroy()
@@ -50,7 +50,6 @@ Http1ServerSession::destroy()
   }
 
   mutex.clear();
-  this->~Http1ServerSession();
   if (httpSessionManager.get_pool_type() == TS_SERVER_SESSION_SHARING_POOL_THREAD) {
     THREAD_FREE(this, httpServerSessionAllocator, this_thread());
   } else {

proxy/http/Http1ServerSession.h
@@ -107,7 +107,7 @@ private:
   IOBufferReader *buf_reader = nullptr;
 };
 
-extern ClassAllocator<Http1ServerSession> httpServerSessionAllocator;
+extern ClassAllocator<Http1ServerSession, true> httpServerSessionAllocator;
 
 ////////////////////////////////////////////
 // INLINE

proxy/http/HttpSM.cc
@@ -1819,7 +1819,6 @@ HttpSM::state_http_server_open(int event, void *data)
     Http1ServerSession *session = (TS_SERVER_SESSION_SHARING_POOL_THREAD == httpSessionManager.get_pool_type()) ?
                                     THREAD_ALLOC_INIT(httpServerSessionAllocator, mutex->thread_holding) :
                                     httpServerSessionAllocator.alloc();
-    new (session) Http1ServerSession();
     session->sharing_pool  = static_cast<TSServerSessionSharingPoolType>(t_state.http_config_param->server_session_sharing_pool);
     session->sharing_match = static_cast<TSServerSessionSharingMatchMask>(t_state.txn_conf->server_session_sharing_match);
 

proxy/http/HttpSessionAccept.cc
@@ -50,7 +50,6 @@ HttpSessionAccept::accept(NetVConnection *netvc, MIOBuffer *iobuf, IOBufferReade
   }
 
   Http1ClientSession *new_session = THREAD_ALLOC_INIT(http1ClientSessionAllocator, this_ethread());
-  new (new_session) Http1ClientSession();
 
   new_session->accept_options = static_cast<Options *>(this);
   new_session->acl            = std::move(acl);

proxy/http2/Http2ClientSession.cc
@@ -47,7 +47,7 @@
     this->session_handler = (handler);     \
   } while (0)
 
-ClassAllocator<Http2ClientSession> http2ClientSessionAllocator("http2ClientSessionAllocator");
+ClassAllocator<Http2ClientSession, true> http2ClientSessionAllocator("http2ClientSessionAllocator");
 
 // memcpy the requested bytes from the IOBufferReader, returning how many were
 // actually copied.
@@ -163,7 +163,6 @@ Http2ClientSession::free()
 
   free_MIOBuffer(this->read_buffer);
   free_MIOBuffer(this->write_buffer);
-  this->~Http2ClientSession();
   THREAD_FREE(this, http2ClientSessionAllocator, this_ethread());
 }
 

commit b5f88200e35c291c314cdaaa92a74f1264b225e8

Fix double destuct on Http2Stream termination
The bug was introduced by #7584.

proxy/http2/Http2Stream.cc
@@ -482,7 +482,6 @@ Http2Stream::terminate_if_possible()
 
     Http2ClientSession *h2_proxy_ssn = static_cast<Http2ClientSession *>(this->_proxy_ssn);
     SCOPED_MUTEX_LOCK(lock, h2_proxy_ssn->connection_state.mutex, this_ethread());
-    this->~Http2Stream();
     THREAD_FREE(this, http2StreamAllocator, this_ethread());
   }
 }