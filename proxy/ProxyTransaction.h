d51a978fc9469ffdee0278d83c3ffe0115dec313
Susan Hinrichs <shinrich@verizonmedia.com>
2021-03-29 08:14:11 -0500

    Add default implementation for allow_half_open (#7630)
    (cherry picked from commit da29e26e5d01920b3f1435e845c36bf46d56276b)

--- proxy/ProxyTransaction.h
+++ proxy/ProxyTransaction.h
@@ -62,7 +62,7 @@ public:
   virtual int get_transaction_id() const = 0;
   virtual int get_transaction_priority_weight() const;
   virtual int get_transaction_priority_dependence() const;
-  virtual bool allow_half_open() const              = 0;
+  virtual bool allow_half_open() const;
   virtual void increment_client_transactions_stat() = 0;
   virtual void decrement_client_transactions_stat() = 0;
 
