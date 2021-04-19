apache/trafficserver

commit fee5ba1f96dc1fd0838eecaaff045970c76edcd6

Allow for regex_remap of pristine URL. (#7347)
Adds a new optional pparam "pristine" to the regex_remap core plugin.  When present, this pparam causes
regex_remap to match on the pristine URL, and to use the pristine URL for $ substitutions.

plugins/regex_remap/regex_remap.cc
@@ -78,14 +78,14 @@ struct UrlComponents {
   UrlComponents() = default;
 
   void
-  populate(TSRemapRequestInfo *rri)
+  populate(TSMBuffer bufp, TSMLoc url)
   {
-    scheme = TSUrlSchemeGet(rri->requestBufp, rri->requestUrl, &scheme_len);
-    host   = TSUrlHostGet(rri->requestBufp, rri->requestUrl, &host_len);
-    path   = TSUrlPathGet(rri->requestBufp, rri->requestUrl, &path_len);
-    query  = TSUrlHttpQueryGet(rri->requestBufp, rri->requestUrl, &query_len);
-    matrix = TSUrlHttpParamsGet(rri->requestBufp, rri->requestUrl, &matrix_len);
-    port   = TSUrlPortGet(rri->requestBufp, rri->requestUrl);
+    scheme = TSUrlSchemeGet(bufp, url, &scheme_len);
+    host   = TSUrlHostGet(bufp, url, &host_len);
+    path   = TSUrlPathGet(bufp, url, &path_len);
+    query  = TSUrlHttpQueryGet(bufp, url, &query_len);
+    matrix = TSUrlHttpParamsGet(bufp, url, &matrix_len);
+    port   = TSUrlPortGet(bufp, url);
 
     url_len = scheme_len + host_len + path_len + query_len + matrix_len + 32;
   }
@@ -626,6 +626,7 @@ struct RemapInstance {
 
   RemapRegex *first  = nullptr;
   RemapRegex *last   = nullptr;
+  bool pristine_url  = false;
   bool profile       = false;
   bool method        = false;
   bool query_string  = true;
@@ -726,6 +727,10 @@ TSRemapNewInstance(int argc, char *argv[], void **ih, char * /* errbuf ATS_UNUSE
       ri->host = true;
     } else if (strncmp(argv[i], "no-host", 7) == 0) {
       ri->host = false;
+    } else if (strcmp(argv[i], "pristine") == 0) {
+      ri->pristine_url = true;
+    } else if (strcmp(argv[i], "no-pristine") == 0) {
+      ri->pristine_url = false;
     } else {
       TSError("[%s] invalid option '%s'", PLUGIN_NAME, argv[i]);
     }
@@ -913,12 +918,36 @@ TSRemapDoRemap(void *ih, TSHttpTxn txnp, TSRemapRequestInfo *rri)
     TSDebug(PLUGIN_NAME, "Falling back to default URL on regex remap without rules");
     return TSREMAP_NO_REMAP;
   }
+  RemapInstance *ri = static_cast<RemapInstance *>(ih);
+
+  struct SrcUrl {
+    TSMBuffer bufp;
+    TSMLoc loc;
+    bool bad;
+  };
+
+  const SrcUrl src_url([=]() -> SrcUrl {
+    SrcUrl u;
+
+    if (ri->pristine_url) {
+      u.bufp = rri->requestBufp;
+      u.loc  = rri->requestUrl;
+      u.bad  = false;
+
+    } else {
+      u.bad = TSHttpTxnPristineUrlGet(txnp, &u.bufp, &u.loc) != TS_SUCCESS;
+    }
+    return u;
+  }());
+
+  if (src_url.bad) {
+    return TSREMAP_NO_REMAP;
+  }
 
   // Populate the request url
   UrlComponents req_url;
-  req_url.populate(rri);
+  req_url.populate(src_url.bufp, src_url.loc);
 
-  RemapInstance *ri = static_cast<RemapInstance *>(ih);
   int ovector[OVECCOUNT];
   int lengths[OVECCOUNT / 2 + 1];
   int dest_len;
@@ -1061,7 +1090,7 @@ TSRemapDoRemap(void *ih, TSHttpTxn txnp, TSRemapRequestInfo *rri)
           const char *start = dest;
 
           // Setup the new URL
-          if (TS_PARSE_ERROR == TSUrlParse(rri->requestBufp, rri->requestUrl, &start, start + dest_len)) {
+          if (TS_PARSE_ERROR == TSUrlParse(src_url.bufp, src_url.loc, &start, start + dest_len)) {
             TSHttpTxnStatusSet(txnp, TS_HTTP_STATUS_INTERNAL_SERVER_ERROR);
             TSError("[%s] can't parse substituted URL string", PLUGIN_NAME);
           }

tests/gold_tests/pluginTest/regex_remap/regex_remap.test.py
@@ -53,15 +53,18 @@ testName = "regex_remap"
 regex_remap_conf_path = os.path.join(ts.Variables.CONFIGDIR, 'regex_remap.conf')
 curl_and_args = 'curl -s -D - -v --proxy localhost:{} '.format(ts.Variables.port)
 
-path1_rule = 'path1 {}\n'.format(int(time.time()) + 600)
-
 ts.Disk.File(regex_remap_conf_path, typename="ats:config").AddLines([
     "# regex_remap configuration\n"
     "^/alpha/bravo/[?]((?!action=(newsfeed|calendar|contacts|notepad)).)*$ http://example.one @status=301\n"
+    "^/charlie http://example.one @status=301\n"
 ])
 
 ts.Disk.remap_config.AddLine(
     "map http://example.one/ http://localhost:{}/ @plugin=regex_remap.so @pparam=regex_remap.conf\n".format(server.Variables.Port)
+)
+ts.Disk.remap_config.AddLine(
+    "map http://example.two/charlie http://localhost:{}/delta ".format(server.Variables.Port) +
+    "@plugin=regex_remap.so @pparam=regex_remap.conf @pparam=pristine\n"
 )
 
 # minimal configuration
@@ -76,6 +79,14 @@ tr.Processes.Default.StartBefore(server)
 tr.Processes.Default.StartBefore(Test.Processes.ts)
 creq = replay_txns[0]['client-request']
 tr.Processes.Default.Command = curl_and_args + '--header "uuid: {}" '.format(creq["headers"]["fields"][1][1]) + creq["url"]
+tr.Processes.Default.ReturnCode = 0
+tr.Processes.Default.Streams.stdout = "gold/regex_remap_smoke.gold"
+tr.StillRunningAfter = ts
+
+tr = Test.AddTestRun("pristine test")
+tr.Processes.Default.Command = (
+    curl_and_args + '--header "uuid: {}" http://example.two/charlie'.format(creq["headers"]["fields"][1][1])
+)
 tr.Processes.Default.ReturnCode = 0
 tr.Processes.Default.Streams.stdout = "gold/regex_remap_smoke.gold"
 tr.StillRunningAfter = ts

commit ca05173f33a7a9e419afd800e0fdae9ff5ea30ec

Fix simple remapping in regex_remap plugin.
Adds a test run to successfully remap a URL with host example.three, resulting in a 200 response.
It includes a bug fix for regex_remap.cc, needed so the test run will succeed.  The bug was probably
introduced by commit fee5ba1f96dc1fd0838eecaaff045970c76edcd6.

plugins/regex_remap/regex_remap.cc
@@ -1067,7 +1067,8 @@ TSRemapDoRemap(void *ih, TSHttpTxn txnp, TSRemapRequestInfo *rri)
           const char *start = dest;
 
           // Setup the new URL
-          if (TS_PARSE_ERROR == TSUrlParse(src_url.bufp, src_url.loc, &start, start + dest_len)) {
+          if (TS_PARSE_ERROR == TSUrlParse(rri->redirect ? src_url.bufp : rri->requestBufp,
+                                           rri->redirect ? src_url.loc : rri->requestUrl, &start, start + dest_len)) {
             TSHttpTxnStatusSet(txnp, TS_HTTP_STATUS_INTERNAL_SERVER_ERROR);
             TSError("[%s] can't parse substituted URL string", PLUGIN_NAME);
           }

tests/gold_tests/pluginTest/regex_remap/gold/regex_remap_simple.gold
@@ -1,0 +1,2 @@
+HTTP/1.1 200 OK
+Content-Length: 6128

tests/gold_tests/pluginTest/regex_remap/regex_remap.test.py
@@ -51,11 +51,18 @@ ts = Test.MakeATSProcess("ts", enable_cache=False)
 testName = "regex_remap"
 
 regex_remap_conf_path = os.path.join(ts.Variables.CONFIGDIR, 'regex_remap.conf')
+regex_remap2_conf_path = os.path.join(ts.Variables.CONFIGDIR, 'regex_remap2.conf')
 curl_and_args = 'curl -s -D - -v --proxy localhost:{} '.format(ts.Variables.port)
 
 ts.Disk.File(regex_remap_conf_path, typename="ats:config").AddLines([
     "# regex_remap configuration\n"
     "^/alpha/bravo/[?]((?!action=(newsfeed|calendar|contacts|notepad)).)*$ http://example.one @status=301\n"
+])
+
+ts.Disk.File(regex_remap2_conf_path, typename="ats:config").AddLines([
+    "# 2nd regex_remap configuration\n"
+    "^/alpha/bravo/[?]((?!action=(newsfeed|calendar|contacts|notepad)).)*$ " +
+    f"http://localhost:{server.Variables.Port}\n"
 ])
 
 ts.Disk.remap_config.AddLine(
@@ -64,6 +71,10 @@ ts.Disk.remap_config.AddLine(
 ts.Disk.remap_config.AddLine(
     "map http://example.two/ http://localhost:{}/ ".format(server.Variables.Port) +
     "@plugin=regex_remap.so @pparam=regex_remap.conf @pparam=pristine\n"
+)
+ts.Disk.remap_config.AddLine(
+    "map http://example.three/ http://wrong.com/ ".format(server.Variables.Port) +
+    "@plugin=regex_remap.so @pparam=regex_remap2.conf @pparam=pristine\n"
 )
 
 # minimal configuration
@@ -93,7 +104,18 @@ tr.Processes.Default.ReturnCode = 0
 tr.Processes.Default.Streams.stdout = "gold/regex_remap_redirect.gold"
 tr.StillRunningAfter = ts
 
-# 2 Test - Crash test.
+# 2 Test - Match and remap
+tr = Test.AddTestRun("2nd pristine test")
+tr.Processes.Default.Command = (
+    curl_and_args + '--header "uuid: {}" '.format(creq["headers"]["fields"][1][1]) +
+    " 'http://example.three/alpha/bravo/?action=newsfed;param0001=00003E;param0002=00004E;param0003=00005E'" +
+    " | grep -e '^HTTP/' -e '^Content-Length'"
+)
+tr.Processes.Default.ReturnCode = 0
+tr.Processes.Default.Streams.stdout = "gold/regex_remap_simple.gold"
+tr.StillRunningAfter = ts
+
+# 3 Test - Crash test.
 tr = Test.AddTestRun("crash test")
 creq = replay_txns[1]['client-request']
 tr.Processes.Default.Command = curl_and_args + \