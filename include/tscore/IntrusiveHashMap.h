a3c65e557fca25041073db7581847658d1214038
Susan Hinrichs <shinrich@verizonmedia.com>
2021-05-17 21:12:07 +0000

    Remove the intrusive hash map change

--- include/tscore/IntrusiveHashMap.h
+++ include/tscore/IntrusiveHashMap.h
@@ -572,37 +572,20 @@ IntrusiveHashMap<H>::erase(iterator const &loc) -> iterator
       --b->_count;
     }
   }
-  _list.erase(v);
+  _list.erase(loc);
   return zret;
 }
 
 template <typename H>
 bool
-IntrusiveHashMap<H>::erase(value_type *v)
+IntrusiveHashMap<H>::erase(value_type *value)
 {
-  ++(this->iterator_for(v)); // get around no const_iterator -> iterator.
-  Bucket *b         = this->bucket_for(H::key_of(v));
-  value_type *nv    = H::next_ptr(v);
-  value_type *limit = b->limit();
-  if (b->_v == v) {    // removed first element in bucket, update bucket
-    if (limit == nv) { // that was also the only element, deactivate bucket
-      _active_buckets.erase(b);
-      b->clear();
-    } else {
-      b->_v = nv;
-      --b->_count;
-    }
+  auto loc = this->find(value);
+  if (loc != this->end()) {
+    this->erase(loc);
+    return true;
   }
-  _list.erase(v);
-  return true;
-  /*
-    auto loc = this->find(v);
-    if (loc != this->end()) {
-      this->erase(loc);
-      return true;
-    }
-    return false;
-  */
+  return false;
 }
 
 template <typename H>
