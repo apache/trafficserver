84f3177f5c0a23ed65418e0a14dab6d0d655a6ec
Damian Meden <damian.meden@gmail.com>
2021-05-24 19:02:22 +0100

    Autest - Proxy Verifier Extension, add context template $-base string substitution in the replay file. (#7866)

--- tests/gold_tests/autest-site/verifier_common.py
+++ tests/gold_tests/autest-site/verifier_common.py
@@ -16,6 +16,9 @@ Common utilities for the Proxy Verifier extensions.
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import os
+import tempfile
+from string import Template
 
 
 def create_address_argument(ports):
@@ -33,3 +36,24 @@ def create_address_argument(ports):
         argument += "127.0.0.1:{}".format(port)
     argument += '"'
     return argument
+
+
+def substitute_context_in_replay_file(process, replay_path, context):
+    '''
+    Perform substitution base on the passed context dict.
+    This function will return the new replay_path file
+    '''
+    # Only files for now
+    if os.path.isdir(replay_path):
+        raise ValueError(f"Mapping substitution not supported for directories.")
+
+    with open(os.path.join(process.TestDirectory, replay_path), 'r') as replay_file:
+        replay_template = Template(replay_file.read())
+        replay_content = replay_template.substitute(context)
+        tf = tempfile.NamedTemporaryFile(delete=False, dir=process.RunDirectory, suffix=f"_{os.path.basename(replay_path)}")
+        replay_path = tf.name
+        with open(replay_path, "w") as new_replay_file:
+            new_replay_file.write(replay_content)
+
+    # use this as replay_path
+    return replay_path
