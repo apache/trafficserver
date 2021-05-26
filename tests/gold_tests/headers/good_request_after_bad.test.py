63b89acbfe85dede4d499a656a15ef379925ebca
Susan Hinrichs <shinrich@verizonmedia.com>
2021-05-26 15:25:26 +0000

    Test bad request behavior

--- tests/gold_tests/headers/good_request_after_bad.test.py
+++ tests/gold_tests/headers/good_request_after_bad.test.py
@@ -24,7 +24,7 @@ Verify that request following a ill-formed request is not processed
 '''
 Test.ContinueOnFail = True
 ts = Test.MakeATSProcess("ts", enable_cache=True)
-
+Test.ContinueOnFail = True
 ts.Disk.records_config.update({'proxy.config.diags.debug.tags': 'http',
                                'proxy.config.diags.debug.enabled': 0,
                                })
@@ -50,16 +50,18 @@ tr.Processes.Default.Command = 'printf "GET / HTTP/1.1\r\nHost: bob\r\n\r\n" | n
 tr.Processes.Default.ReturnCode = 0
 
 tr = Test.AddTestRun("space after header name")
-tr.Processes.Default.Command = 'printf "GET / HTTP/1.1\r\nHost : bob\r\n\r\nGET / HTTP/1.1\r\nHost: boa\r\n\r\n" | nc  127.0.0.1 {}'.format(
+tr.Processes.Default.Command = 'printf "GET /foo HTTP/1.1\r\nHost : bob\r\n\r\nGET / HTTP/1.1\r\nHost: boa\r\n\r\n" | nc  127.0.0.1 {}'.format(
     ts.Variables.port)
 tr.Processes.Default.ReturnCode = 0
 tr.Processes.Default.Streams.stdout = 'gold/bad_good_request.gold'
 
-tr = Test.AddTestRun("Bad protocol number")
-tr.Processes.Default.Command = 'printf "GET / HTTP/11.1\r\nhost: bob\r\n\r\nGET / HTTP/1.1\r\nHost: boa\r\n\r\n" | nc  127.0.0.1 {}'.format(
+# invalid version number
+tr = Test.AddTestRun("Invalid protocol version number")
+tr.Processes.Default.Command = 'printf "GET / HTTP/1.9\r\nHost:bob\r\n\r\nGET / HTTP/1.1\r\nHost: boa\r\n\r\n" | nc  127.0.0.1 {}'.format(
     ts.Variables.port)
 tr.Processes.Default.ReturnCode = 0
 tr.Processes.Default.Streams.stdout = 'gold/bad_protocol_number.gold'
+
 
 tr = Test.AddTestRun("Unsupported Transfer Encoding value")
 tr.Processes.Default.Command = 'printf "GET / HTTP/1.1\r\nhost: bob\r\ntransfer-encoding: random\r\n\r\nGET / HTTP/1.1\r\nHost: boa\r\n\r\n" | nc  127.0.0.1 {}'.format(
@@ -99,3 +101,10 @@ tr.Processes.Default.Command = 'printf "gET / HTTP/1.1\r\nHost:bob\r\n\r\nGET / 
     ts.Variables.port)
 tr.Processes.Default.ReturnCode = 0
 tr.Processes.Default.Streams.stdout = 'gold/bad_method.gold'
+
+# mangled termination
+tr = Test.AddTestRun("mangled line termination")
+tr.Processes.Default.Command = 'printf "GET / HTTP/1.1\r\nHost:bob\r\n \r\nGET / HTTP/1.1\r\nHost: boa\r\n\r\n" | nc  127.0.0.1 {}'.format(
+    ts.Variables.port)
+tr.Processes.Default.ReturnCode = 0
+tr.Processes.Default.Streams.stdout = 'gold/bad_good_request.gold'
