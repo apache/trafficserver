c4f3f19ce33e24fb8f348cad6f8917979b6ced27
Sanjay Singh <ssingh1@ssingh1-mn3.linkedin.biz>
2021-05-27 16:41:27 -0700

    Update TSHttpTxnAborted API to distinguish client aborts

--- src/traffic_server/InkAPI.cc
+++ src/traffic_server/InkAPI.cc
@@ -5676,16 +5676,18 @@ TSHttpTxnShutDown(TSHttpTxn txnp, TSEvent event)
 }
 
 TSReturnCode
-TSHttpTxnAborted(TSHttpTxn txnp)
+TSHttpTxnAborted(TSHttpTxn txnp, bool *client_abort)
 {
   sdk_assert(sdk_sanity_check_txn(txnp) == TS_SUCCESS);
 
+  *client_abort = false;
   HttpSM *sm = (HttpSM *)txnp;
   switch (sm->t_state.squid_codes.log_code) {
   case SQUID_LOG_ERR_CLIENT_ABORT:
   case SQUID_LOG_ERR_CLIENT_READ_ERROR:
   case SQUID_LOG_TCP_SWAPFAIL:
     // check for client abort and cache read error
+    *client_abort = true;
     return TS_SUCCESS;
   default:
     break;
