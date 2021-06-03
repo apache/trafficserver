349e5a866562703104e5c5864696f53463ebdf3f
Randall Meyer <rrm@apache.org>
2021-06-02 20:19:11 -0700

    Propagate proxy.config.net.sock_option_flag_in to newly accepted connections

--- iocore/net/UnixNetAccept.cc
+++ iocore/net/UnixNetAccept.cc
@@ -335,9 +335,10 @@ NetAccept::do_blocking_accept(EThread *t)
     vc->action_     = *action_;
     vc->set_is_transparent(opt.f_inbound_transparent);
     vc->set_is_proxy_protocol(opt.f_proxy_protocol);
-    vc->options.packet_mark = opt.packet_mark;
-    vc->options.packet_tos  = opt.packet_tos;
-    vc->options.ip_family   = opt.ip_family;
+    vc->options.sockopt_flags = opt.sockopt_flags;
+    vc->options.packet_mark   = opt.packet_mark;
+    vc->options.packet_tos    = opt.packet_tos;
+    vc->options.ip_family     = opt.ip_family;
     vc->apply_options();
     vc->set_context(NET_VCONNECTION_IN);
     if (opt.f_mptcp) {
@@ -485,9 +486,10 @@ NetAccept::acceptFastEvent(int event, void *ep)
     vc->action_     = *action_;
     vc->set_is_transparent(opt.f_inbound_transparent);
     vc->set_is_proxy_protocol(opt.f_proxy_protocol);
-    vc->options.packet_mark = opt.packet_mark;
-    vc->options.packet_tos  = opt.packet_tos;
-    vc->options.ip_family   = opt.ip_family;
+    vc->options.sockopt_flags = opt.sockopt_flags;
+    vc->options.packet_mark   = opt.packet_mark;
+    vc->options.packet_tos    = opt.packet_tos;
+    vc->options.ip_family     = opt.ip_family;
     vc->apply_options();
     vc->set_context(NET_VCONNECTION_IN);
     if (opt.f_mptcp) {
