apache/trafficserver

commit f1ea4993c178117199f4f53b263736d277ac4a22

Updates to Nexthop strategies to limit the number of simultaneous (#7744)
transactions that will retry a parent once it's retry_time window
has elapsed.  The limiting prevents thundering retries on a parent
that is still failed and would cause many transactions to hang or
take much longer than necessary to use a good parent without the
limiting.  This PR mirrors the limiting added to Parent selection.

The maximum number of retriers is configured  with
'proxy.config.http.parent_proxy.max_trans_retries' which defaults to 2.

proxy/http/remap/NextHopConsistentHash.cc
@@ -302,20 +302,26 @@ NextHopConsistentHash::findNextHop(TSHttpTxn txnp, void *ih, time_t now)
       host_stat = TS_HOST_STATUS_UP;
     }
   }
-  if (!pRec || (pRec && !pRec->available) || host_stat == TS_HOST_STATUS_DOWN) {
+  if (!pRec || (pRec && !pRec->available.load()) || host_stat == TS_HOST_STATUS_DOWN) {
     do {
-      // check if an unavailable server is now retryable, use it if it is.
-      if (pRec && !pRec->available && host_stat == TS_HOST_STATUS_UP) {
-        _now == 0 ? _now = time(nullptr) : _now = now;
+      // check if an unavailable server is now retryable, use it.
+      if (pRec && !pRec->available.load() && host_stat == TS_HOST_STATUS_UP) {
         // check if the host is retryable.  It's retryable if the retry window has elapsed
-        if ((pRec->failedAt + retry_time) < static_cast<unsigned>(_now)) {
-          nextHopRetry        = true;
-          result->last_parent = pRec->host_index;
-          result->last_lookup = pRec->group_index;
-          result->retry       = nextHopRetry;
-          result->result      = PARENT_SPECIFIED;
-          NH_Debug(NH_DEBUG_TAG, "[%" PRIu64 "] next hop %s is now retryable, marked it available.", sm_id, pRec->hostname.c_str());
-          break;
+        _now == 0 ? _now = time(nullptr) : _now = now;
+        if ((pRec->failedAt.load() + retry_time) < static_cast<unsigned>(_now)) {
+          if (pRec->retriers.fetch_add(1, std::memory_order_relaxed) < max_retriers) {
+            nextHopRetry        = true;
+            result->last_parent = pRec->host_index;
+            result->last_lookup = pRec->group_index;
+            result->retry       = nextHopRetry;
+            result->result      = PARENT_SPECIFIED;
+            NH_Debug(NH_DEBUG_TAG,
+                     "[%" PRIu64 "] next hop %s is now retryable, marked it available, retriers: %d, max_retriers: %d.", sm_id,
+                     pRec->hostname.c_str(), pRec->retriers.load(), max_retriers);
+            break;
+          } else {
+            pRec->retriers--;
+          }
         }
       }
       switch (ring_mode) {
@@ -350,9 +356,9 @@ NextHopConsistentHash::findNextHop(TSHttpTxn txnp, void *ih, time_t now)
           }
         }
         NH_Debug(NH_DEBUG_TAG, "[%" PRIu64 "] Selected a new parent: %s, available: %s, wrapped: %s, lookups: %d.", sm_id,
-                 pRec->hostname.c_str(), (pRec->available) ? "true" : "false", (wrapped) ? "true" : "false", lookups);
+                 pRec->hostname.c_str(), (pRec->available.load()) ? "true" : "false", (wrapped) ? "true" : "false", lookups);
         // use available host.
-        if (pRec->available && host_stat == TS_HOST_STATUS_UP) {
+        if (pRec->available.load() && host_stat == TS_HOST_STATUS_UP) {
           break;
         }
       } else {
@@ -371,14 +377,14 @@ NextHopConsistentHash::findNextHop(TSHttpTxn txnp, void *ih, time_t now)
         }
         break;
       }
-    } while (!pRec || (pRec && !pRec->available) || host_stat == TS_HOST_STATUS_DOWN);
+    } while (!pRec || (pRec && !pRec->available.load()) || host_stat == TS_HOST_STATUS_DOWN);
   }
 
   // ----------------------------------------------------------------------------------------------------
   // Validate and return the final result.
   // ----------------------------------------------------------------------------------------------------
 
-  if (pRec && host_stat == TS_HOST_STATUS_UP && (pRec->available || result->retry)) {
+  if (pRec && host_stat == TS_HOST_STATUS_UP && (pRec->available.load() || result->retry)) {
     result->result      = PARENT_SPECIFIED;
     result->hostname    = pRec->hostname.c_str();
     result->last_parent = pRec->host_index;

proxy/http/remap/NextHopHealthStatus.cc
@@ -57,7 +57,7 @@ NextHopHealthStatus::isNextHopAvailable(TSHttpTxn txn, const char *hostname, con
   }
 
   std::shared_ptr p = iter->second;
-  return p->available;
+  return p->available.load();
 }
 
 /**
@@ -103,17 +103,17 @@ NextHopHealthStatus::markNextHop(TSHttpTxn txn, const char *hostname, const int 
   switch (status) {
   // Mark the host up.
   case NH_MARK_UP:
-    if (!h->available) {
+    if (!h->available.load()) {
       h->set_available();
       NH_Note("[%" PRId64 "] http parent proxy %s restored", sm_id, hostname);
     }
     break;
   // Mark the host down.
   case NH_MARK_DOWN:
-    if (h->failedAt == 0 || result.retry == true) {
+    if (h->failedAt.load() == 0 || result.retry == true) {
       { // lock guard
         std::lock_guard<std::mutex> guard(h->_mutex);
-        if (h->failedAt == 0) {
+        if (h->failedAt.load() == 0) {
           h->failedAt = _now;
           if (result.retry == false) {
             new_fail_count = h->failCount = 1;
@@ -128,7 +128,7 @@ NextHopHealthStatus::markNextHop(TSHttpTxn txn, const char *hostname, const int 
       // if the last failure was outside the retry window, set the failcount to 1 and failedAt to now.
       { // lock guard
         std::lock_guard<std::mutex> lock(h->_mutex);
-        if ((h->failedAt + retry_time) < static_cast<unsigned>(_now)) {
+        if ((h->failedAt.load() + retry_time) < static_cast<unsigned>(_now)) {
           h->failCount = 1;
           h->failedAt  = _now;
         } else {
@@ -144,7 +144,7 @@ NextHopHealthStatus::markNextHop(TSHttpTxn txn, const char *hostname, const int 
       NH_Note("[%" PRId64 "] Failure threshold met failcount:%d >= threshold:%" PRId64 ", http parent proxy %s marked down", sm_id,
               new_fail_count, fail_threshold, h->hostname.c_str());
       NH_Debug(NH_DEBUG_TAG, "[%" PRId64 "] NextHop %s marked unavailable, h->available=%s", sm_id, h->hostname.c_str(),
-               (h->available) ? "true" : "false");
+               (h->available.load()) ? "true" : "false");
     }
     break;
   }

proxy/http/remap/NextHopRoundRobin.cc
@@ -134,15 +134,15 @@ NextHopRoundRobin::findNextHop(TSHttpTxn txnp, void *ih, time_t now)
     NH_Debug(NH_DEBUG_TAG,
              "[%" PRIu64 "] Selected a parent, %s,  failCount (faileAt: %d failCount: %d), FailThreshold: %" PRIu64
              ", request_info->xact_start: %ld",
-             sm_id, cur_host->hostname.c_str(), (unsigned)cur_host->failedAt, cur_host->failCount, fail_threshold,
+             sm_id, cur_host->hostname.c_str(), (unsigned)cur_host->failedAt, cur_host->failCount.load(), fail_threshold,
              request_info.xact_start);
     // check if 'cur_host' is available, mark it up if it is.
-    if ((cur_host->failedAt == 0) || (cur_host->failCount < fail_threshold)) {
-      if (host_stat == TS_HOST_STATUS_UP) {
+    if ((cur_host->failedAt == 0) || (cur_host->failCount.load() < fail_threshold)) {
+      if (cur_host->available.load() && host_stat == TS_HOST_STATUS_UP) {
         NH_Debug(NH_DEBUG_TAG,
                  "[%" PRIu64
                  "] Selecting a parent, %s,  due to little failCount (faileAt: %d failCount: %d), FailThreshold: %" PRIu64,
-                 sm_id, cur_host->hostname.c_str(), (unsigned)cur_host->failedAt, cur_host->failCount, fail_threshold);
+                 sm_id, cur_host->hostname.c_str(), (unsigned)cur_host->failedAt, cur_host->failCount.load(), fail_threshold);
         parentUp = true;
       }
     } else { // if not available, check to see if it can be retried.  If so, set the retry flag and temporairly mark it as
@@ -150,11 +150,16 @@ NextHopRoundRobin::findNextHop(TSHttpTxn txnp, void *ih, time_t now)
       _now == 0 ? _now = time(nullptr) : _now = now;
       if (((result->wrap_around) || (cur_host->failedAt + retry_time) < static_cast<unsigned>(_now)) &&
           host_stat == TS_HOST_STATUS_UP) {
-        // Reuse the parent
-        parentUp    = true;
-        parentRetry = true;
-        NH_Debug(NH_DEBUG_TAG, "[%" PRIu64 "]  NextHop marked for retry %s:%d", sm_id, cur_host->hostname.c_str(),
-                 host_groups[cur_grp_index][cur_hst_index]->getPort(scheme));
+        if (cur_host->retriers.fetch_add(1, std::memory_order_relaxed) < max_retriers) {
+          // Reuse the parent
+          parentUp    = true;
+          parentRetry = true;
+          NH_Debug(NH_DEBUG_TAG, "[%" PRIu64 "]  NextHop marked for retry %s:%d, max_retriers: %d, retriers: %d", sm_id,
+                   cur_host->hostname.c_str(), host_groups[cur_grp_index][cur_hst_index]->getPort(scheme), max_retriers,
+                   cur_host->retriers.load());
+        } else {
+          cur_host->retriers--;
+        }
       } else { // not retryable or available.
         parentUp = false;
       }

proxy/http/remap/NextHopSelectionStrategy.cc
@@ -39,9 +39,17 @@ constexpr const char *policy_strings[] = {"NH_UNDEFINED", "NH_FIRST_LIVE", "NH_R
 
 NextHopSelectionStrategy::NextHopSelectionStrategy(const std::string_view &name, const NHPolicyType &policy)
 {
-  strategy_name = name;
-  policy_type   = policy;
-  NH_Debug(NH_DEBUG_TAG, "Using a selection strategy of type %s", policy_strings[policy]);
+  int _max_retriers = 0;
+  strategy_name     = name;
+  policy_type       = policy;
+  REC_ReadConfigInteger(_max_retriers, "proxy.config.http.parent_proxy.max_trans_retries");
+
+  // config settings may not be available when running unit tests.
+  // so use the max_retriers default setting.
+  if (_max_retriers > 0) {
+    max_retriers = _max_retriers;
+  }
+  NH_Debug(NH_DEBUG_TAG, "Using a selection strategy of type %s, max_retriers: %d", policy_strings[policy], max_retriers);
 }
 
 //
@@ -210,7 +218,7 @@ NextHopSelectionStrategy::nextHopExists(TSHttpTxn txnp, void *ih)
   for (uint32_t gg = 0; gg < groups; gg++) {
     for (auto &hh : host_groups[gg]) {
       HostRecord *p = hh.get();
-      if (p->available) {
+      if (p->available.load()) {
         NH_Debug(NH_DEBUG_TAG, "[%" PRIu64 "] found available next hop %s", sm_id, p->hostname.c_str());
         return true;
       }

proxy/http/remap/NextHopSelectionStrategy.h
@@ -101,14 +101,15 @@ struct NHProtocol {
 struct HostRecord : ATSConsistentHashNode {
   std::mutex _mutex;
   std::string hostname;
-  time_t failedAt;
-  uint32_t failCount;
-  time_t upAt;
+  std::atomic<time_t> failedAt;
+  std::atomic<uint32_t> failCount;
+  std::atomic<time_t> upAt;
   float weight;
   std::string hash_string;
   int host_index;
   int group_index;
   std::vector<std::shared_ptr<NHProtocol>> protocols;
+  std::atomic<int> retriers = 0;
 
   // construct without locking the _mutex.
   HostRecord()
@@ -122,21 +123,23 @@ struct HostRecord : ATSConsistentHashNode {
     host_index  = -1;
     group_index = -1;
     available   = true;
+    retriers    = 0;
   }
 
   // copy constructor to avoid copying the _mutex.
   HostRecord(const HostRecord &o)
   {
     hostname    = o.hostname;
-    failedAt    = o.failedAt;
-    failCount   = o.failCount;
-    upAt        = o.upAt;
+    failedAt    = o.failedAt.load();
+    failCount   = o.failCount.load();
+    upAt        = o.upAt.load();
     weight      = o.weight;
     hash_string = o.hash_string;
     host_index  = -1;
     group_index = -1;
     available   = true;
     protocols   = o.protocols;
+    retriers    = o.available.load();
   }
 
   // assign without copying the _mutex.
@@ -144,14 +147,16 @@ struct HostRecord : ATSConsistentHashNode {
   operator=(const HostRecord &o)
   {
     hostname    = o.hostname;
-    failedAt    = o.failedAt;
-    upAt        = o.upAt;
+    failedAt    = o.failedAt.load();
+    failCount   = o.failCount.load();
+    upAt        = o.upAt.load();
     weight      = o.weight;
     hash_string = o.hash_string;
     host_index  = o.host_index;
     group_index = o.group_index;
     available   = o.available.load();
     protocols   = o.protocols;
+    retriers    = o.retriers.load();
     return *this;
   }
 
@@ -163,6 +168,9 @@ struct HostRecord : ATSConsistentHashNode {
       std::lock_guard<std::mutex> lock(_mutex);
       failedAt  = time(nullptr);
       available = false;
+      if (--retriers < 0) {
+        retriers = 0;
+      }
     }
   }
 
@@ -174,6 +182,7 @@ struct HostRecord : ATSConsistentHashNode {
       std::lock_guard<std::mutex> lock(_mutex);
       failedAt  = 0;
       failCount = 0;
+      retriers  = 0;
       upAt      = time(nullptr);
       available = true;
     }
@@ -250,4 +259,5 @@ public:
   uint32_t hst_index          = 0;
   uint32_t num_parents        = 0;
   uint32_t distance           = 0; // index into the strategies list.
+  int max_retriers            = 1;
 };

commit 1a48a1c72860217362cef87397b1922a406eb1a5

Fixes an issue in ParentSelection and NextHop strategies where a down parent may not retried
Alternative to https://github.com/apache/trafficserver/pull/8098

Fixes and issue in ParentSelection and Nexthop strategies introduced with #7744 where a down
parent may not be retried due to retriers limiting.

Avoids race conditions in incrementing, decrementing and clearing retriers count.

proxy/ParentConsistentHash.cc
@@ -235,7 +235,7 @@ ParentConsistentHash::selectParent(bool first_call, ParentResult *result, Reques
         Debug("parent_select", "Parent.failedAt = %u, retry = %u, xact_start = %u", static_cast<unsigned>(pRec->failedAt.load()),
               static_cast<unsigned>(retry_time), static_cast<unsigned>(request_info->xact_start));
         if ((pRec->failedAt.load() + retry_time) < request_info->xact_start) {
-          if (pRec->retriers.fetch_add(1, std::memory_order_relaxed) < max_retriers) {
+          if (pRec->retriers.inc(max_retriers)) {
             parentRetry = true;
             // make sure that the proper state is recorded in the result structure
             result->last_parent = pRec->idx;
@@ -243,10 +243,8 @@ ParentConsistentHash::selectParent(bool first_call, ParentResult *result, Reques
             result->retry       = parentRetry;
             result->result      = PARENT_SPECIFIED;
             Debug("parent_select", "Down parent %s is now retryable, retriers = %d, max_retriers = %d", pRec->hostname,
-                  pRec->retriers.load(), max_retriers);
+                  pRec->retriers(), max_retriers);
             break;
-          } else {
-            pRec->retriers--;
           }
         }
       }
@@ -395,7 +393,7 @@ ParentConsistentHash::markParentUp(ParentResult *result)
 
   pRec->failedAt = static_cast<time_t>(0);
   int old_count  = pRec->failCount.exchange(0, std::memory_order_relaxed);
-  pRec->retriers = 0;
+  pRec->retriers.clear();
 
   if (old_count > 0) {
     Note("http parent proxy %s:%d restored", pRec->hostname, pRec->port);

proxy/ParentRoundRobin.cc
@@ -157,18 +157,15 @@ ParentRoundRobin::selectParent(bool first_call, ParentResult *result, RequestDat
     } else {
       if ((result->wrap_around) ||
           (((parents[cur_index].failedAt + retry_time) < request_info->xact_start) && host_stat == TS_HOST_STATUS_UP)) {
-        if (parents[cur_index].retriers.fetch_add(1, std::memory_order_relaxed) < max_retriers) {
+        if (parents[cur_index].retriers.inc(max_retriers)) {
           Debug("parent_select",
                 "Parent[%d].failedAt = %u, retry = %u, retriers = %d, max_retriers = %u, xact_start = %" PRId64 " but wrap = %d",
-                cur_index, static_cast<unsigned>(parents[cur_index].failedAt.load()), retry_time,
-                parents[cur_index].retriers.load(), max_retriers, static_cast<int64_t>(request_info->xact_start),
-                result->wrap_around);
+                cur_index, static_cast<unsigned>(parents[cur_index].failedAt.load()), retry_time, parents[cur_index].retriers(),
+                max_retriers, static_cast<int64_t>(request_info->xact_start), result->wrap_around);
           // Reuse the parent
           parentUp    = true;
           parentRetry = true;
           Debug("parent_select", "Parent marked for retry %s:%d", parents[cur_index].hostname, parents[cur_index].port);
-        } else {
-          parents[cur_index].retriers--;
         }
       } else {
         parentUp = false;

proxy/ParentSelection.cc
@@ -539,7 +539,7 @@ ParentRecord::ProcessParents(char *val, bool isPrimary)
       this->parents[i].name                    = this->parents[i].hostname;
       this->parents[i].available               = true;
       this->parents[i].weight                  = weight;
-      this->parents[i].retriers                = 0;
+      this->parents[i].retriers.clear();
       if (tmp3) {
         memcpy(this->parents[i].hash_string, tmp3 + 1, strlen(tmp3));
         this->parents[i].name = this->parents[i].hash_string;
@@ -555,7 +555,7 @@ ParentRecord::ProcessParents(char *val, bool isPrimary)
       this->secondary_parents[i].name                    = this->secondary_parents[i].hostname;
       this->secondary_parents[i].available               = true;
       this->secondary_parents[i].weight                  = weight;
-      this->secondary_parents[i].retriers                = 0;
+      this->secondary_parents[i].retriers.clear();
       if (tmp3) {
         memcpy(this->secondary_parents[i].hash_string, tmp3 + 1, strlen(tmp3));
         this->secondary_parents[i].name = this->secondary_parents[i].hash_string;

proxy/ParentSelection.h
@@ -105,11 +105,70 @@ struct SimpleRetryResponseCodes {
 private:
   std::vector<int> codes;
 };
+// class pRetriers
+//
+//    Count of retriers with atomic read, increment, and decrement.
+//
+class pRetriers
+{
+public:
+  int
+  operator()() const
+  {
+    return _v.load();
+  }
+
+  void
+  clear()
+  {
+    _v = 0;
+  }
+
+  bool
+  inc(int max_retriers)
+  {
+    ink_assert(max_retriers > 0);
+
+    int r = _v.load(std::memory_order_relaxed);
+    while (r < max_retriers) {
+      if (_v.compare_exchange_weak(r, r + 1)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void
+  dec()
+  {
+    int r = _v.load(std::memory_order_relaxed);
+    while (r > 0) {
+      if (_v.compare_exchange_weak(r, r - 1)) {
+        break;
+      }
+    }
+  }
+
+  pRetriers() = default;
+
+  pRetriers(pRetriers const &o) { _v = o._v.load(); }
+
+  pRetriers &
+  operator=(pRetriers const &o)
+  {
+    _v = o._v.load();
+    return *this;
+  }
+
+private:
+  std::atomic<int> _v = 0;
+};
 // struct pRecord
 //
 //    A record for an individual parent
 //
 struct pRecord : ATSConsistentHashNode {
+public:
   char hostname[MAXDNAME + 1];
   int port;
   std::atomic<time_t> failedAt = 0;
@@ -119,7 +178,13 @@ struct pRecord : ATSConsistentHashNode {
   int idx;
   float weight;
   char hash_string[MAXDNAME + 1];
-  std::atomic<int> retriers = 0;
+  pRetriers retriers;
+
+  void
+  retryComplete()
+  {
+    retriers.dec();
+  }
 };
 
 typedef ControlMatcher<ParentRecord, ParentResult> P_table;
@@ -359,6 +424,16 @@ public:
 
   // virtual destructor.
   virtual ~ParentSelectionStrategy(){};
+
+  void
+  retryComplete(ParentResult *result)
+  {
+    pRecord *p = getParents(result);
+    uint32_t n = numParents(result);
+    if (p != nullptr && result->last_parent < n) {
+      p[result->last_parent].retryComplete();
+    }
+  }
 };
 
 class ParentConfigParams : public ConfigInfo
@@ -408,6 +483,15 @@ public:
     } else {
       ink_release_assert(result->rec->selection_strategy != nullptr);
       return result->rec->selection_strategy->numParents(result);
+    }
+  }
+
+  void
+  retryComplete(ParentResult *result)
+  {
+    if (!result->is_api_result()) {
+      ink_release_assert(result != nullptr);
+      result->rec->selection_strategy->retryComplete(result);
     }
   }
 

proxy/ParentSelectionStrategy.cc
@@ -65,11 +65,6 @@ ParentSelectionStrategy::markParentDown(ParentResult *result, unsigned int fail_
     //   must set the count to reflect this
     if (result->retry == false) {
       new_fail_count = pRec->failCount = 1;
-    } else {
-      // this was a retry that failed, decrement the retriers count
-      if ((pRec->retriers--) < 0) {
-        pRec->retriers = 0;
-      }
     }
 
     Note("Parent %s marked as down %s:%d", (result->retry) ? "retry" : "initially", pRec->hostname, pRec->port);
@@ -128,7 +123,7 @@ ParentSelectionStrategy::markParentUp(ParentResult *result)
   pRec->failedAt = static_cast<time_t>(0);
   int old_count  = pRec->failCount.exchange(0, std::memory_order_relaxed);
   // a retry succeeded, just reset retriers
-  pRec->retriers = 0;
+  pRec->retriers.clear();
 
   if (old_count > 0) {
     Note("http parent proxy %s:%d restored", pRec->hostname, pRec->port);

proxy/http/HttpTransact.cc
@@ -281,6 +281,17 @@ nextParent(HttpTransact::State *s)
   }
 }
 
+inline static void
+retryComplete(HttpTransact::State *s)
+{
+  url_mapping *mp = s->url_map.getMapping();
+  if (mp && mp->strategy) {
+    mp->strategy->retryComplete(reinterpret_cast<TSHttpTxn>(s->state_machine), s->parent_result.hostname, s->parent_result.port);
+  } else if (s->parent_params) {
+    s->parent_params->retryComplete(&s->parent_result);
+  }
+}
+
 inline static bool
 is_localhost(const char *name, int len)
 {
@@ -3633,6 +3644,12 @@ HttpTransact::handle_response_from_parent(State *s)
   LookingUp_t next_lookup = UNDEFINED_LOOKUP;
   TxnDebug("http_trans", "[handle_response_from_parent] (hrfp)");
   HTTP_RELEASE_ASSERT(s->current.server == &s->parent_info);
+
+  // if this parent was retried from a markdown, then
+  // notify that the retry has completed.
+  if (s->parent_result.retry) {
+    retryComplete(s);
+  }
 
   simple_or_unavailable_server_retry(s);
 

proxy/http/remap/NextHopConsistentHash.cc
@@ -326,7 +326,8 @@ NextHopConsistentHash::findNextHop(TSHttpTxn txnp, void *ih, time_t now)
         // check if the host is retryable.  It's retryable if the retry window has elapsed
         _now == 0 ? _now = time(nullptr) : _now = now;
         if ((pRec->failedAt.load() + retry_time) < static_cast<unsigned>(_now)) {
-          if (pRec->retriers.fetch_add(1, std::memory_order_relaxed) < max_retriers) {
+          NH_Debug(NH_DEBUG_TAG, "[%" PRIu64 "] next hop %s, retriers: %d", sm_id, pRec->hostname.c_str(), pRec->retriers());
+          if (pRec->retriers.inc(max_retriers)) {
             nextHopRetry        = true;
             result->last_parent = pRec->host_index;
             result->last_lookup = pRec->group_index;
@@ -334,10 +335,8 @@ NextHopConsistentHash::findNextHop(TSHttpTxn txnp, void *ih, time_t now)
             result->result      = PARENT_SPECIFIED;
             NH_Debug(NH_DEBUG_TAG,
                      "[%" PRIu64 "] next hop %s is now retryable, marked it available, retriers: %d, max_retriers: %d.", sm_id,
-                     pRec->hostname.c_str(), pRec->retriers.load(), max_retriers);
+                     pRec->hostname.c_str(), pRec->retriers(), max_retriers);
             break;
-          } else {
-            pRec->retriers--;
           }
         }
       }

proxy/http/remap/NextHopHealthStatus.cc
@@ -149,3 +149,25 @@ NextHopHealthStatus::markNextHop(TSHttpTxn txn, const char *hostname, const int 
     break;
   }
 }
+
+void
+NextHopHealthStatus::retryComplete(TSHttpTxn txn, const char *hostname, const int port)
+{
+  HttpSM *sm          = reinterpret_cast<HttpSM *>(txn);
+  ParentResult result = sm->t_state.parent_result;
+  int64_t sm_id       = sm->sm_id;
+
+  // make sure we're called back with a result structure for a parent that is being retried.
+  if (result.result != PARENT_SPECIFIED && !result.retry) {
+    return;
+  }
+
+  const std::string host_port = HostRecord::makeHostPort(hostname, port);
+  auto iter                   = host_map.find(host_port);
+  if (iter == host_map.end()) {
+    NH_Debug(NH_DEBUG_TAG, "[%" PRId64 "] no host named %s found in host_map", sm_id, host_port.c_str());
+    return;
+  }
+
+  iter->second->retriers.dec();
+}

proxy/http/remap/NextHopRoundRobin.cc
@@ -150,15 +150,13 @@ NextHopRoundRobin::findNextHop(TSHttpTxn txnp, void *ih, time_t now)
       _now == 0 ? _now = time(nullptr) : _now = now;
       if (((result->wrap_around) || (cur_host->failedAt + retry_time) < static_cast<unsigned>(_now)) &&
           host_stat == TS_HOST_STATUS_UP) {
-        if (cur_host->retriers.fetch_add(1, std::memory_order_relaxed) < max_retriers) {
+        if (cur_host->retriers.inc(max_retriers)) {
           // Reuse the parent
           parentUp    = true;
           parentRetry = true;
           NH_Debug(NH_DEBUG_TAG, "[%" PRIu64 "]  NextHop marked for retry %s:%d, max_retriers: %d, retriers: %d", sm_id,
                    cur_host->hostname.c_str(), host_groups[cur_grp_index][cur_hst_index]->getPort(scheme), max_retriers,
-                   cur_host->retriers.load());
-        } else {
-          cur_host->retriers--;
+                   cur_host->retriers());
         }
       } else { // not retryable or available.
         parentUp = false;

proxy/http/remap/NextHopSelectionStrategy.cc
@@ -293,6 +293,12 @@ NextHopSelectionStrategy::responseIsRetryable(int64_t sm_id, HttpTransact::Curre
   return PARENT_RETRY_NONE;
 }
 
+void
+NextHopSelectionStrategy::retryComplete(TSHttpTxn txnp, const char *hostname, const int port)
+{
+  return passive_health.retryComplete(txnp, hostname, port);
+}
+
 namespace YAML
 {
 template <> struct convert<HostRecord> {

proxy/http/remap/NextHopSelectionStrategy.h
@@ -53,6 +53,7 @@ struct NHHealthStatus {
   virtual bool isNextHopAvailable(TSHttpTxn txn, const char *hostname, const int port, void *ih = nullptr) = 0;
   virtual void markNextHop(TSHttpTxn txn, const char *hostname, const int port, const NHCmd status, void *ih = nullptr,
                            const time_t now = 0)                                                           = 0;
+  virtual void retryComplete(TSHttpTxn txn, const char *hostname, const int port)                          = 0;
   virtual ~NHHealthStatus() {}
 };
 
@@ -114,7 +115,7 @@ struct HostRecord : ATSConsistentHashNode {
   int host_index;
   int group_index;
   std::vector<std::shared_ptr<NHProtocol>> protocols;
-  std::atomic<int> retriers = 0;
+  pRetriers retriers;
 
   // construct without locking the _mutex.
   HostRecord()
@@ -128,7 +129,6 @@ struct HostRecord : ATSConsistentHashNode {
     host_index  = -1;
     group_index = -1;
     available   = true;
-    retriers    = 0;
   }
 
   // copy constructor to avoid copying the _mutex.
@@ -144,7 +144,7 @@ struct HostRecord : ATSConsistentHashNode {
     group_index = -1;
     available   = true;
     protocols   = o.protocols;
-    retriers    = o.available.load();
+    retriers    = o.retriers;
   }
 
   // assign without copying the _mutex.
@@ -161,7 +161,7 @@ struct HostRecord : ATSConsistentHashNode {
     group_index = o.group_index;
     available   = o.available.load();
     protocols   = o.protocols;
-    retriers    = o.retriers.load();
+    retriers    = o.retriers;
     return *this;
   }
 
@@ -173,9 +173,7 @@ struct HostRecord : ATSConsistentHashNode {
       std::lock_guard<std::mutex> lock(_mutex);
       failedAt  = time(nullptr);
       available = false;
-      if (--retriers < 0) {
-        retriers = 0;
-      }
+      retriers.clear();
     }
   }
 
@@ -187,9 +185,9 @@ struct HostRecord : ATSConsistentHashNode {
       std::lock_guard<std::mutex> lock(_mutex);
       failedAt  = 0;
       failCount = 0;
-      retriers  = 0;
       upAt      = time(nullptr);
       available = true;
+      retriers.clear();
     }
   }
 
@@ -226,6 +224,7 @@ public:
   bool isNextHopAvailable(TSHttpTxn txn, const char *hostname, const int port, void *ih = nullptr) override;
   void markNextHop(TSHttpTxn txn, const char *hostname, const int port, const NHCmd status, void *ih = nullptr,
                    const time_t now = 0) override;
+  void retryComplete(TSHttpTxn txn, const char *hostname, const int port) override;
   NextHopHealthStatus(){};
 
 private:
@@ -245,6 +244,8 @@ public:
   bool nextHopExists(TSHttpTxn txnp, void *ih = nullptr);
 
   virtual ParentRetry_t responseIsRetryable(int64_t sm_id, HttpTransact::CurrentInfo &current_info, HTTPStatus response_code);
+
+  void retryComplete(TSHttpTxn txn, const char *hostname, const int port);
 
   std::string strategy_name;
   bool go_direct           = true;