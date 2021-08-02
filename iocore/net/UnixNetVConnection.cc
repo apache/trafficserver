6efb8d8090b9f6f820e8dd48505134af143c7005
Masaori Koshiba <masaori@apache.org>
2021-08-03 07:43:53 +0900

    Revert "Remove UnixNetVConnection::startEvent - not actually called. (#7596)
    * Revert "Remove UnixNetVConnection::startEvent - not actually called. (#7596)"
    
    This reverts commit a56638f8ba92c48e2cc8b677438c36e13f393e2b.
    
    * Fix a use-after-free reported by clang-analyzer
    
    Co-authored-by: Tomoaki Tanaka <tomoatan@yahoo-corp.jp>

--- iocore/net/UnixNetVConnection.cc
+++ iocore/net/UnixNetVConnection.cc
@@ -828,7 +828,10 @@ UnixNetVConnection::reenable_re(VIO *vio)
   }
 }
 
-UnixNetVConnection::UnixNetVConnection() {}
+UnixNetVConnection::UnixNetVConnection()
+{
+  SET_HANDLER((NetVConnHandler)&UnixNetVConnection::startEvent);
+}
 
 // Private methods
 
@@ -988,6 +991,22 @@ void
 UnixNetVConnection::netActivity(EThread *lthread)
 {
   net_activity(this, lthread);
+}
+
+int
+UnixNetVConnection::startEvent(int /* event ATS_UNUSED */, Event *e)
+{
+  MUTEX_TRY_LOCK(lock, get_NetHandler(e->ethread)->mutex, e->ethread);
+  if (!lock.is_locked()) {
+    e->schedule_in(HRTIME_MSECONDS(net_retry_delay));
+    return EVENT_CONT;
+  }
+  if (!action_.cancelled) {
+    connectUp(e->ethread, NO_FD);
+  } else {
+    this->free(e->ethread);
+  }
+  return EVENT_DONE;
 }
 
 int
@@ -1277,6 +1296,7 @@ UnixNetVConnection::free(EThread *t)
   con.close();
 
   clear();
+  SET_CONTINUATION_HANDLER(this, (NetVConnHandler)&UnixNetVConnection::startEvent);
   ink_assert(con.fd == NO_FD);
   ink_assert(t == this_ethread());
 
