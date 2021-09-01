6d4f919b733d47d4e0afc5243e6d863853c38bb6
Brian Neradt <brian.neradt@gmail.com>
2021-09-01 12:32:38 -0500

    Revert "autest: double the initial port pool for get_port (#8292)" (#8300)
    This reverts commit d15a0422eedf526de90ee5f76243e5fb8d8bd185.
    
    This port pool expansion should no longer be needed now that the port
    queue was made a unique set (see #8299). This commit therefore reverts
    what was intended to be temporary.

--- tests/gold_tests/autest-site/ports.py
+++ tests/gold_tests/autest-site/ports.py
@@ -115,7 +115,7 @@ def _get_available_port(queue):
     return port
 
 
-def _setup_port_queue(amount=2000):
+def _setup_port_queue(amount=1000):
     """
     Build up the set of ports that the OS in theory will not use.
     """
