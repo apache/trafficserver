9599bdee0294ac1b7854e3d8fed648604e23e168
Serris Lew <lserris@apple.com>
2021-09-07 12:34:04 -0700

    workaround using PUSH for autest

--- tests/gold_tests/proxy_protocol/proxy_serve_stale.test.py
+++ tests/gold_tests/proxy_protocol/proxy_serve_stale.test.py
@@ -17,17 +17,11 @@ Test child proxy serving stale content when parents are exhausted
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-
-def setPid(event):
-    Test.Env['SERVERPID'] = str(event.object.pid())
-
-
 Test.ContinueOnFail = True
 # Set up hierarchical caching processes
 ts_child = Test.MakeATSProcess("ts_child")
-ts_parent = Test.MakeATSProcess("ts_parent")
+ts_parent = "unknown.com:8080"
 server = Test.MakeOriginServer("server")
-ts_parent.RunningEvent.Connect(setPid)
 
 Test.testName = "STALE"
 
@@ -38,7 +32,7 @@ request_header = {"headers":
                   "body": ""}
 # Expected response from the origin server
 response_header = {"headers":
-                   "HTTP/1.1 200 OK\r\nConnection: close\r\nCache-Control: max-age=10,public\r\n\r\n",
+                   "HTTP/1.1 200 OK\r\nConnection: close\r\nCache-Control: max-age=5,public\r\n\r\n",
                    "timestamp": "1469733493.993",
                    "body": "CACHED"}
 
@@ -49,58 +43,26 @@ server.addResponse("sessionlog.log", request_header, response_header)
 ts_child.Disk.records_config.update({
     'proxy.config.diags.debug.enabled': 1,
     'proxy.config.diags.debug.tags': 'http|remap|parent|host',
-    'proxy.config.http.cache.max_stale_age': 20,
+    'proxy.config.http.cache.max_stale_age': 60,
 })
 ts_child.Disk.parent_config.AddLine(
-    'dest_domain=. parent="localhost:{0}" round_robin=consistent_hash go_direct=false'.format(ts_parent.Variables.port)
+    f'dest_domain=. parent="{ts_parent}" round_robin=consistent_hash go_direct=false'
 )
 ts_child.Disk.remap_config.AddLine(
-    'map http://localhost:{0} http://localhost:{1}'.format(ts_child.Variables.port, server.Variables.Port)
+    f'map http://localhost:{ts_child.Variables.port} http://localhost:{server.Variables.Port}'
 )
 
-# Config parent proxy to route to server
-ts_parent.Disk.records_config.update({
-    'proxy.config.diags.debug.enabled': 1,
-    'proxy.config.diags.debug.tags': 'http|remap|parent',
-    'proxy.config.http.cache.max_stale_age': 20,
-})
-ts_parent.Disk.remap_config.AddLine(
-    'map / http://127.0.0.1:{0}'.format(server.Variables.Port)
-)
-ts_parent.Disk.remap_config.AddLine(
-    'map http://localhost:{0} http://localhost:{0}'.format(server.Variables.Port)
+curl_request = (
+    f'curl -X PUSH --data-binary @{Test.TestDirectory}/serve_stale_output "http://localhost:{ts_child.Variables.port}";'
+    f'sleep 10; curl -s -v http://localhost:{ts_child.Variables.port};'  # hit-stale, serve stale with warning
+    f'sleep 60; curl -v http://localhost:{ts_child.Variables.port}'  # max_stale_age expires, can't reach parent
 )
 
-# Request content to cache in parent proxy
-curl_request_running = (
-    'curl -s -v http://localhost:{0};'  # miss
-    'sleep 5;curl -v http://localhost:{0}'  # hit-fresh
-)
-
-# Request stale content
-curl_request_down = (
-    'sleep 10; curl -s -v http://localhost:{0};'  # hit-stale, serve stale with warning
-    'sleep 10; curl -v http://localhost:{0}'  # max_stale_age expires, can't reach parent
-)
-
-# Test case for when parent server is running, so child proxy can cache object
-tr = Test.AddTestRun("Send request with parent running")
-tr.Processes.Default.Command = curl_request_running.format(ts_child.Variables.port, ts_parent.Variables.port)
+# Test case for when parent server is down but child proxy can serve cache object
+tr = Test.AddTestRun()
+tr.Processes.Default.Command = curl_request
 tr.Processes.Default.ReturnCode = 0
 tr.Processes.Default.StartBefore(server)
-tr.Processes.Default.StartBefore(ts_parent)
 tr.Processes.Default.StartBefore(ts_child)
-tr.Processes.Default.Streams.stderr = "gold/proxy_stale_parent_running.gold"
-tr.StillRunningAfter = ts_child
-tr.StillRunningAfter = ts_parent
-
-tr = Test.AddTestRun("Kill parent server")
-tr.Processes.Default.Command = 'kill ${SERVERPID}'
-tr.Processes.Default.ReturnCode = 0
-
-# Test case for when parent server is down, so child can serve stale content
-tr = Test.AddTestRun("Send request with parent down")
-tr.Processes.Default.Command = curl_request_down.format(ts_child.Variables.port)
-tr.Processes.Default.ReturnCode = 0
-tr.Processes.Default.Streams.stderr = "gold/proxy_stale_parent_down.gold"
+tr.Processes.Default.Streams.stderr = "gold/proxy_serve_stale.gold"
 tr.StillRunningAfter = ts_child
