2f1bd0fe3f7f5677d15d7add87c7a55ce1365db1
Brian Neradt <brian.neradt@gmail.com>
2022-01-31 14:56:27 -0600

    Revert fixes for #8539 (#8637)
    Fixing #8539 is easy to do in a common case, but it causing issues in
    corner cases. For now I'm going to revert the fixes for this and circle
    back around later to see whether this can be solved in a different way.
    
    This reverts the following commits:
    
    Revert "Commenting TSHttpTxnCacheLookupStatusGet need_to_revalidate (#8621)"
    This reverts commit 4678ae34321002c584bd40ff6a1e4a128dae8bb1
    
    Revert "TSHttpTxnCacheLookupStatusGet: call need_to_revalidate (#8617)"
    This reverts commit 235c44a11f70d72168915af2d6504d0e63e880b3.
    
    Revert "TSHttpTxnCacheLookupStatusGet: handle cannot respond cases (#8545)"
    This reverts commit 080e236463490367ef94ce4e56fc95f287a917f5.

--- tests/gold_tests/cache/cache-request-method.test.py
+++ tests/gold_tests/cache/cache-request-method.test.py
@@ -17,8 +17,6 @@ Verify correct caching behavior with respect to request method.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import os
-
 Test.Summary = '''
 Verify correct caching behavior with respect to request method.
 '''
@@ -26,15 +24,11 @@ Verify correct caching behavior with respect to request method.
 # Test 0: Verify correct POST response handling when caching POST responses is
 # disabled.
 ts = Test.MakeATSProcess("ts")
-Test.PrepareTestPlugin(os.path.join(Test.Variables.AtsBuildGoldTestsDir,
-                                    'cache', 'plugins', '.libs', 'print_cache_status.so'), ts)
-Test.Disk.File(os.path.join(ts.Variables.LOGDIR, 'print_cache_status.log'),
-               exists=True, content='gold/print_cache_status_cache_post_disabled.gold')
 replay_file = "replay/post_with_post_caching_disabled.replay.yaml"
 server = Test.MakeVerifierServerProcess("server0", replay_file)
 ts.Disk.records_config.update({
     'proxy.config.diags.debug.enabled': 1,
-    'proxy.config.diags.debug.tags': 'http|cache|print_cache_status',
+    'proxy.config.diags.debug.tags': 'http.*|cache.*',
     'proxy.config.http.insert_age_in_response': 0,
 
     # Caching of POST responses is disabled by default. Verify default behavior
@@ -49,26 +43,14 @@ tr.Processes.Default.StartBefore(server)
 tr.Processes.Default.StartBefore(ts)
 tr.AddVerifierClientProcess("client0", replay_file, http_ports=[ts.Variables.port])
 
-# Wait for log file to appear, then wait one extra second to make sure TS is done writing it.
-test_run = Test.AddTestRun()
-test_run.Processes.Default.Command = (
-    os.path.join(Test.Variables.AtsTestToolsDir, 'condwait') + ' 60 1 -f ' +
-    os.path.join(ts.Variables.LOGDIR, 'print_cache_status.log')
-)
-test_run.Processes.Default.ReturnCode = 0
-
 # Test 1: Verify correct POST response handling when caching POST responses is
 # enabled.
 ts = Test.MakeATSProcess("ts-cache-post")
-Test.PrepareTestPlugin(os.path.join(Test.Variables.AtsBuildGoldTestsDir,
-                                    'cache', 'plugins', '.libs', 'print_cache_status.so'), ts)
-Test.Disk.File(os.path.join(ts.Variables.LOGDIR, 'print_cache_status.log'),
-               exists=True, content='gold/print_cache_status_cache_post_enabled.gold')
 replay_file = "replay/post_with_post_caching_enabled.replay.yaml"
 server = Test.MakeVerifierServerProcess("server1", replay_file)
 ts.Disk.records_config.update({
     'proxy.config.diags.debug.enabled': 1,
-    'proxy.config.diags.debug.tags': 'http|cache|print_cache_status',
+    'proxy.config.diags.debug.tags': 'http.*|cache.*',
     'proxy.config.http.insert_age_in_response': 0,
     'proxy.config.http.cache.post_method': 1,
 })
@@ -79,11 +61,3 @@ tr = Test.AddTestRun("Verify correct with POST response caching enabled.")
 tr.Processes.Default.StartBefore(server)
 tr.Processes.Default.StartBefore(ts)
 tr.AddVerifierClientProcess("client1", replay_file, http_ports=[ts.Variables.port])
-
-# Wait for log file to appear, then wait one extra second to make sure TS is done writing it.
-test_run = Test.AddTestRun()
-test_run.Processes.Default.Command = (
-    os.path.join(Test.Variables.AtsTestToolsDir, 'condwait') + ' 60 1 -f ' +
-    os.path.join(ts.Variables.LOGDIR, 'print_cache_status.log')
-)
-test_run.Processes.Default.ReturnCode = 0
