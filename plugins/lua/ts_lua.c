02a5c01e332fa8941b81907cf448b86eaa0a38ab
Kit Chan <kichan@apache.org>
2022-02-15 11:22:29 -0800

    Stop ATS when a global lua script fails to load

--- plugins/lua/ts_lua.c
+++ plugins/lua/ts_lua.c
@@ -839,8 +839,8 @@ TSPluginInit(int argc, const char *argv[])
     ts_lua_add_module(conf, ts_lua_g_main_ctx_array, conf->states, argc - optind, (char **)&argv[optind], errbuf, errbuf_len);
 
   if (ret != 0) {
-    TSError(errbuf, NULL);
-    TSError("[ts_lua][%s] ts_lua_add_module failed", __FUNCTION__);
+    TSError(errbuf);
+    TSEmergency("[ts_lua][%s] ts_lua_add_module failed", __FUNCTION__);
     return;
   }
 
