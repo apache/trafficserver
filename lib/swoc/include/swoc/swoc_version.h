b87db5208376b889e0a988f5cd4c480f005e42f1
Alan M. Carroll <amc@apache.org>
2022-02-18 20:22:24 -0600

    Fix clang build issue for libswoc (#8658)

--- lib/swoc/include/swoc/swoc_version.h
+++ lib/swoc/include/swoc/swoc_version.h
@@ -23,11 +23,11 @@
 #pragma once
 
 #if !defined(SWOC_VERSION_NS)
-#define SWOC_VERSION_NS _1_3_5
+#define SWOC_VERSION_NS _1_3_6
 #endif
 
 namespace swoc { inline namespace SWOC_VERSION_NS {
 static constexpr unsigned MAJOR_VERSION = 1;
 static constexpr unsigned MINOR_VERSION = 3;
-static constexpr unsigned POINT_VERSION = 5;
+static constexpr unsigned POINT_VERSION = 6;
 }} // namespace swoc::SWOC_VERSION_NS
