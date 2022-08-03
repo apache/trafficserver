347adb11a3e2d8f3f87fa108d5e34e40eae5bbc8
Mo Chen <uncorrupt@gmail.com>
2022-08-03 18:08:07 -0500

    Add stack guard pages (#8996)
    Use r/w protected pages on top of stacks to guard against stack
    overflow.  The number of VM pages to use for guarding can be set
    via the config option proxy.config.thread.default.stackguard_pages.
    
    (cherry picked from commit 1abf6c01164e3893f25cb14460d7da0a5007112d)

--- mgmt/RecordsConfig.cc
+++ mgmt/RecordsConfig.cc
@@ -125,6 +125,8 @@ static const RecordElement RecordsConfig[] =
   ,
   {RECT_CONFIG, "proxy.config.thread.default.stacksize", RECD_INT, "1048576", RECU_RESTART_TS, RR_NULL, RECC_INT, "[131072-104857600]", RECA_READ_ONLY}
   ,
+  {RECT_CONFIG, "proxy.config.thread.default.stackguard_pages", RECD_INT, "1", RECU_RESTART_TS, RR_NULL, RECC_INT, "[1-256]", RECA_READ_ONLY}
+  ,
   {RECT_CONFIG, "proxy.config.restart.active_client_threshold", RECD_INT, "0", RECU_DYNAMIC, RR_NULL, RECC_NULL, nullptr, RECA_NULL}
   ,
   {RECT_CONFIG, "proxy.config.restart.stop_listening", RECD_INT, "0", RECU_DYNAMIC, RR_NULL, RECC_INT, "[0-1]", RECA_NULL}
