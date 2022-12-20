e3e969bd82a735e62be50c66d13d5ff1f9c0acf1
Masakazu Kitajo <maskit@apache.org>
2022-12-19 17:42:44 -0700

    Remove cqhv log field (#9258)
    * Remove cqhv log field
    
    * Update a comment

--- proxy/logging/LogAccess.cc
+++ proxy/logging/LogAccess.cc
@@ -916,8 +916,8 @@ LogAccess::unmarshal_http_version(char **buf, char *dest, int len)
 /*-------------------------------------------------------------------------
   LogAccess::unmarshal_http_text
 
-  The http text is simply the fields http_method (cqhm) + url (pqu) +
-  http_version (cqhv), all right next to each other, in that order.
+  The http text is a reproduced HTTP/1.x request line. It's HTTP method (cqhm) + URL (pqu) + HTTP version.
+  This doesn't support HTTP/2 and HTTP/3 since those don't have a request line.
   -------------------------------------------------------------------------*/
 
 int
