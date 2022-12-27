74939942d0bb02b6786e4344a5145c4219c6f45c
Zhengxi Li <lzx404243@hotmail.com>
2022-12-27 20:02:36 +0000

    Added test converage for caching behavior in the following areas:
    - no-store and no-cache cache-control directives in request/response
    - authorization headers
    - cookie-related request/response

--- tests/gold_tests/cache/cache-control.test.py
+++ tests/gold_tests/cache/cache-control.test.py
@@ -75,7 +75,8 @@ tr.Processes.Default.ReturnCode = 0
 tr.Processes.Default.Streams.stdout = "gold/cache_and_req_body-hit.gold"
 tr.StillRunningAfter = ts
 
-# Test 3 - response with no cache control, so cache-miss every time
+# Test 3 - response doesn't have cache control directive, so cache-miss every
+# time
 tr = Test.AddTestRun()
 tr.Processes.Default.Command = "printf 'GET /no_cache_control HTTP/1.1\r\n''x-debug: x-cache,x-cache-key,via\r\n''Host: www.example.com\r\n''\r\n'|nc 127.0.0.1 -w 1 {port}".format(
     port=ts.Variables.port)
@@ -83,15 +84,7 @@ tr.Processes.Default.ReturnCode = 0
 tr.Processes.Default.Streams.stdout = "gold/cache_no_cc.gold"
 tr.StillRunningAfter = ts
 
-# Test 4 - Cache-Control: no-cache (from client), so cache miss every time
-tr = Test.AddTestRun()
-tr.Processes.Default.Command = "printf 'GET /no_cache_control HTTP/1.1\r\n''Cache-Control:no-cache\r\n''x-debug: x-cache,x-cache-key,via\r\n''Host: www.example.com\r\n''\r\n'|nc 127.0.0.1 -w 1 {port}".format(
-    port=ts.Variables.port)
-tr.Processes.Default.ReturnCode = 0
-tr.Processes.Default.Streams.stdout = "gold/cache_no_cc.gold"
-tr.StillRunningAfter = ts
-
-# Test 5 - hit stale cache.
+# Test 4 - hit stale cache.
 tr = Test.AddTestRun()
 tr.Processes.Default.Command = "sleep 15; printf 'GET /max_age_10sec HTTP/1.1\r\n''x-debug: x-cache,x-cache-key,via\r\n''Host: www.example.com\r\n''\r\n'|nc 127.0.0.1 -w 1 {port}".format(
     port=ts.Variables.port)
@@ -99,7 +92,7 @@ tr.Processes.Default.ReturnCode = 0
 tr.Processes.Default.Streams.stdout = "gold/cache_hit_stale.gold"
 tr.StillRunningAfter = ts
 
-# Test 6 - only-if-cached. 504 "Not Cached" should be returned if not in cache
+# Test 5 - only-if-cached. 504 "Not Cached" should be returned if not in cache
 tr = Test.AddTestRun()
 tr.Processes.Default.Command = "printf 'GET /no_cache_control HTTP/1.1\r\n''Cache-Control: only-if-cached\r\n''x-debug: x-cache,x-cache-key,via\r\n''Host: www.example.com\r\n''Cache-control: max-age=300\r\n''\r\n'|nc 127.0.0.1 -w 1 {port}".format(
     port=ts.Variables.port)
@@ -133,8 +126,7 @@ tr.AddVerifierClientProcess("proxy-verifier-client", replay_file, http_ports=[ts
 
 
 #
-# Verify correct handling of cache-control no-cache and interaction with pragma header
-#
+# Verify correct interaction between cache-control no-cache and pragma header
 #
 ts = Test.MakeATSProcess("ts-cache-control-pragma")
 ts.Disk.records_config.update({
@@ -152,3 +144,197 @@ ts.Disk.remap_config.AddLine(
 tr.Processes.Default.StartBefore(server)
 tr.Processes.Default.StartBefore(ts)
 tr.StillRunningAfter = ts
+
+
+class RequestCacheControlDefaultTest:
+    # Verify the proper handling of cache-control directives in requests in
+    # default configuration
+    requestCacheControlReplayFile = "replay/request-cache-control-default.replay.yaml"
+
+    def __init__(self):
+        self.setupOriginServer()
+        self.setupTS()
+
+    def setupOriginServer(self):
+        self.server = Test.MakeVerifierServerProcess(
+            "request-cache-control-default-verifier-server",
+            self.requestCacheControlReplayFile)
+
+    def setupTS(self):
+        self.ts = Test.MakeATSProcess("ts-request-cache-control-default")
+        self.ts.Disk.records_config.update({
+            "proxy.config.diags.debug.enabled": 1,
+            "proxy.config.diags.debug.tags": "http",
+        })
+        self.ts.Disk.remap_config.AddLine(
+            f"map / http://127.0.0.1:{self.server.Variables.http_port}/",
+        )
+
+    def runTraffic(self):
+        tr = Test.AddTestRun()
+        tr.AddVerifierClientProcess(
+            "request-cache-control-default-client",
+            self.requestCacheControlReplayFile,
+            http_ports=[self.ts.Variables.port],
+            other_args='--thread-limit 1')
+        tr.Processes.Default.StartBefore(self.server)
+        tr.Processes.Default.StartBefore(self.ts)
+        tr.StillRunningAfter = self.server
+        tr.StillRunningAfter = self.ts
+
+    def run(self):
+        self.runTraffic()
+
+
+RequestCacheControlDefaultTest().run()
+
+
+class RequestCacheControlHonorClientTest:
+    # Verify the proper handling of cache-control directives in requests when
+    # ATS is configured to honor client's request to bypass the cache
+    requestCacheControlReplayFile = "replay/request-cache-control-honor-client.replay.yaml"
+
+    def __init__(self):
+        self.setupOriginServer()
+        self.setupTS()
+
+    def setupOriginServer(self):
+        self.server = Test.MakeVerifierServerProcess(
+            "request-cache-control-honor-client-verifier-server",
+            self.requestCacheControlReplayFile)
+
+    def setupTS(self):
+        self.ts = Test.MakeATSProcess("ts-request-cache-control-honor-client")
+        self.ts.Disk.records_config.update({
+            "proxy.config.diags.debug.enabled": 1,
+            "proxy.config.diags.debug.tags": "http",
+            # Configured to honor client requests to bypass the cache
+            "proxy.config.http.cache.ignore_client_no_cache": 0
+        })
+        self.ts.Disk.remap_config.AddLine(
+            f"map / http://127.0.0.1:{self.server.Variables.http_port}/",
+        )
+
+        # Verify logs for the request containing no-cache
+        self.ts.Disk.traffic_out.Content += Testers.ContainsExpression(
+            "Revalidate document with server",
+            "Verify that ATS honors the no-cache and performs a revalidation.")
+        # Verify logs for the request containing no-store
+        self.ts.Disk.traffic_out.Content += Testers.ContainsExpression(
+            "client does not permit storing, and cache control does not say to ignore client no-cache",
+            "Verify that ATS honors the no-store.")
+
+    def runTraffic(self):
+        tr = Test.AddTestRun()
+        tr.AddVerifierClientProcess(
+            "request-cache-control-honor-client-client",
+            self.requestCacheControlReplayFile,
+            http_ports=[self.ts.Variables.port],
+            other_args='--thread-limit 1')
+        tr.Processes.Default.StartBefore(self.server)
+        tr.Processes.Default.StartBefore(self.ts)
+        tr.StillRunningAfter = self.server
+        tr.StillRunningAfter = self.ts
+
+    def run(self):
+        self.runTraffic()
+
+
+RequestCacheControlHonorClientTest().run()
+
+
+class ResponseCacheControlDefaultTest:
+    # Verify the proper handling of cache-control directives in responses in
+    # default configuration
+    responseCacheControlReplayFile = "replay/response-cache-control-default.replay.yaml"
+
+    def __init__(self):
+        self.setupOriginServer()
+        self.setupTS()
+
+    def setupOriginServer(self):
+        self.server = Test.MakeVerifierServerProcess(
+            "response-cache-control-default-verifier-server",
+            self.responseCacheControlReplayFile)
+
+    def setupTS(self):
+        self.ts = Test.MakeATSProcess("ts-response-cache-control-default")
+        self.ts.Disk.records_config.update({
+            "proxy.config.diags.debug.enabled": 1,
+            "proxy.config.diags.debug.tags": "http",
+        })
+        self.ts.Disk.remap_config.AddLine(
+            f"map / http://127.0.0.1:{self.server.Variables.http_port}/",
+        )
+
+        # Verify logs for the response containing no-cache or no-store
+        self.ts.Disk.traffic_out.Content += Testers.ContainsExpression(
+            "server does not permit storing and config file does not indicate that server directive should be ignored",
+            "Verify that ATS honors the no-cache(or no-store) in response and bypasses the cache.")
+
+    def runTraffic(self):
+        tr = Test.AddTestRun()
+        tr.AddVerifierClientProcess(
+            "response-cache-control-client-default",
+            self.responseCacheControlReplayFile,
+            http_ports=[self.ts.Variables.port],
+            other_args='--thread-limit 1')
+        tr.Processes.Default.StartBefore(self.server)
+        tr.Processes.Default.StartBefore(self.ts)
+        tr.StillRunningAfter = self.server
+        tr.StillRunningAfter = self.ts
+
+    def run(self):
+        self.runTraffic()
+
+
+ResponseCacheControlDefaultTest().run()
+
+
+class ResponseCacheControlIgnoredTest:
+    # Verify the proper handling of cache-control directives in responses when
+    # ATS is configured to ignore server's request to bypass the cache
+    responseCacheControlReplayFile = "replay/response-cache-control-ignored.replay.yaml"
+
+    def __init__(self):
+        self.setupOriginServer()
+        self.setupTS()
+
+    def setupOriginServer(self):
+        self.server = Test.MakeVerifierServerProcess(
+            "response-cache-control-ignored-verifier-server",
+            self.responseCacheControlReplayFile)
+
+    def setupTS(self):
+        self.ts = Test.MakeATSProcess("ts-response-cache-control-ignored")
+        self.ts.Disk.records_config.update({
+            "proxy.config.diags.debug.enabled": 1,
+            "proxy.config.diags.debug.tags": "http",
+            "proxy.config.http.cache.ignore_server_no_cache": 1
+        })
+        self.ts.Disk.remap_config.AddLine(
+            f"map / http://127.0.0.1:{self.server.Variables.http_port}/",
+        )
+
+        # Verify logs for the response containing no-cache or no-store
+        self.ts.Disk.traffic_out.Content += Testers.ExcludesExpression(
+            "server does not permit storing and config file does not indicate that server directive should be ignored",
+            "Verify that ATS ignores the no-cache(or no-store) in response and caches the responses despite their presence.")
+
+    def runTraffic(self):
+        tr = Test.AddTestRun()
+        tr.AddVerifierClientProcess(
+            "response-cache-control-client-ignored",
+            self.responseCacheControlReplayFile,
+            http_ports=[self.ts.Variables.port],
+            other_args='--thread-limit 1')
+        tr.Processes.Default.StartBefore(self.server)
+        tr.Processes.Default.StartBefore(self.ts)
+        tr.StillRunningAfter = self.server
+        tr.StillRunningAfter = self.ts
+
+    def run(self):
+        self.runTraffic()
+
+
+ResponseCacheControlIgnoredTest().run()
---
+++ tests/gold_tests/cache/cache-cookie.test.py
@@ -0,0 +1,251 @@
+'''
+Test cookie-related caching behaviors
+'''
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+Test.Summary = '''
+Test cookie-related caching behaviors
+'''
+
+Test.ContinueOnFail = True
+
+# **testname is required**
+testName = ""
+
+
+class CookieDefaultTest:
+    # Verify the correct caching behavior when ATS is in default configuration
+    cookieDefaultReplayFile = "replay/cookie-default.replay.yaml"
+
+    def __init__(self):
+        self.setupOriginServer()
+        self.setupTS()
+
+    def setupOriginServer(self):
+        self.server = Test.MakeVerifierServerProcess(
+            "cookie-default-verifier-server",
+            self.cookieDefaultReplayFile)
+
+    def setupTS(self):
+        self.ts = Test.MakeATSProcess("ts-cookie-default")
+        self.ts.Disk.records_config.update({
+            "proxy.config.diags.debug.enabled": 1,
+            "proxy.config.diags.debug.tags": "http",
+        })
+        self.ts.Disk.remap_config.AddLine(
+            f"map / http://127.0.0.1:{self.server.Variables.http_port}/",
+        )
+
+    def runTraffic(self):
+        tr = Test.AddTestRun()
+        tr.AddVerifierClientProcess(
+            "cookie-default-client",
+            self.cookieDefaultReplayFile,
+            http_ports=[self.ts.Variables.port],
+            other_args='--thread-limit 1')
+        tr.Processes.Default.StartBefore(self.server)
+        tr.Processes.Default.StartBefore(self.ts)
+        tr.StillRunningAfter = self.server
+        tr.StillRunningAfter = self.ts
+
+    def run(self):
+        self.runTraffic()
+
+
+CookieDefaultTest().run()
+
+
+class CookieBypassTest:
+    # Verify the correct caching behavior when ATS is configured to not cache
+    # response to cookie for any content type
+    cookieBypassReplayFile = "replay/cookie-bypass-cache.replay.yaml"
+
+    def __init__(self):
+        self.setupOriginServer()
+        self.setupTS()
+
+    def setupOriginServer(self):
+        self.server = Test.MakeVerifierServerProcess(
+            "cookie-bypass-verifier-server",
+            self.cookieBypassReplayFile)
+
+    def setupTS(self):
+        self.ts = Test.MakeATSProcess("ts-cookie-bypass")
+        self.ts.Disk.records_config.update({
+            "proxy.config.diags.debug.enabled": 1,
+            "proxy.config.diags.debug.tags": "http",
+            # Bypass cache for any responses to cookies
+            "proxy.config.http.cache.cache_responses_to_cookies": 0
+        })
+        self.ts.Disk.remap_config.AddLine(
+            f"map / http://127.0.0.1:{self.server.Variables.http_port}/",
+        )
+
+    def runTraffic(self):
+        tr = Test.AddTestRun()
+        tr.AddVerifierClientProcess(
+            "cookie-bypass-client",
+            self.cookieBypassReplayFile,
+            http_ports=[self.ts.Variables.port],
+            other_args='--thread-limit 1')
+        tr.Processes.Default.StartBefore(self.server)
+        tr.Processes.Default.StartBefore(self.ts)
+        tr.StillRunningAfter = self.server
+        tr.StillRunningAfter = self.ts
+
+    def run(self):
+        self.runTraffic()
+
+
+CookieBypassTest().run()
+
+
+class CookieImgOnlyTest:
+    # Verify the correct caching behavior when ATS is configured to cache
+    # response to cookie only for image content type
+    cookieImgOnlyReplayFile = "replay/cookie-cache-img-only.replay.yaml"
+
+    def __init__(self):
+        self.setupOriginServer()
+        self.setupTS()
+
+    def setupOriginServer(self):
+        self.server = Test.MakeVerifierServerProcess(
+            "cookie-img-only-verifier-server",
+            self.cookieImgOnlyReplayFile)
+
+    def setupTS(self):
+        self.ts = Test.MakeATSProcess("ts-cookie-img-only")
+        self.ts.Disk.records_config.update({
+            "proxy.config.diags.debug.enabled": 1,
+            "proxy.config.diags.debug.tags": "http",
+            # Cache only for image types
+            "proxy.config.http.cache.cache_responses_to_cookies": 2
+        })
+        self.ts.Disk.remap_config.AddLine(
+            f"map / http://127.0.0.1:{self.server.Variables.http_port}/",
+        )
+
+    def runTraffic(self):
+        tr = Test.AddTestRun()
+        tr.AddVerifierClientProcess(
+            "cookie-img-only-client",
+            self.cookieImgOnlyReplayFile,
+            http_ports=[self.ts.Variables.port],
+            other_args='--thread-limit 1')
+        tr.Processes.Default.StartBefore(self.server)
+        tr.Processes.Default.StartBefore(self.ts)
+        tr.StillRunningAfter = self.server
+        tr.StillRunningAfter = self.ts
+
+    def run(self):
+        self.runTraffic()
+
+
+CookieImgOnlyTest().run()
+
+
+class CookieAllButTextTest:
+    # Verify the correct caching behavior when ATS is configured to cache
+    # response to cookie for all but text types
+    cookieAllButTextReplayFile = "replay/cookie-all-but-text.replay.yaml"
+
+    def __init__(self):
+        self.setupOriginServer()
+        self.setupTS()
+
+    def setupOriginServer(self):
+        self.server = Test.MakeVerifierServerProcess(
+            "cookie-all-but-text-verifier-server",
+            self.cookieAllButTextReplayFile)
+
+    def setupTS(self):
+        self.ts = Test.MakeATSProcess("ts-cookie-all-but-text")
+        self.ts.Disk.records_config.update({
+            "proxy.config.diags.debug.enabled": 1,
+            "proxy.config.diags.debug.tags": "http",
+            # Cache all content type except text
+            "proxy.config.http.cache.cache_responses_to_cookies": 3
+        })
+        self.ts.Disk.remap_config.AddLine(
+            f"map / http://127.0.0.1:{self.server.Variables.http_port}/",
+        )
+
+    def runTraffic(self):
+        tr = Test.AddTestRun()
+        tr.AddVerifierClientProcess(
+            "cookie-all-but-text-client",
+            self.cookieAllButTextReplayFile,
+            http_ports=[self.ts.Variables.port],
+            other_args='--thread-limit 1')
+        tr.Processes.Default.StartBefore(self.server)
+        tr.Processes.Default.StartBefore(self.ts)
+        tr.StillRunningAfter = self.server
+        tr.StillRunningAfter = self.ts
+
+    def run(self):
+        self.runTraffic()
+
+
+CookieAllButTextTest().run()
+
+
+class CookieAllButTextWithExcpTest:
+    # Verify the correct caching behavior when ATS is configured to cache all
+    # content types but text, but with a few exceptions for text types which
+    # would also be cached
+    cookieAllButTextReplayFile = "replay/cookie-all-but-text-with-excp.replay.yaml"
+
+    def __init__(self):
+        self.setupOriginServer()
+        self.setupTS()
+
+    def setupOriginServer(self):
+        self.server = Test.MakeVerifierServerProcess(
+            "cookie-all-but-text-with-excp-verifier-server",
+            self.cookieAllButTextReplayFile)
+
+    def setupTS(self):
+        self.ts = Test.MakeATSProcess("ts-cookie-all-but-text-with-excp")
+        self.ts.Disk.records_config.update({
+            "proxy.config.diags.debug.enabled": 1,
+            "proxy.config.diags.debug.tags": "http",
+            # Cache all content type but text. Text type also gets cached for
+            # server responses without Set-Cookie or with Cache-Control: public
+            "proxy.config.http.cache.cache_responses_to_cookies": 4
+        })
+        self.ts.Disk.remap_config.AddLine(
+            f"map / http://127.0.0.1:{self.server.Variables.http_port}/",
+        )
+
+    def runTraffic(self):
+        tr = Test.AddTestRun()
+        tr.AddVerifierClientProcess(
+            "cookie-all-but-text-with-excp-client",
+            self.cookieAllButTextReplayFile,
+            http_ports=[self.ts.Variables.port],
+            other_args='--thread-limit 1')
+        tr.Processes.Default.StartBefore(self.server)
+        tr.Processes.Default.StartBefore(self.ts)
+        tr.StillRunningAfter = self.server
+        tr.StillRunningAfter = self.ts
+
+    def run(self):
+        self.runTraffic()
+
+
+CookieAllButTextWithExcpTest().run()
