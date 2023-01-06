ae05b17cbf4f0a1614e463be13e66b722fc7efb9
lzx404243 <lzx404243@hotmail.com>
2023-01-06 14:25:42 -0500

    Updated documentation and added test coverage for some caching behavior  (#9282)
    * Added test converage for caching behavior in the following areas:
    
    - no-store and no-cache cache-control directives in request/response
    - authorization headers
    - cookie-related request/response
    
    * update documentation for certain cache behaviors with respect to 
      no-cache and no-store directives.

--- doc/admin-guide/files/records.config.en.rst
+++ doc/admin-guide/files/records.config.en.rst
@@ -2186,7 +2186,7 @@ Cache Control
    :reloadable:
    :overridable:
 
-   When enabled (``1``), |TS| ignores client requests to bypass the cache.
+   When enabled (``1``), |TS| ignores client requests to bypass the cache. Specifically, ``Pragma: no-cache``, ``Cache-Control: no-cache`` and ``Cache-Control: no-store`` in requests are ignored.
 
 .. ts:cv:: CONFIG proxy.config.http.cache.ims_on_client_no_cache INT 1
    :reloadable:
@@ -2198,7 +2198,7 @@ Cache Control
    :reloadable:
    :overridable:
 
-   When enabled (``1``), |TS| ignores origin server requests to bypass the cache.
+   When enabled (``1``), |TS| ignores origin server requests to bypass the cache. Specifically, ``Pragma: no-cache``, ``Cache-Control: no-cache`` and ``Cache-Control: no-store`` in responses are ignored.
 
 .. ts:cv:: CONFIG proxy.config.http.cache.cache_responses_to_cookies INT 1
    :reloadable:
@@ -2220,8 +2220,8 @@ Cache Control
 .. ts:cv:: CONFIG proxy.config.http.cache.ignore_authentication INT 0
    :overridable:
 
-   When enabled (``1``), |TS| ignores ``WWW-Authentication`` headers in responses ``WWW-Authentication`` headers are removed and
-   not cached.
+   When enabled (``1``), |TS| ignores ``WWW-Authentication`` headers in
+   responses and the responses are cached.
 
 .. ts:cv:: CONFIG proxy.config.http.cache.cache_urls_that_look_dynamic INT 1
    :reloadable:
---
+++ tests/gold_tests/cache/cache-auth.test.py
@@ -0,0 +1,123 @@
+'''
+Test authorization-related caching behaviors
+'''
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+Test.Summary = '''
+Test authorization-related caching behaviors
+'''
+
+Test.ContinueOnFail = True
+
+# **testname is required**
+testName = ""
+
+
+class AuthDefaultTest:
+    # Verify the proper caching behavior for request/response containing
+    # auth-related fields when ATS is in default configuration
+    authDefaultReplayFile = "replay/auth-default.replay.yaml"
+
+    def __init__(self):
+        self.setupOriginServer()
+        self.setupTS()
+
+    def setupOriginServer(self):
+        self.server = Test.MakeVerifierServerProcess(
+            "auth-default-verifier-server",
+            self.authDefaultReplayFile)
+
+    def setupTS(self):
+        self.ts = Test.MakeATSProcess("ts-auth-default")
+        self.ts.Disk.records_config.update({
+            "proxy.config.diags.debug.enabled": 1,
+            "proxy.config.diags.debug.tags": "http",
+        })
+        self.ts.Disk.remap_config.AddLine(
+            f"map / http://127.0.0.1:{self.server.Variables.http_port}/",
+        )
+
+        # Verify log for skipping the WWW-Authenticate response
+        self.ts.Disk.traffic_out.Content += Testers.ContainsExpression(
+            "response has WWW-Authenticate, response is not cacheable",
+            "Verify ATS doesn't store the response with WWW-Authenticate.")
+
+    def runTraffic(self):
+        tr = Test.AddTestRun(
+            "Verify the proper caching behavior for request/response containing auth-related fields when ATS is in default configuration")
+        tr.AddVerifierClientProcess(
+            "auth-default-client",
+            self.authDefaultReplayFile,
+            http_ports=[self.ts.Variables.port],
+            other_args='--thread-limit 1')
+        tr.Processes.Default.StartBefore(self.server)
+        tr.Processes.Default.StartBefore(self.ts)
+        tr.StillRunningAfter = self.server
+        tr.StillRunningAfter = self.ts
+
+    def run(self):
+        self.runTraffic()
+
+
+AuthDefaultTest().run()
+
+
+class AuthIgnoredTest:
+    # Verify the proper caching behavior for request/response containing
+    # auth-related fields when ATS is configured to bypass caching for those
+    authIgnoredReplayFile = "replay/auth-ignored.replay.yaml"
+
+    def __init__(self):
+        self.setupOriginServer()
+        self.setupTS()
+
+    def setupOriginServer(self):
+        self.server = Test.MakeVerifierServerProcess(
+            "auth-ignored-verifier-server",
+            self.authIgnoredReplayFile)
+
+    def setupTS(self):
+        self.ts = Test.MakeATSProcess("ts-auth-ignored")
+        self.ts.Disk.records_config.update({
+            "proxy.config.diags.debug.enabled": 1,
+            "proxy.config.diags.debug.tags": "http",
+            # Configure ATS to ignore the WWW-Authenticate header in
+            # response(allow caching of such response)
+            "proxy.config.http.cache.ignore_authentication": 1
+        })
+        self.ts.Disk.remap_config.AddLine(
+            f"map / http://127.0.0.1:{self.server.Variables.http_port}/",
+        )
+
+    def runTraffic(self):
+        tr = Test.AddTestRun(
+            "Verify the proper caching behavior for request/response containing auth-related fields when ATS is configured to bypass caching for those")
+        tr.AddVerifierClientProcess(
+            "auth-ignored-client",
+            self.authIgnoredReplayFile,
+            http_ports=[self.ts.Variables.port],
+            other_args='--thread-limit 1')
+        tr.Processes.Default.StartBefore(self.server)
+        tr.Processes.Default.StartBefore(self.ts)
+        tr.StillRunningAfter = self.server
+        tr.StillRunningAfter = self.ts
+
+    def run(self):
+        self.runTraffic()
+
+
+AuthIgnoredTest().run()
