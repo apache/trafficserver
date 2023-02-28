2c1c6d2632f147e6858bc758ba7c4ffbc163dc65
Masakazu Kitajo <maskit@apache.org>
2023-02-28 13:34:04 -0700

    Use deprecated OpenSSL APIs for MD5 and SHA256 if available (#9469)

--- include/tscore/MD5.h
+++ include/tscore/MD5.h
@@ -25,29 +25,69 @@
 
 #include "tscore/ink_defs.h"
 #include "tscore/CryptoHash.h"
+#if HAVE_MD5_INIT
+#include <openssl/md5.h>
+#else
 #include <openssl/evp.h>
+#endif
 
 class MD5Context : public ats::CryptoContextBase
 {
 public:
   MD5Context()
   {
+#if HAVE_MD5_INIT
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+    MD5_Init(&_md5ctx);
+#pragma GCC diagnostic pop
+#else
     _ctx = EVP_MD_CTX_new();
     EVP_DigestInit_ex(_ctx, EVP_md5(), nullptr);
+#endif
   }
-  ~MD5Context() { EVP_MD_CTX_free(_ctx); }
+  ~MD5Context()
+  {
+#if HAVE_MD5_INIT
+    // _md5ctx does not need to be freed
+#else
+    EVP_MD_CTX_free(_ctx);
+#endif
+  }
   /// Update the hash with @a data of @a length bytes.
   bool
   update(void const *data, int length) override
   {
+#if HAVE_MD5_INIT
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+    return MD5_Update(&_md5ctx, data, length);
+#pragma GCC diagnostic pop
+#else
     return EVP_DigestUpdate(_ctx, data, length);
+#endif
   }
   /// Finalize and extract the @a hash.
   bool
   finalize(CryptoHash &hash) override
   {
+#if HAVE_MD5_INIT
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+    return MD5_Final(hash.u8, &_md5ctx);
+#pragma GCC diagnostic pop
+#else
     return EVP_DigestFinal_ex(_ctx, hash.u8, nullptr);
+#endif
   }
+
+#if HAVE_MD5_INIT
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+private:
+  MD5_CTX _md5ctx;
+#pragma GCC diagnostic pop
+#endif
 };
 
 typedef CryptoHash INK_MD5;
