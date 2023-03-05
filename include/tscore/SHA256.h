b83f535817373339d49b3a10a20fc587443ddc98
Masakazu Kitajo <maskit@apache.org>
2023-03-05 10:26:28 -0700

    Use deprecated OpenSSL APIs for MD5 and SHA256 if available (#9469) (#9473)
    * Use deprecated OpenSSL APIs for MD5 and SHA256 if available (#9469)
    
    (cherry picked from commit 2c1c6d2632f147e6858bc758ba7c4ffbc163dc65)
    
     Conflicts:
    	include/tscore/INK_MD5.h
    	include/tscore/SHA256.h
    
    * Fix compile errors

--- include/tscore/SHA256.h
+++ include/tscore/SHA256.h
@@ -26,30 +26,68 @@
 #include "tscore/ink_code.h"
 #include "tscore/ink_defs.h"
 #include "tscore/CryptoHash.h"
+#if HAVE_SHA256_INIT
+#include <openssl/sha.h>
+#else
 #include <openssl/evp.h>
+#endif
 
 class SHA256Context : public ats::CryptoContextBase
 {
+#ifndef HAVE_SHA256_INIT
 protected:
   EVP_MD_CTX *ctx;
+#endif
 
 public:
   SHA256Context()
   {
-    ctx = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(ctx, EVP_sha256(), nullptr);
+#if HAVE_SHA256_INIT
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+    SHA256_Init(&_sha256ctx);
+#pragma GCC diagnostic pop
+#else
+    _ctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(_ctx, EVP_sha256(), nullptr);
+#endif
   }
-  ~SHA256Context() { EVP_MD_CTX_free(ctx); }
+  ~SHA256Context()
+  {
+#if HAVE_SHA256_INIT
+    // _sha256ctx does not need to be freed
+#else
+    EVP_MD_CTX_free(_ctx);
+#endif
+  }
   /// Update the hash with @a data of @a length bytes.
   bool
   update(void const *data, int length) override
   {
-    return EVP_DigestUpdate(ctx, data, length);
+#if HAVE_SHA256_INIT
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+    return SHA256_Update(&_sha256ctx, data, length);
+#pragma GCC diagnostic pop
+#else
+    return EVP_DigestUpdate(_ctx, data, length);
+#endif
   }
   /// Finalize and extract the @a hash.
   bool
   finalize(CryptoHash &hash) override
   {
-    return EVP_DigestFinal_ex(ctx, hash.u8, nullptr);
+#if HAVE_SHA256_INIT
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+    return SHA256_Final(hash.u8, &_sha256ctx);
+#pragma GCC diagnostic pop
+#else
+    return EVP_DigestFinal_ex(_ctx, hash.u8, nullptr);
+#endif
   }
+#if HAVE_SHA256_INIT
+private:
+  SHA256_CTX _sha256ctx;
+#endif
 };
