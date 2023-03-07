2aa635d262bf80de373a1fc4851085ca4b023e67
Alan M. Carroll <amc@apache.org>
2023-03-07 13:41:54 -0600

    libswoc: Replace ts::IpAddrSet with swoc::IPRangeSet (#9507)

--- plugins/experimental/maxmind_acl/mmdb.h
+++ plugins/experimental/maxmind_acl/mmdb.h
@@ -36,7 +36,7 @@
 #include <unistd.h>
 #include <iterator>
 #include <maxminddb.h>
-#include "tscpp/util/ts_ip.h"
+#include "swoc/swoc_ip.h"
 
 #ifdef HAVE_PCRE_PCRE_H
 #include <pcre/pcre.h>
@@ -91,8 +91,8 @@ protected:
   std::unordered_map<std::string, std::vector<plugin_regex>> allow_regex;
   std::unordered_map<std::string, std::vector<plugin_regex>> deny_regex;
 
-  ts::IPAddrSet allow_ip_map;
-  ts::IPAddrSet deny_ip_map;
+  swoc::IPRangeSet allow_ip_map;
+  swoc::IPRangeSet deny_ip_map;
 
   // Anonymous blocking default to off
   bool _anonymous_ip      = false;
