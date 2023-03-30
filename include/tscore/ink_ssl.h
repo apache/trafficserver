710e9d7c86f476e3022c6e95aefa523638abc7d5
Masakazu Kitajo <maskit@apache.org>
2023-03-30 09:34:50 -0600

    Define BIO macros in ink_ssl.h (#9557)
    * Define BIO macros in ink_ssl.h
    
    * Add license header

---
+++ include/tscore/ink_ssl.h
@@ -0,0 +1,54 @@
+/** @file
+
+  A brief file description
+
+  @section license License
+
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+#pragma once
+
+#if HAVE_BIO_SET_DATA == 0
+#define HAVE_BIO_SET_DATA     1
+#define BIO_set_data(a, _ptr) ((a)->ptr = (_ptr))
+#endif
+
+#if HAVE_BIO_GET_DATA == 0
+#define HAVE_BIO_GET_DATA 1
+#define BIO_get_data(a)   ((a)->ptr)
+#endif
+
+#if HAVE_BIO_GET_SHUTDOWN == 0
+#define HAVE_BIO_GET_SHUTDOWN 1
+#define BIO_get_shutdown(a)   ((a)->shutdown)
+#endif
+
+#if HAVE_BIO_METH_GET_CTRL == 0
+#define HAVE_BIO_METH_GET_CTRL  1
+#define BIO_meth_get_ctrl(biom) ((biom)->ctrl)
+#endif
+
+#if HAVE_BIO_METH_GET_CREATE == 0
+#define HAVE_BIO_METH_GET_CREATE  1
+#define BIO_meth_get_create(biom) ((biom)->create)
+#endif
+
+#if HAVE_BIO_METH_GET_DESTROY == 0
+#define HAVE_BIO_METH_GET_DESTROY  1
+#define BIO_meth_get_destroy(biom) ((biom)->destroy)
+#endif
