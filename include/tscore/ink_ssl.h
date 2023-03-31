afa6e272c38a8a6ce04ed22bdf73c113b08900d9
Chris McFarlen <cmcfarlen@apple.com>
2023-03-31 10:30:36 -0500

    more fixes

--- include/tscore/ink_ssl.h
+++ include/tscore/ink_ssl.h
@@ -23,32 +23,34 @@
 
 #pragma once
 
-#if HAVE_BIO_SET_DATA == 0
+#include "tscore/ink_config.h"
+
+#ifndef HAVE_BIO_SET_DATA
 #define HAVE_BIO_SET_DATA     1
 #define BIO_set_data(a, _ptr) ((a)->ptr = (_ptr))
 #endif
 
-#if HAVE_BIO_GET_DATA == 0
+#ifndef HAVE_BIO_GET_DATA
 #define HAVE_BIO_GET_DATA 1
 #define BIO_get_data(a)   ((a)->ptr)
 #endif
 
-#if HAVE_BIO_GET_SHUTDOWN == 0
+#ifndef HAVE_BIO_GET_SHUTDOWN
 #define HAVE_BIO_GET_SHUTDOWN 1
 #define BIO_get_shutdown(a)   ((a)->shutdown)
 #endif
 
-#if HAVE_BIO_METH_GET_CTRL == 0
+#ifndef HAVE_BIO_METH_GET_CTRL
 #define HAVE_BIO_METH_GET_CTRL  1
 #define BIO_meth_get_ctrl(biom) ((biom)->ctrl)
 #endif
 
-#if HAVE_BIO_METH_GET_CREATE == 0
+#ifndef HAVE_BIO_METH_GET_CREATE
 #define HAVE_BIO_METH_GET_CREATE  1
 #define BIO_meth_get_create(biom) ((biom)->create)
 #endif
 
-#if HAVE_BIO_METH_GET_DESTROY == 0
+#ifndef HAVE_BIO_METH_GET_DESTROY
 #define HAVE_BIO_METH_GET_DESTROY  1
 #define BIO_meth_get_destroy(biom) ((biom)->destroy)
 #endif
