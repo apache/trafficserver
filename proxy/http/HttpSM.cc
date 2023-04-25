20955fd348535cc2321bddefa9f6bc48d1a6873d
Brian Olsen <bolsen149@comcast.com>
2023-04-25 15:47:21 +0000

    account for dns_info member variable name change

--- proxy/http/HttpSM.cc
+++ proxy/http/HttpSM.cc
@@ -4265,7 +4265,7 @@ HttpSM::do_hostdb_lookup()
 
   // If directed to not look up fqdns then mark as resolved
   if (t_state.http_config_param->no_dns_forward_to_parent && t_state.parent_result.result == PARENT_UNDEFINED) {
-    t_state.dns_info.resolved_p = true;
+    t_state.dns_info.lookup_success = true;
     call_transact_and_set_next_state(nullptr);
     return;
   } else if (t_state.txn_conf->srv_enabled) {
