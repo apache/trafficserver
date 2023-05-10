c5b935cc04550923c1577ef1e3e491dec2ce5866
Masakazu Kitajo <maskit@apache.org>
2023-05-11 01:15:08 +0900

    Add error log for invalid OCSP response (#9674)
    * Add error log for invalid OCSP response
    
    * Check times only if status is available

--- iocore/net/OCSPStapling.cc
+++ iocore/net/OCSPStapling.cc
@@ -958,7 +958,10 @@ stapling_check_response(certinfo *cinf, TS_OCSP_RESPONSE *rsp)
     // If ID not present just pass it back to client
     Error("stapling_check_response: certificate ID not present in response for %s", cinf->certname);
   } else {
-    TS_OCSP_check_validity(thisupd, nextupd, 300, -1);
+    if (!TS_OCSP_check_validity(thisupd, nextupd, 300, -1)) {
+      // The check is just for logging and pass the response back to client anyway
+      Error("stapling_check_response: status in response for %s is not valid already/yet", cinf->certname);
+    }
   }
 
   switch (status) {
