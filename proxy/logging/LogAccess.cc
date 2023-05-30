2c84be4887df7972c6741f88fb6366c86bc81808
Brian Olsen <brian_olsen2@comcast.com>
2023-05-30 06:36:44 -0600

    Add ssrc and surc log fields for server simple/unavail retry counts. (#9694)
    * Add psrc and psuc log fields for parent simple/unavail retry counts.
    
    * rename from 'parent' to 'server
    
    ---------
    
    Co-authored-by: Brian Olsen <bolsen149@comcast.com>

--- proxy/logging/LogAccess.cc
+++ proxy/logging/LogAccess.cc
@@ -2630,6 +2630,32 @@ LogAccess::marshal_server_transact_count(char *buf)
   -------------------------------------------------------------------------*/
 
 int
+LogAccess::marshal_server_simple_retry_count(char *buf)
+{
+  if (buf) {
+    const int64_t attempts = m_http_sm->t_state.current.simple_retry_attempts;
+    marshal_int(buf, attempts);
+  }
+  return INK_MIN_ALIGN;
+}
+
+/*-------------------------------------------------------------------------
+  -------------------------------------------------------------------------*/
+
+int
+LogAccess::marshal_server_unavailable_retry_count(char *buf)
+{
+  if (buf) {
+    const int64_t attempts = m_http_sm->t_state.current.unavailable_server_retry_attempts;
+    marshal_int(buf, attempts);
+  }
+  return INK_MIN_ALIGN;
+}
+
+/*-------------------------------------------------------------------------
+  -------------------------------------------------------------------------*/
+
+int
 LogAccess::marshal_server_connect_attempts(char *buf)
 {
   if (buf) {
