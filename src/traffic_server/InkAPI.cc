274b8aad4df14ba08774b94fbc0b1274f4295ddb
Leif Hedstrom <zwoop@apache.org>
2023-06-09 11:49:16 -0600

    Removes the TOS APIs

--- src/traffic_server/InkAPI.cc
+++ src/traffic_server/InkAPI.cc
@@ -5982,46 +5982,6 @@ TSHttpTxnServerPacketMarkSet(TSHttpTxn txnp, int mark)
 }
 
 TSReturnCode
-TSHttpTxnClientPacketTosSet(TSHttpTxn txnp, int tos)
-{
-  sdk_assert(sdk_sanity_check_txn(txnp) == TS_SUCCESS);
-  HttpSM *sm = (HttpSM *)txnp;
-  if (nullptr == sm->ua_txn) {
-    return TS_ERROR;
-  }
-
-  NetVConnection *vc = sm->ua_txn->get_netvc();
-  if (nullptr == vc) {
-    return TS_ERROR;
-  }
-
-  vc->options.packet_tos = (uint32_t)tos;
-  vc->apply_options();
-  return TS_SUCCESS;
-}
-
-TSReturnCode
-TSHttpTxnServerPacketTosSet(TSHttpTxn txnp, int tos)
-{
-  sdk_assert(sdk_sanity_check_txn(txnp) == TS_SUCCESS);
-  HttpSM *sm = (HttpSM *)txnp;
-
-  // change the tos on an active server session
-  ProxyTransaction *ssn = sm->get_server_txn();
-  if (nullptr != ssn) {
-    NetVConnection *vc = ssn->get_netvc();
-    if (vc != nullptr) {
-      vc->options.packet_tos = (uint32_t)tos;
-      vc->apply_options();
-    }
-  }
-
-  // update the transactions mark config for future connections
-  TSHttpTxnConfigIntSet(txnp, TS_CONFIG_NET_SOCK_PACKET_TOS_OUT, tos);
-  return TS_SUCCESS;
-}
-
-TSReturnCode
 TSHttpTxnClientPacketDscpSet(TSHttpTxn txnp, int dscp)
 {
   sdk_assert(sdk_sanity_check_txn(txnp) == TS_SUCCESS);
