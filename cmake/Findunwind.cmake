cbdefcda3438afd6cff61e2cbae84c0cda508b20
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2023-06-16 06:17:32 -0500

    Add support for libunwind in CMake build
    This adds an option that defaults to ON: ENABLE_UNWIND. On Linux,
    CMake will look for the library and link it globally if it is found.
    On other OSs, CMake will skip the check, because libunwind is only
    available on Linux.

---
+++ cmake/Findunwind.cmake
@@ -0,0 +1,49 @@
+#######################
+#
+#  Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+#  agreements.  See the NOTICE file distributed with this work for additional information regarding
+#  copyright ownership.  The ASF licenses this file to you under the Apache License, Version 2.0
+#  (the "License"); you may not use this file except in compliance with the License.  You may obtain
+#  a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software distributed under the License
+#  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#  or implied. See the License for the specific language governing permissions and limitations under
+#  the License.
+#
+#######################
+
+# Findunwind.cmake
+#
+# This will define the following variables
+#
+#     unwind_FOUND
+#     unwind_LIBRARY
+#     unwind_INCLUDE_DIRS
+#
+# and the following imported targets
+#
+#     unwind::unwind
+#
+
+find_library(unwind_LIBRARY NAMES unwind)
+find_path(unwind_INCLUDE_DIR NAMES unwind.h)
+
+mark_as_advanced(unwind_FOUND unwind_LIBRARY unwind_INCLUDE_DIR)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(unwind
+    REQUIRED_VARS unwind_LIBRARY unwind_INCLUDE_DIR
+)
+
+if(unwind_FOUND)
+    set(unwind_INCLUDE_DIRS ${unwind_INCLUDE_DIR})
+endif()
+
+if(unwind_FOUND AND NOT TARGET unwind::unwind)
+    add_library(unwind::unwind INTERFACE IMPORTED)
+    target_include_directories(unwind::unwind INTERFACE ${unwind_INCLUDE_DIRS})
+    target_link_libraries(unwind::unwind INTERFACE "${unwind_LIBRARY}")
+endif()
