e6d2fb1235d351de2d8d8ac94757932778857163
midchildan <git@midchildan.org>
2023-07-01 11:41:38 +0900

    Fix autest for OpenSSL 3.0 (#9753)
    * Fix forward-non-http autest for OpenSSL 3.0
    
    * Fix tls-verify-override autest for OpenSSL 3.0
    
    * Fix tls_client_versions autest with OpenSSL 3.0
    
    * Update test-nc-s_client.sh
    
    Added an EOF to test-nc-s_client.sh

--- tests/gold_tests/tls/test-nc-s_client.sh
+++ tests/gold_tests/tls/test-nc-s_client.sh
@@ -16,11 +16,12 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+
 # See https://github.com/apache/trafficserver/issues/9880
 ignore_unexpecte_eof=''
 if openssl s_client --help 2>&1 | grep -q ignore_unexpected_eof
 then
   ignore_unexpected_eof='-ignore_unexpected_eof'
 fi
-nc -l -p $1 -c 'echo -e "This is a reply"' -o test.out &
-echo "This is a test" | openssl s_client -servername bar.com -connect localhost:$2 -ign_eof ${ignore_unexpected_eof}
+nc -l -p "$1" -c 'echo -e "This is a reply"' -o test.out &
+echo "This is a test" | openssl s_client -servername bar.com -connect "localhost:$2" -ign_eof ${ignore_unexpected_eof} "${@:3}"
