5915fb2a59c13529d896ddbfe65f9f4abaf07d0d
Brian Neradt <brian.neradt@gmail.com>
2023-07-26 19:04:58 +0000

    build_h3_tools: use openssl-3.1
    OpenSSL 3.1 has multiple performance improvements over 3.0.

--- tools/build_h3_tools.sh
+++ tools/build_h3_tools.sh
@@ -28,7 +28,7 @@ cd "${WORKDIR}"
 echo "Building H3 dependencies in ${WORKDIR} ..."
 
 # Update this as the draft we support updates.
-OPENSSL_BRANCH=${OPENSSL_BRANCH:-"openssl-3.0.9+quic"}
+OPENSSL_BRANCH=${OPENSSL_BRANCH:-"openssl-3.1.0+quic+locks"}
 
 # Set these, if desired, to change these to your preferred installation
 # directory
