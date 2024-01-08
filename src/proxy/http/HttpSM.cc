apache/trafficserver

commit 1894a4756af0ca9a0e0a2da8496930bf3431f0a8

Pull out down origin check from HttpSM::apply_ip_allow_filter (#10831)
In a refactor on master for the ip_allow implementation in HttpSM
(see #9845) put into HttpSM::apply_ip_allow_filter. However, the down
origin feature is orthogonal to ip_allow and it is confusing to
implement its feature in a function that communicates that it implements
ip_allow filtering. This separates the down origin check from that
function like it was before the refactor.

src/proxy/http/HttpSM.cc
@@ -5108,20 +5108,14 @@ HttpSM::get_outbound_sni() const
 bool
 HttpSM::apply_ip_allow_filter()
 {
-  bool result{true};
   // Method allowed on dest IP address check
   IpAllow::ACL acl = IpAllow::match(this->get_server_remote_addr(), IpAllow::DST_ADDR);
 
   if (ip_allow_is_request_forbidden(acl)) {
     ip_allow_deny_request(acl);
-    result = false;
-  } else if (HttpTransact::is_server_negative_cached(&t_state) == true &&
-             t_state.txn_conf->connect_attempts_max_retries_down_server <= 0) {
-    call_transact_and_set_next_state(HttpTransact::OriginDown);
-    result = false;
+    return false;
   }
-
-  return result;
+  return true;
 }
 
 bool
@@ -5276,6 +5270,12 @@ HttpSM::do_http_server_open(bool raw, bool only_direct)
     if (!apply_ip_allow_filter()) {
       return;
     }
+  }
+  if (HttpTransact::is_server_negative_cached(&t_state) == true &&
+      t_state.txn_conf->connect_attempts_max_retries_down_server <= 0) {
+    SMDebug("http_seq", "Not connecting to the server because it is marked down.");
+    call_transact_and_set_next_state(HttpTransact::OriginDown);
+    return;
   }
 
   // Check for self loop.

commit 4773b145e6fcc3f31670e554948ad5129f64bd6f

HttpSM.cc::do_http_server_open SMDebug -> SMDbg
Fixes an old reference to SMDebug that got merged in from an old PR.
The incorrect SMDebug was introduced in #10831.

src/proxy/http/HttpSM.cc
@@ -5273,7 +5273,7 @@ HttpSM::do_http_server_open(bool raw, bool only_direct)
   }
   if (HttpTransact::is_server_negative_cached(&t_state) == true &&
       t_state.txn_conf->connect_attempts_max_retries_down_server <= 0) {
-    SMDebug("http_seq", "Not connecting to the server because it is marked down.");
+    SMDbg(dbg_ctl_http_seq, "Not connecting to the server because it is marked down.");
     call_transact_and_set_next_state(HttpTransact::OriginDown);
     return;
   }