694a7d2f865d191909e401f963bb1499b9430475
Chris McFarlen <chris@mcfarlen.us>
2024-02-07 20:13:35 -0600

    add compile options for imagemagick

--- cmake/magick_target.cmake
+++ cmake/magick_target.cmake
@@ -19,4 +19,9 @@ if(ImageMagick_Magick++_FOUND AND NOT TARGET ImageMagick::Magick++)
   add_library(ImageMagick::Magick++ INTERFACE IMPORTED)
   target_include_directories(ImageMagick::Magick++ INTERFACE ${ImageMagick_Magick++_INCLUDE_DIRS})
   target_link_libraries(ImageMagick::Magick++ INTERFACE ${ImageMagick_Magick++_LIBRARIES})
+  # got these values from running:
+  # $ /usr/lib/x86_64-linux-gnu/ImageMagick-6.9.11/bin-q16/Magick++-config --cxxflags
+  # -I/usr/include/x86_64-linux-gnu/ImageMagick-6 -I/usr/include/ImageMagick-6 -fopenmp -DMAGICKCORE_HDRI_ENABLE=0 -DMAGICKCORE_QUANTUM_DEPTH=16
+  target_compile_options(ImageMagick::Magick++ INTERFACE -fopenmp)
+  target_compile_definitions(ImageMagick::Magick++ INTERFACE MAGICKCORE_HDRI_ENABLE=0 MAGICKCORE_QUANTUM_DEPTH=16)
 endif()
