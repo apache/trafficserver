d733ed0eeae789193f7fb597443e1e81b987b7f6
Simon Knott <info@simonknott.de>
2024-03-11 23:27:05 +0100

    [Fuzzing] Add fuzzing harness for `HTTP3Frame` (#11128)
    * Fuzz Http3Frame
    
    Co-Authored-By: Yannis Hofmann <yannis.hofmann@student.hpi.de>
    
    * format
    
    ---------
    
    Co-authored-by: Yannis Hofmann <yannis.hofmann@student.hpi.de>

---
+++ tests/fuzzing/fuzz_http3frame.cc
@@ -0,0 +1,59 @@
+/** @file
+
+   fuzzing proxy/http3frame
+
+   @section license License
+
+   Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.
+   See the NOTICE file distributed with this work for additional information regarding copyright
+   ownership.  The ASF licenses this file to you under the Apache License, Version 2.0 (the
+   "License"); you may not use this file except in compliance with the License.  You may obtain a
+   copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software distributed under the License
+   is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+   or implied. See the License for the specific language governing permissions and limitations under
+   the License.
+*/
+
+#include "proxy/http3/Http3Frame.h"
+#include "proxy/http3/Http3Config.h"
+
+#include "records/RecordsConfig.h"
+#include "tscore/Layout.h"
+
+#define kMinInputLength 8
+#define kMaxInputLength 1024
+
+#define TEST_THREADS 1
+
+bool
+DoInitialization()
+{
+  Layout::create();
+  RecProcessInit();
+  LibRecordsConfigInit();
+
+  ink_event_system_init(EVENT_SYSTEM_MODULE_PUBLIC_VERSION);
+  eventProcessor.start(TEST_THREADS);
+  ts::Http3Config::startup();
+
+  return true;
+}
+
+extern "C" int
+LLVMFuzzerTestOneInput(const uint8_t *input_data, size_t size_data)
+{
+  if (size_data < kMinInputLength || size_data > kMaxInputLength) {
+    return 1;
+  }
+
+  static bool Initialized = DoInitialization();
+
+  Http3FrameFactory frame_factory;
+  frame_factory.fast_create(input_data, size_data);
+
+  return 0;
+}
