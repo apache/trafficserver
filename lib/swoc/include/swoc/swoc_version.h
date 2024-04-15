3fecba4bdc9c2b7d70282d991754ee7506423cc7
Brian Neradt <brian.neradt@gmail.com>
2024-04-15 14:15:55 -0500

    libswoc: version update to 1.5.12 (#11256)
    I cut a 1.5.12 trafficserver-libswoc release that brings the latter up
    to the source code that ATS lib/swoc has. This updates the version of
    our ATS libswoc to reference the trafficserver-libswoc release version.
    
    Note that this is a version-only update. No production changes come with
    this patch.
    
    (cherry picked from commit a356e443aae64b55cbd69bbe0e6a02ed912a6a6b)

--- lib/swoc/include/swoc/swoc_version.h
+++ lib/swoc/include/swoc/swoc_version.h
@@ -23,11 +23,11 @@
 #pragma once
 
 #if !defined(SWOC_VERSION_NS)
-#define SWOC_VERSION_NS _1_5_11
+#define SWOC_VERSION_NS _1_5_12
 #endif
 
 namespace swoc { inline namespace SWOC_VERSION_NS {
 static constexpr unsigned MAJOR_VERSION = 1;
 static constexpr unsigned MINOR_VERSION = 5;
-static constexpr unsigned POINT_VERSION = 11;
+static constexpr unsigned POINT_VERSION = 12;
 }} // namespace swoc::SWOC_VERSION_NS
