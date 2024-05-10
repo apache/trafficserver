294ffc4819683823f834636ef453b9eaaedbfcb0
Masakazu Kitajo <maskit@apache.org>
2024-05-10 16:13:06 -0600

    Stricten field name check

--- src/proxy/hdrs/unit_tests/test_Hdrs.cc
+++ src/proxy/hdrs/unit_tests/test_Hdrs.cc
@@ -563,6 +563,24 @@ TEST_CASE("HdrTest", "[proxy][hdrtest]")
       // Field Name
       {"Content-Length: 10\r\n",     PARSE_RESULT_CONT },
       {"Content-Length\x0b: 10\r\n", PARSE_RESULT_ERROR},
+      // "(),/:;<=>?@[\]{}
+      {"delimiter_\": 10\r\n",       PARSE_RESULT_ERROR},
+      {"delimiter_(: 0\r\n",         PARSE_RESULT_ERROR},
+      {"delimiter_): 0\r\n",         PARSE_RESULT_ERROR},
+      {"delimiter_,: 0\r\n",         PARSE_RESULT_ERROR},
+      {"delimiter_/: 0\r\n",         PARSE_RESULT_ERROR},
+      {"delimiter_:: 0\r\n",         PARSE_RESULT_CONT }, // Parsed as field name "delimiter_" and field value ": 0", which is valid
+      {"delimiter_;: 0\r\n",         PARSE_RESULT_ERROR},
+      {"delimiter_<: 0\r\n",         PARSE_RESULT_ERROR},
+      {"delimiter_=: 0\r\n",         PARSE_RESULT_ERROR},
+      {"delimiter_>: 0\r\n",         PARSE_RESULT_ERROR},
+      {"delimiter_?: 0\r\n",         PARSE_RESULT_ERROR},
+      {"delimiter_@: 0\r\n",         PARSE_RESULT_CONT }, // Not allowed by the spec, but we use it as internal header indicator
+      {"delimiter_[: 0\r\n",         PARSE_RESULT_ERROR},
+      {"delimiter_\\: 0\r\n",        PARSE_RESULT_ERROR},
+      {"delimiter_]: 0\r\n",         PARSE_RESULT_ERROR},
+      {"delimiter_{: 0\r\n",         PARSE_RESULT_ERROR},
+      {"delimiter_}: 0\r\n",         PARSE_RESULT_ERROR},
       ////
       // Field Value
       // SP
@@ -900,13 +918,11 @@ TEST_CASE("HdrTest", "[proxy][hdrtest]")
       "Cache-Control: private\r\n"
       "accept: foo\r\n"
       "accept: bar\n"
-      ": (null) field name\r\n"
       "aCCept: \n"
       "ACCEPT\r\n"
       "foo: bar\r\n"
       "foo: argh\r\n"
       "foo: three, four\r\n"
-      "word word: word \r\n"
       "accept: \"fazzle, dazzle\"\r\n"
       "accept: 1, 2, 3, 4, 5, 6, 7, 8\r\n"
       "continuation: part1\r\n"
