49d9064a18c971a9745849c4f312b5cb06bd7786
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2024-06-07 13:06:51 -0500

    Fix unit tests

--- plugins/ja3_fingerprint/test_utils.cc
+++ plugins/ja3_fingerprint/test_utils.cc
@@ -33,42 +33,42 @@ TEST_CASE("ja3 word buffer encoding")
 
   SECTION("empty buffer")
   {
+    auto got{ja3::encode_byte_buffer(nullptr, 0)};
+    CHECK("" == got);
+  }
+
+  SECTION("1 value")
+  {
+    auto got{ja3::encode_byte_buffer(buf, 1)};
+    CHECK("8" == got);
+  }
+
+  SECTION("3 values")
+  {
+    auto got{ja3::encode_byte_buffer(buf, 3)};
+    CHECK("8-3-4" == got);
+  }
+}
+
+TEST_CASE("ja3 word buffer encoding")
+{
+  unsigned char const buf[]{0x0, 0x5, 0x0a, 0x0a, 0x0, 0x8, 0xda, 0xda, 0x1, 0x0};
+
+  SECTION("empty buffer")
+  {
     auto got{ja3::encode_word_buffer(nullptr, 0)};
     CHECK("" == got);
   }
 
   SECTION("1 value")
   {
-    auto got{ja3::encode_word_buffer(buf, 1)};
-    CHECK("8" == got);
-  }
-
-  SECTION("3 values")
-  {
-    auto got{ja3::encode_word_buffer(buf, 3)};
-    CHECK("8-3-4" == got);
-  }
-}
-
-TEST_CASE("ja3 dword buffer encoding")
-{
-  unsigned char const buf[]{0x0, 0x5, 0x0a, 0x0a, 0x0, 0x8, 0xda, 0xda, 0x1, 0x0};
-
-  SECTION("empty buffer")
-  {
-    auto got{ja3::encode_dword_buffer(nullptr, 0)};
-    CHECK("" == got);
-  }
-
-  SECTION("1 value")
-  {
-    auto got{ja3::encode_dword_buffer(buf, 2)};
+    auto got{ja3::encode_word_buffer(buf, 2)};
     CHECK("5" == got);
   }
 
   SECTION("5 values including GREASE values")
   {
-    auto got{ja3::encode_dword_buffer(buf, 10)};
+    auto got{ja3::encode_word_buffer(buf, 10)};
     CHECK("5-8-256" == got);
   }
 }
