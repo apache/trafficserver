e5f02fc343561a23045437e39f2f65cc2e6b101b
JosiahWI <41302989+JosiahWI@users.noreply.github.com>
2024-06-27 11:49:11 -0500

    Add UnixSocket class based on SocketManager (#11479)
    * Add UnixSocket class based on SocketManager
    
    UnixSocket Class
    
      The new class is a thin wrapper for a Unix socket file descriptor. It does
      not manage the resource itself, although this would be a good idea for a
      future improvement.
    
    Removed Functions
    
      Some functions are not applicable to sockets and do not belong in a utility
      for managing sockets. The functions open, pwrite, lseek and fsync were not
      transferred to the new class. The function socket() was replaced by a
      UnixSocket constructor.
    
    API Changes
    
      The file descriptor no longer needs to be passed because it is remembered
      by the UnixSocket object. Some parameters that were unused are removed
      from the new interface. There is no longer a socket() function; instead
      a UnixSocket constructor with the same arguments is provided.
    
    Implementation Changes
    
      Other than small cleanups like changing uint64_t to std::uint64_t and
      constifying arguments, all implementations are directly copied from
      the old SocketManager ones except for client_fastopen_supported. It
      was refactored to improve naming and make the use of the bitmask obvious.
    
    Performance Considerations
    
      Since the class is a thin wrapper, there should be no performance overhead
      from the use of a class. All methods that were declared inline in a header
      for UnixSocket are also declared inline in the UnixSocket.h header. There
      could be a small increase in compile times because there is no separate
      P_UnixSocket.h header, but UnixSocket.h is private to inkevent so far, so
      the compilation time difference should be negligible.
    
    * Delegate SocketManager calls to UnixSocket
    
    * Implement change suggested by Chris McFarlen
    
     * Replace TS_INLINE with inline
    
    We decided to leave transient_error where it is because it is specific to
    the UnixSocket implementation and will not likely be useful anywhere else.
    
    * Implement change suggested by Masakazu Kitajo
    
     * Add a method to check whether socket creation succeeded.

---
+++ src/iocore/eventsystem/UnixSocket.cc
@@ -0,0 +1,140 @@
+/** @file
+
+  Provides a wrapper for a Unix socket.
+
+  @section license License
+
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+#include "UnixSocket.h"
+
+#include "tscore/ink_apidefs.h"
+#include "tscore/ink_config.h"
+#include "tscore/TextBuffer.h"
+#include "tscore/ink_memory.h"
+#include "tscore/ink_platform.h"
+#include "tscore/ink_sock.h"
+
+#include <cstdlib>
+
+namespace
+{
+enum class TCPFastopenMask {
+  CLIENT_ENABLE = 1,
+  SERVER_ENABLE,
+  CLIENT_NOCOOKIE,
+  SERVER_NOCOOKIE,
+  SERVER_IMPLICIT_ENABLE,
+
+  MAX_VALUE,
+};
+} // end anonymous namespace
+
+#if !HAVE_ACCEPT4
+static int accept4(int sockfd, struct sockaddr *addr, socklen_t *addrlen, int flags);
+#endif
+static unsigned int read_uint_from_fd(int fd);
+
+int
+UnixSocket::bind(struct sockaddr const *name, int namelen)
+{
+  return safe_bind(this->sock_fd, name, namelen);
+}
+
+int
+UnixSocket::accept4(struct sockaddr *addr, socklen_t *addrlen, int flags) const
+{
+  do {
+    int fd = ::accept4(this->sock_fd, addr, addrlen, flags);
+    if (likely(fd >= 0)) {
+      return fd;
+    }
+  } while (transient_error());
+
+  return -errno;
+}
+
+#if !HAVE_ACCEPT4
+static int
+accept4(int sockfd, struct sockaddr *addr, socklen_t *addrlen, int flags)
+{
+  int fd, err;
+
+  do {
+    fd = accept(sockfd, addr, addrlen);
+    if (likely(fd >= 0))
+      break;
+  } while (transient_error());
+
+  if ((fd >= 0) && (flags & SOCK_CLOEXEC) && (safe_fcntl(fd, F_SETFD, FD_CLOEXEC) < 0)) {
+    err = errno;
+    close(fd);
+    errno = err;
+    return -1;
+  }
+
+  if ((fd >= 0) && (flags & SOCK_NONBLOCK) && (safe_nonblocking(fd) < 0)) {
+    err = errno;
+    close(fd);
+    errno = err;
+    return -1;
+  }
+
+  return fd;
+}
+#endif // !HAVE_ACCEPT4
+
+int
+UnixSocket::close()
+{
+  int res;
+
+  if (this->sock_fd == 0) {
+    return -EACCES;
+  } else if (this->sock_fd < 0) {
+    return -EINVAL;
+  }
+
+  do {
+    res = ::close(this->sock_fd);
+    if (res == -1) {
+      res = -errno;
+    }
+  } while (res == -EINTR);
+  return res;
+}
+
+bool
+UnixSocket::client_fastopen_supported()
+{
+  ats_scoped_fd fd{::open("/proc/sys/net/ipv4/tcp_fastopen", O_RDONLY)};
+  unsigned int  bitfield{read_uint_from_fd(fd.get())};
+  return bitfield & static_cast<unsigned int>(TCPFastopenMask::CLIENT_ENABLE);
+}
+
+static unsigned int
+read_uint_from_fd(int fd)
+{
+  int result{};
+  if (fd) {
+    TextBuffer buffer(16);
+    buffer.slurp(fd);
+    result = std::atoi(buffer.bufPtr());
+  }
+  return static_cast<unsigned int>(result);
+}
---
+++ src/iocore/eventsystem/UnixSocket.h
@@ -0,0 +1,308 @@
+/** @file
+
+  Provides a wrapper for a Unix socket.
+
+  @section license License
+
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+#pragma once
+
+#include "tscore/ink_apidefs.h"
+#include "tscore/ink_platform.h"
+#include "tscore/ink_sock.h"
+
+#include <cstdint>
+
+#define NO_SOCK -1
+
+#ifndef SOCK_NONBLOCK
+#define SOCK_NONBLOCK O_NONBLOCK
+#endif
+
+#ifndef SOCK_CLOEXEC
+#define SOCK_CLOEXEC O_CLOEXEC
+#endif
+
+#ifndef MSG_FASTOPEN
+#if defined(__linux__)
+#define MSG_FASTOPEN 0x20000000
+#else
+#define MSG_FASTOPEN 0
+#endif
+#endif
+
+bool transient_error();
+
+class UnixSocket
+{
+public:
+  UnixSocket(int fd);
+
+  /** Get a new socket.
+   *
+   * Call has_socket() to determine whether this call succeeded. If the call
+   * failed, errno will be set to indicate the error.
+   *
+   * @see has_socket
+   */
+  UnixSocket(int domain, int ctype, int protocol);
+
+  bool has_socket() const;
+
+  int bind(struct sockaddr const *name, int namelen);
+  int accept4(struct sockaddr *addr, socklen_t *addrlen, int flags) const;
+
+  std::int64_t read(void *buf, int size) const;
+
+  int recv(void *buf, int size, int flags) const;
+  int recvfrom(void *buf, int size, int flags, struct sockaddr *addr, socklen_t *addrlen) const;
+  int recvmsg(struct msghdr *m, int flags) const;
+#ifdef HAVE_RECVMMSG
+  int recvmmsg(struct mmsghdr *msgvec, int vlen, int flags, struct timespec *timeout) const;
+#endif
+
+  std::int64_t write(void *buf, int size) const;
+
+  int send(void *buf, int size, int flags) const;
+  int sendto(void *buf, int size, int flags, struct sockaddr const *to, int tolen) const;
+  int sendmsg(struct msghdr const *m, int flags) const;
+
+  static int poll(struct pollfd *fds, unsigned long nfds, int timeout);
+
+  int getsockname(struct sockaddr *sa, socklen_t *sz) const;
+
+  int get_sndbuf_size() const;
+  int get_rcvbuf_size() const;
+  int set_sndbuf_size(int bsz);
+  int set_rcvbuf_size(int bsz);
+
+  int close();
+  int shutdown(int how);
+
+  static bool client_fastopen_supported();
+
+private:
+  int sock_fd{NO_SOCK};
+};
+
+inline UnixSocket::UnixSocket(int fd) : sock_fd{fd} {}
+
+inline UnixSocket::UnixSocket(int domain, int type, int protocol)
+{
+  this->sock_fd = socket(domain, type, protocol);
+}
+
+inline bool
+UnixSocket::has_socket() const
+{
+  return NO_SOCK != this->sock_fd;
+}
+
+inline std::int64_t
+UnixSocket::read(void *buf, int size) const
+{
+  std::int64_t r;
+  do {
+    r = ::read(this->sock_fd, buf, size);
+    if (likely(r >= 0)) {
+      break;
+    }
+    r = -errno;
+  } while (r == -EINTR);
+  return r;
+}
+
+inline int
+UnixSocket::recv(void *buf, int size, int flags) const
+{
+  int r;
+  do {
+    if (unlikely((r = ::recv(this->sock_fd, static_cast<char *>(buf), size, flags)) < 0)) {
+      r = -errno;
+    }
+  } while (r == -EINTR);
+  return r;
+}
+
+inline int
+UnixSocket::recvfrom(void *buf, int size, int flags, struct sockaddr *addr, socklen_t *addrlen) const
+{
+  int r;
+  do {
+    r = ::recvfrom(this->sock_fd, static_cast<char *>(buf), size, flags, addr, addrlen);
+    if (unlikely(r < 0)) {
+      r = -errno;
+    }
+  } while (r == -EINTR);
+  return r;
+}
+
+inline int
+UnixSocket::recvmsg(struct msghdr *m, int flags) const
+{
+  int r;
+  do {
+    if (unlikely((r = ::recvmsg(this->sock_fd, m, flags)) < 0)) {
+      r = -errno;
+    }
+  } while (r == -EINTR);
+  return r;
+}
+
+#ifdef HAVE_RECVMMSG
+inline int
+UnixSocket::recvmmsg(struct mmsghdr *msgvec, int vlen, int flags, struct timespec *timeout) const
+{
+  int r;
+  do {
+    if (unlikely((r = ::recvmmsg(this->sock_fd, msgvec, vlen, flags, timeout)) < 0)) {
+      r = -errno;
+      // EINVAL can ocur if timeout is invalid.
+    }
+  } while (r == -EINTR);
+  return r;
+}
+#endif
+
+inline std::int64_t
+UnixSocket::write(void *buf, int size) const
+{
+  std::int64_t r;
+  do {
+    if (likely((r = ::write(this->sock_fd, buf, size)) >= 0)) {
+      break;
+    }
+    r = -errno;
+  } while (r == -EINTR);
+  return r;
+}
+
+inline int
+UnixSocket::send(void *buf, int size, int flags) const
+{
+  int r;
+  do {
+    if (unlikely((r = ::send(this->sock_fd, static_cast<char *>(buf), size, flags)) < 0)) {
+      r = -errno;
+    }
+  } while (r == -EINTR);
+  return r;
+}
+
+inline int
+UnixSocket::sendto(void *buf, int len, int flags, struct sockaddr const *to, int tolen) const
+{
+  int r;
+  do {
+    if (unlikely((r = ::sendto(this->sock_fd, (char *)buf, len, flags, to, tolen)) < 0)) {
+      r = -errno;
+    }
+  } while (r == -EINTR);
+  return r;
+}
+
+inline int
+UnixSocket::sendmsg(struct msghdr const *m, int flags) const
+{
+  int r;
+  do {
+    if (unlikely((r = ::sendmsg(this->sock_fd, m, flags)) < 0)) {
+      r = -errno;
+    }
+  } while (r == -EINTR);
+  return r;
+}
+
+inline int
+UnixSocket::poll(struct pollfd *fds, unsigned long nfds, int timeout)
+{
+  int r;
+  do {
+    if ((r = ::poll(fds, nfds, timeout)) >= 0) {
+      break;
+    }
+    r = -errno;
+  } while (transient_error());
+  return r;
+}
+
+inline int
+UnixSocket::getsockname(struct sockaddr *sa, socklen_t *sz) const
+{
+  return ::getsockname(this->sock_fd, sa, sz);
+}
+
+inline int
+UnixSocket::get_sndbuf_size() const
+{
+  int bsz = 0;
+  int bszsz, r;
+
+  bszsz = sizeof(bsz);
+  r     = safe_getsockopt(this->sock_fd, SOL_SOCKET, SO_SNDBUF, (char *)&bsz, &bszsz);
+  return (r == 0 ? bsz : r);
+}
+
+inline int
+UnixSocket::get_rcvbuf_size() const
+{
+  int bsz = 0;
+  int bszsz, r;
+
+  bszsz = sizeof(bsz);
+  r     = safe_getsockopt(this->sock_fd, SOL_SOCKET, SO_RCVBUF, (char *)&bsz, &bszsz);
+  return (r == 0 ? bsz : r);
+}
+
+inline int
+UnixSocket::set_sndbuf_size(int bsz)
+{
+  return safe_setsockopt(this->sock_fd, SOL_SOCKET, SO_SNDBUF, (char *)&bsz, sizeof(bsz));
+}
+
+inline int
+UnixSocket::set_rcvbuf_size(int bsz)
+{
+  return safe_setsockopt(this->sock_fd, SOL_SOCKET, SO_RCVBUF, (char *)&bsz, sizeof(bsz));
+}
+
+inline int
+UnixSocket::shutdown(int how)
+{
+  int res;
+  do {
+    if (unlikely((res = ::shutdown(this->sock_fd, how)) < 0)) {
+      res = -errno;
+    }
+  } while (res == -EINTR);
+  return res;
+}
+
+inline bool
+transient_error()
+{
+  bool transient = (errno == EINTR);
+#ifdef ENOMEM
+  transient = transient || (errno == ENOMEM);
+#endif
+#ifdef ENOBUFS
+  transient = transient || (errno == ENOBUFS);
+#endif
+  return transient;
+}
