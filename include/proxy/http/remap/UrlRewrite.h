4dd2b54104e840112ef3ad7f290180abb667149b
Brian Neradt <brian.neradt@gmail.com>
2024-08-02 19:32:18 -0500

    ACL filter action documentation updates (#11635)
    This renames the ACL filter behavior as "legacy" and "modern" and
    documents that the legacy behavior, while the default for 10.x, is
    deprecated and will be removed in 11.x
    
    This also adds documentation for set_allow and set_deny.
    
    (cherry picked from commit 6dfbff32353a34233605ce8f90a62bcb97f2d5e8)

--- include/proxy/http/remap/UrlRewrite.h
+++ include/proxy/http/remap/UrlRewrite.h
@@ -53,9 +53,9 @@ enum mapping_type {
   NONE
 };
 
-enum class ACLMatchingPolicy {
-  MATCH_ON_IP_AND_METHOD = 0,
-  MATCH_ON_IP_ONLY,
+enum class ACLBehaviorPolicy {
+  ACL_BEHAVIOR_LEGACY = 0,
+  ACL_BEHAVIOR_MODERN,
 };
 
 /**
@@ -70,11 +70,11 @@ public:
 
   /** Retrieve the configured ACL matching policy.
    *
-   * @param[out] policy The configured ACL matching policy.
+   * @param[out] policy The configured ACL behavior policy.
    * @return @c true if the policy is configured to an appropriate value, @c
    * false if not.
    */
-  static bool get_acl_matching_policy(ACLMatchingPolicy &policy);
+  static bool get_acl_behavior_policy(ACLBehaviorPolicy &policy);
 
   /** Load the configuration.
    *
@@ -238,7 +238,7 @@ public:
 
 private:
   bool              _valid               = false;
-  ACLMatchingPolicy _acl_matching_policy = ACLMatchingPolicy::MATCH_ON_IP_AND_METHOD;
+  ACLBehaviorPolicy _acl_behavior_policy = ACLBehaviorPolicy::ACL_BEHAVIOR_LEGACY;
 
   bool _mappingLookup(MappingsStore &mappings, URL *request_url, int request_port, const char *request_host, int request_host_len,
                       UrlMappingContainer &mapping_container);
