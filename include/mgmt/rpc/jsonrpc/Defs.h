apache/trafficserver

commit ab952036020bb2ba15a69caaec362719f209e16b

libsowc - remove legacy Errata. (#10986)
libsowc - remove legacy Errata.

---------

Co-authored-by: Alan M. Carroll <amc@apache.org>

include/mgmt/rpc/jsonrpc/Defs.h
@@ -23,6 +23,8 @@
 #include <string>
 #include <optional>
 
+#include "tsutil/ts_errata.h"
+
 #include <yaml-cpp/yaml.h>
 
 #include "mgmt/rpc/jsonrpc/error/RPCError.h"
@@ -40,17 +42,16 @@ const std::string JSONRPC_VERSION{"2.0"};
 class RPCHandlerResponse
 {
 public:
-  YAML::Node result; //!< The response from the registered handler.
-  ts::Errata errata; //!< The  error response from the registered handler.
+  YAML::Node result;   //!< The response from the registered handler.
+  swoc::Errata errata; //!< The  error response from the registered handler.
 };
 
 struct RPCResponseInfo {
   RPCResponseInfo(std::string const &id_) : id{id_} {} // Convenient
   RPCResponseInfo() = default;
-
   struct Error {
-    std::error_code ec;
-    ts::Errata data;
+    std::error_code ec; // protocol error track.
+    swoc::Errata data;  // internal error detail.
   };
 
   std::string id; //!< incoming request id (only used for method calls, empty means it's a notification as requests with empty id

src/mgmt/rpc/jsonrpc/JsonRPCManager.cc
@@ -27,6 +27,7 @@
 #include <condition_variable>
 
 #include <swoc/swoc_meta.h>
+
 #include "mgmt/rpc/jsonrpc/json/YAMLCodec.h"
 
 namespace
@@ -57,15 +58,16 @@ RPCRegistryInfo core_ats_rpc_service_provider_handle = {
 // plugin rpc handling variables.
 std::mutex g_rpcHandlingMutex;
 std::condition_variable g_rpcHandlingCompletion;
-ts::Rv<YAML::Node> g_rpcHandlerResponseData;
+swoc::Rv<YAML::Node> g_rpcHandlerResponseData;
 bool g_rpcHandlerProcessingCompleted{false};
 
 // --- Helpers
-std::pair<ts::Errata, error::RPCErrorCode>
+swoc::Errata
 check_for_blockers(Context const &ctx, TSRPCHandlerOptions const &options)
 {
-  if (auto err = ctx.get_auth().is_blocked(options); !err.isOK()) {
-    return {err, error::RPCErrorCode::Unauthorized};
+  if (auto err = ctx.get_auth().is_blocked(options); !err.is_ok()) {
+    return std::move(err.note(swoc::Errata(std::error_code(unsigned(error::RPCErrorCode::Unauthorized), std::generic_category()),
+                                           ERRATA_ERROR, swoc::Errata::AUTO)));
   }
   return {};
 }
@@ -80,7 +82,7 @@ JsonRPCManager::Dispatcher::register_service_descriptor_handler()
 {
   if (!this->add_handler<Dispatcher::Method, MethodHandlerSignature>(
         "show_registered_handlers",
-        [this](std::string_view const &id, const YAML::Node &req) -> ts::Rv<YAML::Node> {
+        [this](std::string_view const &id, const YAML::Node &req) -> swoc::Rv<YAML::Node> {
           return show_registered_handlers(id, req);
         },
         &core_ats_rpc_service_provider_handle, {{NON_RESTRICTED_API}})) {
@@ -89,7 +91,9 @@ JsonRPCManager::Dispatcher::register_service_descriptor_handler()
 
   if (!this->add_handler<Dispatcher::Method, MethodHandlerSignature>(
         "get_service_descriptor",
-        [this](std::string_view const &id, const YAML::Node &req) -> ts::Rv<YAML::Node> { return get_service_descriptor(id, req); },
+        [this](std::string_view const &id, const YAML::Node &req) -> swoc::Rv<YAML::Node> {
+          return get_service_descriptor(id, req);
+        },
         &core_ats_rpc_service_provider_handle, {{NON_RESTRICTED_API}})) {
     Warning("Handler already registered.");
   }
@@ -108,10 +112,10 @@ JsonRPCManager::Dispatcher::dispatch(Context const &ctx, specs::RPCRequestInfo c
   }
 
   // We have got a valid handler, we will now check if the context holds any restriction for this handler to be called.
-  if (auto &&[errata, ec] = check_for_blockers(ctx, handler.get_options()); !errata.isOK()) {
+  if (auto errata = check_for_blockers(ctx, handler.get_options()); !errata.is_ok()) {
     specs::RPCResponseInfo resp{request.id};
     resp.error.ec   = ec;
-    resp.error.data = errata;
+    resp.error.data = std::move(errata);
     return resp;
   }
 
@@ -152,13 +156,13 @@ JsonRPCManager::Dispatcher::invoke_method_handler(JsonRPCManager::Dispatcher::In
   specs::RPCResponseInfo response{request.id};
 
   try {
-    auto const &rv = handler.invoke(request);
+    auto rv = handler.invoke(request);
 
-    if (rv.isOK()) {
+    if (rv.is_ok()) {
       response.callResult.result = rv.result();
     } else {
       // if we have some errors to log, then include it.
-      response.callResult.errata = rv.errata();
+      response.callResult.errata = std::move(rv.errata());
     }
   } catch (std::exception const &e) {
     Debug(logTag, "Oops, something happened during the callback invocation: %s", e.what());
@@ -238,6 +242,7 @@ JsonRPCManager::handle_call(Context const &ctx, std::string const &request)
       } else {
         // If the request was marked as an error(decode error), we still need to send the error back, so we save it.
         specs::RPCResponseInfo resp{req.id};
+        // resp.error.assign(swoc::Errata(decode_error));
         resp.error.ec = decode_error;
         response.add_message(std::move(resp));
       }
@@ -250,20 +255,22 @@ JsonRPCManager::handle_call(Context const &ctx, std::string const &request)
       resp = Encoder::encode(response);
       Debug(logTagMsg, "<-- JSONRPC Response\n '%s'", (*resp).c_str());
     }
+
     return resp;
   } catch (std::exception const &ex) {
     ec = error::RPCErrorCode::INTERNAL_ERROR;
   }
+
   specs::RPCResponseInfo resp;
   resp.error.ec = ec;
   return {Encoder::encode(resp)};
 }
 
 // ---------------------------- InternalHandler ---------------------------------
-inline ts::Rv<YAML::Node>
+inline swoc::Rv<YAML::Node>
 JsonRPCManager::Dispatcher::InternalHandler::invoke(specs::RPCRequestInfo const &request) const
 {
-  ts::Rv<YAML::Node> ret;
+  swoc::Rv<YAML::Node> ret;
   std::visit(swoc::meta::vary{[](std::monostate) -> void { /* no op */ },
                               [&request](Notification const &handler) -> void {
                                 // Notification handler call. Ignore response, there is no completion cv check in here basically
@@ -284,11 +291,11 @@ JsonRPCManager::Dispatcher::InternalHandler::invoke(specs::RPCRequestInfo const 
                                 std::unique_lock<std::mutex> lock(g_rpcHandlingMutex);
                                 g_rpcHandlingCompletion.wait(lock, []() { return g_rpcHandlerProcessingCompleted; });
                                 g_rpcHandlerProcessingCompleted = false;
-                                // seems to be done, set the response. As the response data is a ts::Rv this will handle both,
+                                // seems to be done, set the response. As the response data is a swoc::Rv this will handle both,
                                 // error and non error cases.
-                                ret = g_rpcHandlerResponseData;
+                                ret = std::move(g_rpcHandlerResponseData);
                                 // clean up the shared data.
-                                g_rpcHandlerResponseData.clear();
+                                //                                g_rpcHandlerResponseData.clear(); // moved so no cleanup?
                                 lock.unlock();
                               }},
              this->_func);
@@ -310,10 +317,10 @@ JsonRPCManager::Dispatcher::InternalHandler::is_method() const
   return false;
 }
 
-ts::Rv<YAML::Node>
+swoc::Rv<YAML::Node>
 JsonRPCManager::Dispatcher::show_registered_handlers(std::string_view const &, const YAML::Node &)
 {
-  ts::Rv<YAML::Node> resp;
+  swoc::Rv<YAML::Node> resp;
   std::lock_guard<std::mutex> lock(_mutex);
   for (auto const &[name, handler] : _handlers) {
     std::string const &key = handler.is_method() ? RPC_SERVICE_METHODS_KEY : RPC_SERVICE_NOTIFICATIONS_KEY;
@@ -324,7 +331,7 @@ JsonRPCManager::Dispatcher::show_registered_handlers(std::string_view const &, c
 
 // -----------------------------------------------------------------------------
 // This jsonrpc handler can provides a service descriptor for the RPC
-ts::Rv<YAML::Node>
+swoc::Rv<YAML::Node>
 JsonRPCManager::Dispatcher::get_service_descriptor(std::string_view const &, const YAML::Node &)
 {
   YAML::Node rpcService;

include/iocore/net/YamlSNIConfig.h
@@ -32,8 +32,7 @@
 #include "iocore/net/SSLTypes.h"
 
 #include "tsutil/ts_ip.h"
-
-#include "tscore/Errata.h"
+#include "tsutil/ts_errata.h"
 
 #define TSDECL(id) constexpr char TS_##id[] = #id
 TSDECL(fqdn);
@@ -129,7 +128,7 @@ struct YamlSNIConfig {
     void populate_sni_actions(action_vector_t &actions);
   };
 
-  ts::Errata loader(const std::string &cfgFilename);
+  swoc::Errata loader(const std::string &cfgFilename);
 
   std::vector<YamlSNIConfig::Item> items;
 };

include/mgmt/config/FileManager.h
@@ -23,18 +23,18 @@
 
 #pragma once
 
-#include "tscore/ink_mutex.h"
-#include "tscore/List.h"
-
-#include "tscore/Errata.h"
-
-#include "mgmt/rpc/jsonrpc/JsonRPC.h"
-
 #include <unordered_map>
 #include <string_view>
 #include <forward_list>
 #include <mutex>
 #include <functional>
+
+#include "tsutil/ts_errata.h"
+
+#include "tscore/ink_mutex.h"
+#include "tscore/List.h"
+
+#include <mgmt/rpc/jsonrpc/JsonRPC.h>
 
 class ConfigUpdateCbTable;
 
@@ -121,7 +121,7 @@ public:
     time_t fileLastModified = 0;
   };
 
-  using CallbackType = std::function<ts::Errata(std::string const &, std::string const &)>;
+  using CallbackType = std::function<swoc::Errata(std::string const &, std::string const &)>;
 
   ~FileManager();
   FileManager(const FileManager &obj)         = delete;
@@ -139,8 +139,8 @@ public:
     _configCallbacks.push_front(std::move(f));
   }
 
-  ts::Errata fileChanged(std::string const &fileName, std::string const &configName);
-  ts::Errata rereadConfig();
+  swoc::Errata fileChanged(std::string const &fileName, std::string const &configName);
+  swoc::Errata rereadConfig();
   bool isConfigStale();
   void configFileChild(const char *parent, const char *child);
 
@@ -169,7 +169,7 @@ private:
   void addFileHelper(const char *fileName, const char *configName, bool root_access_needed, bool isRequired,
                      ConfigManager *parentConfig);
   /// JSONRPC endpoint
-  ts::Rv<YAML::Node> get_files_registry_rpc_endpoint(std::string_view const &id, YAML::Node const &params);
+  swoc::Rv<YAML::Node> get_files_registry_rpc_endpoint(std::string_view const &id, YAML::Node const &params);
 };
 
 void initializeRegistry(); // implemented in AddConfigFilesHere.cc

include/mgmt/rpc/handlers/common/ErrorUtils.h
@@ -26,7 +26,8 @@
 #include <system_error>
 #include <string_view>
 
-#include "tscore/Errata.h"
+#include "tsutil/ts_errata.h"
+#include "tsutil/ts_bw_format.h"
 
 namespace rpc::handlers::errors
 {
@@ -38,7 +39,7 @@ namespace rpc::handlers::errors
 // With this we try to avoid error codes collision. You can also use same error Code for all your
 // errors.
 enum Codes : unsigned int {
-  CONFIGURATION = 1,
+  CONFIGURATION = 999, // go past @c errno
   METRIC        = 1000,
   RECORD        = 2000,
   SERVER        = 3000,
@@ -48,19 +49,24 @@ enum Codes : unsigned int {
   GENERIC = 30000
 };
 
-static constexpr int ERRATA_DEFAULT_ID{1};
+std::error_code make_error_code(rpc::handlers::errors::Codes e);
 
 template <typename... Args>
-static inline ts::Errata
+static inline swoc::Errata
 make_errata(int code, std::string_view fmt, Args &&...args)
 {
-  std::string text;
-  return ts::Errata{}.push(ERRATA_DEFAULT_ID, code, swoc::bwprint(text, fmt, std::forward<Args>(args)...));
+  return swoc::Errata(std::error_code(code, std::generic_category()), fmt, std::forward<Args>(args)...);
 }
 
-static inline ts::Errata
+static inline swoc::Errata
 make_errata(int code, std::string_view text)
 {
-  return ts::Errata{}.push(ERRATA_DEFAULT_ID, code, text);
+  return swoc::Errata(std::error_code(code, std::generic_category()), std::string(text));
 }
 } // namespace rpc::handlers::errors
+namespace std
+{
+template <> struct is_error_code_enum<rpc::handlers::errors::Codes> : true_type {
+};
+
+} // namespace std

include/mgmt/rpc/handlers/common/RecordsUtils.h
@@ -22,13 +22,14 @@
 
 #include <tuple>
 
+#include "tsutil/ts_errata.h"
+
 #include "mgmt/rpc/handlers/common/convert.h"
 #include "mgmt/rpc/handlers/common/ErrorUtils.h"
 
 #include "records/RecCore.h"
 #include "../../../../../src/records/P_RecCore.h"
 #include "tscore/Diags.h"
-#include "tscore/Errata.h"
 
 #include <yaml-cpp/yaml.h>
 

include/mgmt/rpc/handlers/config/Configuration.h
@@ -24,7 +24,7 @@
 
 namespace rpc::handlers::config
 {
-ts::Rv<YAML::Node> set_config_records(std::string_view const &id, YAML::Node const &params);
-ts::Rv<YAML::Node> reload_config(std::string_view const &id, YAML::Node const &params);
+swoc::Rv<YAML::Node> set_config_records(std::string_view const &id, YAML::Node const &params);
+swoc::Rv<YAML::Node> reload_config(std::string_view const &id, YAML::Node const &params);
 
 } // namespace rpc::handlers::config

include/mgmt/rpc/handlers/plugins/Plugins.h
@@ -24,5 +24,5 @@
 
 namespace rpc::handlers::plugins
 {
-ts::Rv<YAML::Node> plugin_send_basic_msg(std::string_view const &id, YAML::Node const &params);
+swoc::Rv<YAML::Node> plugin_send_basic_msg(std::string_view const &id, YAML::Node const &params);
 } // namespace rpc::handlers::plugins

commit 4d23ff3f8f1e6c182e733570c68c3273d2870ecd

RPC: Make sure any internal error gets properly set. (#11730)
Thus, the RPC server respond with a proper error response.
This issue was introduced by #10986 when migrated from ts:Errata to swoc::Errata. This PR makes sure that the right error code gets set

include/mgmt/rpc/jsonrpc/Defs.h
@@ -50,8 +50,9 @@ struct RPCResponseInfo {
   RPCResponseInfo(std::string const &id_) : id{id_} {} // Convenient
   RPCResponseInfo() = default;
   struct Error {
-    std::error_code ec;   // protocol error track.
-    swoc::Errata    data; // internal error detail.
+    std::error_code ec; // Protocol error track, @see rpc::error::RPCErrorCode. !Important: If there is an internal error happening
+                        // before we call the rpc handler this should be set, otherwise the encoder wont render the error properly.
+    swoc::Errata data;  // Internal error detail. Optional.
   };
 
   std::string id; //!< incoming request id (only used for method calls, empty means it's a notification as requests with empty id

src/mgmt/rpc/jsonrpc/JsonRPCManager.cc
@@ -62,12 +62,11 @@ swoc::Rv<YAML::Node>    g_rpcHandlerResponseData;
 bool                    g_rpcHandlerProcessingCompleted{false};
 
 // --- Helpers
-swoc::Errata
+std::pair<swoc::Errata, error::RPCErrorCode>
 check_for_blockers(Context const &ctx, TSRPCHandlerOptions const &options)
 {
-  if (auto err = ctx.get_auth().is_blocked(options); !err.is_ok()) {
-    return std::move(err.note(swoc::Errata(std::error_code(unsigned(error::RPCErrorCode::Unauthorized), std::generic_category()),
-                                           ERRATA_ERROR, swoc::Errata::AUTO)));
+  if (auto &&err = ctx.get_auth().is_blocked(options); !err.is_ok()) {
+    return std::make_pair(std::move(err), error::RPCErrorCode::Unauthorized);
   }
   return {};
 }
@@ -112,7 +111,7 @@ JsonRPCManager::Dispatcher::dispatch(Context const &ctx, specs::RPCRequestInfo c
   }
 
   // We have got a valid handler, we will now check if the context holds any restriction for this handler to be called.
-  if (auto errata = check_for_blockers(ctx, handler.get_options()); !errata.is_ok()) {
+  if (auto &&[errata, ec] = check_for_blockers(ctx, handler.get_options()); !errata.is_ok()) {
     specs::RPCResponseInfo resp{request.id};
     resp.error.ec   = ec;
     resp.error.data = std::move(errata);