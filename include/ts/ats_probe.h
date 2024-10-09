86d0ba58eaf5e80f8382ba37ff0edb4ab57d5b04
Mo Chen <mochen@apache.org>
2024-10-09 10:02:48 -0500

    Add support for ATS_PROBES with up to twelve arguments (#11809)
    (cherry picked from commit 468dcf32b7af9156e37f99bafd1af322ea306306)

--- include/ts/ats_probe.h
+++ include/ts/ats_probe.h
@@ -28,14 +28,43 @@
 
 #include <sys/sdt.h>
 
-#define ATS_PROBE(probe)                  DTRACE_PROBE(trafficserver, probe)
-#define ATS_PROBE1(probe, param1)         DTRACE_PROBE1(trafficserver, probe, param1)
-#define ATS_PROBE2(probe, param1, param2) DTRACE_PROBE2(trafficserver, probe, param1, param2)
+#define ATS_PROBE(probe)                                  DTRACE_PROBE(trafficserver, probe)
+#define ATS_PROBE1(probe, param1)                         DTRACE_PROBE1(trafficserver, probe, param1)
+#define ATS_PROBE2(probe, param1, param2)                 DTRACE_PROBE2(trafficserver, probe, param1, param2)
+#define ATS_PROBE3(probe, param1, param2, param3)         DTRACE_PROBE3(trafficserver, probe, param1, param2, param3)
+#define ATS_PROBE4(probe, param1, param2, param3, param4) DTRACE_PROBE4(trafficserver, probe, param1, param2, param3, param4)
+#define ATS_PROBE5(probe, param1, param2, param3, param4, param5) \
+  DTRACE_PROBE5(trafficserver, probe, param1, param2, param3, param4, param5)
+#define ATS_PROBE6(probe, param1, param2, param3, param4, param5, param6) \
+  DTRACE_PROBE6(trafficserver, probe, param1, param2, param3, param4, param5, param6)
+#define ATS_PROBE7(probe, param1, param2, param3, param4, param5, param6, param7) \
+  DTRACE_PROBE7(trafficserver, probe, param1, param2, param3, param4, param5, param6, param7)
+#define ATS_PROBE8(probe, param1, param2, param3, param4, param5, param6, param7, param8) \
+  DTRACE_PROBE8(trafficserver, probe, param1, param2, param3, param4, param5, param6, param7, param8)
+#define ATS_PROBE9(probe, param1, param2, param3, param4, param5, param6, param7, param8, param9) \
+  DTRACE_PROBE9(trafficserver, probe, param1, param2, param3, param4, param5, param6, param7, param8, param9)
+#define ATS_PROBE10(probe, param1, param2, param3, param4, param5, param6, param7, param8, param9, param10) \
+  DTRACE_PROBE10(trafficserver, probe, param1, param2, param3, param4, param5, param6, param7, param8, param9, param10)
+#define ATS_PROBE11(probe, param1, param2, param3, param4, param5, param6, param7, param8, param9, param10, param11) \
+  DTRACE_PROBE11(trafficserver, probe, param1, param2, param3, param4, param5, param6, param7, param8, param9, param10, param11)
+#define ATS_PROBE12(probe, param1, param2, param3, param4, param5, param6, param7, param8, param9, param10, param11, param12)    \
+  DTRACE_PROBE12(trafficserver, probe, param1, param2, param3, param4, param5, param6, param7, param8, param9, param10, param11, \
+                 param12)
 
 #else
 
 #define ATS_PROBE(...)
 #define ATS_PROBE1(...)
 #define ATS_PROBE2(...)
+#define ATS_PROBE3(...)
+#define ATS_PROBE4(...)
+#define ATS_PROBE5(...)
+#define ATS_PROBE6(...)
+#define ATS_PROBE7(...)
+#define ATS_PROBE8(...)
+#define ATS_PROBE9(...)
+#define ATS_PROBE10(...)
+#define ATS_PROBE11(...)
+#define ATS_PROBE12(...)
 
 #endif
