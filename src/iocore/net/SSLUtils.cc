e5558f4a0cc35d160ad2114fa75c353a71fa6e9d
Masaori Koshiba <masaori@apache.org>
2024-11-05 15:55:03 +0900

    Get rid of #ifdef for old openssl

--- src/iocore/net/SSLUtils.cc
+++ src/iocore/net/SSLUtils.cc
@@ -1529,9 +1529,8 @@ SSLMultiCertConfigLoader::_set_cipher_suites([[maybe_unused]] SSL_CTX *ctx)
 }
 
 bool
-SSLMultiCertConfigLoader::_set_curves([[maybe_unused]] SSL_CTX *ctx)
+SSLMultiCertConfigLoader::_set_curves(SSL_CTX *ctx)
 {
-#ifdef OPENSSL_IS_BORINGSSL
   if (this->_params->server_groups_list != nullptr) {
     if (!SSL_CTX_set1_groups_list(ctx, this->_params->server_groups_list)) {
       SSLError("invalid groups list for server in %s", ts::filename::RECORDS);
@@ -1539,20 +1538,6 @@ SSLMultiCertConfigLoader::_set_curves([[maybe_unused]] SSL_CTX *ctx)
     }
   }
 
-#else
-#if defined(SSL_CTX_set1_groups_list) || defined(SSL_CTX_set1_curves_list)
-  if (this->_params->server_groups_list != nullptr) {
-#ifdef SSL_CTX_set1_groups_list
-    if (!SSL_CTX_set1_groups_list(ctx, this->_params->server_groups_list)) {
-#else
-    if (!SSL_CTX_set1_curves_list(ctx, this->_params->server_groups_list)) {
-#endif
-      SSLError("invalid groups list for server in %s", ts::filename::RECORDS);
-      return false;
-    }
-  }
-#endif
-#endif // OPENSSL_IS_BORINGSSL
   return true;
 }
 
