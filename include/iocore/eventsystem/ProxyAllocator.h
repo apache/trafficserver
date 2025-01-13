69481736ac6da5342af93abc8915cf8bd27e1a7a
Mo Chen <mochen@apache.org>
2025-01-13 17:54:31 -0600

    Clean up the type conversion

--- include/iocore/eventsystem/ProxyAllocator.h
+++ include/iocore/eventsystem/ProxyAllocator.h
@@ -94,7 +94,7 @@ thread_free(T *p, Allocator &global_allocator, ProxyAllocator &thread_allocator,
 {
   global_allocator.destroy_if_enabled(p);
   if (tin && !cmd_disable_pfreelist) {
-    *(reinterpret_cast<char **>(p)) = reinterpret_cast<char *>(thread_allocator.freelist);
+    *(reinterpret_cast<void **>(p)) = thread_allocator.freelist;
     thread_allocator.freelist       = p;
     ++thread_allocator.allocated;
     if (thread_freelist_high_watermark > 0 && thread_allocator.allocated > thread_freelist_high_watermark) {
