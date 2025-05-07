afbb1f5d6d60996db410b765817e4763eb27650c
Brian Neradt <brian.neradt@gmail.com>
2025-05-07 16:18:36 -0500

    Call SSL_get_negotiated_group not during read early data (#12224)
    Recent versions of OpenSSL define SSL_CB_HANDSHAKE_DONE as follows:
    
    > Callback has been called because a handshake is finished. It also
    > occurs if the handshake is paused to allow the exchange of early data.
    
    Calling SSL_get_negotiated_group duing early data situations was causing
    a crash. This moves the calling of SSL_get_negotiated_group next to
    _record_tls_handshake_end_time where it will reliably not be called in
    the context of processing early data.
    
    Fixes: #12140

--- src/iocore/net/TLSBasicSupport.cc
+++ src/iocore/net/TLSBasicSupport.cc
@@ -24,6 +24,9 @@
 
 #include "SSLStats.h"
 #include "iocore/net/TLSBasicSupport.h"
+#if defined(OPENSSL_IS_BORINGSSL)
+#include "tscore/Diags.h" // For Warning
+#endif                    // OPENSSL_IS_BORINGSSL
 #include "tsutil/DbgCtl.h"
 
 #include <cinttypes>
@@ -216,4 +219,34 @@ TLSBasicSupport::_record_tls_handshake_end_time()
 
   Dbg(dbg_ctl_ssl, "ssl handshake time:%" PRId64, ssl_handshake_time);
   Metrics::Counter::increment(ssl_rsb.total_handshake_time, ssl_handshake_time);
+}
+
+void
+TLSBasicSupport::_update_end_of_handshake_stats()
+{
+  Metrics::Counter::increment(ssl_rsb.total_success_handshake_count_in);
+
+#if defined(OPENSSL_IS_BORINGSSL)
+  SSL     *ssl      = this->_get_ssl_object();
+  uint16_t group_id = SSL_get_group_id(ssl);
+  if (group_id != 0) {
+    const char *group_name = SSL_get_group_name(group_id);
+    if (auto it = tls_group_map.find(group_name); it != tls_group_map.end()) {
+      Metrics::Counter::increment(it->second);
+    } else {
+      Warning("Unknown TLS Group");
+    }
+  }
+#elif defined(SSL_get_negotiated_group)
+  SSL *ssl = this->_get_ssl_object();
+  int  nid = SSL_get_negotiated_group(const_cast<SSL *>(ssl));
+  if (nid != NID_undef) {
+    if (auto it = tls_group_map.find(nid); it != tls_group_map.end()) {
+      Metrics::Counter::increment(it->second);
+    } else {
+      auto other = tls_group_map.find(SSL_GROUP_STAT_OTHER_KEY);
+      Metrics::Counter::increment(other->second);
+    }
+  }
+#endif // OPENSSL_IS_BORINGSSL or SSL_get_negotiated_group
 }
