9c78a9e24519f09abc65a4d49af9121b656219e0
Shu Kit Chan <kichan@c40b7d47.home-kichan.bf2.ows.oath.cloud>
2025-06-16 18:00:51 +0000

    fix build error

--- plugins/esi/esi.cc
+++ plugins/esi/esi.cc
@@ -1633,7 +1633,7 @@ esiPluginInit(int argc, const char *argv[], struct OptionInfo *pOptionInfo)
     gHandlerManager = new HandlerManager(HANDLER_MGR_DEBUG_TAG, &TSDebug, &TSError);
   }
 
-  memset((void *)pOptionInfo, 0, sizeof(struct OptionInfo));
+  new (pOptionInfo) OptionInfo;
 
   if (argc > 1) {
     int c;
