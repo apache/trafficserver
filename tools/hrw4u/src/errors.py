68ceb2f34e71ed48e96052924f3b527e592778ae
Miles Libbey <mlibbey@apache.org>
2025-06-20 16:09:47 -0700

    hrw4u -- accept text from stdin
    Sometimes it it nice to have hrw4u accept input from stdin instead
    of a file

--- tools/hrw4u/src/errors.py
+++ tools/hrw4u/src/errors.py
@@ -48,7 +48,7 @@ class Hrw4uSyntaxError(Exception):
         super().__init__(self._format_error(filename, line, column, message, source_line))
 
     def _format_error(self, filename, line, col, message, source_line):
-        error = f"{filename}:{line}:{col}: error: {message}"
+        error = f"{filename.name}:{line}:{col}: error: {message}"
 
         lineno = f"{line:4d}"
         code_line = f"{lineno} | {source_line}"
@@ -63,7 +63,7 @@ class SymbolResolutionError(Exception):
         super().__init__(message or f"Unrecognized symbol: '{name}'")
 
 
-# Main errror handling function, use this in the visitor and symbols
+# Main error handling function, use this in the visitor and symbols
 def hrw4u_error(filename, ctx, exc):
     if isinstance(exc, Hrw4uSyntaxError):
         return exc
