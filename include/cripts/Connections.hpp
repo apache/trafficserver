a7034845c6a2d6421d94235dddaffd1c33142077
Leif Hedstrom <zwoop@apache.org>
2025-07-29 01:20:36 +0200

    Cripts: Add Geo APIs to the cripts::IP object (#12383)

--- include/cripts/Connections.hpp
+++ include/cripts/Connections.hpp
@@ -77,6 +77,15 @@ public:
   uint64_t Hasher(unsigned ipv4_cidr = 32, unsigned ipv6_cidr = 128);
   bool     Sample(double rate, uint32_t seed = 0, unsigned ipv4_cidr = 32, unsigned ipv6_cidr = 128);
 
+  // Convert IP to sockaddr structure
+  [[nodiscard]] sockaddr Socket() const;
+
+  // Geo-IP functionality - can be used with any IP address
+  [[nodiscard]] cripts::string ASN() const;
+  [[nodiscard]] cripts::string ASNName() const;
+  [[nodiscard]] cripts::string Country() const;
+  [[nodiscard]] cripts::string CountryCode() const;
+
 private:
   char     _str[INET6_ADDRSTRLEN + 1];
   uint64_t _hash    = 0;
