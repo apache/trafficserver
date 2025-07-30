fa32d525cbaa942f613c18d293efd1db221b2771
Brian Neradt <brian.neradt@gmail.com>
2025-07-30 20:08:51 +0000

    Fix user_agent_session_miss for ATS SSL session cache
    proxy.config.ssl.session_cache.value allows the user to configure
    whether OpenSSL internally manages the TLS session cache for resumption
    or whether ATS does via callbacks. For our user_agent_session_* metrics,
    we use the SSL_CTX_sess_* OpenSSL API to populate them. This works for
    hit, timeout, etc, for both when the session cache is managed by OpenSSL
    and when it is managed by ATS, with the exception of
    SSL_CTX_sess_misses. That metric only counts internal cache misses:
    
    > SSL_CTX_sess_misses() returns the number of sessions proposed by
    > clients that were not found in the internal session cache in server
    > mode.
    
    The result is that when ATS manages the session cache, which is the
    default configuration, proxy.process.ssl.user_agent_session_miss is
    always zero. This patch uses our ssl_session_cache_miss metric to
    augment the stat to be accurate for ATS-managed caching.

--- src/iocore/net/SSLStats.h
+++ src/iocore/net/SSLStats.h
@@ -55,11 +55,13 @@ struct SSLStatsBlock {
   Metrics::Counter::AtomicType *origin_server_wrong_version                    = nullptr;
   Metrics::Counter::AtomicType *origin_session_cache_hit                       = nullptr;
   Metrics::Counter::AtomicType *origin_session_cache_miss                      = nullptr;
+  Metrics::Counter::AtomicType *origin_session_cache_timeout                   = nullptr;
   Metrics::Counter::AtomicType *origin_session_reused_count                    = nullptr;
   Metrics::Counter::AtomicType *session_cache_eviction                         = nullptr;
   Metrics::Counter::AtomicType *session_cache_hit                              = nullptr;
   Metrics::Counter::AtomicType *session_cache_lock_contention                  = nullptr;
   Metrics::Counter::AtomicType *session_cache_miss                             = nullptr;
+  Metrics::Counter::AtomicType *session_cache_timeout                          = nullptr;
   Metrics::Counter::AtomicType *session_cache_new_session                      = nullptr;
   Metrics::Counter::AtomicType *sni_name_set_failure                           = nullptr;
   Metrics::Counter::AtomicType *total_attempts_handshake_count_in              = nullptr;
@@ -96,10 +98,17 @@ struct SSLStatsBlock {
   Metrics::Counter::AtomicType *user_agent_version_too_high                    = nullptr;
   Metrics::Counter::AtomicType *user_agent_version_too_low                     = nullptr;
   Metrics::Counter::AtomicType *user_agent_wrong_version                       = nullptr;
-  Metrics::Gauge::AtomicType   *user_agent_session_hit                         = nullptr;
-  Metrics::Gauge::AtomicType   *user_agent_session_miss                        = nullptr;
-  Metrics::Gauge::AtomicType   *user_agent_session_timeout                     = nullptr;
-  Metrics::Gauge::AtomicType   *user_agent_sessions                            = nullptr;
+
+  // Note: The following user_agent_session_* metrics are implemented as Gauge types
+  // even though they semantically represent cumulative counters. This is because
+  // they are periodically synchronized from external counter sources (OpenSSL's
+  // built-in session cache or ATS's session cache) and need to be "set" to specific
+  // values rather than incremented. From a monitoring perspective, these should be
+  // treated as counters for calculating rates.
+  Metrics::Gauge::AtomicType *user_agent_session_hit     = nullptr;
+  Metrics::Gauge::AtomicType *user_agent_session_miss    = nullptr;
+  Metrics::Gauge::AtomicType *user_agent_session_timeout = nullptr;
+  Metrics::Gauge::AtomicType *user_agent_sessions        = nullptr;
 };
 
 extern SSLStatsBlock                                                   ssl_rsb;
