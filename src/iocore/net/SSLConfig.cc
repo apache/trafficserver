7055283e8a141af183b384e2e1a6ac846067667c
Brian Neradt <brian.neradt@gmail.com>
2025-08-04 19:54:12 -0500

    server.honor_cipher_order: Clarify documentation (#12416) (#12420)
    Clarify in records.yaml.en.rst that server.honor_cipher_order controls
    TLS server preference for TLS groups and signature algorithms in
    addition to ciphers.
    
    This also makes use of the SSL_OP_SERVER_PREFERENCE instead of the
    misleading SSL_OP_CIPHER_SERVER_PREFERENCE when available.
    
    Fixes: #12382
    (cherry picked from commit a5beffc4f108362a33c928f15e55d770e9031521)

--- src/iocore/net/SSLConfig.cc
+++ src/iocore/net/SSLConfig.cc
@@ -459,12 +459,17 @@ SSLConfigParams::initialize()
     ats_free(clientALPNProtocols);
   }
 
-#ifdef SSL_OP_CIPHER_SERVER_PREFERENCE
+#if defined(SSL_OP_SERVER_PREFERENCE) || defined(SSL_OP_CIPHER_SERVER_PREFERENCE)
   REC_ReadConfigInteger(option, "proxy.config.ssl.server.honor_cipher_order");
   if (option) {
+    // Prefer the newer, more accurately named flag when available.
+#ifdef SSL_OP_SERVER_PREFERENCE
+    ssl_ctx_options |= SSL_OP_SERVER_PREFERENCE;
+#else
     ssl_ctx_options |= SSL_OP_CIPHER_SERVER_PREFERENCE;
-  }
 #endif
+  }
+#endif // defined(SSL_OP_SERVER_PREFERENCE) || defined(SSL_OP_CIPHER_SERVER_PREFERENCE)
 
 #ifdef SSL_OP_PRIORITIZE_CHACHA
   REC_ReadConfigInteger(option, "proxy.config.ssl.server.prioritize_chacha");
