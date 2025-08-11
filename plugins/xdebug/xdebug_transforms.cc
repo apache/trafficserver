1a7ca941a53b00511034b214cb8b02d3267bf625
mlibbey <mlibbey@apache.org>
2025-08-11 15:52:22 -0400

    XDebug Probe missing comma prevents valid json (#12435)
    The Probe setting outputs a mix of text and json in the body, but, the json is invalid as it misses a comma. For instance:
    
    ```
    --- ATS xDebug Probe Injection Boundary ---
    
    {'xDebugProbeAt' : 'myats.example.com'
       'captured':[{'type':'response', 'side':'server', 'headers': {
    ```

--- plugins/xdebug/xdebug_transforms.cc
+++ plugins/xdebug/xdebug_transforms.cc
@@ -34,7 +34,7 @@ static std::string
 getPreBody(TSHttpTxn txn)
 {
   std::stringstream output;
-  output << "{'xDebugProbeAt' : '" << Hostname << "'\n   'captured':[";
+  output << "{'xDebugProbeAt' : '" << Hostname << "',\n   'captured':[";
   print_request_headers(txn, output);
   output << "\n   ]\n}";
   output << MultipartBoundary;
@@ -46,7 +46,7 @@ getPostBody(TSHttpTxn txn)
 {
   std::stringstream output;
   output << MultipartBoundary;
-  output << "{'xDebugProbeAt' : '" << Hostname << "'\n   'captured':[";
+  output << "{'xDebugProbeAt' : '" << Hostname << "',\n   'captured':[";
   print_response_headers(txn, output);
   output << "\n   ]\n}";
   return output.str();
