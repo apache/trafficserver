84bd7c7089914acfbf4f7d579030ef07e2b84b17
Brian Neradt <brian.neradt@gmail.com>
2025-08-20 15:15:20 +0000

    xdebug probe-full-json: encoding the origin body
    For aything beyond very simple text bodies, JSON parsers for
    probe-full-json were breaking upon the content of the body (such as html
    bodies, for example). This patch adds escaping and hex encoding of body
    content to preserve the JSON format. It also adds a way to not include
    the body if the user so desires.

--- plugins/xdebug/xdebug_types.h
+++ plugins/xdebug/xdebug_types.h
@@ -32,8 +32,19 @@ namespace xdebug
 
 enum class ProbeType { PROBE_STANDARD, PROBE_FULL_JSON };
 
+/** Encoding strategy for embedding the origin server body in probe-full-json
+ * output.
+ */
+enum class BodyEncoding_t {
+  AUTO,      ///< Auto-detect the encoding based on the original response Content-Type.
+  ESCAPE,    ///< JSON-escape the body.
+  HEX,       ///< Hex-encode the body, treating it as binary data.
+  OMIT_BODY, ///< Omit the body entirely.
+};
+
 struct BodyBuilder {
   atscppapi::TSContUniqPtr     transform_connp;
+  atscppapi::TSContUniqPtr     resolve_encoding_connp;
   atscppapi::TSIOBufferUniqPtr output_buffer;
   // It's important that output_reader comes after output_buffer so it will be deleted first.
   atscppapi::TSIOBufferReaderUniqPtr output_reader;
@@ -43,6 +54,9 @@ struct BodyBuilder {
   bool                               hdr_ready     = false;
   std::atomic_flag                   wrote_postbody;
   ProbeType                          probe_type = ProbeType::PROBE_STANDARD;
+
+  BodyEncoding_t body_encoding       = BodyEncoding_t::AUTO;
+  bool           server_body_started = false;
 
   int64_t nbytes = 0;
 };
