a0b7abed36afae92f7facd7ff6aa0f9b29606e1a
Masakazu Kitajo <maskit@apache.org>
2025-09-17 10:37:43 -0600

    Apply the change for private_slot

--- plugins/header_rewrite/operators.cc
+++ plugins/header_rewrite/operators.cc
@@ -1622,5 +1622,8 @@ OperatorSetEffectiveAddress::exec(const Resources &res) const
   private_data.raw       = reinterpret_cast<uint64_t>(TSUserArgGet(res.state.txnp, _txn_private_slot));
   private_data.ip_source = IP_SRC_PLUGIN;
 
+  Dbg(pi_dbg_ctl, "   Setting plugin control %d to %d", static_cast<int>(_name), _value);
+  TSUserArgSet(res.state.txnp, _txn_private_slot, reinterpret_cast<void *>(private_data.raw));
+
   return true;
 }
