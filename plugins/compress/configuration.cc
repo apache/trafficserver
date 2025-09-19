5865e5234ee5a8a79f5e06fc0f44317c5026e4df
jake champion <jake.champion@netlify.com>
2025-09-19 22:29:08 +0100

    Replaces function pointer cast with lambda for isspace

--- plugins/compress/configuration.cc
+++ plugins/compress/configuration.cc
@@ -200,7 +200,7 @@ strip_params(std::string_view v)
 {
   swoc::TextView tv{v};
   tv = tv.take_prefix_at(';');
-  tv.rtrim_if(static_cast<int (*)(int)>(&isspace));
+  tv.rtrim_if([](int ch) { return std::isspace(ch); });
   return tv;
 }
 
