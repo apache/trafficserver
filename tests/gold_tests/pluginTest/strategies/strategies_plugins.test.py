7f1a2216025f00a97c43e8b49679ef876829aab9
Brian Olsen <bnolsen@gmail.com>
2025-09-19 10:02:17 -0600

    convert autest to using 3 origins

--- tests/gold_tests/pluginTest/strategies/strategies_plugins.test.py
+++ tests/gold_tests/pluginTest/strategies/strategies_plugins.test.py
@@ -29,51 +29,52 @@ Combined header_rewrite/regex_remap/tslua strategies tests
 Test.SkipUnless(
     Condition.PluginExists('header_rewrite.so'),
     Condition.PluginExists('regex_remap.so'),
-    Condition.PluginExists('tslua.so'),
+    #Condition.PluginExists('tslua.so'),
 )
 Test.ContinueOnFail = False
 
-# set up dns
 dns = Test.MakeDNServer("dns")
-dns.ReturnCode = Any(0, -2)
 
-ts_nh = []
-num_nh = 3
-
-# Define next hop trafficserver instances.
-# strategy, strategy, parent.config
-for ind in range(num_nh):
+origins = []
+num_origins = 3
+for ind in range(num_origins):
     name = f"nh{ind}"
-    ts = Test.MakeATSProcess(name, use_traffic_out=False, command=f"traffic_server 2>nh_trace{ind}.log")
-    ts.ReturnCode = Any(0, -2)
-    ts.Disk.plugin_config.AddLine("header_rewrite.so hdr_rw.config")
-    ts.Disk.MakeConfigFile("hdr_rw.config").AddLines([
-        "cond %{READ_REQUEST_HDR_HOOK}",
-        "set-status 404",
-    ])
-    ts.Disk.records_config.update(
-        {
-            'proxy.config.http.cache.http': 0,
-            'proxy.config.diags.debug.enabled': 1,
-            'proxy.config.http.insert_response_via_str': 1,
-            'proxy.config.proxy_name': name,
-            'proxy.config.diags.debug.tags': "http|header_rewrite",
-            'proxy.config.dns.nameservers': f"127.0.0.1:{dns.Variables.Port}",
-            'proxy.config.dns.resolv_conf': "NULL",
-        })
-    ts.Disk.remap_config.AddLine("map / http://not_used")
+    origin = Test.MakeOriginServer(name, options={"--verbose": ""})
+    request_header = {
+        "headers": f"GET / HTTP/1.1\r\nHost: origin\r\n\r\n",
+        "timestamp": "1469733493.993",
+        "body": "",
+    }
+    response_header = {
+        "headers": "HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n",
+        "timestamp": "1469733493.993",
+        "body": "",
+    }
+    origin.addResponse("sessionfile.log", request_header, response_header)
+    request_header = {
+        "headers": "GET /path HTTP/1.1\r\nHost: origin\r\n\r\n",
+        "timestamp": "1469733493.993",
+        "body": "",
+    }
+    response_header = {
+        "headers": "HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n",
+        "timestamp": "1469733493.993",
+        "body": name,
+    }
+    origin.addResponse("sessionfile.log", request_header, response_header)
+    origin.ReturnCode = Any(0, -2)
+    origins.append(origin)
     dns.addRecords(records={name: ["127.0.0.1"]})
-    ts_nh.append(ts)
 
 # Define ATS and configure
-ts = Test.MakeATSProcess("ts", use_traffic_out=False, command="traffic_server 2>ts_trace.log")
+ts = Test.MakeATSProcess("ts", enable_cache=False)
 ts.ReturnCode = Any(0, -2)
 ts.Disk.records_config.update(
     {
         'proxy.config.dns.nameservers': f"127.0.0.1:{dns.Variables.Port}",
         'proxy.config.dns.resolv_conf': "NULL",
         'proxy.config.http.cache.http': 0,
-        "proxy.config.http.insert_response_via_str": 0,
+        "proxy.config.http.insert_response_via_str": 1,
         'proxy.config.http.uncacheable_requests_bypass_parent': 0,
         'proxy.config.http.no_dns_just_forward_to_parent': 1,
         'proxy.config.http.parent_proxy.mark_down_hostdb': 0,
@@ -92,14 +93,14 @@ ts.Disk.MakeConfigFile("hdr_rw.config").AddLines(
 
 # parent.config
 ts.Disk.parent_config.AddLines(
-    [f'dest_domain=. parent="nh2:{ts_nh[2].Variables.port}" round_robin=false go_direct=false parent_is_proxy=false'])
+    [f'dest_domain=. parent="nh2:{origins[2].Variables.Port}" round_robin=false go_direct=false parent_is_proxy=false'])
 
 # build strategies.yaml file
 ts.Disk.File(ts.Variables.CONFIGDIR + "/strategies.yaml", id="strategies", typename="ats:config")
 
 s = ts.Disk.strategies
 s.AddLine("groups:")
-for ind in range(num_nh - 1):
+for ind in range(num_origins - 1):
     name = f"nh{ind}"
     s.AddLines(
         [
@@ -107,48 +108,58 @@ for ind in range(num_nh - 1):
             f"    - host: {name}",
             f"      protocol:",
             f"      - scheme: http",
-            f"        port: {ts_nh[ind].Variables.port}",
+            f"        port: {origins[ind].Variables.Port}",
             f"      weight: 1.0",
         ])
 
 s.AddLine("strategies:")
 
 # third ts_nh
-for ind in range(num_nh - 1):
-    name = f"nh{ind}"
+for ind in range(num_origins - 1):
     s.AddLines(
         [
-            f"  - strategy: {name}", f"    policy: consistent_hash", f"    hash_key: path", f"    go_direct: false",
-            f"    parent_is_proxy: true", f"    ignore_self_detect: true", f"    groups:", f"      - *g{ind}", f"    scheme: http"
+            f"  - strategy: strategy-{ind}",
+            f"    policy: consistent_hash",
+            f"    hash_key: path",
+            f"    go_direct: false",
+            f"    parent_is_proxy: false",
+            f"    ignore_self_detect: true",
+            f"    groups:",
+            f"      - *g{ind}",
+            f"    scheme: http",
         ])
 
 ts.Disk.remap_config.AddLines(
     [
-        "map http://nh0/ http://not_used @strategy=nh0",
-        "map http://nh1/ http://not_used @strategy=nh1",
-        "map http://nh2/ http://not_used",
+        "map http://nh0 http://origin @strategy=strategy-0",
+        "map http://nh1 http://origin @strategy=strategy-1",
+        "map http://nh2 http://nh2",
     ])
 
 # Tests
 
-curl_and_args = '-s -D /dev/stdout -o /dev/stderr -x localhost:{}'.format(ts.Variables.port)
+# stdout for body, stderr for headers
+curl_and_args = '-s -o /dev/stdout -D /dev/stderr -x localhost:{}'.format(ts.Variables.port)
 
 # 0 Test - nh0 default request
 tr = Test.AddTestRun("nh0 straight through request")
 ps = tr.Processes.Default
-tr.Processes.Default.StartBefore(dns)
-for ind in range(num_nh):
-    tr.Processes.Default.StartBefore(ts_nh[ind])
-tr.Processes.Default.StartBefore(Test.Processes.ts)
-tr.MakeCurlCommand(curl_and_args + " http://nh0/req", ts=ts)
+for ind in range(num_origins):
+    origin = origins[ind]
+    ps.StartBefore(origin, ready=When.PortOpen(origin.Variables.Port))
+    tr.StillRunningAfter = origin
+ps.StartBefore(dns)
+ps.StartBefore(Test.Processes.ts)
+tr.MakeCurlCommand(curl_and_args + " http://nh0/path", ts=ts)
 ps.ReturnCode = Any(0, -2)
 ps.Streams.stdout.Content = Testers.ContainsExpression("nh0", "expected nh0")
 tr.StillRunningAfter = ts
+tr.StillRunnerAfter = dns
 
 # 1 Test - nh1 default request
 tr = Test.AddTestRun("nh1 straight through request")
 ps = tr.Processes.Default
-tr.MakeCurlCommand(curl_and_args + " http://nh1/req", ts=ts)
+tr.MakeCurlCommand(curl_and_args + " http://nh1/path", ts=ts)
 ps.ReturnCode = Any(0, -2)
 ps.Streams.stdout.Content = Testers.ContainsExpression("nh1", "expected nh1")
 tr.StillRunningAfter = ts
@@ -156,7 +167,7 @@ tr.StillRunningAfter = ts
 # 2 Test - nh2 default request
 tr = Test.AddTestRun("nh2 straight through request")
 ps = tr.Processes.Default
-tr.MakeCurlCommand(curl_and_args + " http://nh2/req", ts=ts)
+tr.MakeCurlCommand(curl_and_args + " http://nh2/path", ts=ts)
 ps.ReturnCode = Any(0, -2)
 ps.Streams.stdout.Content = Testers.ContainsExpression("nh2", "expected nh2")
 tr.StillRunningAfter = ts
