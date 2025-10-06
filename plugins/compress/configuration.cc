bd8878107d72a2c2ad329ada008a5b8d04ecb7eb
jake champion <jake.champion@netlify.com>
2025-10-06 10:52:55 +0100

    remove now unused functions

--- plugins/compress/configuration.cc
+++ plugins/compress/configuration.cc
@@ -39,37 +39,6 @@ namespace Gzip
 {
 using namespace std;
 
-void
-ltrim_if(string &s, int (*fp)(int))
-{
-  for (size_t i = 0; i < s.size();) {
-    if (fp(s[i])) {
-      s.erase(i, 1);
-    } else {
-      break;
-    }
-  }
-}
-
-void
-rtrim_if(string &s, int (*fp)(int))
-{
-  for (ssize_t i = static_cast<ssize_t>(s.size()) - 1; i >= 0; i--) {
-    if (fp(s[i])) {
-      s.erase(i, 1);
-    } else {
-      break;
-    }
-  }
-}
-
-void
-trim_if(string &s, int (*fp)(int))
-{
-  ltrim_if(s, fp);
-  rtrim_if(s, fp);
-}
-
 swoc::TextView
 extractFirstToken(swoc::TextView &view, int (*fp)(int))
 {
