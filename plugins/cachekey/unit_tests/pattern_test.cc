246ad5cb09e2afd31b53f14866d22e619edffdaa
Bryan Call <bcall@apache.org>
2025-10-06 12:58:44 -0700

    Added more tests

--- plugins/cachekey/unit_tests/pattern_test.cc
+++ plugins/cachekey/unit_tests/pattern_test.cc
@@ -57,4 +57,65 @@ TEST_CASE("Pattern compile and match behavior", "[cachekey][pattern]")
     // malformed pattern (unclosed parentheses)
     CHECK(p.init("(unclosed") == false);
   }
+
+  SECTION("Greedy vs non-greedy capture")
+  {
+    Pattern pg;
+    Pattern png;
+    REQUIRE(pg.init("a(.*)b"));   // greedy
+    REQUIRE(png.init("a(.*?)b")); // non-greedy
+
+    StringVector caps_g;
+    StringVector caps_ng;
+    REQUIRE(pg.capture("a123b456b", caps_g));
+    REQUIRE(png.capture("a123b456b", caps_ng));
+
+    // greedy should capture up to the last 'b'
+    CHECK(caps_g.size() >= 2);
+    CHECK(caps_g[1] == "123b456");
+
+    // non-greedy should capture up to the first 'b'
+    CHECK(caps_ng.size() >= 2);
+    CHECK(caps_ng[1] == "123");
+  }
+
+  SECTION("Empty-string anchors")
+  {
+    Pattern p;
+    REQUIRE(p.init("^$"));
+    // Pattern::match uses PCRE_NOTEMPTY which prevents empty-string matches.
+    // Therefore '^$' will NOT match an empty subject with the current implementation.
+    CHECK(p.match("") == false);
+    CHECK(p.match("not-empty") == false);
+  }
+
+  SECTION("Case-insensitive inline flag")
+  {
+    Pattern p;
+    // PCRE inline flag for case-insensitive
+    REQUIRE(p.init("(?i)AbC"));
+    CHECK(p.match("aBc") == true);
+    CHECK(p.match("ABC") == true);
+  }
+
+  SECTION("Repeated captures and empty captures")
+  {
+    Pattern p;
+    REQUIRE(p.init("(\\w*)-(\\w*)"));
+    StringVector caps;
+    REQUIRE(p.capture("-foo", caps));
+    CHECK(caps.size() == 3);
+    // first group before '-' is empty
+    CHECK(caps[1] == "");
+    CHECK(caps[2] == "foo");
+  }
+
+  SECTION("Long subject match")
+  {
+    Pattern p;
+    REQUIRE(p.init("^a+$"));
+    // create a long string of 'a' characters
+    std::string long_s(10000, 'a');
+    CHECK(p.match(long_s.c_str()) == true);
+  }
 }
