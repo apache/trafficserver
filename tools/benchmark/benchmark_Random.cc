b49b38742730e6ec6bd3e3a54050b95a7901a410
Chris McFarlen <cmcfarlen@apple.com>
2025-10-24 17:22:53 -0500

    Add distribution test

--- tools/benchmark/benchmark_Random.cc
+++ tools/benchmark/benchmark_Random.cc
@@ -23,9 +23,12 @@ limitations under the License.
 */
 
 #include "tscore/ink_rand.h"
+#include <cmath>
+#include <limits>
 #define CATCH_CONFIG_ENABLE_BENCHMARKING
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/generators/catch_generators_random.hpp>
 
 #include "tscore/Random.h"
 
@@ -93,4 +96,31 @@ TEST_CASE("BenchRandom", "[bench][random]")
     }
     return sum;
   };
+}
+
+TEST_CASE("RandomDistribution", "[random][distribution]")
+{
+  auto          g = Catch::Generators::random(std::numeric_limits<uint64_t>::min(), std::numeric_limits<uint64_t>::max());
+  InkRand       gen(g.get());
+  int           iterations      = 1000000;
+  constexpr int buckets         = 100;
+  int           counts[buckets] = {0};
+
+  for (int i = 0; i < iterations; i++) {
+    counts[gen.random() % buckets]++;
+  }
+
+  double expected = static_cast<double>(iterations) / static_cast<double>(buckets);
+
+  double min = std::numeric_limits<double>::max();
+  double max = std::numeric_limits<double>::min();
+
+  for (int count : counts) {
+    double ratio = static_cast<double>(count) / expected;
+
+    min = std::min(ratio, min);
+    max = std::max(ratio, max);
+  }
+  REQUIRE(0.95 < min);
+  REQUIRE(max < 1.05);
 }
