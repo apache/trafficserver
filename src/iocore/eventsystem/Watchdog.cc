f4a5d4680f2786885dd4d0a2e52f9b8d8480ec28
Mo Chen <mochen@apache.org>
2025-11-10 11:13:22 -0600

    Upgrade the assert in Monitor constructor

--- src/iocore/eventsystem/Watchdog.cc
+++ src/iocore/eventsystem/Watchdog.cc
@@ -44,7 +44,7 @@ Monitor::Monitor(EThread *threads[], size_t n_threads, std::chrono::milliseconds
 {
   // Precondition: timeout_ms must be > 0. A timeout of 0 indicates the watchdog is disabled
   // and the caller should not instantiate the Monitor (see traffic_server.cc).
-  ink_assert(timeout_ms.count() > 0);
+  ink_release_assert(timeout_ms.count() > 0);
   _watchdog_thread = std::thread(std::bind_front(&Monitor::monitor_loop, this));
 }
 
