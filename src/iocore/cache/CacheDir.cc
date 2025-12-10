apache/trafficserver

commit 14f2d496aaf4b1669f5dd88730a833883d8dbcc9

Rename Vol to Stripe (#10666)
* Rename struct Vol to Stripe

* Fix clang-format

* Fix docs

* Rename DiskVol to DiskStripe

* Fix comments

src/iocore/cache/CacheDir.cc
@@ -213,7 +213,7 @@ dir_bucket_loop_check(Dir *start_dir, Dir *seg)
 // adds all the directory entries
 // in a segment to the segment freelist
 void
-dir_init_segment(int s, Vol *vol)
+dir_init_segment(int s, Stripe *vol)
 {
   vol->header->freelist[s] = 0;
   Dir *seg                 = vol->dir_segment(s);
@@ -230,7 +230,7 @@ dir_init_segment(int s, Vol *vol)
 // break the infinite loop in directory entries
 // Note : abuse of the token bit in dir entries
 int
-dir_bucket_loop_fix(Dir *start_dir, int s, Vol *vol)
+dir_bucket_loop_fix(Dir *start_dir, int s, Stripe *vol)
 {
   if (!dir_bucket_loop_check(start_dir, vol->dir_segment(s))) {
     Warning("Dir loop exists, clearing segment %d", s);
@@ -241,7 +241,7 @@ dir_bucket_loop_fix(Dir *start_dir, int s, Vol *vol)
 }
 
 int
-dir_freelist_length(Vol *vol, int s)
+dir_freelist_length(Stripe *vol, int s)
 {
   int free = 0;
   Dir *seg = vol->dir_segment(s);
@@ -257,7 +257,7 @@ dir_freelist_length(Vol *vol, int s)
 }
 
 int
-dir_bucket_length(Dir *b, int s, Vol *vol)
+dir_bucket_length(Dir *b, int s, Stripe *vol)
 {
   Dir *e   = b;
   int i    = 0;
@@ -277,7 +277,7 @@ dir_bucket_length(Dir *b, int s, Vol *vol)
 }
 
 int
-check_dir(Vol *vol)
+check_dir(Stripe *vol)
 {
   int i, s;
   Dbg(dbg_ctl_cache_check_dir, "inside check dir");
@@ -300,7 +300,7 @@ check_dir(Vol *vol)
 }
 
 inline void
-unlink_from_freelist(Dir *e, int s, Vol *vol)
+unlink_from_freelist(Dir *e, int s, Stripe *vol)
 {
   Dir *seg = vol->dir_segment(s);
   Dir *p   = dir_from_offset(dir_prev(e), seg);
@@ -316,7 +316,7 @@ unlink_from_freelist(Dir *e, int s, Vol *vol)
 }
 
 inline Dir *
-dir_delete_entry(Dir *e, Dir *p, int s, Vol *vol)
+dir_delete_entry(Dir *e, Dir *p, int s, Stripe *vol)
 {
   Dir *seg           = vol->dir_segment(s);
   int no             = dir_next(e);
@@ -346,7 +346,7 @@ dir_delete_entry(Dir *e, Dir *p, int s, Vol *vol)
 }
 
 inline void
-dir_clean_bucket(Dir *b, int s, Vol *vol)
+dir_clean_bucket(Dir *b, int s, Stripe *vol)
 {
   Dir *e = b, *p = nullptr;
   Dir *seg = vol->dir_segment(s);
@@ -363,7 +363,7 @@ dir_clean_bucket(Dir *b, int s, Vol *vol)
 #endif
     if (!dir_valid(vol, e) || !dir_offset(e)) {
       if (dbg_ctl_dir_clean.on()) {
-        Dbg(dbg_ctl_dir_clean, "cleaning Vol:%s: %p tag %X boffset %" PRId64 " b %p p %p bucket len %d", vol->hash_text.get(), e,
+        Dbg(dbg_ctl_dir_clean, "cleaning Stripe:%s: %p tag %X boffset %" PRId64 " b %p p %p bucket len %d", vol->hash_text.get(), e,
             dir_tag(e), dir_offset(e), b, p, dir_bucket_length(b, s, vol));
       }
       if (dir_offset(e)) {
@@ -379,7 +379,7 @@ dir_clean_bucket(Dir *b, int s, Vol *vol)
 }
 
 void
-dir_clean_segment(int s, Vol *vol)
+dir_clean_segment(int s, Stripe *vol)
 {
   Dir *seg = vol->dir_segment(s);
   for (int64_t i = 0; i < vol->buckets; i++) {
@@ -389,7 +389,7 @@ dir_clean_segment(int s, Vol *vol)
 }
 
 void
-dir_clean_vol(Vol *vol)
+dir_clean_vol(Stripe *vol)
 {
   for (int64_t i = 0; i < vol->segments; i++) {
     dir_clean_segment(i, vol);
@@ -398,7 +398,7 @@ dir_clean_vol(Vol *vol)
 }
 
 void
-dir_clear_range(off_t start, off_t end, Vol *vol)
+dir_clear_range(off_t start, off_t end, Stripe *vol)
 {
   for (off_t i = 0; i < vol->buckets * DIR_DEPTH * vol->segments; i++) {
     Dir *e = dir_index(vol, i);
@@ -425,7 +425,7 @@ check_bucket_not_contains(Dir *b, Dir *e, Dir *seg)
 }
 
 void
-freelist_clean(int s, Vol *vol)
+freelist_clean(int s, Stripe *vol)
 {
   dir_clean_segment(s, vol);
   if (vol->header->freelist[s]) {
@@ -449,7 +449,7 @@ freelist_clean(int s, Vol *vol)
 }
 
 inline Dir *
-freelist_pop(int s, Vol *vol)
+freelist_pop(int s, Stripe *vol)
 {
   Dir *seg = vol->dir_segment(s);
   Dir *e   = dir_from_offset(vol->header->freelist[s], seg);
@@ -471,7 +471,7 @@ freelist_pop(int s, Vol *vol)
 }
 
 int
-dir_segment_accounted(int s, Vol *vol, int offby, int *f, int *u, int *et, int *v, int *av, int *as)
+dir_segment_accounted(int s, Stripe *vol, int offby, int *f, int *u, int *et, int *v, int *av, int *as)
 {
   int free = dir_freelist_length(vol, s);
   int used = 0, empty = 0;
@@ -524,7 +524,7 @@ dir_segment_accounted(int s, Vol *vol, int offby, int *f, int *u, int *et, int *
 }
 
 void
-dir_free_entry(Dir *e, int s, Vol *vol)
+dir_free_entry(Dir *e, int s, Stripe *vol)
 {
   Dir *seg        = vol->dir_segment(s);
   unsigned int fo = vol->header->freelist[s];
@@ -537,7 +537,7 @@ dir_free_entry(Dir *e, int s, Vol *vol)
 }
 
 int
-dir_probe(const CacheKey *key, Vol *vol, Dir *result, Dir **last_collision)
+dir_probe(const CacheKey *key, Stripe *vol, Dir *result, Dir **last_collision)
 {
   ink_assert(vol->mutex->thread_holding == this_ethread());
   int s    = key->slice32(0) % vol->segments;
@@ -606,7 +606,7 @@ Lagain:
 }
 
 int
-dir_insert(const CacheKey *key, Vol *vol, Dir *to_part)
+dir_insert(const CacheKey *key, Stripe *vol, Dir *to_part)
 {
   ink_assert(vol->mutex->thread_holding == this_ethread());
   int s  = key->slice32(0) % vol->segments, l;
@@ -674,7 +674,7 @@ Lfill:
 }
 
 int
-dir_overwrite(const CacheKey *key, Vol *vol, Dir *dir, Dir *overwrite, bool must_overwrite)
+dir_overwrite(const CacheKey *key, Stripe *vol, Dir *dir, Dir *overwrite, bool must_overwrite)
 {
   ink_assert(vol->mutex->thread_holding == this_ethread());
   int s          = key->slice32(0) % vol->segments, l;
@@ -761,7 +761,7 @@ Lfill:
 }
 
 int
-dir_delete(const CacheKey *key, Vol *vol, Dir *del)
+dir_delete(const CacheKey *key, Stripe *vol, Dir *del)
 {
   ink_assert(vol->mutex->thread_holding == this_ethread());
   int s    = key->slice32(0) % vol->segments;
@@ -801,7 +801,7 @@ dir_delete(const CacheKey *key, Vol *vol, Dir *del)
 // Lookaside Cache
 
 int
-dir_lookaside_probe(const CacheKey *key, Vol *vol, Dir *result, EvacuationBlock **eblock)
+dir_lookaside_probe(const CacheKey *key, Stripe *vol, Dir *result, EvacuationBlock **eblock)
 {
   ink_assert(vol->mutex->thread_holding == this_ethread());
   int i              = key->slice32(3) % LOOKASIDE_SIZE;
@@ -824,7 +824,7 @@ dir_lookaside_probe(const CacheKey *key, Vol *vol, Dir *result, EvacuationBlock 
 }
 
 int
-dir_lookaside_insert(EvacuationBlock *eblock, Vol *vol, Dir *to)
+dir_lookaside_insert(EvacuationBlock *eblock, Stripe *vol, Dir *to)
 {
   CacheKey *key = &eblock->evac_frags.earliest_key;
   DDbg(dbg_ctl_dir_lookaside, "insert %X %X, offset %d phase %d", key->slice32(0), key->slice32(1), (int)dir_offset(to),
@@ -843,7 +843,7 @@ dir_lookaside_insert(EvacuationBlock *eblock, Vol *vol, Dir *to)
 }
 
 int
-dir_lookaside_fixup(const CacheKey *key, Vol *vol)
+dir_lookaside_fixup(const CacheKey *key, Stripe *vol)
 {
   ink_assert(vol->mutex->thread_holding == this_ethread());
   int i              = key->slice32(3) % LOOKASIDE_SIZE;
@@ -866,7 +866,7 @@ dir_lookaside_fixup(const CacheKey *key, Vol *vol)
 }
 
 void
-dir_lookaside_cleanup(Vol *vol)
+dir_lookaside_cleanup(Stripe *vol)
 {
   ink_assert(vol->mutex->thread_holding == this_ethread());
   for (auto &i : vol->lookaside) {
@@ -889,7 +889,7 @@ dir_lookaside_cleanup(Vol *vol)
 }
 
 void
-dir_lookaside_remove(const CacheKey *key, Vol *vol)
+dir_lookaside_remove(const CacheKey *key, Stripe *vol)
 {
   ink_assert(vol->mutex->thread_holding == this_ethread());
   int i              = key->slice32(3) % LOOKASIDE_SIZE;
@@ -931,7 +931,7 @@ CacheSync::aio_write(int fd, char *b, int n, off_t o)
 }
 
 uint64_t
-dir_entries_used(Vol *vol)
+dir_entries_used(Stripe *vol)
 {
   uint64_t full  = 0;
   uint64_t sfull = 0;
@@ -978,7 +978,7 @@ sync_cache_dir_on_shutdown()
     // dont release the volume's lock, there could
     // be another aggWrite in progress
     MUTEX_TAKE_LOCK(gvol[i]->mutex, t);
-    Vol *vol = gvol[i];
+    Stripe *vol = gvol[i];
 
     if (DISK_BAD(vol->disk)) {
       Dbg(dbg_ctl_cache_dir_sync, "Dir %s: ignoring -- bad disk", vol->hash_text.get());
@@ -1090,7 +1090,7 @@ Lrestart:
     return EVENT_CONT;
   }
 
-  Vol *vol = gvol[vol_idx]; // must be named "vol" to make STAT macros work.
+  Stripe *vol = gvol[vol_idx]; // must be named "vol" to make STAT macros work.
 
   if (event == AIO_EVENT_DONE) {
     // AIO Thread
@@ -1122,7 +1122,7 @@ Lrestart:
       goto Ldone;
     }
 
-    int headerlen = ROUND_TO_STORE_BLOCK(sizeof(VolHeaderFooter));
+    int headerlen = ROUND_TO_STORE_BLOCK(sizeof(StripteHeaderFooter));
     size_t dirlen = vol->dirlen();
     if (!writepos) {
       // start

src/iocore/cache/P_CacheDir.h
@@ -31,7 +31,7 @@
 // aio
 #include "iocore/aio/AIO.h"
 
-struct Vol;
+struct Stripe;
 struct InterimCacheVol;
 struct CacheVC;
 class CacheEvacuateDocVC;
@@ -203,7 +203,7 @@ struct Dir {
 // INKqa11166 - Cache can not store 2 HTTP alternates simultaneously.
 // To allow this, move the vector from the CacheVC to the OpenDirEntry.
 // Each CacheVC now maintains a pointer to this vector. Adding/Deleting
-// alternates from this vector is done under the Vol::lock. The alternate
+// alternates from this vector is done under the Stripe::lock. The alternate
 // is deleted/inserted into the vector just before writing the vector disk
 // (CacheVC::updateVector).
 LINK_FORWARD_DECLARATION(CacheVC, opendir_link) // forward declaration
@@ -264,29 +264,29 @@ struct CacheSync : public Continuation {
 
 // Global Functions
 
-int dir_probe(const CacheKey *, Vol *, Dir *, Dir **);
-int dir_insert(const CacheKey *key, Vol *vol, Dir *to_part);
-int dir_overwrite(const CacheKey *key, Vol *vol, Dir *to_part, Dir *overwrite, bool must_overwrite = true);
-int dir_delete(const CacheKey *key, Vol *vol, Dir *del);
-int dir_lookaside_probe(const CacheKey *key, Vol *vol, Dir *result, EvacuationBlock **eblock);
-int dir_lookaside_insert(EvacuationBlock *b, Vol *vol, Dir *to);
-int dir_lookaside_fixup(const CacheKey *key, Vol *vol);
-void dir_lookaside_cleanup(Vol *vol);
-void dir_lookaside_remove(const CacheKey *key, Vol *vol);
-void dir_free_entry(Dir *e, int s, Vol *vol);
+int dir_probe(const CacheKey *, Stripe *, Dir *, Dir **);
+int dir_insert(const CacheKey *key, Stripe *vol, Dir *to_part);
+int dir_overwrite(const CacheKey *key, Stripe *vol, Dir *to_part, Dir *overwrite, bool must_overwrite = true);
+int dir_delete(const CacheKey *key, Stripe *vol, Dir *del);
+int dir_lookaside_probe(const CacheKey *key, Stripe *vol, Dir *result, EvacuationBlock **eblock);
+int dir_lookaside_insert(EvacuationBlock *b, Stripe *vol, Dir *to);
+int dir_lookaside_fixup(const CacheKey *key, Stripe *vol);
+void dir_lookaside_cleanup(Stripe *vol);
+void dir_lookaside_remove(const CacheKey *key, Stripe *vol);
+void dir_free_entry(Dir *e, int s, Stripe *vol);
 void dir_sync_init();
-int check_dir(Vol *vol);
-void dir_clean_vol(Vol *vol);
-void dir_clear_range(off_t start, off_t end, Vol *vol);
-int dir_segment_accounted(int s, Vol *vol, int offby = 0, int *free = nullptr, int *used = nullptr, int *empty = nullptr,
+int check_dir(Stripe *vol);
+void dir_clean_vol(Stripe *vol);
+void dir_clear_range(off_t start, off_t end, Stripe *vol);
+int dir_segment_accounted(int s, Stripe *vol, int offby = 0, int *free = nullptr, int *used = nullptr, int *empty = nullptr,
                           int *valid = nullptr, int *agg_valid = nullptr, int *avg_size = nullptr);
-uint64_t dir_entries_used(Vol *vol);
+uint64_t dir_entries_used(Stripe *vol);
 void sync_cache_dir_on_shutdown();
-int dir_freelist_length(Vol *vol, int s);
+int dir_freelist_length(Stripe *vol, int s);
 
-int dir_bucket_length(Dir *b, int s, Vol *vol);
-int dir_freelist_length(Vol *vol, int s);
-void dir_clean_segment(int s, Vol *vol);
+int dir_bucket_length(Dir *b, int s, Stripe *vol);
+int dir_freelist_length(Stripe *vol, int s);
+void dir_clean_segment(int s, Stripe *vol);
 
 // Inline Functions
 

commit d215dd0a45011b79be132b4aac51365ac2a92312

Rename `Stripte` identifier prefix to `Stripe`
The `StripteHeaderFooter` typo was introduced in 14f2d496aa and reported by Leif.

src/iocore/cache/CacheDir.cc
@@ -990,7 +990,7 @@ Lrestart:
       goto Ldone;
     }
 
-    int    headerlen = ROUND_TO_STORE_BLOCK(sizeof(StripteHeaderFooter));
+    int    headerlen = ROUND_TO_STORE_BLOCK(sizeof(StripeHeaderFooter));
     size_t dirlen    = stripe->dirlen();
     if (!writepos) {
       // start

src/iocore/cache/P_CacheDir.h
@@ -252,7 +252,7 @@ struct CacheSync : public Continuation {
   CacheSync() : Continuation(new_ProxyMutex()) { SET_HANDLER(&CacheSync::mainEvent); }
 };
 
-struct StripteHeaderFooter {
+struct StripeHeaderFooter {
   unsigned int      magic;
   ts::VersionNumber version;
   time_t            create_time;
@@ -271,14 +271,14 @@ struct StripteHeaderFooter {
 };
 
 struct Directory {
-  char                *raw_dir{nullptr};
-  Dir                 *dir{};
-  StripteHeaderFooter *header{};
-  StripteHeaderFooter *footer{};
-  int                  segments{};
-  off_t                buckets{};
-  size_t               raw_dir_size{0};     // size of raw_dir allocation (for freeing hugepages)
-  bool                 raw_dir_huge{false}; // true if raw_dir was allocated with hugepages
+  char               *raw_dir{nullptr};
+  Dir                *dir{};
+  StripeHeaderFooter *header{};
+  StripeHeaderFooter *footer{};
+  int                 segments{};
+  off_t               buckets{};
+  size_t              raw_dir_size{0};     // size of raw_dir allocation (for freeing hugepages)
+  bool                raw_dir_huge{false}; // true if raw_dir was allocated with hugepages
 
   /* Total number of dir entries.
    */

src/iocore/cache/Stripe.cc
@@ -41,7 +41,7 @@ namespace
 
 DbgCtl dbg_ctl_cache_init{"cache_init"};
 
-constexpr int DIRECTORY_FOOTER_SIZE{ROUND_TO_STORE_BLOCK(sizeof(StripteHeaderFooter))};
+constexpr int DIRECTORY_FOOTER_SIZE{ROUND_TO_STORE_BLOCK(sizeof(StripeHeaderFooter))};
 
 int
 compare_ushort(void const *a, void const *b)
@@ -164,9 +164,9 @@ Stripe::_init_directory(std::size_t directory_size, int header_size, int footer_
   }
   this->directory.raw_dir_size = directory_size;
   this->directory.dir          = reinterpret_cast<Dir *>(this->directory.raw_dir + header_size);
-  this->directory.header       = reinterpret_cast<StripteHeaderFooter *>(this->directory.raw_dir);
+  this->directory.header       = reinterpret_cast<StripeHeaderFooter *>(this->directory.raw_dir);
   std::size_t const footer_offset{directory_size - static_cast<std::size_t>(footer_size)};
-  this->directory.footer = reinterpret_cast<StripteHeaderFooter *>(this->directory.raw_dir + footer_offset);
+  this->directory.footer = reinterpret_cast<StripeHeaderFooter *>(this->directory.raw_dir + footer_offset);
 }
 
 Stripe::~Stripe()

src/iocore/cache/Stripe.h
@@ -167,7 +167,7 @@ Stripe::round_to_approx_size(uint32_t l) const
 inline int
 Stripe::headerlen() const
 {
-  return ROUND_TO_STORE_BLOCK(sizeof(StripteHeaderFooter) + sizeof(uint16_t) * (this->directory.segments - 1));
+  return ROUND_TO_STORE_BLOCK(sizeof(StripeHeaderFooter) + sizeof(uint16_t) * (this->directory.segments - 1));
 }
 
 inline size_t
@@ -175,7 +175,7 @@ Stripe::dirlen() const
 {
   return this->headerlen() +
          ROUND_TO_STORE_BLOCK(((size_t)this->directory.buckets) * DIR_DEPTH * this->directory.segments * SIZEOF_DIR) +
-         ROUND_TO_STORE_BLOCK(sizeof(StripteHeaderFooter));
+         ROUND_TO_STORE_BLOCK(sizeof(StripeHeaderFooter));
 }
 
 /**

src/iocore/cache/StripeSM.cc
@@ -179,7 +179,7 @@ StripeSM::init(bool clear)
   }
 
   init_info           = new StripeInitInfo();
-  int   footerlen     = ROUND_TO_STORE_BLOCK(sizeof(StripteHeaderFooter));
+  int   footerlen     = ROUND_TO_STORE_BLOCK(sizeof(StripeHeaderFooter));
   off_t footer_offset = this->dirlen() - footerlen;
   // try A
   off_t as = skip;
@@ -222,7 +222,7 @@ StripeSM::handle_dir_clear(int event, void *data)
       /* clear the header for directory B. We don't need to clear the
          whole of directory B. The header for directory B starts at
          skip + len */
-      op->aiocb.aio_nbytes = ROUND_TO_STORE_BLOCK(sizeof(StripteHeaderFooter));
+      op->aiocb.aio_nbytes = ROUND_TO_STORE_BLOCK(sizeof(StripeHeaderFooter));
       op->aiocb.aio_offset = skip + dir_len;
       ink_assert(ink_aio_write(op));
       return EVENT_DONE;
@@ -566,7 +566,7 @@ Ldone: {
     aio->thread           = AIO_CALLBACK_THREAD_ANY;
     aio->then             = (i < 2) ? &(init_info->vol_aio[i + 1]) : nullptr;
   }
-  int    footerlen = ROUND_TO_STORE_BLOCK(sizeof(StripteHeaderFooter));
+  int    footerlen = ROUND_TO_STORE_BLOCK(sizeof(StripeHeaderFooter));
   size_t dirlen    = this->dirlen();
   int    B         = directory.header->sync_serial & 1;
   off_t  ss        = skip + (B ? dirlen : 0);
@@ -628,14 +628,14 @@ new_DocEvacuator(int nbytes, StripeSM *stripe)
 int
 StripeSM::handle_header_read(int event, void *data)
 {
-  AIOCallback         *op;
-  StripteHeaderFooter *hf[4];
+  AIOCallback        *op;
+  StripeHeaderFooter *hf[4];
   switch (event) {
   case AIO_EVENT_DONE:
     op = static_cast<AIOCallback *>(data);
     for (auto &i : hf) {
       ink_assert(op != nullptr);
-      i = static_cast<StripteHeaderFooter *>(op->aiocb.aio_buf);
+      i = static_cast<StripeHeaderFooter *>(op->aiocb.aio_buf);
       if (!op->ok()) {
         Note("Header read failed: clearing cache directory %s", this->hash_text.get());
         clear_dir_aio();

src/iocore/cache/unit_tests/test_Stripe.cc
@@ -116,7 +116,7 @@ attach_tmpfile_to_stripe(StripeSM &stripe)
 // We can't return a stripe from this function because the copy
 // and move constructors are deleted.
 static std::FILE *
-init_stripe_for_writing(StripeSM &stripe, StripteHeaderFooter &header, CacheVol &cache_vol)
+init_stripe_for_writing(StripeSM &stripe, StripeHeaderFooter &header, CacheVol &cache_vol)
 {
   stripe.cache_vol                             = &cache_vol;
   cache_rsb.write_bytes                        = ts::Metrics::Counter::createPtr("unit_test.write.bytes");
@@ -200,12 +200,12 @@ TEST_CASE("aggWrite behavior with f.evacuator unset")
 {
   CacheDisk disk;
   init_disk(disk);
-  StripeSM            stripe{&disk, 10, 0};
-  StripteHeaderFooter header;
-  CacheVol            cache_vol;
-  auto               *file{init_stripe_for_writing(stripe, header, cache_vol)};
-  WaitingVC           vc{&stripe};
-  char const         *source = "yay";
+  StripeSM           stripe{&disk, 10, 0};
+  StripeHeaderFooter header;
+  CacheVol           cache_vol;
+  auto              *file{init_stripe_for_writing(stripe, header, cache_vol)};
+  WaitingVC          vc{&stripe};
+  char const        *source = "yay";
   vc.set_test_data(source, 4);
   vc.set_write_len(4);
   vc.set_agg_len(stripe.round_to_approx_size(vc.write_len + vc.header_len + vc.frag_len));
@@ -309,14 +309,14 @@ TEST_CASE("aggWrite behavior with f.evacuator set")
 {
   CacheDisk disk;
   init_disk(disk);
-  StripeSM            stripe{&disk, 10, 0};
-  StripteHeaderFooter header;
-  CacheVol            cache_vol;
-  auto               *file{init_stripe_for_writing(stripe, header, cache_vol)};
-  WaitingVC           vc{&stripe};
-  char               *source = new char[sizeof(Doc) + 4]{};
-  const char         *yay    = "yay";
-  Doc                 doc{};
+  StripeSM           stripe{&disk, 10, 0};
+  StripeHeaderFooter header;
+  CacheVol           cache_vol;
+  auto              *file{init_stripe_for_writing(stripe, header, cache_vol)};
+  WaitingVC          vc{&stripe};
+  char              *source = new char[sizeof(Doc) + 4]{};
+  const char        *yay    = "yay";
+  Doc                doc{};
   doc.magic     = DOC_MAGIC;
   doc.len       = sizeof(Doc) + 4;
   doc.total_len = 4;