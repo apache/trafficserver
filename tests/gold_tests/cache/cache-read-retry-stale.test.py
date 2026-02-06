b372261a8d063a7105599acf8049ff71e5f45885
Evan Zelkowitz <eze@apache.org>
2026-02-06 13:34:42 -0700

    Clean up comments in cache-read-retry-stale test
    Removed unnecessary comment about test case.

--- tests/gold_tests/cache/cache-read-retry-stale.test.py
+++ tests/gold_tests/cache/cache-read-retry-stale.test.py
@@ -1,6 +1,3 @@
-'''
-Test cache_open_write_fail_action = 6 (READ_RETRY with stale fallback)
-'''
 #  Licensed to the Apache Software Foundation (ASF) under one
 #  or more contributor license agreements.  See the NOTICE file
 #  distributed with this work for additional information
