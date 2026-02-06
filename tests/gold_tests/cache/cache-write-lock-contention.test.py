37980431f09fe5aaacfaf66ff85c944a99250489
Evan Zelkowitz <e_zelkowitz@apple.com>
2026-02-06 13:44:36 -0700

    formatting

--- tests/gold_tests/cache/cache-write-lock-contention.test.py
+++ tests/gold_tests/cache/cache-write-lock-contention.test.py
@@ -13,7 +13,6 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-
 '''
 Test cache write lock contention behavior with parallel requests for actions 5 and 6.
 
