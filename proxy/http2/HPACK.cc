71f0d0ea4b18d2cea25251f4e54aa8903a8e2515
Brian Neradt <brian.neradt@gmail.com>
2026-05-19 14:38:29 -0500

    9.2.x: Reject oversized HPACK indexes

--- proxy/http2/HPACK.cc
+++ proxy/http2/HPACK.cc
@@ -667,7 +667,11 @@ decode_indexed_header_field(MIMEFieldWrapper &header, const uint8_t *buf_start, 
     return HPACK_ERROR_COMPRESSION_ERROR;
   }
 
-  if (indexing_table.get_header_field(index, header) == HPACK_ERROR_COMPRESSION_ERROR) {
+  if (index > UINT32_MAX) {
+    return HPACK_ERROR_COMPRESSION_ERROR;
+  }
+
+  if (indexing_table.get_header_field(static_cast<uint32_t>(index), header) == HPACK_ERROR_COMPRESSION_ERROR) {
     return HPACK_ERROR_COMPRESSION_ERROR;
   }
 
@@ -712,13 +716,17 @@ decode_literal_header_field(MIMEFieldWrapper &header, const uint8_t *buf_start, 
     return HPACK_ERROR_COMPRESSION_ERROR;
   }
 
+  if (index > UINT32_MAX) {
+    return HPACK_ERROR_COMPRESSION_ERROR;
+  }
+
   p += len;
 
   Arena arena;
 
   // Decode header field name
   if (index) {
-    if (indexing_table.get_header_field(index, header) == HPACK_ERROR_COMPRESSION_ERROR) {
+    if (indexing_table.get_header_field(static_cast<uint32_t>(index), header) == HPACK_ERROR_COMPRESSION_ERROR) {
       return HPACK_ERROR_COMPRESSION_ERROR;
     }
   } else {
