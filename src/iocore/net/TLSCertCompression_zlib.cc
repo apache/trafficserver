4f1d3594dde2f2e7ce2b0d3496cde9bd06082c4e
Chris McFarlen <cmcfarlen@apple.com>
2026-05-21 15:35:16 -0500

    Revert "Cap uncompressed length in TLS Certificate Compression (#13117)"
    This reverts commit a65725d423dfc32906e7acec6696ac9acbf91236.

--- src/iocore/net/TLSCertCompression_zlib.cc
+++ src/iocore/net/TLSCertCompression_zlib.cc
@@ -22,7 +22,6 @@
  */
 
 #include "TLSCertCompression_zlib.h"
-#include "TLSCertCompression.h"
 #include "SSLStats.h"
 #include <openssl/ssl.h>
 #include <zlib.h>
@@ -54,12 +53,6 @@ compression_func_zlib(SSL * /* ssl */, CBB *out, const uint8_t *in, size_t in_le
 int
 decompression_func_zlib(SSL * /* ssl */, CRYPTO_BUFFER **out, size_t uncompressed_len, const uint8_t *in, size_t in_len)
 {
-  if (uncompressed_len > MAX_CERT_UNCOMPRESSED_LEN) {
-    *out = nullptr;
-    Metrics::Counter::increment(ssl_rsb.cert_decompress_zlib_failure);
-    return 0;
-  }
-
   uint8_t *buf;
 
   *out = CRYPTO_BUFFER_alloc(&buf, uncompressed_len);
