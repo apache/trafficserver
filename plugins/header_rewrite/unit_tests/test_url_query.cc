6a6ce190e455947223ecd79614ae37817ac3f312
Juan Posadas <jposadasc@gmail.com>
2026-07-22 11:42:40 -0600

    header_rewrite: add sort_query() helper
    Adds a pure, TS-API-free sort_query() that stably sorts a URL
    query string's parameters by name, plus a Catch2 unit test. This
    helper is reused by the new sort-destination operator and, in a
    later PR, by the cache/parent-key operators.

---
+++ plugins/header_rewrite/unit_tests/test_url_query.cc
@@ -0,0 +1,48 @@
+/*
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+#include <catch2/catch_test_macros.hpp>
+
+#include "url_query.h"
+
+TEST_CASE("sort_query orders params by name", "[header_rewrite][url_query]")
+{
+  SECTION("out-of-order params are sorted")
+  {
+    CHECK(sort_query("b=2&a=1") == "a=1&b=2");
+  }
+
+  SECTION("valueless params sort by their own name")
+  {
+    CHECK(sort_query("b&a=1") == "a=1&b");
+  }
+
+  SECTION("params with duplicate names keep their relative order")
+  {
+    CHECK(sort_query("x=1&a=2&x=3") == "a=2&x=1&x=3");
+  }
+
+  SECTION("empty query stays empty")
+  {
+    CHECK(sort_query("") == "");
+  }
+
+  SECTION("single param is unaffected")
+  {
+    CHECK(sort_query("a=1") == "a=1");
+  }
+}
