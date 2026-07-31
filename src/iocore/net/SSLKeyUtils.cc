9a2d94a62e4f8f3b0cff6e7ad0355a46e42b7139
Josiah VanderZee <jvanderzee@apache.org>
2026-07-31 07:48:13 -0500

    Remove other production code changes

--- src/iocore/net/SSLKeyUtils.cc
+++ src/iocore/net/SSLKeyUtils.cc
@@ -23,24 +23,22 @@
 #include "P_SSLUtils.h"
 
 #include <tscore/Diags.h>
+#ifdef OPENSSL_IS_OPENSSL3
 #include <tscore/ink_assert.h>
+#else
 #include <tscore/ink_config.h>
-
-#include <openssl/bio.h>
-#include <openssl/err.h>
-#include <openssl/evp.h>
-#include <openssl/pem.h>
-#include <openssl/ssl.h>
-
-#if HAVE_ENGINE_GET_DEFAULT_RSA && HAVE_ENGINE_LOAD_PRIVATE_KEY
-#include <openssl/engine.h>
 #endif
 
 #ifdef OPENSSL_IS_OPENSSL3
+#include <openssl/evp.h>
 #include <openssl/decoder.h>
 #include <openssl/params.h>
+#include <openssl/ssl.h>
 #else
+#include <openssl/bio.h>
 #include <openssl/bn.h>
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
 #endif
 
 #ifdef OPENSSL_IS_OPENSSL3
@@ -190,55 +188,3 @@ set_ctx_dh(SSL_CTX *ctx, dh_key_t *pkey)
 }
 
 #endif // OPENSSL_IS_OPENSSL3
-
-bool
-use_pkey_from_file(SSL_CTX *ctx, const char *keyPath)
-{
-  ink_assert(keyPath && keyPath[0] != '\0');
-
-#if HAVE_ENGINE_GET_DEFAULT_RSA && HAVE_ENGINE_LOAD_PRIVATE_KEY
-  // A key held by an HSM or other hardware store is named rather than stored:
-  // keyPath is a key identifier the engine resolves, and there is no file to
-  // read. Ask the engine first so that a configured device takes precedence over
-  // any same-named file on disk. Absent a configured engine there is nothing to
-  // ask, and keyPath is left to the file load below.
-  if (ENGINE *e = ENGINE_get_default_RSA(); e != nullptr) {
-    EVP_PKEY *pkey = ENGINE_load_private_key(e, keyPath, nullptr, nullptr);
-
-    if (pkey != nullptr) {
-      bool const result{1 == SSL_CTX_use_PrivateKey(ctx, pkey)};
-
-      EVP_PKEY_free(pkey);
-      if (result) {
-        return true;
-      }
-    }
-    // Not finding the key in hardware is the ordinary case for a file-based
-    // configuration, so leave no errors behind for the caller to misread.
-    ERR_clear_error();
-  }
-#endif
-
-  int const result{SSL_CTX_use_PrivateKey_file(ctx, keyPath, SSL_FILETYPE_PEM)};
-
-  return 1 == result;
-}
-
-bool
-use_pkey_from_secret_data(SSL_CTX *ctx, const char *secret_data, int secret_data_len)
-{
-  scoped_BIO bio(BIO_new_mem_buf(secret_data, secret_data_len));
-
-  pem_password_cb *password_cb = SSL_CTX_get_default_passwd_cb(ctx);
-  void            *u           = SSL_CTX_get_default_passwd_cb_userdata(ctx);
-  EVP_PKEY        *pkey        = PEM_read_bio_PrivateKey(bio.get(), nullptr, password_cb, u);
-  if (nullptr == pkey) {
-    return false;
-  }
-  if (!SSL_CTX_use_PrivateKey(ctx, pkey)) {
-    EVP_PKEY_free(pkey);
-    return false;
-  }
-  EVP_PKEY_free(pkey);
-  return true;
-}
