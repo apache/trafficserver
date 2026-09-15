25c048122a44fc3d4fc4836d599e47f776ede2e0
Bryan Call <bcall@apache.org>
2026-09-14 21:13:03 -0700

    Mark the ~CacheWriteTest uncaught exception a false positive
    The destructor only frees an MIOBuffer and destroys two HTTPInfos, both
    of which assert. The cache unit tests link the aborting _ink_assert()
    from tscore, but Coverity resolves the symbol to the throwing test-only
    definition in eventsystem/unit_tests/test_MIOBufferWriter.cc.
    
    Coverity CID 1528646.

--- src/iocore/cache/unit_tests/main.h
+++ src/iocore/cache/unit_tests/main.h
@@ -188,6 +188,9 @@ public:
     build_hdrs(this->info, url);
   }
 
+  // free_MIOBuffer() and HTTPInfo::destroy() assert; Coverity resolves
+  // _ink_assert() to the throwing definition in test_MIOBufferWriter.cc.
+  // coverity[UNCAUGHT_EXCEPT:FALSE]
   ~CacheWriteTest() override
   {
     if (this->_write_buffer) {
