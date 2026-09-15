a1db3787d9522af3c81ab68c37e0a02c3177cfd3
Bryan Call <bcall@apache.org>
2026-09-14 21:02:58 -0700

    Build story paths with std::string in the HPACK test runner
    prepare() strcat'd the caller-supplied input directory into a fixed
    PATH_MAX buffer and then indexed that buffer with the directory's
    length, neither of which is bounded by the buffer's size.
    
    Coverity CID 1523657.

--- src/proxy/http2/test_HPACK.cc
+++ src/proxy/http2/test_HPACK.cc
@@ -331,13 +331,11 @@ prepare()
     cerr << "Cannot open " << input_dir << endl;
     return 1;
   }
-  struct stat st;
-  char        name[PATH_MAX + 1] = "";
-  strcat(name, input_dir.c_str());
   while ((d = readdir(dir)) != nullptr) {
-    name[input_dir.length()] = '\0';
-    ink_strlcat(name, d->d_name, sizeof(name));
-    stat(name, &st);
+    struct stat st;
+    string      name = input_dir + d->d_name;
+
+    stat(name.c_str(), &st);
     if (!S_ISDIR(st.st_mode)) {
       ++last;
     }
